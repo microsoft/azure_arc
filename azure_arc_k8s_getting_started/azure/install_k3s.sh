a5dd8ef77228bf265ba34b150079cf22973dabb8
likamrat <likamrat@microsoft.com>
2020-05-02 16:08:41 -0700

    initial commit

--- azure_arc_k8s_getting_started/azure/install_k3s.sh
+++ azure_arc_k8s_getting_started/azure/install_k3s.sh
@@ -10,19 +10,3 @@ sudo install k3sup /usr/local/bin/
 
 k3sup install --local --user $USER --context vmwk3s --local-path $HOME/.kube/config
 sudo chmod 644 /etc/rancher/k3s/k3s.yaml
-
-
-
-
-
-
-# echo "@reboot cd /opt/fahclient && ./FAHClient" | crontab -
-
-# sudo cat <<EOT >> run_fah.sh
-# #!/bin/bash
-# cd /opt/fahclient && ./FAHClient
-# EOT
-
-# sudo chmod +x run_fah.sh
-
-# at now + 1 minutes -f run_fah.sh
