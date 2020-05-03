533aeccbc1e0c09395d77b6b39fd859525bc28b0
likamrat <likamrat@microsoft.com>
2020-05-02 17:40:53 -0700

    template

--- azure_arc_k8s_getting_started/azure/install_k3s.sh
+++ azure_arc_k8s_getting_started/azure/install_k3s.sh
@@ -8,5 +8,19 @@ curl -sLS https://get.k3sup.dev | sh
 sudo install k3sup /usr/local/bin/
 
 
-k3sup install --local --user $USER --context vmwk3s --local-path $HOME/.kube/config
+k3sup install --local --user $USER --context arck3sdemo --local-path $HOME/.kube/config
 sudo chmod 644 /etc/rancher/k3s/k3s.yaml
+export KUBECONFIG=/home/$USER/.kube/config
+
+
+# (
+#   set -x; cd "$(mktemp -d)" &&
+#   curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/krew.{tar.gz,yaml}" &&
+#   tar zxvf krew.tar.gz &&
+#   KREW=./krew-"$(uname | tr '[:upper:]' '[:lower:]')_amd64" &&
+#   "$KREW" install --manifest=krew.yaml --archive=krew.tar.gz &&
+#   "$KREW" update
+# )
+# export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
+
+# kubectl krew install ctx
