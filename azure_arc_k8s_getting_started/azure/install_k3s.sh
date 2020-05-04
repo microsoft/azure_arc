99733575b4bf1c3faa06bc87ee22689519237b18
likamrat <likamrat@microsoft.com>
2020-05-03 18:28:59 -0700

    kubeconfig

--- azure_arc_k8s_getting_started/azure/install_k3s.sh
+++ azure_arc_k8s_getting_started/azure/install_k3s.sh
@@ -9,10 +9,10 @@ sudo chown -R $USER $HOME/.kube
 curl -sLS https://get.k3sup.dev | sh
 sudo install k3sup /usr/local/bin/
 
-k3sup install --local --user $USER --context arck3sdemo
+k3sup install --local --user $USER --context arck3sdemo --local-path ~/.kube/config
 sudo chmod 644 /etc/rancher/k3s/k3s.yaml
-export KUBECONFIG=/home/$USER/.kube/config
-cp /home/$USER/kubeconfig ~/.kube/config
+# export KUBECONFIG=/home/$USER/.kube/config
+# cp /home/$USER/kubeconfig ~/.kube/config
 
 # Install Helm 3
 sudo snap install helm --classic
@@ -22,8 +22,8 @@ sudo apt-get update
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 
 # Install Azure CLI Arc Extensions
-mkdir /home/$USER/az_extensions
-curl https://arcgettingstarted.blob.core.windows.net/az-extentions/connectedk8s-0.1.3-py2.py3-none-any.whl --output /home/$USER/az_extensions/connectedk8s-0.1.3-py2.py3-none-any.whl
-curl https://arcgettingstarted.blob.core.windows.net/az-extentions/k8sconfiguration-0.1.6-py2.py3-none-any.whl --output /home/$USER/az_extensions/k8sconfiguration-0.1.6-py2.py3-none-any.whl
-az extension add --source ./home/$USER/az_extensions/connectedk8s-0.1.3-py2.py3-none-any.whl --yes
-az extension add --source ./home/$USER/az_extensions/k8sconfiguration-0.1.6-py2.py3-none-any.whl --yes
+mkdir az_extensions
+curl https://arcgettingstarted.blob.core.windows.net/az-extentions/connectedk8s-0.1.3-py2.py3-none-any.whl --output az_extensions/connectedk8s-0.1.3-py2.py3-none-any.whl
+curl https://arcgettingstarted.blob.core.windows.net/az-extentions/k8sconfiguration-0.1.6-py2.py3-none-any.whl --output az_extensions/k8sconfiguration-0.1.6-py2.py3-none-any.whl
+az extension add --source ./az_extensions/connectedk8s-0.1.3-py2.py3-none-any.whl --yes
+az extension add --source ./az_extensions/k8sconfiguration-0.1.6-py2.py3-none-any.whl --yes
