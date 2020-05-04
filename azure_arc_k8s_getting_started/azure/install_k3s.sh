feed26c461873a3d089d5adec2b3c155d63a1148
likamrat <likamrat@microsoft.com>
2020-05-04 14:46:05 -0700

    -u

--- azure_arc_k8s_getting_started/azure/install_k3s.sh
+++ azure_arc_k8s_getting_started/azure/install_k3s.sh
@@ -7,17 +7,17 @@ curl -sLS https://get.k3sup.dev | sh
 sudo install k3sup /usr/local/bin/
 sudo cp k3sup /usr/local/bin/k3sup
 
-k3sup install --local --context arck3sdemo
-sudo chmod 644 /etc/rancher/k3s/k3s.yaml
-kubectl get nodes
+# k3sup install --local --context arck3sdemo
+# sudo chmod 644 /etc/rancher/k3s/k3s.yaml
+# kubectl get nodes
 # cp kubeconfig ~/.kube/config
 
-# Install Helm 3
-sudo snap install helm --classic
+# # Install Helm 3
+# sudo snap install helm --classic
 
-# Install Azure CLI
-sudo apt-get update
-curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+# # Install Azure CLI
+# sudo apt-get update
+# curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 
 # mkdir ~/az_extensions
 # curl https://arcgettingstarted.blob.core.windows.net/az-extentions/connectedk8s-0.1.3-py2.py3-none-any.whl --output ~/az_extensions/connectedk8s-0.1.3-py2.py3-none-any.whl
