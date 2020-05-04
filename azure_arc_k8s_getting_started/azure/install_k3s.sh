75093b89b7611392ed549aa75679ef6626cfe677
likamrat <likamrat@microsoft.com>
2020-05-04 12:34:22 -0700

    -u

--- azure_arc_k8s_getting_started/azure/install_k3s.sh
+++ azure_arc_k8s_getting_started/azure/install_k3s.sh
@@ -3,22 +3,24 @@
 sudo apt-get update
 
 # mkdir ~/.kube
-# # sudo chown -R $USER ~/.kube
+# sudo chown -R $USER ~/.kube
 
-# # Install Rancer K3s single master cluster 
-# curl -sLS https://get.k3sup.dev | sh
-# sudo install k3sup /usr/local/bin/
+# Install Rancer K3s single master cluster using k3sup
+curl -sLS https://get.k3sup.dev | sh
+sudo install k3sup /usr/local/bin/
+sudo cp k3sup /usr/local/bin/k3sup
 
-# k3sup install --local --user $USER --context arck3sdemo --local-path ~/.kube/config
-# sudo chmod 644 /etc/rancher/k3s/k3s.yaml
-# # cp /home/$USER/kubeconfig ~/.kube/config
+k3sup install --local --user $USER --context arck3sdemo
+sudo chmod 644 /etc/rancher/k3s/k3s.yaml
+mkdir ~/.kube
+cp kubeconfig ~/.kube/config
 
-# # Install Helm 3
-# sudo snap install helm --classic
+# Install Helm 3
+sudo snap install helm --classic
 
-# # Install Azure CLI
-# sudo apt-get update
-# curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+# Install Azure CLI
+sudo apt-get update
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 
 # mkdir ~/az_extensions
 # curl https://arcgettingstarted.blob.core.windows.net/az-extentions/connectedk8s-0.1.3-py2.py3-none-any.whl --output ~/az_extensions/connectedk8s-0.1.3-py2.py3-none-any.whl
