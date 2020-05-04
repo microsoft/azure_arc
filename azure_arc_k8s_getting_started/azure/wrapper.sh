cf0858080c710582cd3d98fc2a6742e22ab459ca
likamrat <likamrat@microsoft.com>
2020-05-04 15:28:39 -0700

    wrapper

--- azure_arc_k8s_getting_started/azure/wrapper.sh
+++ azure_arc_k8s_getting_started/azure/wrapper.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl https://raw.githubusercontent.com/likamrat/azure_arc/master/azure_arc_k8s_getting_started/azure/install_k3s.sh
+wget https://raw.githubusercontent.com/likamrat/azure_arc/master/azure_arc_k8s_getting_started/azure/install_k3s.sh
 sudo chmod +x install_k3s.sh
 ./install_k3s.sh
