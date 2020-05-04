877d57609794f0e51ea92f32c8dcec4a3f310bdb
likamrat <likamrat@microsoft.com>
2020-05-04 16:10:40 -0700

    wrapper

--- azure_arc_k8s_getting_started/azure/install_k3s.sh
+++ azure_arc_k8s_getting_started/azure/install_k3s.sh
@@ -9,16 +9,16 @@ sudo cp k3sup /usr/local/bin/k3sup
 k3sup install --local --context arck3sdemo
 sudo chmod 644 /etc/rancher/k3s/k3s.yaml
 kubectl get nodes
-mv kubeconfig ~/.kube/config
+# mv kubeconfig ~/.kube/config
 
-# Install Helm 3
-sudo snap install helm --classic
+# # Install Helm 3
+# sudo snap install helm --classic
 
-# Install Azure CLI
-sudo apt-get update
-curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+# # Install Azure CLI
+# sudo apt-get update
+# curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 
-curl https://arcgettingstarted.blob.core.windows.net/az-extentions/connectedk8s-0.1.3-py2.py3-none-any.whl --output connectedk8s-0.1.3-py2.py3-none-any.whl
-curl https://arcgettingstarted.blob.core.windows.net/az-extentions/k8sconfiguration-0.1.6-py2.py3-none-any.whl --output k8sconfiguration-0.1.6-py2.py3-none-any.whl
-az extension add --source connectedk8s-0.1.3-py2.py3-none-any.whl --yes
-az extension add --source k8sconfiguration-0.1.6-py2.py3-none-any.whl --yes
+# curl https://arcgettingstarted.blob.core.windows.net/az-extentions/connectedk8s-0.1.3-py2.py3-none-any.whl --output connectedk8s-0.1.3-py2.py3-none-any.whl
+# curl https://arcgettingstarted.blob.core.windows.net/az-extentions/k8sconfiguration-0.1.6-py2.py3-none-any.whl --output k8sconfiguration-0.1.6-py2.py3-none-any.whl
+# az extension add --source connectedk8s-0.1.3-py2.py3-none-any.whl --yes
+# az extension add --source k8sconfiguration-0.1.6-py2.py3-none-any.whl --yes
--- azure_arc_k8s_getting_started/azure/wrapper.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-curl https://raw.githubusercontent.com/likamrat/azure_arc/master/azure_arc_k8s_getting_started/azure/install_k3s.sh --output install_k3s.sh
-sudo chmod +x install_k3s.sh
-./install_k3s.sh
