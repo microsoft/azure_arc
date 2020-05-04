51e4a0fa3b41eb5014dc76d988f5e66fe7e29a92
likamrat <likamrat@microsoft.com>
2020-05-03 17:36:16 -0700

    env_vars

--- azure_arc_k8s_getting_started/azure/install_k3s.sh
+++ azure_arc_k8s_getting_started/azure/install_k3s.sh
@@ -9,16 +9,21 @@ sudo chown -R $USER $HOME/.kube
 curl -sLS https://get.k3sup.dev | sh
 sudo install k3sup /usr/local/bin/
 
-k3sup install --local --user $USER --context arck3sdemo --local-path $HOME/.kube/config
+k3sup install --local --user $USER --context arck3sdemo
 sudo chmod 644 /etc/rancher/k3s/k3s.yaml
-export KUBECONFIG=/home/$USER/.kube/config
+# export KUBECONFIG=/home/$USER/.kube/config
+cp /home/$USER/kubeconfig ~/.kube/config
+
+# Install Helm 3
+sudo snap install helm --classic
 
 # Install Azure CLI
 sudo apt-get update
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 
 # Install Azure CLI Arc Extensions
-curl https://arcgettingstarted.blob.core.windows.net/az-extentions/connectedk8s-0.1.3-py2.py3-none-any.whl --output connectedk8s-0.1.3-py2.py3-none-any.whl
-curl https://arcgettingstarted.blob.core.windows.net/az-extentions/k8sconfiguration-0.1.6-py2.py3-none-any.whl --output k8sconfiguration-0.1.6-py2.py3-none-any.whl
-az extension add --source ./connectedk8s-0.1.3-py2.py3-none-any.whl --yes
-az extension add --source ./k8sconfiguration-0.1.6-py2.py3-none-any.whl --yes
+mkdir /home/$USER/az_extensions
+curl https://arcgettingstarted.blob.core.windows.net/az-extentions/connectedk8s-0.1.3-py2.py3-none-any.whl --output /home/$USER/az_extensions/connectedk8s-0.1.3-py2.py3-none-any.whl
+curl https://arcgettingstarted.blob.core.windows.net/az-extentions/k8sconfiguration-0.1.6-py2.py3-none-any.whl --output /home/$USER/az_extensions/k8sconfiguration-0.1.6-py2.py3-none-any.whl
+az extension add --source ./home/$USER/az_extensions/connectedk8s-0.1.3-py2.py3-none-any.whl --yes
+az extension add --source ./home/$USER/az_extensions/k8sconfiguration-0.1.6-py2.py3-none-any.whl --yes
--- azure_arc_k8s_getting_started/azure/set_env_vars.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Export Arc Service Principle Name Onboarding Parameters
-export appId=40bc3876-dfe9-46fa-8210-7ecf757e127f
-export password=e15fa6e6-f453-42e9-b024-746f1379ce59
-export tenant=72f988bf-86f1-41af-91ab-2d7cd011db47
