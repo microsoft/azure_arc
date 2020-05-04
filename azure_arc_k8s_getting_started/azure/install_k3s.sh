9d935d58755656f63e1e4fad29988110fbebf5d0
likamrat <likamrat@microsoft.com>
2020-05-04 16:58:16 -0700

    log

--- azure_arc_k8s_getting_started/azure/install_k3s.sh
+++ azure_arc_k8s_getting_started/azure/install_k3s.sh
@@ -6,7 +6,7 @@ sudo apt-get update
 curl -sLS https://get.k3sup.dev | sh
 sudo cp k3sup /usr/local/bin/k3sup
 
-mkdir ~/.kube
+sudo mkdir ~/.kube
 sudo k3sup install --local --context arck3sdemo --local-path ~/.kube/config
 sudo chmod 644 /etc/rancher/k3s/k3s.yaml
 kubectl get nodes
