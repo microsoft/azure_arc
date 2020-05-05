6958eb66c5b008184596ef5c7081d9d080f8a967
likamrat <likamrat@microsoft.com>
2020-05-04 19:15:15 -0700

    script

--- azure_arc_k8s_getting_started/azure/install_k3s.sh
+++ azure_arc_k8s_getting_started/azure/install_k3s.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-sudo mkdir ~/.kube
+# sudo mkdir ~/.kube
 
 # sudo apt-get update
 
-# # Install Rancer K3s single master cluster using k3sup
-# curl -sLS https://get.k3sup.dev | sh
-# sudo cp k3sup /usr/local/bin/k3sup
-# sudo k3sup install --local --context arck3sdemo
-# sudo chmod 644 /etc/rancher/k3s/k3s.yaml
-# sudo mv /var/lib/waagent/custom-script/download/0/kubeconfig ~/.kube/config
-# kubectl get nodes
+# Install Rancer K3s single master cluster using k3sup
+curl -sLS https://get.k3sup.dev | sh
+sudo cp k3sup /usr/local/bin/k3sup
+sudo k3sup install --local --context arck3sdemo
+sudo chmod 644 /etc/rancher/k3s/k3s.yaml
+kubectl get nodes
+sudo mv /var/lib/waagent/custom-script/download/0/kubeconfig ~/.kube/config
+
 
 # # Install Helm 3
 # sudo snap install helm --classic
