0c6eb88f95f06a49e61b98d54b1708df21151e5c
likamrat <likamrat@microsoft.com>
2020-05-04 21:19:39 -0700

    script

--- azure_arc_k8s_getting_started/azure/install_k3s.sh
+++ azure_arc_k8s_getting_started/azure/install_k3s.sh
@@ -9,7 +9,7 @@
 # sudo cp k3sup /usr/local/bin/k3sup
 # sudo k3sup install --local --context arck3sdemo
 # sudo chmod 644 /etc/rancher/k3s/k3s.yaml
-sudo mkdir ~/.kube
+sudo -u arcdemo mkdir /home/arcdemo/.kube
 # sudo mv /var/lib/waagent/custom-script/download/0/kubeconfig ~/.kube/config
 
 
