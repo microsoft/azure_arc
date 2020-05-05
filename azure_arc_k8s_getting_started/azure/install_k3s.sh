83e4e7e633238de16fb71bc937d8e3ff13eabac3
likamrat <likamrat@microsoft.com>
2020-05-05 10:49:45 -0700

    script

--- azure_arc_k8s_getting_started/azure/install_k3s.sh
+++ azure_arc_k8s_getting_started/azure/install_k3s.sh
@@ -5,7 +5,8 @@ sudo apt-get update
 # Testing vars
 
 echo $adminUsername >> vars1.txt
-echo $adminUsername:$1 >> vars2.txt
+echo $adminPasswordOrKey >> vars2.txt
+sudo -u $adminPasswordOrKey mkdir /home/${adminPasswordOrKey}/lior
 
 # Install Rancer K3s single master cluster using k3sup
 ADMINUSER=`awk -F: 'END { print $1}' /etc/passwd`
@@ -25,5 +26,9 @@ curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 
 curl https://arcgettingstarted.blob.core.windows.net/az-extentions/connectedk8s-0.1.3-py2.py3-none-any.whl --output connectedk8s-0.1.3-py2.py3-none-any.whl
 curl https://arcgettingstarted.blob.core.windows.net/az-extentions/k8sconfiguration-0.1.6-py2.py3-none-any.whl --output k8sconfiguration-0.1.6-py2.py3-none-any.whl
-sudo az extension add --source connectedk8s-0.1.3-py2.py3-none-any.whl --yes
-sudo az extension add --source k8sconfiguration-0.1.6-py2.py3-none-any.whl --yes
+
+sudo cat <<EOT >> az_extension.sh
+#!/bin/bash
+az extension add --source connectedk8s-0.1.3-py2.py3-none-any.whl --yes
+az extension add --source k8sconfiguration-0.1.6-py2.py3-none-any.whl --yes
+EOT
