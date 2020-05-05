9ed7a1fcae795e15e5c34af30f110ccafc77a7a9
likamrat <likamrat@microsoft.com>
2020-05-05 16:15:53 -0700

    script

--- azure_arc_k8s_getting_started/azure/install_az_extensions.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-curl https://arcgettingstarted.blob.core.windows.net/az-extentions/connectedk8s-0.1.3-py2.py3-none-any.whl --output connectedk8s-0.1.3-py2.py3-none-any.whl
-curl https://arcgettingstarted.blob.core.windows.net/az-extentions/k8sconfiguration-0.1.6-py2.py3-none-any.whl --output k8sconfiguration-0.1.6-py2.py3-none-any.whl
-
-az extension add --source connectedk8s-0.1.3-py2.py3-none-any.whl --yes
-az extension add --source k8sconfiguration-0.1.6-py2.py3-none-any.whl --yes
--- azure_arc_k8s_getting_started/azure/install_k3s.sh
+++ azure_arc_k8s_getting_started/azure/install_k3s.sh
@@ -19,17 +19,7 @@ sed -i '6s/^/export tenantId=/' vars.sh
 chmod +x vars.sh 
 . ./vars.sh
 
-# echo $adminUsername >> test1
-# echo $adminPasswordOrKey >> test2
-# echo $appId >> test3
-# echo $password >> test4
-# echo $tenantId >> test5
-
-# sudo -u $SPN_USER mkdir /home/${SPN_USER}/lior
-
 # Install Rancer K3s single master cluster using k3sup
-# ADMINUSER=`awk -F: 'END { print $1}' /etc/passwd`
-# sudo -u $ADMINUSER mkdir /home/${ADMINUSER}/.kube
 sudo -u $adminUsername mkdir /home/${adminUsername}/.kube
 curl -sLS https://get.k3sup.dev | sh
 sudo cp k3sup /usr/local/bin/k3sup
@@ -47,18 +37,11 @@ curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 curl https://arcgettingstarted.blob.core.windows.net/az-extentions/connectedk8s-0.1.3-py2.py3-none-any.whl --output connectedk8s-0.1.3-py2.py3-none-any.whl
 curl https://arcgettingstarted.blob.core.windows.net/az-extentions/k8sconfiguration-0.1.6-py2.py3-none-any.whl --output k8sconfiguration-0.1.6-py2.py3-none-any.whl
 
-# sudo cat <<EOT >> az_extensions.sh
-# #!/bin/bash
-# az extension add --source ./connectedk8s-0.1.3-py2.py3-none-any.whl --yes
-# az extension add --source ./k8sconfiguration-0.1.6-py2.py3-none-any.whl --yes
-# EOT
-
-# chmod +x az_extensions.sh
-
-# . ./az_extensions.sh
 sudo -u $adminUsername mkdir /home/${adminUsername}/az_extensions
 sudo cp connectedk8s-0.1.3-py2.py3-none-any.whl /home/${adminUsername}/az_extensions
 sudo cp k8sconfiguration-0.1.6-py2.py3-none-any.whl /home/${adminUsername}/az_extensions
 
 sudo -u $adminUsername az extension add --source /home/arcdemo/az_extensions/connectedk8s-0.1.3-py2.py3-none-any.whl --yes
 sudo -u $adminUsername az extension add --source /home/arcdemo/az_extensions/k8sconfiguration-0.1.6-py2.py3-none-any.whl --yes
+
+az login --service-principal --username ${appId} --password ${password} --tenant ${tenantId}
