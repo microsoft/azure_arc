758258371db81df5fb191a55c146647803449517
likamrat <likamrat@microsoft.com>
2020-05-05 14:07:12 -0700

    script

--- azure_arc_k8s_getting_started/azure/install_k3s.sh
+++ azure_arc_k8s_getting_started/azure/install_k3s.sh
@@ -6,26 +6,36 @@ sudo apt-get update
 
 echo '#!/bin/bash' >> vars.sh
 echo $adminUsername:$1 | awk '{print substr($1,2); }' >> vars.sh
-echo $adminPasswordOrKey:$1 | awk '{print substr($1,2); }' >> vars.sh
-sed -i '2s/^/export SPN_USER=/' vars.sh
-sed -i '3s/^/export SPN_PASSWORD=/' vars.sh
+echo $adminPasswordOrKey:$2 | awk '{print substr($1,2); }' >> vars.sh
+echo $appId:$3 | awk '{print substr($1,2); }' >> vars.sh
+echo $password:$4 | awk '{print substr($1,2); }' >> vars.sh
+echo $tenantId:$5 | awk '{print substr($1,2); }' >> vars.sh
+sed -i '2s/^/export adminUsername=/' vars.sh
+sed -i '3s/^/export adminPasswordOrKey=/' vars.sh
+sed -i '4s/^/export appId=/' vars.sh
+sed -i '5s/^/export password=/' vars.sh
+sed -i '6s/^/export tenantId=/' vars.sh
 
 chmod +x vars.sh 
 . ./vars.sh
 
-echo $SPN_USER >> test1
-echo $SPN_PASSWORD >> test2
+echo $adminUsername >> test1
+echo $adminPasswordOrKey >> test2
+echo $appId >> test3
+echo $password >> test4
+echo $tenantId >> test5
 
-sudo -u $SPN_USER mkdir /home/${SPN_USER}/lior
+# sudo -u $SPN_USER mkdir /home/${SPN_USER}/lior
 
 # Install Rancer K3s single master cluster using k3sup
-ADMINUSER=`awk -F: 'END { print $1}' /etc/passwd`
-sudo -u $ADMINUSER mkdir /home/${ADMINUSER}/.kube
+# ADMINUSER=`awk -F: 'END { print $1}' /etc/passwd`
+# sudo -u $ADMINUSER mkdir /home/${ADMINUSER}/.kube
+sudo -u $adminUsername mkdir /home/${adminUsername}/.kube
 curl -sLS https://get.k3sup.dev | sh
 sudo cp k3sup /usr/local/bin/k3sup
 sudo k3sup install --local --context arck3sdemo
 sudo chmod 644 /etc/rancher/k3s/k3s.yaml
-sudo cp kubeconfig /home/${ADMINUSER}/.kube/config
+sudo cp kubeconfig /home/${adminUsername}/.kube/config
 
 # Install Helm 3
 sudo snap install helm --classic
@@ -43,5 +53,5 @@ az extension add --source connectedk8s-0.1.3-py2.py3-none-any.whl --yes
 az extension add --source k8sconfiguration-0.1.6-py2.py3-none-any.whl --yes
 EOT
 
-sudo chmod +x az_extension.sh
+chmod +x az_extension.sh
 . ./az_extension.sh
