1c96b94b6ed888ab5be85b5dc661e3cca3b77c6a
likamrat <likamrat@microsoft.com>
2020-05-04 19:00:44 -0700

    script

--- azure_arc_k8s_getting_started/azure/install_k3s.sh
+++ azure_arc_k8s_getting_started/azure/install_k3s.sh
@@ -2,24 +2,24 @@
 
 mkdir ~/.kube
 
-sudo apt-get update
+# sudo apt-get update
 
-# Install Rancer K3s single master cluster using k3sup
-curl -sLS https://get.k3sup.dev | sh
-sudo cp k3sup /usr/local/bin/k3sup
-sudo k3sup install --local --context arck3sdemo
-sudo chmod 644 /etc/rancher/k3s/k3s.yaml
-sudo mv /var/lib/waagent/custom-script/download/0/kubeconfig ~/.kube/config
-kubectl get nodes
+# # Install Rancer K3s single master cluster using k3sup
+# curl -sLS https://get.k3sup.dev | sh
+# sudo cp k3sup /usr/local/bin/k3sup
+# sudo k3sup install --local --context arck3sdemo
+# sudo chmod 644 /etc/rancher/k3s/k3s.yaml
+# sudo mv /var/lib/waagent/custom-script/download/0/kubeconfig ~/.kube/config
+# kubectl get nodes
 
-# Install Helm 3
-sudo snap install helm --classic
+# # Install Helm 3
+# sudo snap install helm --classic
 
-# Install Azure CLI
-sudo apt-get update
-curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+# # Install Azure CLI
+# sudo apt-get update
+# curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 
-curl https://arcgettingstarted.blob.core.windows.net/az-extentions/connectedk8s-0.1.3-py2.py3-none-any.whl --output connectedk8s-0.1.3-py2.py3-none-any.whl
-curl https://arcgettingstarted.blob.core.windows.net/az-extentions/k8sconfiguration-0.1.6-py2.py3-none-any.whl --output k8sconfiguration-0.1.6-py2.py3-none-any.whl
-az extension add --source ./connectedk8s-0.1.3-py2.py3-none-any.whl --yes
-az extension add --source ./k8sconfiguration-0.1.6-py2.py3-none-any.whl --yes
+# curl https://arcgettingstarted.blob.core.windows.net/az-extentions/connectedk8s-0.1.3-py2.py3-none-any.whl --output connectedk8s-0.1.3-py2.py3-none-any.whl
+# curl https://arcgettingstarted.blob.core.windows.net/az-extentions/k8sconfiguration-0.1.6-py2.py3-none-any.whl --output k8sconfiguration-0.1.6-py2.py3-none-any.whl
+# az extension add --source ./connectedk8s-0.1.3-py2.py3-none-any.whl --yes
+# az extension add --source ./k8sconfiguration-0.1.6-py2.py3-none-any.whl --yes
