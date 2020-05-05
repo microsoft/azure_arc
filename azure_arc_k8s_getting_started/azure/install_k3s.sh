e8db760aa2e2ef27d7f4c9ebcce3ff2d2df0b7ab
likamrat <likamrat@microsoft.com>
2020-05-05 07:03:28 -0700

    script

--- azure_arc_k8s_getting_started/azure/install_k3s.sh
+++ azure_arc_k8s_getting_started/azure/install_k3s.sh
@@ -14,14 +14,14 @@ sudo chmod 644 /etc/rancher/k3s/k3s.yaml
 # sudo cp /var/lib/waagent/custom-script/download/0/kubeconfig ~/.kube/config ## Need to see if can be changed to just kubeconfig
 
 
-# # Install Helm 3
-# sudo snap install helm --classic
+# Install Helm 3
+sudo snap install helm --classic
 
-# # Install Azure CLI
-# sudo apt-get update
-# curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+# Install Azure CLI
+sudo apt-get update
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 
-# curl https://arcgettingstarted.blob.core.windows.net/az-extentions/connectedk8s-0.1.3-py2.py3-none-any.whl --output connectedk8s-0.1.3-py2.py3-none-any.whl
-# curl https://arcgettingstarted.blob.core.windows.net/az-extentions/k8sconfiguration-0.1.6-py2.py3-none-any.whl --output k8sconfiguration-0.1.6-py2.py3-none-any.whl
-# az extension add --source ./connectedk8s-0.1.3-py2.py3-none-any.whl --yes
-# az extension add --source ./k8sconfiguration-0.1.6-py2.py3-none-any.whl --yes
+curl https://arcgettingstarted.blob.core.windows.net/az-extentions/connectedk8s-0.1.3-py2.py3-none-any.whl --output connectedk8s-0.1.3-py2.py3-none-any.whl
+curl https://arcgettingstarted.blob.core.windows.net/az-extentions/k8sconfiguration-0.1.6-py2.py3-none-any.whl --output k8sconfiguration-0.1.6-py2.py3-none-any.whl
+az extension add --source ./connectedk8s-0.1.3-py2.py3-none-any.whl --yes
+az extension add --source ./k8sconfiguration-0.1.6-py2.py3-none-any.whl --yes
