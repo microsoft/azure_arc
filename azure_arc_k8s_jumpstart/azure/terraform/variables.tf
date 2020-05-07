59cd7f55d418b1fb328580142303a5db1c9e210f
likamrat <likamrat@microsoft.com>
2020-05-07 11:01:24 -0700

    initial commit

---
+++ azure_arc_k8s_jumpstart/azure/terraform/variables.tf
@@ -0,0 +1,59 @@
+# Declare Azure variables
+
+variable "subscription_id" {
+}
+
+variable "client_id" {
+}
+
+variable "client_secret" {
+}
+
+variable "tenant_id" {
+}
+
+variable "location" {
+}
+
+variable "azure_resource_group" {
+}
+
+variable "azure_vnet" {
+}
+
+variable "azure_vnet_address_space" {
+}
+
+variable "azure_vnet_subnet" {
+}
+
+variable "azure_subnet_address_prefix" {
+}
+
+variable "azure_public_ip" {
+}
+
+variable "azure_nsg" {
+}
+
+variable "azure_vm_nic" {
+}
+
+variable "azure_vm_aset_name" {
+}
+
+variable "azure_vm_name" {
+}
+
+variable "azure_vm_size" {
+}
+
+variable "azure_vm_os_disk_size_gb" {
+}
+
+variable "admin_username" {
+}
+
+variable "admin_password" {
+}
+
