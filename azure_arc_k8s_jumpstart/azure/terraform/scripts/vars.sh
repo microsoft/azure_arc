176d31b5c212754b96770f17629ce8be8b01a006
likamrat <likamrat@microsoft.com>
2020-05-07 11:00:33 -0700

    initial commit

--- azure_arc_k8s_jumpstart/azure/terraform/scripts/vars.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# <--- Change the following environment variables according to your Azure Service Principle name --->
-
-export appId=40bc3876-dfe9-46fa-8210-7ecf757e127f 
-export password=e15fa6e6-f453-42e9-b024-746f1379ce59
-export tenantId=72f988bf-86f1-41af-91ab-2d7cd011db47
