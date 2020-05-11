a2666e628ea8530861d71d68bd213e217910051a
likamrat <likamrat@microsoft.com>
2020-05-11 08:24:51 -0700

    publicIp

--- azure_arc_k8s_jumpstart/azure/arm_template/scripts/install_k3s.sh
+++ azure_arc_k8s_jumpstart/azure/arm_template/scripts/install_k3s.sh
@@ -7,13 +7,17 @@ echo '#!/bin/bash' >> vars.sh
 echo $adminUsername:$1 | awk '{print substr($1,2); }' >> vars.sh
 echo $adminPasswordOrKey:$2 | awk '{print substr($1,2); }' >> vars.sh
 echo $appId:$3 | awk '{print substr($1,2); }' >> vars.sh
-echo $password:$4 | awk '{print substr($1,2); }' >> vars.sh
+echo $password:$4 | awk '{print substr($1,2); }'| awk '{print substr($1,2); }'
 echo $tenantId:$5 | awk '{print substr($1,2); }' >> vars.sh
 sed -i '2s/^/export adminUsername=/' vars.sh
 sed -i '3s/^/export adminPasswordOrKey=/' vars.sh
 sed -i '4s/^/export appId=/' vars.sh
 sed -i '5s/^/export password=/' vars.sh
 sed -i '6s/^/export tenantId=/' vars.sh
+
+publicIp=$(curl icanhazip.com)
+echo $publicIp:$6 | awk '{print substr($1,2); }' >> vars.sh
+sed -i '7s/^/export publicIp=/' vars.sh
 
 chmod +x vars.sh 
 . ./vars.sh
@@ -22,7 +26,7 @@ chmod +x vars.sh
 sudo -u $adminUsername mkdir /home/${adminUsername}/.kube
 curl -sLS https://get.k3sup.dev | sh
 sudo cp k3sup /usr/local/bin/k3sup
-sudo k3sup install --local --context arck3sdemo
+sudo k3sup install --local --context arck3sdemo --ip $publicIp
 sudo chmod 644 /etc/rancher/k3s/k3s.yaml
 sudo cp kubeconfig /home/${adminUsername}/.kube/config
 chown -R $adminUsername /home/${adminUsername}/.kube/
