7ac814928e0d0568c7240876a503db07058b385c
likamrat <likamrat@microsoft.com>
2020-05-12 12:59:09 -0700

    initial vagrant

--- azure_arc_servers_jumpstart/vagrant/scripts/install_k3s.sh
+++ azure_arc_servers_jumpstart/vagrant/scripts/install_k3s.sh
@@ -1,39 +1,40 @@
 #!/bin/bash
 
-# sudo apt-get update
+sudo apt-get update
 
-# # Injecting environment variables
-# source /tmp/vars.sh
-# publicIp=$(curl icanhazip.com)
+# Injecting environment variables
+curl https://raw.githubusercontent.com/likamrat/azure_arc/master/azure_arc_servers_jumpstart/vagrant/scripts/vars.sh -o /tmp/vars
+source /tmp/vars.sh
+publicIp=$(curl icanhazip.com)
 
-# # Installing Rancer K3s single master cluster using k3sup
-# sudo mkdir ~/.kube
-# sudo curl -sLS https://get.k3sup.dev | sh
-# sudo cp k3sup /usr/local/bin/k3sup
-# sudo k3sup install --local --context arck3sdemo --ip $publicIp --local-path ~/.kube/config
-# sudo chmod 644 /etc/rancher/k3s/k3s.yaml
+# Installing Rancer K3s single master cluster using k3sup
+sudo mkdir ~/.kube
+sudo curl -sLS https://get.k3sup.dev | sh
+sudo cp k3sup /usr/local/bin/k3sup
+sudo k3sup install --local --context arck3sdemo --ip $publicIp --local-path ~/.kube/config
+sudo chmod 644 /etc/rancher/k3s/k3s.yaml
 
-# # Installing Helm 3
-# sudo snap install helm --classic
+# Installing Helm 3
+sudo snap install helm --classic
 
 # Installing Azure CLI & Azure Arc Extensions
 sudo apt-get update
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 
-# sudo az extension add --name connectedk8s
-# sudo az extension add --name k8sconfiguration
+sudo az extension add --name connectedk8s
+sudo az extension add --name k8sconfiguration
 
-# sudo az login --service-principal --username $appId --password $password --tenant $tenantId
+sudo az login --service-principal --username $appId --password $password --tenant $tenantId
 
-# sudo cat <<EOT >> az.sh
-# #!/bin/sh
-# sudo chown -R $USER /home/${USER}/.kube
-# sudo chown -R $USER /home/${USER}/.kube/config
-# sudo chown -R $USER /home/${USER}/.azure/config
-# sudo chown -R $USER /home/${USER}/.azure
-# sudo chmod -R 777 /home/${USER}/.azure/config
-# sudo chmod -R 777 /home/${USER}/.azure
-# EOT
-# sudo chmod +x az.sh
-# . ./az.sh
-# sudo rm az.sh
+sudo cat <<EOT >> az.sh
+#!/bin/sh
+sudo chown -R $USER /home/${USER}/.kube
+sudo chown -R $USER /home/${USER}/.kube/config
+sudo chown -R $USER /home/${USER}/.azure/config
+sudo chown -R $USER /home/${USER}/.azure
+sudo chmod -R 777 /home/${USER}/.azure/config
+sudo chmod -R 777 /home/${USER}/.azure
+EOT
+sudo chmod +x az.sh
+. ./az.sh
+sudo rm az.sh
--- azure_arc_servers_jumpstart/vagrant/scripts/vars.sh
+++ azure_arc_servers_jumpstart/vagrant/scripts/vars.sh
@@ -2,6 +2,7 @@
 
 # <--- Change the following environment variables according to your Azure Service Principle name --->
 
-export appId=<Your Azure Service Principle name>
-export password=<Your Azure Service Principle password>
-export tenantId=<Your Azure tenant ID>
+export subscriptionId=e73c1dbe-2574-4f38-9e8f-c813757b1786
+export appId=051b9a58-4a83-48de-b610-0e7ae1bca3fb
+export password=53ed1458-a77d-4201-9c21-4fe24a0981fa
+export tenantId=72f988bf-86f1-41af-91ab-2d7cd011db47
