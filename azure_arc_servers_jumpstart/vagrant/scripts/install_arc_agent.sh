166f1692d2d352aa805c19a071ab0bd2393aeaa8
likamrat <likamrat@microsoft.com>
2020-05-12 13:25:18 -0700

    initial vagrant

--- azure_arc_servers_jumpstart/vagrant/scripts/install_arc_agent.sh
+++ azure_arc_servers_jumpstart/vagrant/scripts/install_arc_agent.sh
@@ -8,16 +8,15 @@ source /tmp/vars.sh
 
 
 # Installing Azure CLI & Azure Arc Extensions
-apt-get update
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 
 az login --service-principal --username $appId --password $password --tenant $tenantId
 
 # Download the installation package
-wget https://aka.ms/azcmagent -O ~/install_linux_azcmagent.sh
+wget https://aka.ms/azcmagent -O /tmp/install_linux_azcmagent.sh
 
 # Install the hybrid agent
-bash ~/install_linux_azcmagent.sh
+. ./tmp/install_linux_azcmagent.sh
 
 # Run connect command
 # azcmagent connect --resource-group "Arc-Dev" --tenant-id "72f988bf-86f1-41af-91ab-2d7cd011db47" --location "eastus" --subscription-id "e73c1dbe-2574-4f38-9e8f-c813757b1786"
