511df79b6963a121d7e7fa781ebd24b98685788e
likamrat <likamrat@microsoft.com>
2020-05-12 13:47:55 -0700

    initial vagrant

--- azure_arc_servers_jumpstart/vagrant/scripts/install_arc_agent.sh
+++ azure_arc_servers_jumpstart/vagrant/scripts/install_arc_agent.sh
@@ -14,11 +14,11 @@ az login --service-principal --username $appId --password $password --tenant $te
 
 az group create --location $location --name $resourceGroup --subscription $subscriptionId
 
-# Download the installation package
-wget https://aka.ms/azcmagent -O ~/install_linux_azcmagent.sh
+# # Download the installation package
+# wget https://aka.ms/azcmagent -O ~/install_linux_azcmagent.sh
 
-# Install the hybrid agent
-bash ~/install_linux_azcmagent.sh
+# # Install the hybrid agent
+# bash ~/install_linux_azcmagent.sh
 
 # Run connect command
 # azcmagent connect --resource-group "Arc-Dev" --tenant-id "72f988bf-86f1-41af-91ab-2d7cd011db47" --location "eastus" --subscription-id "e73c1dbe-2574-4f38-9e8f-c813757b1786"
