09e103c2e55765b888f15ca7e1a38509514d1780
likamrat <likamrat@microsoft.com>
2020-05-12 17:11:36 -0700

    initial vagrant

--- azure_arc_servers_jumpstart/vagrant/ubuntu/scripts/install_arc_agent.sh
+++ azure_arc_servers_jumpstart/vagrant/ubuntu/scripts/install_arc_agent.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-do-release-upgrade
 apt-get update
 
 # Injecting environment variables
 # curl https://raw.githubusercontent.com/likamrat/azure_arc/master/azure_arc_servers_jumpstart/vagrant/scripts/vars.sh --output /tmp/vars.sh
 
 source /tmp/vars.sh
-
 
 # Installing Azure CLI
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
@@ -17,24 +15,10 @@ az login --service-principal --username $appId --password $password --tenant $te
 az group create --location $location --name $resourceGroup --subscription $subscriptionId
 
 cat <<EOT >> delete_rg.sh
-#!/bin/sh
+#!/bin/bash
 az group delete --name $resourceGroup --subscription $subscriptionId --yes
 EOT
 chmod +x delete_rg.sh
-
-# Download the installation package
-
-# cat <<EOT >> arc.sh
-# #!/bin/sh
-# # Download the installation package
-# wget https://aka.ms/azcmagent -O ~/install_linux_azcmagent.sh
-
-# # Install the hybrid agent
-# bash ~/install_linux_azcmagent.sh
-# EOT
-
-# chmod +x arc.sh
-# . ./arc.sh
 
 # Download the installation package
 wget https://aka.ms/azcmagent -O ~/install_linux_azcmagent.sh
