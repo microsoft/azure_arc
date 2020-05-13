8adde1d64fcaf09e3c06df895481b0d33bd8c4b6
likamrat <likamrat@microsoft.com>
2020-05-13 16:15:42 -0700

    vagrant win

--- azure_arc_servers_jumpstart/local/vagrant/windows/scripts/install_arc_agent.ps1
+++ azure_arc_servers_jumpstart/local/vagrant/windows/scripts/install_arc_agent.ps1
@@ -5,12 +5,12 @@ Invoke-Expression "C:\runtime\vars.ps1"
 
 # Installing Azure CLI
 Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi; Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'; rm .\AzureCLI.msi
-az login --service-principal --username $appId --password $password --tenant $tenantId
-az group create --location $location --name $resourceGroup --subscription $subscriptionId
+az login --service-principal --username $env:appId --password $env:password --tenant $env:tenantId
+az group create --location $env:location --name $env:resourceGroup --subscription $env:subscriptionId 
 
 # Creating cleanup script for 'vagrant destory'
 New-Item C:\runtime\delete_rg.ps1
-Set-Content C:\runtime\delete_rg.ps1 'az group delete --name $resourceGroup --subscription $subscriptionId --yes'
+Set-Content C:\runtime\delete_rg.ps1 'az group delete --name $env:resourceGroup --subscription $env:subscriptionId --yes'
 
 # Download the package
 function download() {$ProgressPreference="SilentlyContinue"; Invoke-WebRequest -Uri https://aka.ms/AzureConnectedMachineAgent -OutFile AzureConnectedMachineAgent.msi}
@@ -19,11 +19,11 @@ download
 # Install the package
 msiexec /i AzureConnectedMachineAgent.msi /l*v installationlog.txt /qn | Out-String
 
-# Run connect command
-& "$env:ProgramFiles\AzureConnectedMachineAgent\azcmagent.exe" connect `
-  --appId "{serviceprincipalAppID}" `
-  --password "{serviceprincipalPassword}" `
-  --resourceGroup "{ResourceGroupName}" `
-  --tenantId "{tenantID}" `
-  --location "{resourceLocation}" `
-  --subscriptionId "{subscriptionID}"
+ # Run connect command
+ & "$env:ProgramFiles\AzureConnectedMachineAgent\azcmagent.exe" connect `
+ --service-principal-id "{$env:appId}" `
+ --service-principal-secret "{$env:password}" `
+ --resource-group "{$env:resourceGroup}" `
+ --tenant-id "{$env:tenantId}" `
+ --location "{$env:location}" `
+ --subscription-id "{$env:subscriptionId}" 
