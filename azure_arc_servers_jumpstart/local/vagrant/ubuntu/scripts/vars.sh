8fb38bd9baf474b421fa1288a4a637200e8c87fd
likamrat <likamrat@microsoft.com>
2020-05-13 08:19:13 -0700

    vagrant vars

---
+++ azure_arc_servers_jumpstart/local/vagrant/ubuntu/scripts/vars.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# <--- Change the following environment variables according to your Azure Service Principle name --->
+
+export subscriptionId=<Your Azure Subscription ID>
+export appId=<Your Azure Service Principle name>
+export password=<Your Azure Service Principle password>
+export tenantId=<Your Azure tenant ID>
+export resourceGroup=<Azure Resource Group Name>
+export location=<Azure Region>
