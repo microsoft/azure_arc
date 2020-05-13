9bd521da28fc310846a13ccb433f06ae7ef816e2
Dale Kirby <dakir@microsoft.com>
2020-05-13 18:20:09 -0500

    initial commit

---
+++ azure_arc_servers_jumpstart/gcp/terraform/variables.tf
@@ -0,0 +1,39 @@
+# Declare TF variables
+variable "gcp_project_name" {
+}
+
+variable "gcp_region" {
+    default = "us-west1"
+}
+
+variable "gcp_zone" {
+    default = "us-west1-a"
+}
+
+variable "admin_username" {
+    default = "arcadmin"
+}
+
+variable "admin_password" {
+    default = "arcpass123!!"
+}
+
+variable "azure_location" {
+    default = "westus2"
+}
+
+variable "azure_resource_group" {
+    default = "Arc-Servers-Demo"
+}
+
+variable "subscription_id" {
+}
+
+variable "client_id" {
+}
+
+variable "client_secret" {
+}
+
+variable "tenant_id" {
+}
