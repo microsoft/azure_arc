918dcfee3cf3023e7584cba06fb3f96339653357
likamrat <likamrat@microsoft.com>
2020-05-13 21:09:09 -0700

    vagrant win

--- azure_arc_servers_jumpstart/local/vagrant/windows/scripts/vars.ps1
+++ azure_arc_servers_jumpstart/local/vagrant/windows/scripts/vars.ps1
@@ -1,6 +1,13 @@
 
 # <--- Change the following environment variables according to your Azure Service Principle name --->
 
+# $env:subscriptionId=<Your Azure Subscription ID>
+# $env:appId=<Your Azure Service Principle name>
+# $env:password=<Your Azure Service Principle password>
+# $env:tenantId=<Your Azure tenant ID>
+# $env:resourceGroup=<Azure Resource Group Name>
+# $env:location=<Azure Region>
+
 $env:subscriptionId="e73c1dbe-2574-4f38-9e8f-c813757b1786"
 $env:appId="051b9a58-4a83-48de-b610-0e7ae1bca3fb"
 $env:password="53ed1458-a77d-4201-9c21-4fe24a0981fa"
