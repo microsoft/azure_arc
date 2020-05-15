a78c8ce63cda059e996af19a3623607b3811960d
Dale Kirby <dakir@microsoft.com>
2020-05-15 15:11:48 -0500

    add lf and change comment per PR feedback

--- azure_arc_servers_jumpstart/gcp/terraform/scripts/vars.sh
+++ azure_arc_servers_jumpstart/gcp/terraform/scripts/vars.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-# Replace with values for your environment surrounded by ""
+
+# <--- Change the following environment variables according to your Azure Service Principal and GCP project details --->
 export TF_VAR_subscription_id={subscription id}
 export TF_VAR_client_id={client id}
 export TF_VAR_client_secret={client secret}
