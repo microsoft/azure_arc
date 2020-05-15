4c75d9c7c78890b8a56987752b5780f4c60c88af
likamrat <likamrat@microsoft.com>
2020-05-15 14:38:15 -0700

    remove secrets

--- azure_arc_servers_jumpstart/local/vagrant/ubuntu/scripts/vars.sh
+++ azure_arc_servers_jumpstart/local/vagrant/ubuntu/scripts/vars.sh
@@ -2,9 +2,9 @@
 
 # <--- Change the following environment variables according to your Azure Service Principle name --->
 
-# export subscriptionId=<Your Azure Subscription ID>
-# export appId=<Your Azure Service Principle name>
-# export password=<Your Azure Service Principle password>
-# export tenantId=<Your Azure tenant ID>
-# export resourceGroup=<Azure Resource Group Name>
-# export location=<Azure Region>
+export subscriptionId=<Your Azure Subscription ID>
+export appId=<Your Azure Service Principle name>
+export password=<Your Azure Service Principle password>
+export tenantId=<Your Azure tenant ID>
+export resourceGroup=<Azure Resource Group Name>
+export location=<Azure Region>
