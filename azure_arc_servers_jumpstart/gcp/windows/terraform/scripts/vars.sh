3d246ab609dab7cc9bed8cab9893c92fd361c10d
Dale Kirby <dakir@microsoft.com>
2020-05-20 14:40:22 -0500

    add vars.sh

--- azure_arc_servers_jumpstart/gcp/windows/terraform/scripts/vars.sh
+++ azure_arc_servers_jumpstart/gcp/windows/terraform/scripts/vars.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# <--- Change the following environment variables according to your Azure Service Principal and GCP project details --->
+# <--- Change the following environment variables according to your Azure Service Principal and GCP project details ---> 
 export TF_VAR_subscription_id={subscription id}
 export TF_VAR_client_id={client id}
 export TF_VAR_client_secret={client secret}
