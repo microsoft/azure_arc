80f882c2acab7e3ae158e049ace122bcd0948c03
likamrat <likamrat@microsoft.com>
2020-05-20 19:31:27 -0700

    aks

---
+++ azure_arc_k8s_jumpstart/aks/arm_template/scripts/az_connect_aks.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# <--- Change the following environment variables according to your Azure Service Principle name --->
+
+echo "Exporting environment variables"
+export appId=<Your Azure Service Principle name>
+export password=<Your Azure Service Principle password>
+export tenantId=<Your Azure tenant ID>
+export resourceGroup=<Azure Resource Group Name>
+export arcClusterName=<The name of your k8s cluster as it will be shown in Azure Arc>
+
+# Getting AKS credentials
+echo "Log in with Service Principle & Getting AKS credentials"
+az login --service-principal --username $appId --password $password --tenant $tenantId
+az aks get-credentials --name $arcClusterName --resource-group $resourceGroup --overwrite-existing
+
+# Installing Azure Arc k8s Extensions
+echo "Installing Azure CLI & Azure Arc Extensions"
+az extension add --name connectedk8s
+az extension add --name k8sconfiguration
+
+echo "Connecting the cluster to Azure Arc"
+az connectedk8s connect --name $arcClusterName --resource-group $resourceGroup
