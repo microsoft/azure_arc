28db8f0a326935a12e90d628693645ccc8781931
likamrat <likamrat@microsoft.com>
2020-05-23 10:32:52 -0700

    fix vars syntax

--- azure_arc_k8s_jumpstart/rancher_k3s/terraform/scripts/vars.sh
+++ azure_arc_k8s_jumpstart/rancher_k3s/terraform/scripts/vars.sh
@@ -2,6 +2,6 @@
 
 # <--- Change the following environment variables according to your Azure Service Principle name --->
 
-export appId=<Your Azure Service Principle name>
-export password=<Your Azure Service Principle password>
-export tenantId=<Your Azure tenant ID>
+export appId='<Your Azure Service Principle name>'
+export password='<Your Azure Service Principle password>'
+export tenantId='<Your Azure tenant ID>'
