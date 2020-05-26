87ee90d33fd3e90d065b3716d42250a12d46c3e3
Dale Kirby <dakir@microsoft.com>
2020-05-26 12:17:38 -0500

    orig vars.sh

---
+++ azure_arc_servers_jumpstart/aws/al2/terraform/scripts/vars.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# <--- Change the following environment variables according to your Azure Service Principal and GCP project details --->
+export TF_VAR_subscription_id={subscription id}
+export TF_VAR_client_id={client id}
+export TF_VAR_client_secret={client secret}
+export TF_VAR_tenant_id={tenant id}
+export AWS_ACCESS_KEY_ID={aws access key}
+export AWS_SECRET_ACCESS_KEY={aws secret key}
