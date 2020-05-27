0f071549954e4061863cbf444b6fc96098a623cf
Dale Kirby <dakir@microsoft.com>
2020-05-27 08:37:37 -0500

    init

--- azure_arc_servers_jumpstart/aws/al2/terraform/main.tf
+++ azure_arc_servers_jumpstart/aws/al2/terraform/main.tf
@@ -48,22 +48,6 @@ resource "aws_subnet" "subnet1" {
   availability_zone = var.aws_availabilityzone
 }
 
-# data "aws_ami" "ubuntu" {
-#     most_recent = true
-
-#     filter {
-#         name   = "name"
-#         values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
-#     }
-
-#     filter {
-#         name   = "virtualization-type"
-#         values = ["hvm"]
-#     }
-
-#     owners = ["099720109477"] # Canonical
-# }
-
 data "aws_ami" "amazon-linux-2" {
  most_recent = true
  owners = ["amazon"]
