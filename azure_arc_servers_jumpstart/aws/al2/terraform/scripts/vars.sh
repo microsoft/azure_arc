11d16fe1f33acf0ce5e9c350ebe49ba4d340ef53
Dale Kirby <dakir@microsoft.com>
2020-05-27 14:50:13 -0500

    init

--- azure_arc_servers_jumpstart/aws/al2/terraform/scripts/vars.sh
+++ azure_arc_servers_jumpstart/aws/al2/terraform/scripts/vars.sh
@@ -5,5 +5,5 @@ export TF_VAR_subscription_id={subscription id}
 export TF_VAR_client_id={client id}
 export TF_VAR_client_secret={client secret}
 export TF_VAR_tenant_id={tenant id}
-export AWS_ACCESS_KEY_ID={aws access key id}
+export AWS_ACCESS_KEY_ID={aws access key}
 export AWS_SECRET_ACCESS_KEY={aws secret key}
