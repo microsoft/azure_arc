08e4013b26f3a86c9cb7e90f5feeba45519b97e0
Ali Hussain <alhussai@MININT-DVQPRS9.redmond.corp.microsoft.com>
2020-05-26 18:50:12 -0700

    FinalPhase

--- azure_arc_k8s_jumpstart/eks/terraform/eks-worker-nodes.tf
+++ azure_arc_k8s_jumpstart/eks/terraform/eks-worker-nodes.tf
@@ -4,8 +4,8 @@
 #  * EKS Node Group to launch worker nodes
 #
 
-resource "aws_iam_role" "demo-node" {
-  name = "terraform-eks-demo-node"
+resource "aws_iam_role" "arcdemo-node" {
+  name = "terraform-eks-arcdemo-node"
 
   assume_role_policy = <<POLICY
 {
@@ -23,26 +23,26 @@ resource "aws_iam_role" "demo-node" {
 POLICY
 }
 
-resource "aws_iam_role_policy_attachment" "demo-node-AmazonEKSWorkerNodePolicy" {
+resource "aws_iam_role_policy_attachment" "arcdemo-node-AmazonEKSWorkerNodePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = aws_iam_role.demo-node.name
+  role       = aws_iam_role.arcdemo-node.name
 }
 
-resource "aws_iam_role_policy_attachment" "demo-node-AmazonEKS_CNI_Policy" {
+resource "aws_iam_role_policy_attachment" "arcdemo-node-AmazonEKS_CNI_Policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = aws_iam_role.demo-node.name
+  role       = aws_iam_role.arcdemo-node.name
 }
 
-resource "aws_iam_role_policy_attachment" "demo-node-AmazonEC2ContainerRegistryReadOnly" {
+resource "aws_iam_role_policy_attachment" "arcdemo-node-AmazonEC2ContainerRegistryReadOnly" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = aws_iam_role.demo-node.name
+  role       = aws_iam_role.arcdemo-node.name
 }
 
-resource "aws_eks_node_group" "demo" {
-  cluster_name    = aws_eks_cluster.demo.name
-  node_group_name = "demo"
-  node_role_arn   = aws_iam_role.demo-node.arn
-  subnet_ids      = aws_subnet.demo[*].id
+resource "aws_eks_node_group" "arcdemo" {
+  cluster_name    = aws_eks_cluster.arcdemo.name
+  node_group_name = "arcdemo"
+  node_role_arn   = aws_iam_role.arcdemo-node.arn
+  subnet_ids      = aws_subnet.arcdemo[*].id
 
   scaling_config {
     desired_size = 1
@@ -51,8 +51,8 @@ resource "aws_eks_node_group" "demo" {
   }
 
   depends_on = [
-    aws_iam_role_policy_attachment.demo-node-AmazonEKSWorkerNodePolicy,
-    aws_iam_role_policy_attachment.demo-node-AmazonEKS_CNI_Policy,
-    aws_iam_role_policy_attachment.demo-node-AmazonEC2ContainerRegistryReadOnly,
+    aws_iam_role_policy_attachment.arcdemo-node-AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.arcdemo-node-AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.arcdemo-node-AmazonEC2ContainerRegistryReadOnly,
   ]
 }
--- azure_arc_k8s_jumpstart/eks/terraform/outputs.tf
+++ azure_arc_k8s_jumpstart/eks/terraform/outputs.tf
@@ -13,7 +13,7 @@ metadata:
   namespace: kube-system
 data:
   mapRoles: |
-    - rolearn: ${aws_iam_role.demo-node.arn}
+    - rolearn: ${aws_iam_role.arcdemo-node.arn}
       username: system:node:{{EC2PrivateDNSName}}
       groups:
         - system:bootstrappers
@@ -26,8 +26,8 @@ CONFIGMAPAWSAUTH
 apiVersion: v1
 clusters:
 - cluster:
-    server: ${aws_eks_cluster.demo.endpoint}
-    certificate-authority-data: ${aws_eks_cluster.demo.certificate_authority.0.data}
+    server: ${aws_eks_cluster.arcdemo.endpoint}
+    certificate-authority-data: ${aws_eks_cluster.arcdemo.certificate_authority.0.data}
   name: kubernetes
 contexts:
 - context:
