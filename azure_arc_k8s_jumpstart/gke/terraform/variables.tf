697dd6e128f1851b56f70ef018cbafd6a70198be
likamrat <likamrat@microsoft.com>
2020-06-08 19:51:23 -0700

    gke fix

---
+++ azure_arc_k8s_jumpstart/gke/terraform/variables.tf
@@ -0,0 +1,20 @@
+variable "gcp_project_id" {
+}
+
+variable "gcp_credentials_filename" {
+}
+
+variable "gcp_region" {
+}
+
+variable "gke_cluster_name" {
+}
+
+variable "admin_username" {
+}
+
+variable "admin_password" {
+}
+
+variable "gke_cluster_node_count" {
+}
