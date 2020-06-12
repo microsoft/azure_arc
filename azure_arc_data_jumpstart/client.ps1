8d5954eff0dac6a5fed3ddb314ed11321ae244af
likamrat <likamrat@microsoft.com>
2020-06-12 16:57:27 -0700

    Arc Tools

--- azure_arc_data_jumpstart/client.ps1
+++ azure_arc_data_jumpstart/client.ps1
@@ -1,38 +1,38 @@
 #Script based on https://raw.githubusercontent.com/Azure/azure-quickstart-templates/master/visual-studio-dev-vm-chocolatey/scripts/SetupChocolatey.ps1
-param([Parameter(Mandatory=$true)][string]$chocoPackages)
+# param([Parameter(Mandatory=$true)][string]$chocoPackages)
 
-Write-Host "File packages URL: $linktopackages"
+# Write-Host "File packages URL: $linktopackages"
 
-#Changing ExecutionPolicy
-Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force
+# #Changing ExecutionPolicy
+# Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force
 
-#Change securoty protocol
-[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+# #Change securoty protocol
+# [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
 
-# Install Choco
-$sb = { iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1')) }
-Invoke-Command -ScriptBlock $sb 
+# # Install Choco
+# $sb = { iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1')) }
+# Invoke-Command -ScriptBlock $sb 
 
-$sb = { Set-ItemProperty -path HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System -name EnableLua -value 0 }
-Invoke-Command -ScriptBlock $sb 
+# $sb = { Set-ItemProperty -path HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System -name EnableLua -value 0 }
+# Invoke-Command -ScriptBlock $sb 
 
-#Install Chocolatey Packages
-$chocoPackages.Split(";") | ForEach {
-    choco install $_ -y -force
-}
+# #Install Chocolatey Packages
+# $chocoPackages.Split(";") | ForEach {
+#     choco install $_ -y -force
+# }
 
-Write-Host "Packages from choco.org were installed"
+# Write-Host "Packages from choco.org were installed"
 
 New-Item -Path "C:\" -Name "tmp" -ItemType "directory"
+New-Item -Path "C:\Users\$env:USERNAME\" -Name ".azuredatastudio-insiders\extensions" -ItemType "directory"
 # Invoke-WebRequest "https://private-repo.microsoft.com/python/azure-arc-data/private-preview-may-2020/msi/Azure%20Data%20CLI.msi" -OutFile "C:\tmp\AZDataCLI.msi"
 Invoke-WebRequest "https://azuredatastudio-update.azurewebsites.net/latest/win32-x64-archive/insider" -OutFile "C:\tmp\azuredatastudio_insiders.zip"
 Invoke-WebRequest "https://github.com/microsoft/azuredatastudio/archive/master.zip" -OutFile "C:\tmp\azuredatastudio_repo.zip"
 
+
 $ExtensionsDestination = "C:\Users\$env:USERNAME\.azuredatastudio-insiders\extensions"
 Expand-Archive C:\tmp\azuredatastudio_insiders.zip -DestinationPath 'C:\Program Files\Azure Data Studio - Insiders'
 Expand-Archive C:\tmp\azuredatastudio_repo.zip -DestinationPath 'C:\tmp\azuredatastudio_repo'
-# Invoke-Item 'C:\Program Files\Azure Data Studio - Insider\azuredatastudio-insiders.exe'
-Start-Process -FilePath "C:\Program Files\Azure Data Studio - Insiders\azuredatastudio-insiders.exe"
 
 # $TargetFile             = "C:\Program Files\Azure Data Studio - Insiders\azuredatastudio-insiders.exe"
 # $ShortcutFile           = "C:\Users\$env:USERNAME\Desktop\Azure Data Studio - Insiders.lnk"
