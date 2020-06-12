0469031b5100bbed68ad4c0074c6fa98dc3d3bef
likamrat <likamrat@microsoft.com>
2020-06-12 13:30:04 -0700

    Arc Tools

--- azure_arc_data_jumpstart/clientTools.ps1
+++ azure_arc_data_jumpstart/clientTools.ps1
@@ -19,7 +19,6 @@ Expand-Archive C:\tmp\azuredatastudio_repo.zip -DestinationPath 'C:\tmp\azuredat
 $ExtensionsDestination = "C:\Users\$env:UserName\.azuredatastudio-insiders\extensions" | Out-Null
 Copy-Item -Path "C:\tmp\azuredatastudio_repo\azuredatastudio-master\extensions\arc" -Destination $ExtensionsDestination -Recurse -Force -ErrorAction Continue
 
-Creating new Azure Arc Resource Group
 az login --service-principal --username $env:appId --password $env:password --tenant $env:tenantId
 az aks get-credentials --name $env:arcClusterName --resource-group $env:resourceGroup --overwrite-existing
 
