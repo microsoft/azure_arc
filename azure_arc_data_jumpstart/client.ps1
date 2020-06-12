d49cf7ce55430324ec680e7493f28344dc11c021
likamrat <likamrat@microsoft.com>
2020-06-12 15:29:05 -0700

    Arc Tools

--- azure_arc_data_jumpstart/client.ps1
+++ azure_arc_data_jumpstart/client.ps1
@@ -29,20 +29,20 @@ Invoke-WebRequest "https://azuredatastudio-update.azurewebsites.net/latest/win32
 Expand-Archive C:\tmp\azuredatastudio_insider.zip -DestinationPath 'C:\Program Files\Azure Data Studio - Insider'
 Invoke-Item 'C:\Program Files\Azure Data Studio - Insider\azuredatastudio-insiders.exe'
 
-# $TargetFile   = "C:\Program Files\Azure Data Studio - Insider\azuredatastudio-insiders.exe"
-# $ShortcutFile = "C:\Users\$env:USERNAME\Desktop\Azure Data Studio - Insider.lnk"
-# $WScriptShell = New-Object -ComObject WScript.Shell
-# $Shortcut     = $WScriptShell.CreateShortcut($ShortcutFile)
-# $Shortcut.TargetPath = $TargetFile
-# $Shortcut.Save()
+$TargetFile             = "C:\Program Files\Azure Data Studio - Insider\azuredatastudio-insiders.exe"
+$ShortcutFile           = "C:\Users\$env:USERNAME\Desktop\Azure Data Studio - Insider.lnk"
+$WScriptShell           = New-Object -ComObject WScript.Shell
+$Shortcut               = $WScriptShell.CreateShortcut($ShortcutFile)
+$Shortcut.TargetPath    = $TargetFile
+$Shortcut.Save()
 
-# Install-Package msi -provider PowerShellGet -Force
-# Install-MSIProduct C:\tmp\AZDataCLI.msi
+Install-Package msi -provider PowerShellGet -Force
+Install-MSIProduct C:\tmp\AZDataCLI.msi
 
-# Invoke-WebRequest "https://github.com/microsoft/azuredatastudio/archive/master.zip" -OutFile "C:\tmp\azuredatastudio_repo.zip"
-# Expand-Archive C:\tmp\azuredatastudio_repo.zip -DestinationPath 'C:\tmp\azuredatastudio_repo'
-# $ExtensionsDestination = "C:\Users\$env:USERNAME\.azuredatastudio-insiders\extensions"
-# Copy-Item -Path "C:\tmp\azuredatastudio_repo\azuredatastudio-master\extensions\arc" -Destination $ExtensionsDestination -Recurse -Force -ErrorAction Continue
+Invoke-WebRequest "https://github.com/microsoft/azuredatastudio/archive/master.zip" -OutFile "C:\tmp\azuredatastudio_repo.zip"
+Expand-Archive C:\tmp\azuredatastudio_repo.zip -DestinationPath 'C:\tmp\azuredatastudio_repo'
+$ExtensionsDestination = "C:\Users\$env:USERNAME\.azuredatastudio-insiders\extensions"
+Copy-Item -Path "C:\tmp\azuredatastudio_repo\azuredatastudio-master\extensions\arc" -Destination $ExtensionsDestination -Recurse -Force -ErrorAction Continue
 
 az login --service-principal --username $env:appId --password $env:password --tenant $env:tenantId
 az aks get-credentials --name $env:arcClusterName --resource-group $env:resourceGroup --overwrite-existing
