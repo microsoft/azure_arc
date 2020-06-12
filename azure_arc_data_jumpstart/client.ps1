1b367eaa4ce1613649a498e3a2cc625a4f0f27be
likamrat <likamrat@microsoft.com>
2020-06-12 16:44:04 -0700

    Arc Tools

--- azure_arc_data_jumpstart/client.ps1
+++ azure_arc_data_jumpstart/client.ps1
@@ -24,23 +24,24 @@ $chocoPackages.Split(";") | ForEach {
 Write-Host "Packages from choco.org were installed"
 
 New-Item -Path "C:\" -Name "tmp" -ItemType "directory"
-Invoke-WebRequest "https://private-repo.microsoft.com/python/azure-arc-data/private-preview-may-2020/msi/Azure%20Data%20CLI.msi" -OutFile "C:\tmp\AZDataCLI.msi"
+# Invoke-WebRequest "https://private-repo.microsoft.com/python/azure-arc-data/private-preview-may-2020/msi/Azure%20Data%20CLI.msi" -OutFile "C:\tmp\AZDataCLI.msi"
 Invoke-WebRequest "https://azuredatastudio-update.azurewebsites.net/latest/win32-x64-archive/insider" -OutFile "C:\tmp\azuredatastudio_insiders.zip"
-Expand-Archive C:\tmp\azuredatastudio_insider.zip -DestinationPath 'C:\Program Files\Azure Data Studio - Insiders'
+Invoke-WebRequest "https://github.com/microsoft/azuredatastudio/archive/master.zip" -OutFile "C:\tmp\azuredatastudio_repo.zip"
+
+$ExtensionsDestination = "C:\Users\$env:USERNAME\.azuredatastudio-insiders\extensions"
+Expand-Archive C:\tmp\azuredatastudio_insiders.zip -DestinationPath 'C:\Program Files\Azure Data Studio - Insiders'
+Expand-Archive C:\tmp\azuredatastudio_repo.zip -DestinationPath 'C:\tmp\azuredatastudio_repo'
 # Invoke-Item 'C:\Program Files\Azure Data Studio - Insider\azuredatastudio-insiders.exe'
 Start-Process -FilePath "C:\Program Files\Azure Data Studio - Insiders\azuredatastudio-insiders.exe"
 
-$TargetFile             = "C:\Program Files\Azure Data Studio - Insiders\azuredatastudio-insiders.exe"
-$ShortcutFile           = "C:\Users\$env:USERNAME\Desktop\Azure Data Studio - Insiders.lnk"
-$WScriptShell           = New-Object -ComObject WScript.Shell
-$Shortcut               = $WScriptShell.CreateShortcut($ShortcutFile)
-$Shortcut.TargetPath    = $TargetFile
-$Shortcut.Save()
+# $TargetFile             = "C:\Program Files\Azure Data Studio - Insiders\azuredatastudio-insiders.exe"
+# $ShortcutFile           = "C:\Users\$env:USERNAME\Desktop\Azure Data Studio - Insiders.lnk"
+# $WScriptShell           = New-Object -ComObject WScript.Shell
+# $Shortcut               = $WScriptShell.CreateShortcut($ShortcutFile)
+# $Shortcut.TargetPath    = $TargetFile
+# $Shortcut.Save()
 
 # Install-Package msi -provider PowerShellGet -Force
 # Install-MSIProduct C:\tmp\AZDataCLI.msi
 
-Invoke-WebRequest "https://github.com/microsoft/azuredatastudio/archive/master.zip" -OutFile "C:\tmp\azuredatastudio_repo.zip"
-Expand-Archive C:\tmp\azuredatastudio_repo.zip -DestinationPath 'C:\tmp\azuredatastudio_repo'
-$ExtensionsDestination = "C:\Users\$env:USERNAME\.azuredatastudio-insiders\extensions"
 Copy-Item -Path "C:\tmp\azuredatastudio_repo\azuredatastudio-master\extensions\arc" -Destination $ExtensionsDestination -Recurse -Force -ErrorAction Continue
