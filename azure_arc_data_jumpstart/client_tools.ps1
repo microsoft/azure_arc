862a14ad683862362642522552ade31b20116d81
likamrat <likamrat@microsoft.com>
2020-06-12 12:06:10 -0700

    data temp

--- azure_arc_data_jumpstart/client_tools.ps1
+++ azure_arc_data_jumpstart/client_tools.ps1
@@ -1,28 +1,28 @@
-$chocolateyAppList = "kubernetes-cli"
+# $chocolateyAppList = "kubernetes-cli"
 
-if ([string]::IsNullOrWhiteSpace($chocolateyAppList) -eq $false)
-{
-    try{
-        choco config get cacheLocation
-    }catch{
-        Write-Output "Chocolatey not detected, trying to install now"
-        iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-    }
-}
+# if ([string]::IsNullOrWhiteSpace($chocolateyAppList) -eq $false)
+# {
+#     try{
+#         choco config get cacheLocation
+#     }catch{
+#         Write-Output "Chocolatey not detected, trying to install now"
+#         iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+#     }
+# }
 
-if ([string]::IsNullOrWhiteSpace($chocolateyAppList) -eq $false){   
-    Write-Host "Chocolatey Apps Specified"  
+# if ([string]::IsNullOrWhiteSpace($chocolateyAppList) -eq $false){   
+#     Write-Host "Chocolatey Apps Specified"  
     
-    $appsToInstall = $chocolateyAppList -split "," | foreach { "$($_.Trim())" }
+#     $appsToInstall = $chocolateyAppList -split "," | foreach { "$($_.Trim())" }
 
-    foreach ($app in $appsToInstall)
-    {
-        Write-Host "Installing $app"
-        & choco install $app /y | Write-Output
-    }
-}
+#     foreach ($app in $appsToInstall)
+#     {
+#         Write-Host "Installing $app"
+#         & choco install $app /y | Write-Output
+#     }
+# }
 
-# Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi; Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'; rm .\AzureCLI.msi
+Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi; Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'; rm .\AzureCLI.msi
 # $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
 
 # New-Item -Path "C:\" -Name "tmp" -ItemType "directory"
