9d1f99565c0cf5089710bb45c002bb529a87bd9c
likamrat <likamrat@microsoft.com>
2020-06-12 10:52:26 -0700

    data temp

--- azure_arc_data_jumpstart/client_tools.ps1
+++ azure_arc_data_jumpstart/client_tools.ps1
@@ -1,4 +1,4 @@
-$env:chocolateyAppList = "kubernetes-cli"
+$env:chocolateyAppList = "azure-cli, kubernetes-cli"
 
 if ([string]::IsNullOrWhiteSpace($env:chocolateyAppList) -eq $false)
 {
@@ -22,8 +22,8 @@ if ([string]::IsNullOrWhiteSpace($env:chocolateyAppList) -eq $false){
     }
 }
 
-Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi; Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'; rm .\AzureCLI.msi
-# $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
+# Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi; Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'; rm .\AzureCLI.msi
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
 
 # New-Item -Path "C:\" -Name "tmp" -ItemType "directory"
 # Invoke-WebRequest "https://private-repo.microsoft.com/python/azure-arc-data/private-preview-may-2020/msi/Azure%20Data%20CLI.msi" -OutFile "C:\tmp\AZDataCLI.msi"
