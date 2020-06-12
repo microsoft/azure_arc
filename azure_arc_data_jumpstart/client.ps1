807fda853ce0436bccaa0553893ba2edac940e8d
likamrat <likamrat@microsoft.com>
2020-06-12 16:02:24 -0700

    Arc Tools

--- azure_arc_data_jumpstart/client.ps1
+++ azure_arc_data_jumpstart/client.ps1
@@ -43,6 +43,3 @@ Invoke-WebRequest "https://github.com/microsoft/azuredatastudio/archive/master.z
 Expand-Archive C:\tmp\azuredatastudio_repo.zip -DestinationPath 'C:\tmp\azuredatastudio_repo'
 $ExtensionsDestination = "C:\Users\$env:USERNAME\.azuredatastudio-insiders\extensions"
 Copy-Item -Path "C:\tmp\azuredatastudio_repo\azuredatastudio-master\extensions\arc" -Destination $ExtensionsDestination -Recurse -Force -ErrorAction Continue
-
-az login --service-principal --username $env:appId --password $env:password --tenant $env:tenantId
-az aks get-credentials --name $env:arcClusterName --resource-group $env:resourceGroup --overwrite-existing
