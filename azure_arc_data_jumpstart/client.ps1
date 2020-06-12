27822f24091b0ae8a3d668e87bcb4a047245f7e3
likamrat <likamrat@microsoft.com>
2020-06-12 16:21:52 -0700

    Arc Tools

--- azure_arc_data_jumpstart/client.ps1
+++ azure_arc_data_jumpstart/client.ps1
@@ -25,9 +25,10 @@ Write-Host "Packages from choco.org were installed"
 
 New-Item -Path "C:\" -Name "tmp" -ItemType "directory"
 Invoke-WebRequest "https://private-repo.microsoft.com/python/azure-arc-data/private-preview-may-2020/msi/Azure%20Data%20CLI.msi" -OutFile "C:\tmp\AZDataCLI.msi"
-Invoke-WebRequest "https://azuredatastudio-update.azurewebsites.net/latest/win32-x64-archive/insider" -OutFile "C:\tmp\azuredatastudio_insider.zip"
-Expand-Archive C:\tmp\azuredatastudio_insider.zip -DestinationPath 'C:\Program Files\Azure Data Studio - Insider'
-Invoke-Item 'C:\Program Files\Azure Data Studio - Insider\azuredatastudio-insiders.exe'
+Invoke-WebRequest "https://azuredatastudio-update.azurewebsites.net/latest/win32-x64-archive/insider" -OutFile "C:\tmp\azuredatastudio_insiders.zip"
+Expand-Archive C:\tmp\azuredatastudio_insider.zip -DestinationPath 'C:\Program Files\Azure Data Studio - Insiders' | Out-Null
+# Invoke-Item 'C:\Program Files\Azure Data Studio - Insider\azuredatastudio-insiders.exe'
+Start-Process -FilePath "C:\Program Files\Azure Data Studio - Insider\azuredatastudio-insiders.exe"
 
 $TargetFile             = "C:\Program Files\Azure Data Studio - Insider\azuredatastudio-insiders.exe"
 $ShortcutFile           = "C:\Users\$env:USERNAME\Desktop\Azure Data Studio - Insider.lnk"
@@ -40,6 +41,6 @@ $Shortcut.Save()
 # Install-MSIProduct C:\tmp\AZDataCLI.msi
 
 Invoke-WebRequest "https://github.com/microsoft/azuredatastudio/archive/master.zip" -OutFile "C:\tmp\azuredatastudio_repo.zip"
-Expand-Archive C:\tmp\azuredatastudio_repo.zip -DestinationPath 'C:\tmp\azuredatastudio_repo'
-$ExtensionsDestination = "C:\Users\$env:USERNAME\.azuredatastudio-insiders\extensions"
+Expand-Archive C:\tmp\azuredatastudio_repo.zip -DestinationPath 'C:\tmp\azuredatastudio_repo' | Out-Null
+$ExtensionsDestination = "C:\Users\$env:USERNAME\.azuredatastudio-insiders\extensions" | Out-Null
 Copy-Item -Path "C:\tmp\azuredatastudio_repo\azuredatastudio-master\extensions\arc" -Destination $ExtensionsDestination -Recurse -Force -ErrorAction Continue
