b4c4fdc5d66410af04ce358986fa54f7760f2e0f
likamrat <likamrat@microsoft.com>
2020-06-12 08:44:02 -0700

    data temp

--- azure_arc_data_jumpstart/client_tools.ps1
+++ azure_arc_data_jumpstart/client_tools.ps1
@@ -1,18 +1,21 @@
+$env:chocolateyAppList = "kubernetes-cli"
+
+Enable-WSManCredSSP -Role Server -Force
+Enable-WSManCredSSP -Role Client -DelegateComputer * -Force
+New-Item -Path HKLM:\SOFTWARE\Policies\Microsoft\Windows\CredentialsDelegation -Name AllowFreshCredentialsWhenNTLMOnly -Force
+New-ItemProperty -Path HKLM:\SOFTWARE\Policies\Microsoft\Windows\CredentialsDelegation\AllowFreshCredentialsWhenNTLMOnly -Name 1 -Value * -PropertyType String
+
+
 $securePassword = ConvertTo-SecureString $env:adminPassword -AsPlainText -Force
 $credential = New-Object System.Management.Automation.PSCredential $env:computername\$env:UserName, $securePassword
 Invoke-Command -Authentication CredSSP -ScriptBlock {
+    New-Item -Path "C:\" -Name "tmp" -ItemType "directory"
+    Invoke-WebRequest "https://private-repo.microsoft.com/python/azure-arc-data/private-preview-may-2020/msi/Azure%20Data%20CLI.msi" -OutFile "C:\tmp\AZDataCLI.msi"
+    Invoke-WebRequest "https://azuredatastudio-update.azurewebsites.net/latest/win32-x64-archive/insider" -OutFile "C:\tmp\azuredatastudio_insider.zip"
+    Expand-Archive C:\tmp\azuredatastudio_insider.zip -DestinationPath 'C:\Program Files\Azure Data Studio - Insider'
+} -ComputerName $env:computername -Credential $credential
 
-New-Item -Path "C:\" -Name "tmp" -ItemType "directory"
-Invoke-WebRequest "https://private-repo.microsoft.com/python/azure-arc-data/private-preview-may-2020/msi/Azure%20Data%20CLI.msi" -OutFile "C:\tmp\AZDataCLI.msi"
-Invoke-WebRequest "https://azuredatastudio-update.azurewebsites.net/latest/win32-x64-archive/insider" -OutFile "C:\tmp\azuredatastudio_insider.zip"
-Expand-Archive C:\tmp\azuredatastudio_insider.zip -DestinationPath 'C:\Program Files\Azure Data Studio - Insider'
-Invoke-Item 'C:\Program Files\Azure Data Studio - Insider\azuredatastudio-insiders.exe'
-# Stop-Process -Name "azuredatastudio-insiders" -Force
-
-#Install-Package msi -provider PowerShellGet -Force
-#Install-MSIProduct C:\tmp\AZDataCLI.msi
-Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi; Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'; rm .\AzureCLI.msi
-$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
+Invoke-Item 'C:\Program Files\Azure Data Studio - Insider\azuredatastudio-insiders.exe' | Out-Null
 
 $TargetFile   = "C:\Program Files\Azure Data Studio - Insider\azuredatastudio-insiders.exe"
 $ShortcutFile = "C:\Users\$env:UserName\Desktop\Azure Data Studio - Insider.lnk"
@@ -21,7 +24,7 @@ $Shortcut     = $WScriptShell.CreateShortcut($ShortcutFile)
 $Shortcut.TargetPath = $TargetFile
 $Shortcut.Save()
 
-if ([string]::IsNullOrWhiteSpace($chocolateyAppList) -eq $false -or [string]::IsNullOrWhiteSpace($dismAppList) -eq $false)
+if ([string]::IsNullOrWhiteSpace($env:chocolateyAppList) -eq $false)
 {
     try{
         choco config get cacheLocation
@@ -31,10 +34,10 @@ if ([string]::IsNullOrWhiteSpace($chocolateyAppList) -eq $false -or [string]::Is
     }
 }
 
-if ([string]::IsNullOrWhiteSpace($chocolateyAppList) -eq $false){   
+if ([string]::IsNullOrWhiteSpace($env:chocolateyAppList) -eq $false){   
     Write-Host "Chocolatey Apps Specified"  
     
-    $appsToInstall = $chocolateyAppList -split "," | foreach { "$($_.Trim())" }
+    $appsToInstall = $env:chocolateyAppList -split "," | foreach { "$($_.Trim())" }
 
     foreach ($app in $appsToInstall)
     {
@@ -43,15 +46,24 @@ if ([string]::IsNullOrWhiteSpace($chocolateyAppList) -eq $false){
     }
 }
 
+#Install-Package msi -provider PowerShellGet -Force
+#Install-MSIProduct C:\tmp\AZDataCLI.msi
+
+
+
 Invoke-WebRequest "https://github.com/microsoft/azuredatastudio/archive/master.zip" -OutFile "C:\tmp\azuredatastudio_repo.zip"
 Expand-Archive C:\tmp\azuredatastudio_repo.zip -DestinationPath 'C:\tmp\azuredatastudio_repo' | Out-Null
 $ExtensionsDestination = "C:\Users\$env:UserName\.azuredatastudio-insiders\extensions"
 Copy-Item -Path "C:\tmp\azuredatastudio_repo\azuredatastudio-master\extensions\arc" -Destination $ExtensionsDestination -Recurse -Force -ErrorAction Continue
 
+Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi; Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'; rm .\AzureCLI.msi
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
+
 # Creating new Azure Arc Resource Group
 az login --service-principal --username $env:appId --password $env:password --tenant $env:tenantId
-az aks get-credentials --name $arcClusterName --resource-group $resourceGroup --overwrite-existing
-} -ComputerName $env:computername -Credential $credential
+az aks get-credentials --name $env:arcClusterName --resource-group $env:resourceGroup --overwrite-existing
+
+ 
 
 
 
