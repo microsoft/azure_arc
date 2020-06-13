fa94d0d8c8ee634456bb0bb6cc80f6a7211a9da6
likamrat <likamrat@microsoft.com>
2020-06-13 00:57:28 -0700

    Arc Tools

--- azure_arc_data_jumpstart/lucky2.ps1
+++ azure_arc_data_jumpstart/lucky2.ps1
@@ -1,2 +1,4 @@
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
+
 az login --service-principal --username $env:appId --password $env:password --tenant $env:tenantId
 az aks get-credentials --name $env:arcClusterName --resource-group $env:resourceGroup --overwrite-existing
