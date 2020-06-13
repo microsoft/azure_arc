72afb9b3807f8ab24b1f2d7416e610c343b921fd
likamrat <likamrat@microsoft.com>
2020-06-13 10:53:47 -0700

    Arc Tools

--- azure_arc_data_jumpstart/lucky2.ps1
+++ azure_arc_data_jumpstart/lucky2.ps1
@@ -1,5 +1,6 @@
-Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi; Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'; rm .\AzureCLI.msi
-$env:Path = [System.Environment]::ExpandEnvironmentVariables([System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User"))
+$azurePassword = ConvertTo-SecureString $password -AsPlainText -Force
+$psCred = New-Object System.Management.Automation.PSCredential($appId , $azurePassword)
+Connect-AzAccount -Credential $psCred -TenantId $tenantId -ServicePrincipal 
 
-az login --service-principal --username $env:appId --password $env:password --tenant $env:tenantId
-az aks get-credentials --name $env:arcClusterName --resource-group $env:resourceGroup --overwrite-existing
+Import-AzAksCredential -ResourceGroupName $resourceGroup -Name $arcClusterName
+
