497b4668c2d11e27a899b8d02fb0738999ea768d
likamrat <likamrat@microsoft.com>
2020-06-12 22:19:18 -0700

    Arc Tools

--- azure_arc_data_jumpstart/lucky.ps1
+++ azure_arc_data_jumpstart/lucky.ps1
@@ -1,14 +1,16 @@
 
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
-Write-Host "Installing Azure CLI"
-choco install azure-cli -y
+# Write-Host "Installing Azure CLI"
+# choco install azure-cli -y
 
 Write-Host "Installing Kubernetes CLI"
 choco install kubernetes-cli -y
 
-# Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi; Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'; rm .\AzureCLI.msi
+Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi; Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'; rm .\AzureCLI.msi
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
 
-az login --service-principal --username $appId --password $password --tenant $tenantId
-az aks get-credentials --name $arcClusterName --resource-group $resourceGroup --overwrite-existing
+$env:appId | Write-Output C:\debug.txt
+
+az login --service-principal --username $env:appId --password $env:password --tenant $env:tenantId
+az aks get-credentials --name $env:arcClusterName --resource-group $env:resourceGroup --overwrite-existing
