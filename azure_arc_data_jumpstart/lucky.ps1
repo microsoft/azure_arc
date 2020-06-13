82089ac741d62820079ea28c0080790d0f08121d
likamrat <likamrat@microsoft.com>
2020-06-13 09:46:21 -0700

    Arc Tools

--- azure_arc_data_jumpstart/lucky.ps1
+++ azure_arc_data_jumpstart/lucky.ps1
@@ -6,10 +6,13 @@ param (
     [string]$resourceGroup
 )
 
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+# Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
-choco install azure-cli -y 
-choco install kubernetes-cli -y 
+# choco install azure-cli -y 
+# choco install kubernetes-cli -y 
+
+Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi; Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'; rm .\AzureCLI.msi
+
 
 if ($PSVersionTable.PSEdition -eq 'Desktop' -and (Get-Module -Name AzureRM -ListAvailable)) {
     Write-Warning -Message ('Az module not installed. Having both the AzureRM and ' +
@@ -17,6 +20,7 @@ if ($PSVersionTable.PSEdition -eq 'Desktop' -and (Get-Module -Name AzureRM -List
 } else {
     Install-Module -Name Az -AllowClobber -Scope AllUsers -Force
 }
+
 
 [System.Environment]::SetEnvironmentVariable('appId', $appId,[System.EnvironmentVariableTarget]::Machine)
 [System.Environment]::SetEnvironmentVariable('password', $password,[System.EnvironmentVariableTarget]::Machine)
@@ -33,7 +37,9 @@ if ($PSVersionTable.PSEdition -eq 'Desktop' -and (Get-Module -Name AzureRM -List
 # Invoke-WebRequest "https://github.com/microsoft/azuredatastudio/archive/master.zip" -OutFile "C:\tmp\azuredatastudio_repo.zip"
 # $env:Path = [System.Environment]::ExpandEnvironmentVariables([System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User"))
 
-Connect-AzAccount
+$azurePassword = ConvertTo-SecureString $env:password -AsPlainText -Force
+$psCred = New-Object System.Management.Automation.PSCredential($env:appId , $azurePassword)
+Connect-AzAccount -Credential $psCred -TenantId $env:tenantId  -ServicePrincipal 
 
 # az login --service-principal --username $env:appId --password $env:password --tenant $env:tenantId
 # az aks get-credentials --name $env:arcClusterName --resource-group $env:resourceGroup --overwrite-existing
