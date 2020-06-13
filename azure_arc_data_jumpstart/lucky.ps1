8b679755d9c43edea607e7e69c1e7001c05602a2
likamrat <likamrat@microsoft.com>
2020-06-13 12:12:37 -0700

    Arc Tools

--- azure_arc_data_jumpstart/lucky.ps1
+++ azure_arc_data_jumpstart/lucky.ps1
@@ -44,7 +44,7 @@ if ([string]::IsNullOrWhiteSpace($chocolateyAppList) -eq $false){
 
 $azurePassword = ConvertTo-SecureString $password -AsPlainText -Force
 $psCred = New-Object System.Management.Automation.PSCredential($appId , $azurePassword)
-Connect-AzAccount -Credential $psCred -TenantId $env:tenantId -ServicePrincipal 
+Connect-AzAccount -Credential $psCred -TenantId $tenantId -ServicePrincipal 
 
 Import-AzAksCredential -ResourceGroupName $env:resourceGroup -Name $env:arcClusterName -Force
 kubectl get nodes
