73f1b343ddb879a62d7d22f71c1973c55b8e02eb
likamrat <likamrat@microsoft.com>
2020-06-13 01:36:59 -0700

    Arc Tools

--- azure_arc_data_jumpstart/lucky2.ps1
+++ azure_arc_data_jumpstart/lucky2.ps1
@@ -1,3 +1,4 @@
+Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi; Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'; rm .\AzureCLI.msi
 $env:Path = [System.Environment]::ExpandEnvironmentVariables([System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User"))
 
 az login --service-principal --username $env:appId --password $env:password --tenant $env:tenantId
