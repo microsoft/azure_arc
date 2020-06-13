4ad8e75dbd804c2671833d70221c8741d3bbddf8
likamrat <likamrat@microsoft.com>
2020-06-12 17:13:00 -0700

    Arc Tools

--- azure_arc_data_jumpstart/client.ps1
+++ azure_arc_data_jumpstart/client.ps1
@@ -1,5 +1,6 @@
 #Script based on https://raw.githubusercontent.com/Azure/azure-quickstart-templates/master/visual-studio-dev-vm-chocolatey/scripts/SetupChocolatey.ps1
 # param([Parameter(Mandatory=$true)][string]$chocoPackages)
+param([Parameter(Mandatory=$true)][string]$adminUsername)
 
 # Write-Host "File packages URL: $linktopackages"
 
@@ -24,18 +25,18 @@
 # Write-Host "Packages from choco.org were installed"
 
 New-Item -Path "C:\" -Name "tmp" -ItemType "directory"
-New-Item -Path "C:\Users\$env:adminUsername\" -Name ".azuredatastudio-insiders\extensions" -ItemType "directory"
+New-Item -Path "C:\Users\$adminUsername\" -Name ".azuredatastudio-insiders\extensions" -ItemType "directory"
 # Invoke-WebRequest "https://private-repo.microsoft.com/python/azure-arc-data/private-preview-may-2020/msi/Azure%20Data%20CLI.msi" -OutFile "C:\tmp\AZDataCLI.msi"
 Invoke-WebRequest "https://azuredatastudio-update.azurewebsites.net/latest/win32-x64-archive/insider" -OutFile "C:\tmp\azuredatastudio_insiders.zip"
 Invoke-WebRequest "https://github.com/microsoft/azuredatastudio/archive/master.zip" -OutFile "C:\tmp\azuredatastudio_repo.zip"
 
 
-$ExtensionsDestination = "C:\Users\$env:adminUsername\.azuredatastudio-insiders\extensions"
-Expand-Archive C:\tmp\azuredatastudio_insiders.zip -DestinationPath 'C:\Program Files\Azure Data Studio - Insiders'
-Expand-Archive C:\tmp\azuredatastudio_repo.zip -DestinationPath 'C:\tmp\azuredatastudio_repo'
+# $ExtensionsDestination = "C:\Users\$adminUsername\.azuredatastudio-insiders\extensions"
+# Expand-Archive C:\tmp\azuredatastudio_insiders.zip -DestinationPath 'C:\Program Files\Azure Data Studio - Insiders'
+# Expand-Archive C:\tmp\azuredatastudio_repo.zip -DestinationPath 'C:\tmp\azuredatastudio_repo'
 
 # $TargetFile             = "C:\Program Files\Azure Data Studio - Insiders\azuredatastudio-insiders.exe"
-# $ShortcutFile           = "C:\Users\$env:USERNAME\Desktop\Azure Data Studio - Insiders.lnk"
+# $ShortcutFile           = "C:\Users\$env:adminUsername\Desktop\Azure Data Studio - Insiders.lnk"
 # $WScriptShell           = New-Object -ComObject WScript.Shell
 # $Shortcut               = $WScriptShell.CreateShortcut($ShortcutFile)
 # $Shortcut.TargetPath    = $TargetFile
