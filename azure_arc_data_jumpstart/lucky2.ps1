943a1be75e1175aecf2e80c2e11534c386312888
likamrat <likamrat@microsoft.com>
2020-06-12 22:55:12 -0700

    Arc Tools

---
+++ azure_arc_data_jumpstart/lucky2.ps1
@@ -0,0 +1,2 @@
+az login --service-principal --username $env:appId --password $env:password --tenant $env:tenantId
+az aks get-credentials --name $env:arcClusterName --resource-group $env:resourceGroup --overwrite-existing
