2ee1a8dac58e3d2c13ab263575ae2415707a0fea
likamrat <likamrat@microsoft.com>
2020-06-13 18:24:19 -0700

    Arc Tools

--- azure_arc_data_jumpstart/lucky.ps1
+++ azure_arc_data_jumpstart/lucky.ps1
@@ -7,53 +7,55 @@ param (
     [string]$adminUsername
 )
 
-$chocolateyAppList = "azure-cli,az.powershell,kubernetes-cli"
+# $chocolateyAppList = "azure-cli,az.powershell,kubernetes-cli"
 
-if ([string]::IsNullOrWhiteSpace($chocolateyAppList) -eq $false)
-{
-    try{
-        choco config get cacheLocation
-    }catch{
-        Write-Output "Chocolatey not detected, trying to install now"
-        iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-    }
-}
+# if ([string]::IsNullOrWhiteSpace($chocolateyAppList) -eq $false)
+# {
+#     try{
+#         choco config get cacheLocation
+#     }catch{
+#         Write-Output "Chocolatey not detected, trying to install now"
+#         iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+#     }
+# }
 
-if ([string]::IsNullOrWhiteSpace($chocolateyAppList) -eq $false){   
-    Write-Host "Chocolatey Apps Specified"  
+# if ([string]::IsNullOrWhiteSpace($chocolateyAppList) -eq $false){   
+#     Write-Host "Chocolatey Apps Specified"  
     
-    $appsToInstall = $chocolateyAppList -split "," | foreach { "$($_.Trim())" }
+#     $appsToInstall = $chocolateyAppList -split "," | foreach { "$($_.Trim())" }
 
-    foreach ($app in $appsToInstall)
-    {
-        Write-Host "Installing $app"
-        & choco install $app /y | Write-Output
-    }
-}
+#     foreach ($app in $appsToInstall)
+#     {
+#         Write-Host "Installing $app"
+#         & choco install $app /y | Write-Output
+#     }
+# }
 
-[System.Environment]::SetEnvironmentVariable('appId', $appId,[System.EnvironmentVariableTarget]::Machine)
-[System.Environment]::SetEnvironmentVariable('password', $password,[System.EnvironmentVariableTarget]::Machine)
-[System.Environment]::SetEnvironmentVariable('tenantId', $tenantId,[System.EnvironmentVariableTarget]::Machine)
-[System.Environment]::SetEnvironmentVariable('arcClusterName', $arcClusterName,[System.EnvironmentVariableTarget]::Machine)
-[System.Environment]::SetEnvironmentVariable('resourceGroup', $resourceGroup,[System.EnvironmentVariableTarget]::Machine)
-[System.Environment]::SetEnvironmentVariable('adminUsername', $adminUsername,[System.EnvironmentVariableTarget]::Machine)
+# [System.Environment]::SetEnvironmentVariable('appId', $appId,[System.EnvironmentVariableTarget]::Machine)
+# [System.Environment]::SetEnvironmentVariable('password', $password,[System.EnvironmentVariableTarget]::Machine)
+# [System.Environment]::SetEnvironmentVariable('tenantId', $tenantId,[System.EnvironmentVariableTarget]::Machine)
+# [System.Environment]::SetEnvironmentVariable('arcClusterName', $arcClusterName,[System.EnvironmentVariableTarget]::Machine)
+# [System.Environment]::SetEnvironmentVariable('resourceGroup', $resourceGroup,[System.EnvironmentVariableTarget]::Machine)
+# [System.Environment]::SetEnvironmentVariable('adminUsername', $adminUsername,[System.EnvironmentVariableTarget]::Machine)
 
 
-$azurePassword = ConvertTo-SecureString $password -AsPlainText -Force
-$psCred = New-Object System.Management.Automation.PSCredential($appId , $azurePassword)
-Connect-AzAccount -Credential $psCred -TenantId $tenantId -ServicePrincipal 
+# $azurePassword = ConvertTo-SecureString $password -AsPlainText -Force
+# $psCred = New-Object System.Management.Automation.PSCredential($appId , $azurePassword)
+# Connect-AzAccount -Credential $psCred -TenantId $tenantId -ServicePrincipal 
 
-Import-AzAksCredential -ResourceGroupName $resourceGroup -Name $arcClusterName -Force
-kubectl get nodes
+# Import-AzAksCredential -ResourceGroupName $resourceGroup -Name $arcClusterName -Force
+# kubectl get nodes
 
-$variableNameToAdd = "KUBECONFIG"
-$variableValueToAdd = "C:\Windows\System32\config\systemprofile\.kube\config"
-[System.Environment]::SetEnvironmentVariable($variableNameToAdd, $variableValueToAdd, [System.EnvironmentVariableTarget]::Machine)
-[System.Environment]::SetEnvironmentVariable($variableNameToAdd, $variableValueToAdd, [System.EnvironmentVariableTarget]::Process)
-[System.Environment]::SetEnvironmentVariable($variableNameToAdd, $variableValueToAdd, [System.EnvironmentVariableTarget]::User)
+# $variableNameToAdd = "KUBECONFIG"
+# $variableValueToAdd = "C:\Windows\System32\config\systemprofile\.kube\config"
+# [System.Environment]::SetEnvironmentVariable($variableNameToAdd, $variableValueToAdd, [System.EnvironmentVariableTarget]::Machine)
+# [System.Environment]::SetEnvironmentVariable($variableNameToAdd, $variableValueToAdd, [System.EnvironmentVariableTarget]::Process) ## Check if can be removed
+# [System.Environment]::SetEnvironmentVariable($variableNameToAdd, $variableValueToAdd, [System.EnvironmentVariableTarget]::User) ## Check if can be removed
+
+# {Arc Data Controller HERE}
 
 New-Item -Path "C:\" -Name "tmp" -ItemType "directory"
-New-Item -Path "C:\Users\$adminUsername\" -Name ".azuredatastudio-insiders\extensions" -ItemType "directory"
+New-Item -Path "C:\Users\$env:USERNAME\" -Name ".azuredatastudio-insiders\extensions" -ItemType "directory"
 # Invoke-WebRequest "https://private-repo.microsoft.com/python/azure-arc-data/private-preview-may-2020/msi/Azure%20Data%20CLI.msi" -OutFile "C:\tmp\AZDataCLI.msi"
 Invoke-WebRequest "https://azuredatastudio-update.azurewebsites.net/latest/win32-x64-archive/insider" -OutFile "C:\tmp\azuredatastudio_insiders.zip"
 Invoke-WebRequest "https://github.com/microsoft/azuredatastudio/archive/master.zip" -OutFile "C:\tmp\azuredatastudio_repo.zip"
@@ -63,5 +65,15 @@ Invoke-WebRequest "https://github.com/microsoft/azuredatastudio/archive/master.z
 
 Expand-Archive C:\tmp\azuredatastudio_insiders.zip -DestinationPath 'C:\Program Files\Azure Data Studio - Insiders'
 Expand-Archive C:\tmp\azuredatastudio_repo.zip -DestinationPath 'C:\tmp\azuredatastudio_repo' | Out-Null
-$ExtensionsDestination = "C:\Users\$adminUsername\.azuredatastudio-insiders\extensions"
+$ExtensionsDestination = "C:\Users\$env:USERNAME\.azuredatastudio-insiders\extensions"
 Copy-Item -Path "C:\tmp\azuredatastudio_repo\azuredatastudio-master\extensions\arc" -Destination $ExtensionsDestination -Recurse -Force -ErrorAction Continue
+
+
+
+
+
+# Remove-Item –path "C:\tmp" -Recurse
+
+# Add Cleanup
+    # System Vars
+    # C:\tmp
