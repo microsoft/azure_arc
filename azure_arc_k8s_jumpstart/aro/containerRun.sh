37dcbf896a474db79a481e8b47949146e3622609
Ali Hussain <alhussai@Alis-MacBook-Pro.local>
2020-06-15 08:31:23 -0700

    correct md

--- azure_arc_k8s_jumpstart/aro/containerRun.sh
+++ azure_arc_k8s_jumpstart/aro/containerRun.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl https://raw.githubusercontent.com/alihhussain/azure_arc/master/azure_arc_k8s_jumpstart/aro/containerRun.sh | bash
+curl https://raw.githubusercontent.com/likamrat/azure_arc/master/azure_arc_k8s_jumpstart/aro/containerRun.sh | bash
