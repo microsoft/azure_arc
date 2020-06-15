f80069cd04b152f6d74d2adcd0be88e160252f72
likamrat <likamrat@microsoft.com>
2020-06-14 20:05:26 -0700

    tagging

--- azure_arc_data_jumpstart/lucky2.ps1
+++
@@ -1,6 +0,0 @@
-$azurePassword = ConvertTo-SecureString $password -AsPlainText -Force
-$psCred = New-Object System.Management.Automation.PSCredential($appId , $azurePassword)
-Connect-AzAccount -Credential $psCred -TenantId $tenantId -ServicePrincipal 
-
-Import-AzAksCredential -ResourceGroupName $resourceGroup -Name $arcClusterName
-
