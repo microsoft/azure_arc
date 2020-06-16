e5bb74e4388282ec6ecff1ed6217424a32491f14
likamrat <likamrat@microsoft.com>
2020-06-16 16:35:47 -0700

    ClientTools

--- azure_arc_data_jumpstart/ClientTools.ps1
+++ azure_arc_data_jumpstart/ClientTools.ps1
@@ -20,23 +20,14 @@ workflow ClientTools_01
             #Run commands in parallel.
             Parallel 
             {
-                if ([string]::IsNullOrWhiteSpace($chocolateyAppList) -eq $false)
-                {
-                    try{
-                        choco config get cacheLocation
-                    }catch{
-                        Write-Output "Chocolatey not detected, trying to install now"
-                        iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-                    }
-                }
                 choco install azure-cli -y -Force
                 choco install az.powershell -y -Force
                 choco install kubernetes-cli -y -Force
 
                 Invoke-WebRequest "https://azuredatastudio-update.azurewebsites.net/latest/win32-x64-archive/insider" -OutFile "C:\tmp\azuredatastudio_insiders.zip"                
                 Invoke-WebRequest "https://private-repo.microsoft.com/python/azure-arc-data/private-preview-may-2020/msi/Azure%20Data%20CLI.msi" -OutFile "C:\tmp\AZDataCLI.msi"
+                Expand-Archive C:\tmp\azuredatastudio_insiders.zip -DestinationPath 'C:\Program Files\Azure Data Studio - Insiders'
                 Install-Package msi -provider PowerShellGet -Force
-
             }
         }
 
@@ -45,7 +36,6 @@ ClientTools_01 | ft
 # workflow ClientTools_02
 #         {
 #             #Run commands in parallel.
-            
 #             {
 #                 Expand-Archive C:\tmp\azuredatastudio_insiders.zip -DestinationPath 'C:\Program Files\Azure Data Studio - Insiders'
 #             }
@@ -53,19 +43,16 @@ ClientTools_01 | ft
         
 # ClientTools_02 | ft 
 
-# $variableNameToAdd = "KUBECONFIG"
-# $variableValueToAdd = "C:\Windows\System32\config\systemprofile\.kube\config"
-# [System.Environment]::SetEnvironmentVariable($variableNameToAdd, $variableValueToAdd, [System.EnvironmentVariableTarget]::Machine)
-# [System.Environment]::SetEnvironmentVariable($variableNameToAdd, $variableValueToAdd, [System.EnvironmentVariableTarget]::Process)
-# [System.Environment]::SetEnvironmentVariable($variableNameToAdd, $variableValueToAdd, [System.EnvironmentVariableTarget]::User) ## Check if can be removed
+$variableNameToAdd = "KUBECONFIG"
+$variableValueToAdd = "C:\Windows\System32\config\systemprofile\.kube\config"
+[System.Environment]::SetEnvironmentVariable($variableNameToAdd, $variableValueToAdd, [System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable($variableNameToAdd, $variableValueToAdd, [System.EnvironmentVariableTarget]::Process)
+[System.Environment]::SetEnvironmentVariable($variableNameToAdd, $variableValueToAdd, [System.EnvironmentVariableTarget]::User) ## Check if can be removed
 
-# $azurePassword = ConvertTo-SecureString $password -AsPlainText -Force
-# $psCred = New-Object System.Management.Automation.PSCredential($appId , $azurePassword)
-# Connect-AzAccount -Credential $psCred -TenantId $tenantId -ServicePrincipal 
-# Import-AzAksCredential -ResourceGroupName $resourceGroup -Name $arcClusterName -Force
-# kubectl get nodes
-
-
-
+$azurePassword = ConvertTo-SecureString $password -AsPlainText -Force
+$psCred = New-Object System.Management.Automation.PSCredential($appId , $azurePassword)
+Connect-AzAccount -Credential $psCred -TenantId $tenantId -ServicePrincipal 
+Import-AzAksCredential -ResourceGroupName $resourceGroup -Name $arcClusterName -Force
+kubectl get nodes
 
 # Install-MSIProduct C:\tmp\AZDataCLI.msi
