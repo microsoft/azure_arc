6fdd6153ca89e9e3de38a6e2ea6af1c86afaa41f
likamrat <likamrat@microsoft.com>
2020-06-18 07:35:20 -0700

    ClientTools

--- azure_arc_data_jumpstart/ClientTools.ps1
+++ azure_arc_data_jumpstart/ClientTools.ps1
@@ -73,11 +73,11 @@ $variableValueToAdd = "C:\Windows\System32\config\systemprofile\.kube\config"
 [System.Environment]::SetEnvironmentVariable($variableNameToAdd, $variableValueToAdd, [System.EnvironmentVariableTarget]::Process)
 # [System.Environment]::SetEnvironmentVariable($variableNameToAdd, $variableValueToAdd, [System.EnvironmentVariableTarget]::User) ## Check if can be removed
 
-New-Item -path alias:azdata -value 'C:\ProgramData\chocolatey\lib\kubernetes-cli\tools\kubernetes\client\bin\kubectl.exe'
-$variableNameToAdd = "azdata"
-$variableValueToAdd = "C:\Program Files (x86)\Microsoft SDKs\Azdata\CLI\wbin\azdata.cmd"
-[System.Environment]::SetEnvironmentVariable($variableNameToAdd, $variableValueToAdd, [System.EnvironmentVariableTarget]::Machine)
-[System.Environment]::SetEnvironmentVariable($variableNameToAdd, $variableValueToAdd, [System.EnvironmentVariableTarget]::Process)
+New-Item -path alias:azdata -value 'C:\Program Files (x86)\Microsoft SDKs\Azdata\CLI\wbin\azdata.cmd'
+# $variableNameToAdd = "azdata"
+# $variableValueToAdd = "C:\Program Files (x86)\Microsoft SDKs\Azdata\CLI\wbin\azdata.cmd"
+# [System.Environment]::SetEnvironmentVariable($variableNameToAdd, $variableValueToAdd, [System.EnvironmentVariableTarget]::Machine)
+# [System.Environment]::SetEnvironmentVariable($variableNameToAdd, $variableValueToAdd, [System.EnvironmentVariableTarget]::Process)
 
 $azurePassword = ConvertTo-SecureString $password -AsPlainText -Force
 $psCred = New-Object System.Management.Automation.PSCredential($appId , $azurePassword)
