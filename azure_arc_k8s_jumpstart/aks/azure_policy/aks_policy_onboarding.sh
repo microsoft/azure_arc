e1425777d1acce7b776e7651386cdf50036bd304
likamrat <likamrat@microsoft.com>
2020-06-23 14:14:53 -0700

    aks_policy

--- azure_arc_k8s_jumpstart/aks/azure_policy/aks_policy_onboarding.sh
+++ azure_arc_k8s_jumpstart/aks/azure_policy/aks_policy_onboarding.sh
@@ -9,24 +9,11 @@ export tenantId='<Your Azure tenant ID>'
 export resourceGroup='<Azure Resource Group Name>'
 export arcClusterName='<The name of your k8s cluster as it will be shown in Azure Arc>'
 
-# Installing Helm 3
-echo "Installing Helm 3"
-curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
-chmod 700 get_helm.sh
-./get_helm.sh
-
-# Installing Azure CLI
-echo "Installing Azure CLI & Azure Arc Extensions"
-sudo apt-get update
-sudo apt-get install -y ca-certificates curl apt-transport-https lsb-release gnupg
-curl -sL https://packages.microsoft.com/keys/microsoft.asc |
-gpg --dearmor |
-sudo tee /etc/apt/trusted.gpg.d/microsoft.asc.gpg > /dev/null
-AZ_REPO=$(lsb_release -cs)
-echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" |
-sudo tee /etc/apt/sources.list.d/azure-cli.list
-sudo apt-get update
-sudo apt-get install azure-cli
+echo "Log in to Azure with Service Principle & Getting Connected Cluster Azure Resource ID"
+az login --service-principal --username $appId --password $password --tenant $tenantId
+az aks get-credentials --name $arcClusterName --resource-group $resourceGroup --overwrite-existing
+export clusterId="$(az resource show --resource-group $resourceGroup --name $arcClusterName --resource-type "Microsoft.Kubernetes/connectedClusters" --query id)"
+export clusterId="$(echo "$clusterId" | sed -e 's/^"//' -e 's/"$//')" 
 
 # Installing NGINX
 echo "Installing NGINX Ingress Controller"
@@ -36,11 +23,6 @@ helm install hello-arc stable/nginx-ingress \
     --set controller.replicaCount=2 \
     --set controller.nodeSelector."beta\.kubernetes\.io/os"=linux \
     --set defaultBackend.nodeSelector."beta\.kubernetes\.io/os"=linux
-
-echo "Log in to Azure with Service Principle & Getting Connected Cluster Azure Resource ID"
-az login --service-principal --username $appId --password $password --tenant $tenantId
-export clusterId="$(az resource show --resource-group $resourceGroup --name $arcClusterName --resource-type "Microsoft.Kubernetes/connectedClusters" --query id)"
-export clusterId="$(echo "$clusterId" | sed -e 's/^"//' -e 's/"$//')" 
 
 # Installing Azure Policy add-on
 echo "Installing Azure Policy add-on"
