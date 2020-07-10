1383e0a16de3b7e1921dcb999d0ebe847d44076e
likamrat <likamrat@microsoft.com>
2020-07-09 18:26:04 -0700

    aks_gitops_helm

--- azure_arc_k8s_jumpstart/aks/gitops/helm/az_k8sconfig_helm_cleanup.sh
+++ azure_arc_k8s_jumpstart/aks/gitops/helm/az_k8sconfig_helm_cleanup.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 
+# <--- Change the following environment variables according to your Azure Service Principle name --->
+
+echo "Exporting environment variables"
+export resourceGroup='<Azure Resource Group Name>'
+export arcClusterName='<The name of your k8s cluster as it will be shown in Azure Arc>'
+
 # Deleting GitOps Configurations from Azure Arc Kubernetes cluster
 echo "Deleting GitOps Configurations from Azure Arc Kubernetes cluster"
 az k8sconfiguration delete --name hello-arc --cluster-name $arcClusterName --resource-group $resourceGroup --cluster-type connectedClusters -y
 az k8sconfiguration delete --name nginx-ingress --cluster-name $arcClusterName --resource-group $resourceGroup --cluster-type connectedClusters -y
 
 # Cleaning Kubernetes cluster
-echo "Cleaning Kubernetes cluster"
+echo "Cleaning Kubernetes cluster. Can safely ignored non-exist resources"
 kubectl delete ns prod
 kubectl delete ns cluster-mgmt
 
