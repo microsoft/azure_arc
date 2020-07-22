781be8fea064f63e36cb7cab1ff61e218706ac77
likamrat <likamrat@microsoft.com>
2020-07-22 14:10:21 -0700

    initial eks_data

--- azure_arc_data_jumpstart/eks/terraform/scripts/DC_Cleanup.ps1
+++ azure_arc_data_jumpstart/eks/terraform/scripts/DC_Cleanup.ps1
@@ -1,7 +1,7 @@
 Start-Transcript -Path C:\tmp\dc_cleanup.log
 
 # Deleting Azure Arc Data Controller namespace and it's resources
-start Powershell {kubectl get pods -n $env:ARC_DC_NAME -w}
+start Powershell {for (0 -lt 1) {kubectl get pod -n $env:ARC_DC_NAME; sleep 5; clear }}
 kubectl delete ns $env:ARC_DC_NAME
 kubectl delete crd databases.mssqlcontroller.k8s.io
 kubectl delete crd registrations.tina-instances.com
