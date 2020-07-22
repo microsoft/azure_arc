e5ca7c66b933b6677752e58907fb3ffef0053b99
likamrat <likamrat@microsoft.com>
2020-07-22 10:42:59 -0700

    initial eks_data

---
+++ azure_arc_data_jumpstart/eks/terraform/scripts/ClientTools.ps1
@@ -0,0 +1,98 @@
+Start-Transcript -Path C:\tmp\ClientTools.log
+
+# Installing Chocolatey packages
+Param(  
+    [string]$chocolateyAppList  
+)
+
+$chocolateyAppList = "azure-cli,az.powershell,kubernetes-cli,aws-iam-authenticator"
+
+if ([string]::IsNullOrWhiteSpace($chocolateyAppList) -eq $false)
+{
+    try{
+        choco config get cacheLocation
+    }catch{
+        Write-Output "Chocolatey not detected, trying to install now"
+        iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+    }
+}
+
+if ([string]::IsNullOrWhiteSpace($chocolateyAppList) -eq $false){   
+    Write-Host "Chocolatey Apps Specified"  
+    
+    $appsToInstall = $chocolateyAppList -split "," | foreach { "$($_.Trim())" }
+
+    foreach ($app in $appsToInstall)
+    {
+        Write-Host "Installing $app"
+        & choco install $app /y | Write-Output
+    }
+}
+
+# Download & Install Azure Data Studio and azdata CLI
+Write-Host "Download & Install Azure Data Studio and azdata CLI"
+Write-Host "`n"
+Invoke-WebRequest "https://azuredatastudio-update.azurewebsites.net/latest/win32-x64-archive/insider" -OutFile "C:\tmp\azuredatastudio_insiders.zip"
+Invoke-WebRequest "https://github.com/microsoft/azuredatastudio/archive/master.zip" -OutFile "C:\tmp\azuredatastudio_repo.zip"
+Invoke-WebRequest "https://github.com/microsoft/azuredatastudio-postgresql/archive/v0.2.6.zip" -OutFile "C:\tmp\pgsqltoolsservice-win-x64.zip"
+Invoke-WebRequest "https://private-repo.microsoft.com/python/azure-arc-data/private-preview-jun-2020/msi/Azure%20Data%20CLI.msi" -OutFile "C:\tmp\AZDataCLI.msi"
+Invoke-WebRequest "https://raw.githubusercontent.com/microsoft/azure_arc/master/azure_arc_data_jumpstart/eks/terraform/scripts/DC_Cleanup.ps1" -OutFile "C:\tmp\DC_Cleanup.ps1"
+Invoke-WebRequest "https://raw.githubusercontent.com/microsoft/azure_arc/master/azure_arc_data_jumpstart/eks/terraform/scripts/DC_Deploy.ps1" -OutFile "C:\tmp\DC_Deploy.ps1"
+
+Expand-Archive C:\tmp\azuredatastudio_insiders.zip -DestinationPath 'C:\Program Files\Azure Data Studio - Insiders'
+Expand-Archive C:\tmp\azuredatastudio_repo.zip -DestinationPath 'C:\tmp\azuredatastudio_repo'
+Expand-Archive C:\tmp\pgsqltoolsservice-win-x64.zip -DestinationPath 'C:\tmp\'
+Start-Process msiexec.exe -Wait -ArgumentList '/I C:\tmp\AZDataCLI.msi /quiet'
+
+Write-Host "Copying Azure Data Studio Extentions"
+Write-Host "`n"
+$ExtensionsDestination = "C:\Users\Administrator\.azuredatastudio-insiders\extensions\arc"
+Copy-Item -Path "C:\tmp\azuredatastudio_repo\azuredatastudio-master\extensions\arc" -Destination $ExtensionsDestination -Recurse -Force -ErrorAction Continue
+$ExtensionsDestination = "C:\Users\Administrator\.azuredatastudio-insiders\extensions\azuredatastudio-postgresql-0.2.6"
+Copy-Item -Path "C:\tmp\azuredatastudio-postgresql-0.2.6\" -Destination $ExtensionsDestination -Recurse -Force -ErrorAction Continue 
+
+Write-Host "Creating Azure Data Studio Desktop shortcut"
+Write-Host "`n"
+$TargetFile = "C:\Program Files\Azure Data Studio - Insiders\azuredatastudio-insiders.exe"
+$ShortcutFile = "C:\Users\Administrator\Desktop\Azure Data Studio - Insiders.lnk"
+$WScriptShell = New-Object -ComObject WScript.Shell
+$Shortcut = $WScriptShell.CreateShortcut($ShortcutFile)
+$Shortcut.TargetPath = $TargetFile
+$Shortcut.Save()
+
+Write-Host "Deleting AWS Desktop shortcuts"
+Write-Host "`n"
+Remove-Item -Path "C:\Users\Administrator\Desktop\EC2 Microsoft Windows Guide.website" -Force
+Remove-Item -Path "C:\Users\Administrator\Desktop\EC2 Feedback.website" -Force
+
+# Setting up the kubectl & azdata environment
+Write-Host "Setting up the kubectl & azdata environment"
+Write-Host "`n"
+New-Item -path alias:kubectl -value 'C:\ProgramData\chocolatey\lib\kubernetes-cli\tools\kubernetes\client\bin\kubectl.exe'
+kubectl version
+kubectl apply -f "C:\tmp\configmap.yml"
+kubectl get nodes
+New-Item -path alias:azdata -value 'C:\Program Files (x86)\Microsoft SDKs\Azdata\CLI\wbin\azdata.cmd'
+azdata --version
+
+$LogonScript = @'
+Start-Transcript -Path C:\tmp\LogonScript.log
+
+start Powershell {kubectl get pods -n $env:ARC_DC_NAME -w}
+azdata arc dc create -c azure-arc-eks-private-preview --namespace $env:ARC_DC_NAME --name $env:ARC_DC_NAME --subscription $env:ARC_DC_SUBSCRIPTION --resource-group $env:ARC_DC_RG --location $env:ARC_DC_REGION --connectivity-mode indirect
+
+Unregister-ScheduledTask -TaskName "LogonScript" -Confirm:$false
+
+# Stopping log for LogonScript.ps1
+Stop-Transcript
+Stop-Process -Name kubectl -Force
+Stop-Process -Name powershell -Force
+'@ > C:\tmp\LogonScript.ps1
+
+# Creating LogonScript Windows Scheduled Task
+$Trigger = New-ScheduledTaskTrigger -AtLogOn
+$Action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument 'C:\tmp\LogonScript.ps1'
+Register-ScheduledTask -TaskName "LogonScript" -Trigger $Trigger -User "Administrator" -Action $Action -RunLevel "Highest" -Force
+
+# Stopping log for ClientTools.ps1
+Stop-Transcript
---
+++ azure_arc_data_jumpstart/eks/terraform/scripts/DC_Deploy.ps1
@@ -0,0 +1,11 @@
+Start-Transcript -Path C:\tmp\dc_deploy.log
+
+# Deploying Azure Arc Data Controller
+start Powershell {kubectl get pods -n $env:ARC_DC_NAME -w}
+azdata arc dc create -c azure-arc-eks-private-preview --namespace $env:ARC_DC_NAME --name $env:ARC_DC_NAME --subscription $env:ARC_DC_SUBSCRIPTION --resource-group $env:ARC_DC_RG --location $env:ARC_DC_REGION --connectivity-mode indirect
+azdata login -n $env:ARC_DC_NAME
+
+Stop-Transcript
+
+Stop-Process -Name kubectl -Force
+Stop-Process -name powershell -Force
