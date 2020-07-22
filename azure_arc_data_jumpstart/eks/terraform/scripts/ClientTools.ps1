ab7dfa8ca2c01a7311de22e512896baabb8a45b1
likamrat <likamrat@microsoft.com>
2020-07-22 14:10:21 -0700

    initial eks_data

--- azure_arc_data_jumpstart/eks/terraform/scripts/ClientTools.ps1
+++ azure_arc_data_jumpstart/eks/terraform/scripts/ClientTools.ps1
@@ -78,7 +78,7 @@ azdata --version
 $LogonScript = @'
 Start-Transcript -Path C:\tmp\LogonScript.log
 
-start Powershell {kubectl get pods -n $env:ARC_DC_NAME -w}
+start Powershell {for (0 -lt 1) {kubectl get pod -n $env:ARC_DC_NAME; sleep 5; clear }}
 azdata arc dc create -c azure-arc-eks-private-preview --namespace $env:ARC_DC_NAME --name $env:ARC_DC_NAME --subscription $env:ARC_DC_SUBSCRIPTION --resource-group $env:ARC_DC_RG --location $env:ARC_DC_REGION --connectivity-mode indirect
 
 Unregister-ScheduledTask -TaskName "LogonScript" -Confirm:$false
