3ccf4e75f9664550693096f4ab099737777183ff
likamrat <likamrat@microsoft.com>
2020-07-30 14:23:33 -0700

    kubeadm_azdata_jul

--- azure_arc_data_jumpstart/aks/arm_template/dc_vanilla/ClientTools.ps1
+++ azure_arc_data_jumpstart/aks/arm_template/dc_vanilla/ClientTools.ps1
@@ -125,7 +125,7 @@ $Shortcut.TargetPath = $TargetFile
 $Shortcut.Save()
 
 start Powershell {kubectl get pods -n $env:ARC_DC_NAME -w}
-azdata arc dc create -c azure-arc-aks-private-preview --namespace $env:ARC_DC_NAME --name $env:ARC_DC_NAME --subscription $env:ARC_DC_SUBSCRIPTION --resource-group $env:resourceGroup --location $env:ARC_DC_REGION --connectivity-mode indirect
+azdata arc dc create -p azure-arc-aks-private-preview --namespace $env:ARC_DC_NAME --name $env:ARC_DC_NAME --subscription $env:ARC_DC_SUBSCRIPTION --resource-group $env:resourceGroup --location $env:ARC_DC_REGION --connectivity-mode indirect
 
 Unregister-ScheduledTask -TaskName "LogonScript" -Confirm:$false
 
