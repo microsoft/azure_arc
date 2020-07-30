ab71bd1f0b3e708168b24582cfe5576da12a2059
likamrat <likamrat@microsoft.com>
2020-07-30 13:43:10 -0700

    aks_azdata_jul

--- azure_arc_data_jumpstart/aks/arm_template/dc_vanilla/ClientTools.ps1
+++ azure_arc_data_jumpstart/aks/arm_template/dc_vanilla/ClientTools.ps1
@@ -67,7 +67,7 @@ workflow ClientTools_01
                     Invoke-WebRequest "https://azuredatastudio-update.azurewebsites.net/latest/win32-x64-archive/insider" -OutFile "C:\tmp\azuredatastudio_insiders.zip"
                     Invoke-WebRequest "https://github.com/microsoft/azuredatastudio/archive/master.zip" -OutFile "C:\tmp\azuredatastudio_repo.zip"
                     Invoke-WebRequest "https://github.com/microsoft/azuredatastudio-postgresql/archive/v0.2.6.zip" -OutFile "C:\tmp\pgsqltoolsservice-win-x64.zip"
-                    Invoke-WebRequest "https://private-repo.microsoft.com/python/azure-arc-data/private-preview-jun-2020/msi/Azure%20Data%20CLI.msi" -OutFile "C:\tmp\AZDataCLI.msi"
+                    Invoke-WebRequest "https://private-repo.microsoft.com/python/azure-arc-data/private-preview-jul-2020/msi/Azure%20Data%20CLI.msi" -OutFile "C:\tmp\AZDataCLI.msi"
                     Invoke-WebRequest "https://raw.githubusercontent.com/microsoft/azure_arc/master/azure_arc_data_jumpstart/aks/arm_template/dc_vanilla/DC_Cleanup.ps1" -OutFile "C:\tmp\DC_Cleanup.ps1"
                     Invoke-WebRequest "https://raw.githubusercontent.com/microsoft/azure_arc/master/azure_arc_data_jumpstart/aks/arm_template/dc_vanilla/DC_Deploy.ps1" -OutFile "C:\tmp\DC_Deploy.ps1"
                 }
