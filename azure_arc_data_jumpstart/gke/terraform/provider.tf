9bebd7f9426e82df1f2d45b5cc38246e7961a4d8
likamrat <likamrat@microsoft.com>
2020-07-30 20:06:50 -0700

    gke_data_initial

---
+++ azure_arc_data_jumpstart/gke/terraform/provider.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_version = "~> 0.12"
+  required_providers {
+    google  = ">= 3.21"
+    local   = "~> 1.4"
+    azurerm = "~> 2.0.0"
+  }
+}
+
+provider "google" {
+  version     = "3.21"
+  credentials = file(var.gcp_credentials_filename)
+  project     = var.gcp_project_id
+  region      = var.gcp_region
+}
+
+provider "azurerm" {
+  subscription_id = var.ARC_DC_SUBSCRIPTION
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+  features {}
+}
