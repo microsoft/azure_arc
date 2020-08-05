b751b3c6289ce3e9ac0ec37c1e0b907a556ef550
likamrat <likamrat@microsoft.com>
2020-08-05 15:24:58 -0700

    k3s_arm_uri_fix

--- azure_arc_k8s_jumpstart/eks/terraform/variables.tf
+++ azure_arc_k8s_jumpstart/eks/terraform/variables.tf
@@ -2,7 +2,17 @@
 # Variables Configuration
 #
 
+variable "AWS_ACCESS_KEY_ID" {
+  description = "Your AWS Access Key ID"
+  type        = string
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  description = "Your AWS Secret Key"
+  type        = string
+}
+
 variable "cluster-name" {
-  default = "arcdemo"
+  default = "arc-eks-demo"
   type    = string
 }
