59ac11d8dc6505acf0110ac90d881ec9e8aef902
likamrat <likamrat@microsoft.com>
2020-08-07 09:14:12 -0700

    aks_pshs_initial

--- azure_arc_data_jumpstart/aks/arm_template/postgres_hs/scripts/PSHS_Deploy.ps1
+++ azure_arc_data_jumpstart/aks/arm_template/postgres_hs/scripts/PSHS_Deploy.ps1
@@ -1,20 +1,19 @@
 Start-Transcript -Path C:\tmp\mssql_deploy.log
 
 # Deploying Azure Arc Data Controller
-start Powershell {kubectl get pods -n $env:ARC_DC_NAME -w}
+start Powershell {for (0 -lt 1) {kubectl get pod -n $env:ARC_DC_NAME; sleep 5; clear }}
 azdata arc dc create -p azure-arc-aks-private-preview --namespace $env:ARC_DC_NAME --name $env:ARC_DC_NAME --subscription $env:ARC_DC_SUBSCRIPTION --resource-group $env:resourceGroup --location $env:ARC_DC_REGION --connectivity-mode indirect
 
-# Deploying Azure Arc SQL Managed Instance
+# Deploying Azure Arc PostgreSQL Hyperscale Instance
 azdata login -n $env:ARC_DC_NAME
-azdata sql instance create -n $env:MSSQL_MI_NAME -c $env:MSSQL_MI_vCores -s $env:ARC_DC_SUBSCRIPTION -r $env:resourceGroup
-azdata sql instance list
+azdata postgres server create --name $env:PSHS_NAME --namespace $env:ARC_DC_NAME --password $env:AZDATA_PASSWORD -w $env:PSHS_WORKER_NODE_COUNT --dataSizeMb $env:PSHS_DATASIZE --serviceType $env:PSHS_SERVICE_TYPE
+azdata postgres server list -ns $env:ARC_DC_NAME
 
 # Cleaning MSSQL Instance connectivity details
-Start-Process powershell -ArgumentList "C:\tmp\sql_connectivity.ps1" -WindowStyle Hidden -Wait
+Start-Process powershell -ArgumentList "C:\tmp\pshs_connectivity.ps1" -WindowStyle Hidden -Wait
 
 Stop-Transcript
 
 # Starting Azure Data Studio
 Start-Process -FilePath "C:\Program Files\Azure Data Studio - Insiders\azuredatastudio-insiders.exe" -WindowStyle Maximized
-Stop-Process -Name kubectl -Force
 Stop-Process -name powershell -Force
