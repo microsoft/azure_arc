cb62ee59a3019ba0636079cea98e237c58762c96
Dale Kirby <saitcho@outlook.com>
2020-08-20 12:20:41 -0500

    update providers for consistency

--- azure_arc_k8s_jumpstart/eks/terraform/providers.tf
+++ azure_arc_k8s_jumpstart/eks/terraform/providers.tf
@@ -4,7 +4,7 @@
 
 provider "aws" {
   region  = "us-west-2"
-  version = ">= 2.38.0"
+  version = "2.38.0"
   access_key = var.AWS_ACCESS_KEY_ID
   secret_key = var.AWS_SECRET_ACCESS_KEY  
 }
--- azure_arc_servers_jumpstart/vmware/ubuntu/terraform/provider.tf
+++ azure_arc_servers_jumpstart/vmware/ubuntu/terraform/provider.tf
@@ -1,21 +1,28 @@
+#
+# Providers Configuration
+#
+
+terraform {
+  required_version = "~> 0.12"
+  required_providers {
+    vsphere = "~> 1.18.1"
+    local   = "~> 1.4"
+    azurerm = "~> 2.9.0"
+  }
+}
+
 provider "vsphere" {
   user           = var.vsphere_user
   password       = var.vsphere_password
   vsphere_server = var.vsphere_server
-  version        = "~> 1.18.1"
   # If you have a self-signed cert
   allow_unverified_ssl = true
 }
 
 provider "azurerm" {
-  version = "2.9.0"
   features {}
   subscription_id = var.subscription_id
   client_id       = var.client_id
   client_secret   = var.client_secret
   tenant_id       = var.tenant_id
-}
-
-provider "local" {
-  version = "1.4"
 }
