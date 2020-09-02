14c4a108e1e40ad991f55d1ccd67ff43ed8810d1
likamrat <likamrat@microsoft.com>
2020-09-01 23:08:46 -0700

    aug_release_test

--- azure_arc_data_jumpstart/aks/arm_template/mssql_mi/scripts/MSSQL_MI_Cleanup.ps1
+++ azure_arc_data_jumpstart/aks/arm_template/mssql_mi/scripts/MSSQL_MI_Cleanup.ps1
@@ -2,13 +2,8 @@ Start-Transcript -Path C:\tmp\mssql_cleanup.log
 
 # Deleting Azure Arc Data Controller namespace and it's resources (MSSQL incl.)
 start Powershell {for (0 -lt 1) {kubectl get pod -n $env:ARC_DC_NAME; sleep 5; clear }}
+azdata arc dc delete --name $env:ARC_DC_NAME --namespace $env:ARC_DC_NAME --force
 kubectl delete ns $env:ARC_DC_NAME
-kubectl delete crd databases.mssqlcontroller.k8s.io
-kubectl delete crd registrations.tina-instances.com
-kubectl delete clusterrolebinding arcdatactrl:crb-mssql-metricsdc-reader
-kubectl delete clusterrolebinding crb-arcdatactrl-controller
-kubectl delete clusterrolebinding operator-rolebinding
-kubectl delete clusterrole arcdatactrl:cr-mssql-metricsdc-reader
 
 # Restoring State
 Copy-Item -Path "C:\tmp\hosts_backup" -Destination "C:\Windows\System32\drivers\etc\hosts" -Recurse -Force -ErrorAction Continue
