97254c2ac56f0779a96393e8424f513e1db42127
Travis Wright <twright@microsoft.com>
2020-09-20 21:52:07 -0700

    adding logging

--- azure_arc_data_jumpstart/aks/arm_template/dc_vanilla/scripts/ClientTools.ps1
+++ azure_arc_data_jumpstart/aks/arm_template/dc_vanilla/scripts/ClientTools.ps1
@@ -138,6 +138,16 @@ $Shortcut = $WScriptShell.CreateShortcut($ShortcutFile)
 $Shortcut.TargetPath = $TargetFile
 $Shortcut.Save()
 
+Write-Host "Registry info params:"
+Write-Host $DOCKER_REGISTRY
+Write-Host $DOCKER_REPOSITORY
+Write-Host $DOCKER_TAG
+
+Write-Host "Registry info env vars:"
+Write-Host $DOCKER_REGISTRY
+Write-Host $DOCKER_REPOSITORY
+Write-Host $DOCKER_TAG
+
 # Deploying Azure Arc Data Controller
 start Powershell {for (0 -lt 1) {kubectl get pod -n $env:ARC_DC_NAME; sleep 5; clear }}
 azdata arc dc config init --source azure-arc-aks-premium-storage --path ./custom
