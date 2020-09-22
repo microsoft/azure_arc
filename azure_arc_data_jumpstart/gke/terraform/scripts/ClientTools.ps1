5da96fe4bf0108e5b6a028bd6a01302c4b7b25e7
likamrat <likamrat@microsoft.com>
2020-09-22 12:28:18 -0700

    data_public

--- azure_arc_data_jumpstart/gke/terraform/scripts/ClientTools.ps1
+++ azure_arc_data_jumpstart/gke/terraform/scripts/ClientTools.ps1
@@ -40,9 +40,9 @@ Invoke-WebRequest "https://raw.githubusercontent.com/microsoft/azure_arc/master/
 
 Expand-Archive C:\tmp\azuredatastudio.zip -DestinationPath 'C:\Program Files\Azure Data Studio'
 
-Start-Process azuredatastudio -Wait -ArgumentList '--install-extension Microsoft.azdata'
-Start-Process azuredatastudio -Wait -ArgumentList '--install-extension Microsoft.arc'
-Start-Process azuredatastudio -Wait -ArgumentList '--install-extension Microsoft.azuredatastudio-postgresql'
+# Start-Process azuredatastudio -Wait -ArgumentList '--install-extension Microsoft.azdata'
+# Start-Process azuredatastudio -Wait -ArgumentList '--install-extension Microsoft.arc'
+# Start-Process azuredatastudio -Wait -ArgumentList '--install-extension Microsoft.azuredatastudio-postgresql'
 
 Start-Process msiexec.exe -Wait -ArgumentList '/I C:\tmp\AZDataCLI.msi /quiet'
 
@@ -51,6 +51,16 @@ Start-Process -FilePath "C:\Program Files\Azure Data Studio\azuredatastudio.exe"
 Start-Sleep -s 5
 Stop-Process -Name "azuredatastudio" -Force
 Copy-Item -Path "C:\tmp\settings.json" -Destination $SettingsDestination -Recurse -Force -ErrorAction Continue
+
+Write-Host "Installing Azure Data Studio Extentions"
+Write-Host "`n"
+
+$env:argument1="--install-extension"
+$env:argument2="Microsoft.arc"
+$env:argument3="microsoft.azuredatastudio-postgresql"
+
+& "C:\Program Files\Azure Data Studio\bin\azuredatastudio.cmd" $env:argument1 $env:argument2
+& "C:\Program Files\Azure Data Studio\bin\azuredatastudio.cmd" $env:argument1 $env:argument3
 
 Write-Host "Creating Azure Data Studio Desktop shortcut"
 Write-Host "`n"
