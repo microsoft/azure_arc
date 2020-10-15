ed7e5007eb006191fc660998a152c87e6dad4ae7
Ricardo Fiel <rifiel@microsoft.com>
2020-10-15 15:42:28 +0100

    Moved Windows scripts to PowerShell. Export MicroK8s config to use kubectl directly

---
+++ azure_arc_k8s_jumpstart/microk8s/gitops/helm/az_k8sconfig_helm_cleanup_microk8s_windows.ps1
@@ -0,0 +1,27 @@
+@ECHO OFF
+# <--- Change the following environment variables according to your Azure Service Principal name --->
+
+Write-Output "Exporting environment variables"
+$resourceGroup="<Your resource group name>"
+$arcClusterName="<Your Arc cluster name>"
+$appId="<Your Azure Service Principal name>"
+$password="<Your Azure Service Principal password>"
+$tenantId="<Your Azure tenant ID>"
+
+Write-Output "Log in to Azure with Service Principal"
+call az login --service-principal --username $appId --password $password --tenant $tenantId
+
+
+Write-Output "Deleting GitOps Configurations from Azure Arc Kubernetes cluster"
+call az k8sconfiguration delete --name hello-arc --cluster-name $arcClusterName --resource-group $resourceGroup --cluster-type connectedClusters -y
+
+Write-Output "Cleaning Kubernetes cluster. You Can safely ignore non-exist resources"
+microk8s kubectl delete ns prod
+
+microk8s kubectl delete clusterrole hello-arc-helm-prod-helm-operator-crd
+
+microk8s kubectl delete clusterrolebinding hello-arc-helm-prod-helm-operator
+
+microk8s kubectl delete -f  https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v0.35.0/deploy/static/provider/baremetal/deploy.yaml
+
+microk8s kubectl delete secret sh.helm.release.v1.azure-arc.v1 -n default
---
+++ azure_arc_k8s_jumpstart/microk8s/gitops/helm/az_k8sconfig_helm_microk8s_windows.ps1
@@ -0,0 +1,27 @@
+# <--- Change the following environment variables according to your Azure Service Principal name --->
+
+Write-Output "Exporting environment variables"
+$appId="<Your Azure Service Principal name>"
+$password="<Your Azure Service Principal password>"
+$tenantId="<Your Azure tenant ID>"
+$resourceGroup="<Azure Resource Group Name>"
+$arcClusterName="<The name of your k8s cluster as it will be shown in Azure Arc>"
+$appClonedRepo="<The URL for the 'Hello Arc' cloned GitHub repository>"
+
+# Logging in to Azure using service principal
+Write-Output "Log in to Azure with Service Principal"
+az login --service-principal --username $appId --password $password --tenant $tenantId
+
+EXIT 0
+
+# Create Namespace-level GitOps-Config for deploying the "Hello Arc" application
+Write-Output "Create Namespace-level GitOps-Config for deploying the 'Hello Arc' application"
+az k8sconfiguration create `
+--name hello-arc `
+--cluster-name $arcClusterName --resource-group $resourceGroup `
+--operator-instance-name hello-arc --operator-namespace prod `
+--enable-helm-operator --helm-operator-version="0.6.0" `
+--helm-operator-params="--set helm.versions=v3" `
+--repository-url $appClonedRepo `
+--scope namespace --cluster-type connectedClusters `
+--operator-params="--git-poll-interval 3s --git-readonly --git-path=releases/prod"
