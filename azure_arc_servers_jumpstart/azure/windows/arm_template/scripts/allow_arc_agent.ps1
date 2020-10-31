ccc56dbd83c0a4fcd3de01acc4a00619eb39da2f
likamrat <likamrat@microsoft.com>
2020-10-31 08:57:01 -0700

    initial azure servers

--- azure_arc_servers_jumpstart/azure/windows/arm_template/scripts/allow_arc_agent.ps1
+++ azure_arc_servers_jumpstart/azure/windows/arm_template/scripts/allow_arc_agent.ps1
@@ -18,18 +18,41 @@ param (
 
 New-Item -Path "C:\" -Name "tmp" -ItemType "directory" -Force
 
+$chocolateyAppList = "az.powershell,azure-cli"
+
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+if ([string]::IsNullOrWhiteSpace($chocolateyAppList) -eq $false){   
+    Write-Host "Chocolatey Apps Specified"  
+    
+    $appsToInstall = $chocolateyAppList -split "," | foreach { "$($_.Trim())" }
+
+    foreach ($app in $appsToInstall)
+    {
+        Write-Host "Installing $app"
+        & choco install $app /y
+    }
+}
+
 # Creating Powershell Logon Script
 $LogonScript = @'
 Start-Transcript -Path C:\tmp\LogonScript.log
 
+Write-Host "Log in to Azure"
+$azurePassword = ConvertTo-SecureString $password -AsPlainText -Force
+$psCred = New-Object System.Management.Automation.PSCredential($appId , $azurePassword)
+Connect-AzAccount -Credential $psCred -TenantId $tenantId -ServicePrincipal
+
 ## Configure the OS to allow Azure Arc Agent to be deploy on an Azure VM
 
+Write-Host "Configure the OS to allow Azure Arc Agent to be deploy on an Azure VM"
 Set-Service WindowsAzureGuestAgent -StartupType Disabled
 Stop-Service WindowsAzureGuestAgent -Force
 New-NetFirewallRule -Name BlockAzureIMDS -DisplayName "Block access to Azure IMDS" -Enabled True -Profile Any -Direction Outbound -Action Block -RemoteAddress 169.254.169.254 
 
 ## Azure Arc agent Installation
 
+Write-Host "Onboarding to Azure Arc"
 # Download the package
 function download() {$ProgressPreference="SilentlyContinue"; Invoke-WebRequest -Uri https://aka.ms/AzureConnectedMachineAgent -OutFile AzureConnectedMachineAgent.msi}
 download
