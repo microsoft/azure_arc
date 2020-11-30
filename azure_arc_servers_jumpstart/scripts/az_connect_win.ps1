c6d3804eea543e54d1be5be48d3cc9a2158bb931
DarkoArnautov <darkoservantes@outlook.com>
2020-11-30 21:40:33 +0100

    move all md file to the docs and update content

--- azure_arc_servers_jumpstart/scripts/az_connect_win.ps1
+++ azure_arc_servers_jumpstart/scripts/az_connect_win.ps1
@@ -21,5 +21,4 @@ msiexec /i AzureConnectedMachineAgent.msi /l*v installationlog.txt /qn | Out-Str
  --resource-group $env:resourceGroup `
  --tenant-id $env:tenantId `
  --location $env:location `
- --subscription-id $env:subscriptionId `
- --tags "Project=jumpstart_azure_arc_servers"
+ --subscription-id $env:subscriptionId
