79e20285d6751f17e5bf4b3cdf7f56b27636ed36
likamrat <likamrat@microsoft.com>
2020-12-21 11:03:21 -0800

    gke deploy + helm update

--- azure_arc_k8s_jumpstart/gke/terraform/providers.tf
+++ azure_arc_k8s_jumpstart/gke/terraform/providers.tf
@@ -1,7 +1,26 @@
-# Configure the Google Provider
+#
+# Providers Configuration
+#
+
+terraform {
+  required_version = "~> 0.12"
+  required_providers {
+    google  = "~> 3.21"
+    azurerm = "~> 2.0.0"
+  }
+}
+
 provider "google" {
-  version     = "3.21"
   credentials = file(var.gcp_credentials_filename)
   project     = var.gcp_project_id
   region      = var.gcp_region
+  # zone        = var.gcp_zone // (Optional) The location (region or zone) in which the cluster master will be created, as well as the default node location. If you specify a zone (such as us-central1-a), the cluster will be a zonal cluster with a single cluster master. If you specify a region (such as us-west1), the cluster will be a regional cluster with multiple masters spread across zones in the region, and with default node locations in those zones as well.
+}
+
+provider "azurerm" {
+  subscription_id = var.subscriptionId
+  client_id       = var.servicePrincipalAppId
+  client_secret   = var.servicePrincipalSecret
+  tenant_id       = var.servicePrincipalTenantId
+  features {}
 }
--- azure_arc_sqlsrv_jumpstart/gcp/winsrv/terraform/provider.tf
+++ azure_arc_sqlsrv_jumpstart/gcp/winsrv/terraform/provider.tf
@@ -6,8 +6,7 @@ terraform {
   required_version = "~> 0.12"
   required_providers {
     google  = "~> 3.21"
-    local   = "~> 1.4"
-    azurerm = "~> 2.9.0"
+    azurerm = "~> 2.0.0"
   }
 }
 
@@ -15,13 +14,13 @@ provider "google" {
   credentials = file(var.gcp_credentials_filename)
   project     = var.gcp_project_id
   region      = var.gcp_region
-  zone        = var.gcp_zone
+  # zone        = var.gcp_zone // (Optional) The location (region or zone) in which the cluster master will be created, as well as the default node location. If you specify a zone (such as us-central1-a), the cluster will be a zonal cluster with a single cluster master. If you specify a region (such as us-west1), the cluster will be a regional cluster with multiple masters spread across zones in the region, and with default node locations in those zones as well.
 }
 
 provider "azurerm" {
-  features {}
-  subscription_id = var.subId
+  subscription_id = var.subscriptionId
   client_id       = var.servicePrincipalAppId
   client_secret   = var.servicePrincipalSecret
   tenant_id       = var.servicePrincipalTenantId
+  features {}
 }
