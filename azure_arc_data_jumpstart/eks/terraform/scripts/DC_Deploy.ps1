16cb3f6342e00196d1417024b3147a0e44c44ba0
likamrat <likamrat@microsoft.com>
2021-01-05 06:57:07 -0800

    EKS Vanilla Direct

--- azure_arc_data_jumpstart/eks/terraform/scripts/DC_Deploy.ps1
+++ azure_arc_data_jumpstart/eks/terraform/scripts/DC_Deploy.ps1
@@ -2,7 +2,7 @@ Start-Transcript -Path C:\tmp\dc_deploy.log
 
 # Deploying Azure Arc Data Controller
 start PowerShell {for (0 -lt 1) {kubectl get pod -n $env:ARC_DC_NAME; sleep 5; clear }}
-azdata arc dc create --namespace $env:ARC_DC_NAME --name $env:ARC_DC_NAME --subscription $env:ARC_DC_SUBSCRIPTION --resource-group $env:ARC_DC_RG --location $env:ARC_DC_REGION --connectivity-mode indirect --path "C:\tmp\custom"
+azdata arc dc create --namespace $env:ARC_DC_NAME --name $env:ARC_DC_NAME --subscription $env:ARC_DC_SUBSCRIPTION --resource-group $env:ARC_DC_RG --location $env:ARC_DC_REGION --connectivity-mode direct --path "C:\tmp\custom"
 azdata login --namespace $env:ARC_DC_NAME
 
 Stop-Transcript
