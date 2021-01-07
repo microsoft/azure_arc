fd4b9f8331feb71ff86fb0d125f7a2db48b4c073
Jerry@MSFT <jerryemis@me.com>
2021-01-07 09:47:02 -0500

    Update azure_arc_servers_jumpstart/oci/ubuntu/terraform/main.tf
    Co-authored-by: Lior Kamrat <46730876+likamrat@users.noreply.github.com>

--- azure_arc_servers_jumpstart/oci/ubuntu/terraform/main.tf
+++ azure_arc_servers_jumpstart/oci/ubuntu/terraform/main.tf
@@ -9,6 +9,9 @@ variable "ssh_public_key" {}
 resource "azurerm_resource_group" "azure_rg" {
   name     = var.azure_resource_group
   location = var.azure_location
+  tags = {
+    project = "jumpstart_azure_arc_servers"
+  }
 }
 
 variable "ad_region_mapping" {
