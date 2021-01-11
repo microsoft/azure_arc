5f9f6c2415ee8caf53f23884f2b5eb7fe9041090
Jerry@MSFT <jerryemis@me.com>
2021-01-11 14:36:55 -0500

    Update variables.tf
    added image variables

--- azure_arc_servers_jumpstart/oci/ubuntu/terraform/variables.tf
+++ azure_arc_servers_jumpstart/oci/ubuntu/terraform/variables.tf
@@ -53,5 +53,16 @@ variable "ad_region_mapping" {
   }
 }
 
+variable "images" {
+  type = map(string)
+  default = {
+    # See https://docs.us-phoenix-1.oraclecloud.com/images/
+    # Oracle-provided image "Ubuntu-Linux-18.4-lts"
+    us-phoenix-1 = "ocid1.image.oc3.us-gov-phoenix-1.aaaaaaaaqhxpqz5jrml5twxtoe7z37fw7qnprlpi4gbx6rgprfwg3vpup2zq"
+    us-ashburn-1 = "ocid1.image.oc1.iad.aaaaaaaah2ms24xr6slrtpppgaipfixozl7utwnf2qwqonb2muk4g43wfzgq"
+  }
+}
+
+
 
 
