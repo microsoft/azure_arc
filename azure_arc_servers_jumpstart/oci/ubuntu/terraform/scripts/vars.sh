bd21e3cb9772df9289b9e405d0e8d27f77199759
Jerry@MSFT <jerryemis@me.com>
2021-01-12 16:24:24 -0500

    Update azure_arc_servers_jumpstart/oci/ubuntu/terraform/scripts/vars.sh
    Co-authored-by: Lior Kamrat <46730876+likamrat@users.noreply.github.com>

--- azure_arc_servers_jumpstart/oci/ubuntu/terraform/scripts/vars.sh
+++ azure_arc_servers_jumpstart/oci/ubuntu/terraform/scripts/vars.sh
@@ -2,20 +2,20 @@
 
 # Change the following environment variables to your OCI details
 
-export TF_VAR_tenancy_ocid=<Oracle tenanacy OCID>
-export TF_VAR_user_ocid=<Oracle user OCID>
-export TF_VAR_fingerprint=<Oracle API FingerPrint>
-export TF_VAR_private_key_path=<path to pem key>
+export TF_VAR_tenancy_ocid='<Oracle tenanacy OCID>'
+export TF_VAR_user_ocid='<Oracle user OCID>'
+export TF_VAR_fingerprint='<Oracle API FingerPrint>'
+export TF_VAR_private_key_path='<path to pem key>'
 export TF_VAR_ssh_public_key=$(cat my_oci_key.pub) 
 ### OCI Region
-export TF_VAR_region=<OCI region>
+export TF_VAR_region='<OCI region>'
 ### Compartment
-export TF_VAR_compartment_ocid=<Oracle compartment OCID>
+export TF_VAR_compartment_ocid='<Oracle compartment OCID>'
 
 # Change the following environment variables according to your Azure Service Principal
 
-export TF_VAR_subscription_id=<Azure subscription ID>
-export TF_VAR_client_id=<Azure service principal ID>
-export TF_VAR_client_secret=<Azure service principal password>
-export TF_VAR_tenant_id=<Azure tenant ID>
-export TF_VAR_azure_location=<Azure region e.g. westus2>
+export TF_VAR_subscription_id='<Azure subscription ID>'
+export TF_VAR_client_id='<Azure service principal ID>'
+export TF_VAR_client_secret='<Azure service principal password>'
+export TF_VAR_tenant_id='<Azure tenant ID>'
+export TF_VAR_azure_location='<Azure region e.g. westus2>'
