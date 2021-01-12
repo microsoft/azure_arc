e03126295897085f43e7f1690eac30c07f6e32f7
Jerry@MSFT <jerryemis@me.com>
2021-01-12 16:09:37 -0500

    Update main.tf
    removed variables

--- azure_arc_servers_jumpstart/oci/ubuntu/terraform/main.tf
+++ azure_arc_servers_jumpstart/oci/ubuntu/terraform/main.tf
@@ -6,25 +6,6 @@ resource "azurerm_resource_group" "azure_rg" {
   }
 }
 
-variable "ad_region_mapping" {
-  type = map(string)
-  default = {
-    us-phoenix-1 = 2
-    us-ashburn-1 = 1
-  }
-}
-
-variable "images" {
-  type = map(string)
-
-  default = {
-    # See https://docs.us-phoenix-1.oraclecloud.com/images/
-    # Oracle-provided image "Ubuntu-Linux-18.4-lts"
-    us-phoenix-1   = "ocid1.image.oc3.us-gov-phoenix-1.aaaaaaaaqhxpqz5jrml5twxtoe7z37fw7qnprlpi4gbx6rgprfwg3vpup2zq"
-    us-ashburn-1   = "ocid1.image.oc1.iad.aaaaaaaah2ms24xr6slrtpppgaipfixozl7utwnf2qwqonb2muk4g43wfzgq"
-  }
-}
-
 data "oci_identity_availability_domain" "ad" {
   compartment_id = var.tenancy_ocid
   ad_number      = var.ad_region_mapping[var.region]
