708f2ba6e56aea7628d42cea457d71f403f651e2
dkirby-ms <saitcho@outlook.com>
2021-01-21 08:52:19 -0600

    init

--- azure_arc_data_jumpstart/gke/mssql_mi/terraform/scripts/ClientTools.ps1
+++ azure_arc_data_jumpstart/gke/mssql_mi/terraform/scripts/ClientTools.ps1
@@ -34,14 +34,13 @@ Invoke-WebRequest "https://aka.ms/azdata-msi" -OutFile "C:\tmp\AZDataCLI.msi" | 
 Invoke-WebRequest "https://raw.githubusercontent.com/microsoft/azure_arc/gke_sqlmi/azure_arc_data_jumpstart/gke/mssql_mi/terraform/scripts/MSSQL_MI_Cleanup.ps1" -OutFile "C:\tmp\MSSQL_MI_Cleanup.ps1"
 Invoke-WebRequest "https://raw.githubusercontent.com/microsoft/azure_arc/gke_sqlmi/azure_arc_data_jumpstart/gke/mssql_mi/terraform/scripts/MSSQL_MI_Deploy.ps1" -OutFile "C:\tmp\MSSQL_MI_Deploy.ps1"
 
-New-Item -path alias:kubectl -value 'C:\ProgramData\chocolatey\lib\kubernetes-cli\tools\kubernetes\client\bin\kubectl.exe'
-New-Item -path alias:azdata -value 'C:\Program Files (x86)\Microsoft SDKs\Azdata\CLI\wbin\azdata.cmd'
-
 # Creating PowerShell sql_connectivity Script
 $sql_connectivity = @'
 
 Start-Transcript "C:\tmp\sql_connectivity.log"
 New-Item -Path "C:\Users\$env:adminUsername\AppData\Roaming\azuredatastudio\" -Name "User" -ItemType "directory" -Force
+New-Item -path alias:kubectl -value 'C:\ProgramData\chocolatey\lib\kubernetes-cli\tools\kubernetes\client\bin\kubectl.exe'
+New-Item -path alias:azdata -value 'C:\Program Files (x86)\Microsoft SDKs\Azdata\CLI\wbin\azdata.cmd'
 
 # Retreving SQL Managed Instance IP
 azdata arc sql mi list | Tee-Object "C:\tmp\sql_instance_list.txt"
@@ -119,7 +118,7 @@ Write-Host "`n"
 Expand-Archive 'C:\tmp\azuredatastudio.zip' -DestinationPath 'C:\Program Files\Azure Data Studio'
 Start-Process msiexec.exe -Wait -ArgumentList '/I "C:\tmp\AZDataCLI.msi" /quiet'
 
-$SettingsDestination = "C:\Users\$env:USERNAME\AppData\Roaming\azuredatastudio\User"
+$SettingsDestination = "C:\Users\$env:windows_username\AppData\Roaming\azuredatastudio\User"
 Start-Process -FilePath "C:\Program Files\Azure Data Studio\azuredatastudio.exe" -WindowStyle Hidden
 Start-Sleep -s 5
 Stop-Process -Name "azuredatastudio" -Force
@@ -138,7 +137,7 @@ $env:argument3="microsoft.azuredatastudio-postgresql"
 Write-Host "Creating Azure Data Studio Desktop shortcut"
 Write-Host "`n"
 $TargetFile = "C:\Program Files\Azure Data Studio\azuredatastudio.exe"
-$ShortcutFile = "C:\Users\$env:USERNAME\Desktop\Azure Data Studio.lnk"
+$ShortcutFile = "C:\Users\$env:windows_username\Desktop\Azure Data Studio.lnk"
 $WScriptShell = New-Object -ComObject WScript.Shell
 $Shortcut = $WScriptShell.CreateShortcut($ShortcutFile)
 $Shortcut.TargetPath = $TargetFile
@@ -147,15 +146,16 @@ $Shortcut.Save()
 # Setting up the kubectl & azdata environment
 Write-Host "Setting up the kubectl & azdata environment"
 Write-Host "`n"
+New-Item -path alias:kubectl -value 'C:\ProgramData\chocolatey\lib\kubernetes-cli\tools\kubernetes\client\bin\kubectl.exe'
 $env:gcp_credentials_file_path="C:\tmp\$env:gcp_credentials_filename"
 gcloud auth activate-service-account --key-file $env:gcp_credentials_file_path
 gcloud container clusters get-credentials $env:gke_cluster_name --region $env:gcp_region  
 kubectl version
 kubectl apply -f 'C:\tmp\local_ssd_sc.yaml'
 
+New-Item -path alias:azdata -value 'C:\Program Files (x86)\Microsoft SDKs\Azdata\CLI\wbin\azdata.cmd'
 azdata --version
 
-start Powershell {for (0 -lt 1) {kubectl get pod -n $env:ARC_DC_NAME; sleep 5; clear }}
 azdata arc dc config init --source azure-arc-gke --path "C:\tmp\custom" --force
 azdata arc dc config replace --path "C:\tmp\custom\control.json" --json-values "spec.storage.data.className=local-ssd"
 azdata arc dc config replace --path "C:\tmp\custom\control.json" --json-values "spec.storage.logs.className=local-ssd"
@@ -174,7 +174,8 @@ if(($env:DOCKER_TAG -ne $NULL) -or ($env:DOCKER_TAG -ne ""))
     azdata arc dc config replace --path "C:\tmp\custom\control.json" --json-values "spec.docker.imageTag=$env:DOCKER_TAG"
 }
 
-azdata arc dc create --namespace $env:ARC_DC_NAME --name $env:ARC_DC_NAME --subscription $env:ARC_DC_SUBSCRIPTION --resource-group $env:ARC_DC_RG --location $env:ARC_DC_REGION --connectivity-mode direct --path "C:\tmp\custom"
+start Powershell {for (0 -lt 1) {kubectl get pod -n $env:ARC_DC_NAME; sleep 5; clear }}
+azdata arc dc create --path "C:\tmp\custom" --namespace $env:ARC_DC_NAME --name $env:ARC_DC_NAME --subscription $env:ARC_DC_SUBSCRIPTION --resource-group $env:ARC_DC_RG --location $env:ARC_DC_REGION --connectivity-mode direct
 
 # Deploying Azure Arc SQL Managed Instance
 azdata login --namespace $env:ARC_DC_NAME
@@ -196,7 +197,7 @@ Stop-Process -Name powershell -Force
 # Creating LogonScript Windows Scheduled Task
 $Trigger = New-ScheduledTaskTrigger -AtLogOn
 $Action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument 'C:\tmp\LogonScript.ps1'
-Register-ScheduledTask -TaskName "LogonScript" -Trigger $Trigger -User $env:USERNAME -Action $Action -RunLevel "Highest" -Force
+Register-ScheduledTask -TaskName "LogonScript" -Trigger $Trigger -User "$env:windows_username" -Action $Action -RunLevel "Highest" -Force
 
 # Disabling Windows Server Manager Scheduled Task
 Get-ScheduledTask -TaskName ServerManager | Disable-ScheduledTask
