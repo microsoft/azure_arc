3ca0e02f6d3353ef29813baf89fa34aa8f45f96c
likamrat <likamrat@microsoft.com>
2021-01-29 07:33:02 -0800

    typo

--- azure_arc_k8s_jumpstart/aks/terraform/main.tf
+++ azure_arc_k8s_jumpstart/aks/terraform/main.tf
@@ -9,6 +9,8 @@ resource "azurerm_kubernetes_cluster" "arcdemo" {
   resource_group_name = azurerm_resource_group.arcdemo.name
   dns_prefix          = var.prefix
 
+  kubernetes_version = var.kubernetes_version
+
   default_node_pool {
     name       = "default"
     node_count = var.node_count
