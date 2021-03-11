08803b866d6dd126c7607c448652c149097eea22
Nicolas Yuen <nicolas.yuen@gmail.com>
2021-03-11 16:33:21 +0800

    Adding documentation for Container insights on microK8s arc enabled clusters

---
+++ azure_arc_k8s_jumpstart/microk8s/azure_monitor/microk8s_monitor_onboarding.ps1
@@ -0,0 +1,22 @@
+# <--- Change the following environment variables according to your Azure service principal name --->
+
+Write-Output "Exporting environment variables"
+$env:subscriptionId="${subscriptionId}"
+$env:appId="${appId}"
+$env:password="${appPassword}"
+$env:tenantId="${tenantId}"
+$env:resourceGroup="${resourceGroup}"
+$env:arcClusterName="${arcClusterName}"
+
+Write-Output  "Downloading the Azure Monitor onboarding script"
+Invoke-WebRequest https://aka.ms/enable-monitoring-powershell-script -OutFile enable-monitoring.ps1
+
+Write-Output "Onboarding the Azure Arc enabled Kubernetes cluster to Azure Monitor for containers"
+az login --service-principal --username $env:appId --password $env:password --tenant $env:tenantId
+microk8s config > config
+$env:azureArcClusterResourceId = $(az resource show --resource-group $env:resourceGroup --name $env:arcClusterName --resource-type "Microsoft.Kubernetes/connectedClusters" --query id -o tsv)
+$env:kubeContext = kubectl config current-context
+
+.\enable-monitoring.ps1 -clusterResourceId $env:azureArcClusterResourceId -servicePrincipalClientId $env:appId -servicePrincipalClientSecret $env:password -tenantId $env:tenantId -kubeContext $env:kubeContext
+
+rm enable-monitoring.ps1
