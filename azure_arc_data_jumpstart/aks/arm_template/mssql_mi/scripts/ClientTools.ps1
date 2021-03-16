800a607fe3351ab982320ffc4aeb9cca2d3847d3
likamrat <likamrat@microsoft.com>
2021-03-16 09:02:37 -0700

    new script

--- azure_arc_data_jumpstart/aks/arm_template/mssql_mi/scripts/ClientTools.ps1
+++ azure_arc_data_jumpstart/aks/arm_template/mssql_mi/scripts/ClientTools.ps1
@@ -156,6 +156,15 @@ azdata arc sql mi create --name $env:MSSQL_MI_NAME --storage-class-data managed-
 
 azdata arc sql mi list
 
+Write-Host "Waiting for all pods to be completely ready for work"
+$podname = "$env:MSSQL_MI_NAME" + "-0"
+
+Start-Sleep -Seconds 300
+Write-Host "Testing Done"
+kubectl exec $podname -n $env:ARC_DC_NAME -c arc-sqlmi -- wget https://github.com/Microsoft/sql-server-samples/releases/download/adventureworks/AdventureWorks2019.bak -O /var/opt/mssql/data/AdventureWorks2019.bak
+Start-Sleep -Seconds 5
+kubectl exec $podname -n $env:ARC_DC_NAME -c arc-sqlmi -- /opt/mssql-tools/bin/sqlcmd -S localhost -U $env:AZDATA_USERNAME -P $env:AZDATA_PASSWORD -Q "RESTORE DATABASE AdventureWorks2019 FROM  DISK = N'/var/opt/mssql/data/AdventureWorks2019.bak' WITH MOVE 'AdventureWorks2017' TO '/var/opt/mssql/data/AdventureWorks2019.mdf', MOVE 'AdventureWorks2017_Log' TO '/var/opt/mssql/data/AdventureWorks2019_Log.ldf'"
+
 # Creating Azure Data Studio settings for SQL Managed Instance connectio
 Write-Output "Creating Azure Data Studio settings for SQL Managed Instance connection"
 New-Item -Path "C:\Users\$env:adminUsername\AppData\Roaming\azuredatastudio\" -Name "User" -ItemType "directory" -Force
@@ -178,15 +187,6 @@ Write-Host ""
 Write-Host "Cleaning garbage"
 Write-Host ""
 Remove-Item "C:\tmp\sql_instance_list.txt" -Force
-
-Write-Host "Waiting for all pods to be completely ready for work"
-$podname = "$env:MSSQL_MI_NAME" + "-0"
-
-Start-Sleep -Seconds 300
-Write-Host "Testing Done"
-kubectl exec $podname -n $env:ARC_DC_NAME -c arc-sqlmi -- wget https://github.com/Microsoft/sql-server-samples/releases/download/adventureworks/AdventureWorks2019.bak -O /var/opt/mssql/data/AdventureWorks2019.bak
-Start-Sleep -Seconds 5
-kubectl exec $podname -n $env:ARC_DC_NAME -c arc-sqlmi -- /opt/mssql-tools/bin/sqlcmd -S localhost -U $env:AZDATA_USERNAME -P $env:AZDATA_PASSWORD -Q "RESTORE DATABASE AdventureWorks2019 FROM  DISK = N'/var/opt/mssql/data/AdventureWorks2019.bak' WITH MOVE 'AdventureWorks2017' TO '/var/opt/mssql/data/AdventureWorks2019.mdf', MOVE 'AdventureWorks2017_Log' TO '/var/opt/mssql/data/AdventureWorks2019_Log.ldf'"
 
 Unregister-ScheduledTask -TaskName "LogonScript" -Confirm:$false
 
