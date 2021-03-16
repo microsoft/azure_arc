6ba26f1fcb56d4a5793530a62ce47cd117225ce8
Nicolas Yuen <nicolas.yuen@gmail.com>
2021-03-16 17:50:05 +0800

    Implementing code review feedback for PR #453

--- azure_arc_k8s_jumpstart/microk8s/azure_monitor/microk8s_monitor_onboarding.sh
+++ azure_arc_k8s_jumpstart/microk8s/azure_monitor/microk8s_monitor_onboarding.sh
@@ -15,7 +15,8 @@ curl -o enable-monitoring.sh -L https://aka.ms/enable-monitoring-bash-script
 
 echo "Onboarding the Azure Arc enabled Kubernetes cluster to Azure Monitor for containers"
 az login --service-principal --username $appId --password $password --tenant $tenantId
-microk8s config > ~/. kube/config
+mkdir -p ~/.kube
+microk8s config > ~/.kube/config
 export azureArcClusterResourceId=$(az resource show --resource-group $resourceGroup --name $arcClusterName --resource-type "Microsoft.Kubernetes/connectedClusters" --query id -o tsv)
 export kubeContext="$(kubectl config current-context)"
 bash enable-monitoring.sh --resource-id $azureArcClusterResourceId --client-id $appId --client-secret $password --tenant-id $tenantId --kube-context $kubeContext
