03cb7bed4849b273f9b63e35845dd7754a811f73
likamrat <likamrat@microsoft.com>
2021-03-16 19:38:57 -0700

    new script

--- azure_arc_data_jumpstart/aks/arm_template/mssql_mi/scripts/ClientTools.ps1
+++ azure_arc_data_jumpstart/aks/arm_template/mssql_mi/scripts/ClientTools.ps1
@@ -159,7 +159,7 @@ azdata arc sql mi list
 Write-Host "Waiting for all pods to be completely ready for work"
 
 $podname = "$env:MSSQL_MI_NAME" + "-0"
-Start-Sleep -Seconds 180
+Start-Sleep -Seconds 220
 Write-Host "Ready to go!"
 kubectl exec $podname -n $env:ARC_DC_NAME -c arc-sqlmi -- wget https://github.com/Microsoft/sql-server-samples/releases/download/adventureworks/AdventureWorks2019.bak -O /var/opt/mssql/data/AdventureWorks2019.bak
 Start-Sleep -Seconds 5
