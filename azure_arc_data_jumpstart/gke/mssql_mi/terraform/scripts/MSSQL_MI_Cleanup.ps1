60c8924e6da2633fe80ca1223fe38f46e4c64bf1
likamrat <likamrat@microsoft.com>
2021-04-06 15:52:16 -0700

    removing direct mode

--- azure_arc_data_jumpstart/gke/mssql_mi/terraform/scripts/MSSQL_MI_Cleanup.ps1
+++ azure_arc_data_jumpstart/gke/mssql_mi/terraform/scripts/MSSQL_MI_Cleanup.ps1
@@ -1,21 +1,19 @@
-Start-Transcript -Path C:\tmp\mssql_mi_cleanup.log
+Start-Transcript -Path C:\tmp\mssql_cleanup.log
 
-# Deleting Azure Arc Data Controller namespace and it's resources
-start PowerShell {for (0 -lt 1) {kubectl get pod -n $env:ARC_DC_NAME; sleep 5; clear }}
+# Deleting Azure Arc Data Controller namespace and it's resources (SQLMI incl.)
+Start-Process PowerShell {for (0 -lt 1) {kubectl get pod -n $env:ARC_DC_NAME; sleep 5; clear }}
 azdata arc sql mi delete --name $env:MSSQL_MI_NAME
-azdata arc dc delete --name $env:ARC_DC_NAME --namespace $env:ARC_DC_NAME --force
-kubectl delete ns $env:ARC_DC_NAME
+azdata arc dc delete --name $env:ARC_DC_NAME --namespace $env:ARC_DC_NAME --yes
 
-az login --service-principal -u $env:SPN_CLIENT_ID -p $env:SPN_CLIENT_SECRET --tenant $env:SPN_TENANT_ID --output none
-az resource delete -g $env:ARC_DC_RG -n $env:ARC_DC_NAME --namespace "Microsoft.AzureArcData" --resource-type "dataControllers"
+# az login --service-principal --username $env:SPN_CLIENT_ID --password $env:SPN_CLIENT_SECRET --tenant $env:SPN_TENANT_ID --output none // Will be comment-out upon "Directly Connected" functionality restore 
+# az resource delete -g $env:resourceGroup -n $env:ARC_DC_NAME --namespace "Microsoft.AzureArcData" --resource-type "dataControllers" // Will be comment-out upon "Directly Connected" functionality restore
 
-# Restoring State
-Copy-Item -Path "C:\tmp\hosts_backup" -Destination "C:\Windows\System32\drivers\etc\hosts" -Recurse -Force -ErrorAction Continue
-Copy-Item -Path "C:\tmp\settings_backup.json" -Destination "C:\tmp\settings.json" -Recurse -Force -ErrorAction Continue
-
-Remove-Item "C:\Users\$env:adminUsername\AppData\Roaming\azuredatastudio\User\settings.json" -Force
-Remove-Item "C:\tmp\hosts_backup" -Force
-Remove-Item "C:\tmp\settings_backup.json" -Force
+# Restoring Azure Data Studio settings
+Write-Host "Restoring Azure Data Studio settings"
+Stop-Process -Name azuredatastudio
+$settingsFile = "C:\Users\$env:adminUsername\AppData\Roaming\azuredatastudio\User\settings.json"
+Remove-Item $settingsFile -Force
+Remove-Item "C:\tmp\sql_instance_list.txt"
 
 Stop-Transcript
 
