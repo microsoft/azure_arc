59b38e3f3d92ca16446496ae21462b00f827ab01
likamrat <likamrat@microsoft.com>
2021-04-07 08:00:45 -0700

    removing direct mode

--- azure_arc_data_jumpstart/gke/mssql_mi/terraform/scripts/MSSQL_MI_Cleanup.ps1
+++ azure_arc_data_jumpstart/gke/mssql_mi/terraform/scripts/MSSQL_MI_Cleanup.ps1
@@ -1,7 +1,7 @@
 Start-Transcript -Path C:\tmp\mssql_cleanup.log
 
 # Deleting Azure Arc Data Controller namespace and it's resources (SQLMI incl.)
-Start-Process PowerShell {for (0 -lt 1) {kubectl get pod -n $env:ARC_DC_NAME; sleep 5; clear }}
+Start-Process PowerShell {for (0 -lt 1) {kubectl get pod -n $env:ARC_DC_NAME; Start-Sleep 5; Clear-Host }}
 azdata arc sql mi delete --name $env:MSSQL_MI_NAME
 azdata arc dc delete --name $env:ARC_DC_NAME --namespace $env:ARC_DC_NAME --yes
 
@@ -11,7 +11,7 @@ azdata arc dc delete --name $env:ARC_DC_NAME --namespace $env:ARC_DC_NAME --yes
 # Restoring Azure Data Studio settings
 Write-Host "Restoring Azure Data Studio settings"
 Stop-Process -Name azuredatastudio
-$settingsFile = "C:\Users\$env:adminUsername\AppData\Roaming\azuredatastudio\User\settings.json"
+$settingsFile = "C:\Users\$env:windows_username\AppData\Roaming\azuredatastudio\User\settings.json"
 Remove-Item $settingsFile -Force
 Remove-Item "C:\tmp\sql_instance_list.txt"
 
--- azure_arc_data_jumpstart/gke/mssql_mi/terraform/scripts/MSSQL_MI_Deploy.ps1
+++ azure_arc_data_jumpstart/gke/mssql_mi/terraform/scripts/MSSQL_MI_Deploy.ps1
@@ -1,12 +1,12 @@
 Start-Transcript -Path C:\tmp\mssql_mi_deploy.log
 
 # Deploying Azure Arc Data Controller
-start PowerShell {for (0 -lt 1) {kubectl get pod -n $env:ARC_DC_NAME; sleep 5; clear }}
-azdata arc dc create --namespace $env:ARC_DC_NAME --name $env:ARC_DC_NAME --subscription $env:ARC_DC_SUBSCRIPTION --resource-group $env:ARC_DC_RG --location $env:ARC_DC_REGION --connectivity-mode indirect --path "C:\tmp\custom"
+Start-Process PowerShell {for (0 -lt 1) {kubectl get pod -n $env:ARC_DC_NAME; Start-Sleep 5; Clear-Host }}
+azdata arc dc create --path "C:\tmp\custom" --namespace $env:ARC_DC_NAME --name $env:ARC_DC_NAME --subscription $env:ARC_DC_SUBSCRIPTION --resource-group $env:ARC_DC_RG --location $env:ARC_DC_REGION --connectivity-mode indirect
 
 # Deploying Azure Arc SQL Managed Instance
 azdata login --namespace $env:ARC_DC_NAME
-azdata arc sql mi create --name $env:MSSQL_MI_NAME --storage-class-data local-ssd --storage-class-logs local-ssd
+azdata arc sql mi create --name $env:MSSQL_MI_NAME
 azdata arc sql mi list
 
 
@@ -14,17 +14,15 @@ azdata arc sql mi list
 Write-Host "Waiting for 5min for all pods to be completely ready for work"
 
 $podname = "$env:MSSQL_MI_NAME" + "-0"
-# Start-Sleep -Seconds 300
-# Write-Host "Ready to go!"
 kubectl exec $podname -n $env:ARC_DC_NAME -c arc-sqlmi -- wget https://github.com/Microsoft/sql-server-samples/releases/download/adventureworks/AdventureWorks2019.bak -O /var/opt/mssql/data/AdventureWorks2019.bak
 Start-Sleep -Seconds 5
 kubectl exec $podname -n $env:ARC_DC_NAME -c arc-sqlmi -- /opt/mssql-tools/bin/sqlcmd -S localhost -U $env:AZDATA_USERNAME -P $env:AZDATA_PASSWORD -Q "RESTORE DATABASE AdventureWorks2019 FROM  DISK = N'/var/opt/mssql/data/AdventureWorks2019.bak' WITH MOVE 'AdventureWorks2017' TO '/var/opt/mssql/data/AdventureWorks2019.mdf', MOVE 'AdventureWorks2017_Log' TO '/var/opt/mssql/data/AdventureWorks2019_Log.ldf'"
 
 Write-Host ""
 Write-Host "Creating Azure Data Studio settings for SQL Managed Instance connection"
-New-Item -Path "C:\Users\$env:adminUsername\AppData\Roaming\azuredatastudio\" -Name "User" -ItemType "directory" -Force
-Copy-Item -Path "C:\tmp\settings_template.json" -Destination "C:\Users\$env:adminUsername\AppData\Roaming\azuredatastudio\User\settings.json"
-$settingsFile = "C:\Users\$env:adminUsername\AppData\Roaming\azuredatastudio\User\settings.json"
+New-Item -Path "C:\Users\$env:windows_username\AppData\Roaming\azuredatastudio\" -Name "User" -ItemType "directory" -Force
+Copy-Item -Path "C:\tmp\settings_template.json" -Destination "C:\Users\$env:windows_username\AppData\Roaming\azuredatastudio\User\settings.json"
+$settingsFile = "C:\Users\$env:windows_username\AppData\Roaming\azuredatastudio\User\settings.json"
 azdata arc sql mi list | Tee-Object "C:\tmp\sql_instance_list.txt"
 $file = "C:\tmp\sql_instance_list.txt"
 (Get-Content $file | Select-Object -Skip 2) | Set-Content $file
