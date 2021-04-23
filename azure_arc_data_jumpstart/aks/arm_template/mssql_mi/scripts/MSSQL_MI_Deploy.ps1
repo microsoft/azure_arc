25320f721361ddf38ef881b2165e217d9e8b9a2a
likamrat <likamrat@microsoft.com>
2021-04-23 06:29:25 -0700

    300

--- azure_arc_data_jumpstart/aks/arm_template/mssql_mi/scripts/MSSQL_MI_Deploy.ps1
+++ azure_arc_data_jumpstart/aks/arm_template/mssql_mi/scripts/MSSQL_MI_Deploy.ps1
@@ -1,7 +1,7 @@
 Start-Transcript -Path C:\tmp\mssql_deploy.log
 
 # Deploying Azure Arc Data Controller
-Start-Process PowerShell {for (0 -lt 1) {kubectl get pod -n $env:ARC_DC_NAME; sleep 5; clear }}
+Start-Process PowerShell {for (0 -lt 1) {kubectl get pod -n $env:ARC_DC_NAME; Start-Service 5; Clear-Host }}
 azdata arc dc create --profile-name azure-arc-aks-premium-storage --namespace $env:ARC_DC_NAME --name $env:ARC_DC_NAME --subscription $env:ARC_DC_SUBSCRIPTION --resource-group $env:resourceGroup --location $env:ARC_DC_REGION --connectivity-mode indirect
 
 # Deploying Azure Arc SQL Managed Instance
