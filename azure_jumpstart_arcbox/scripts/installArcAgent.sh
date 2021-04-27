6b08c3e1bf16cfae98bee7095bef3f47cb59635a
Dale Kirby <32211549+dkirby-ms@users.noreply.github.com>
2021-04-27 08:18:13 -0500

    Update installArcAgent.sh

--- azure_jumpstart_arcbox/scripts/installArcAgent.sh
+++ azure_jumpstart_arcbox/scripts/installArcAgent.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# Block Azure IMDS
+sudo ufw --force enable
+sudo ufw deny out from any to 169.254.169.254
+sudo ufw default allow incoming
+
 apt-get update
 
 # Download the installation package
