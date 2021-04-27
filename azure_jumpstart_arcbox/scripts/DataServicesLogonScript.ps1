83c30386cfcdc1198dbdec042072b24a34f95d55
Dale Kirby <32211549+dkirby-ms@users.noreply.github.com>
2021-04-27 13:36:35 -0500

    Update DataServicesLogonScript.ps1

--- azure_jumpstart_arcbox/scripts/DataServicesLogonScript.ps1
+++ azure_jumpstart_arcbox/scripts/DataServicesLogonScript.ps1
@@ -72,7 +72,7 @@ Workflow DatabaseDeploy
             azdata arc postgres endpoint list --name $env:POSTGRES_NAME
             # Downloading demo database and restoring onto Postgres
             $podname = "$env:POSTGRES_NAME" + "c-0"
-            #Start-Sleep -Seconds 300
+            Start-Sleep -Seconds 300
             Write-Host "Downloading AdventureWorks.sql template for Postgres... (1/3)"
             kubectl exec $podname -n $env:arcDcName -c postgres -- /bin/bash -c "cd /tmp && curl -k -O https://raw.githubusercontent.com/microsoft/azure_arc/main/azure_jumpstart_arcbox/scripts/AdventureWorks.sql" 2>&1 $null
             Write-Host "Creating AdventureWorks database on Postgres... (2/3)"
@@ -87,7 +87,7 @@ Workflow DatabaseDeploy
             azdata arc sql mi list
             # Downloading demo database and restoring onto SQL MI
             $podname = "$env:mssqlMiName" + "-0"
-            # Start-Sleep -Seconds 300
+            Start-Sleep -Seconds 300
             Write-Host "Downloading AdventureWorks database for MS SQL... (1/2)"
             kubectl exec $podname -n $env:arcDcName -c arc-sqlmi -- wget https://github.com/Microsoft/sql-server-samples/releases/download/adventureworks/AdventureWorks2019.bak -O /var/opt/mssql/data/AdventureWorks2019.bak 2>&1 $null
             Write-Host "Restoring AdventureWorks database for MS SQL. (2/2)"
