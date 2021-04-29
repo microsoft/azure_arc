5683458915606f4d3d1af450c662407b5dfe4419
Laura Nicolas <51124125+lanicolas@users.noreply.github.com>
2021-04-29 18:32:48 +0000

    Update azure_arc_k8s_jumpstart/cluster_api/capi_defender_extension/azure_defender_k8s_extension.sh
    Co-authored-by: Lior Kamrat <46730876+likamrat@users.noreply.github.com>

--- azure_arc_k8s_jumpstart/cluster_api/capi_defender_extension/azure_defender_k8s_extension.sh
+++ azure_arc_k8s_jumpstart/cluster_api/capi_defender_extension/azure_defender_k8s_extension.sh
@@ -56,5 +56,5 @@ echo ""
 echo "Login to Az CLI using the service principal"
 az login --service-principal --username $appId --password $password --tenant $tenantId
 
-echo "Create monitor k8s extension instance"
+echo "Create Azure Defender Kubernetes extension instance"
 az k8s-extension create --name $k8sExtensionName --cluster-name $arcClusterName --resource-group $resourceGroup --cluster-type connectedClusters --extension-type microsoft.azuredefender.kubernetes
