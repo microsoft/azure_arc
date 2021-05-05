3a4ed6ba06271cfb5910f7032ab8b06ab5bedb58
dkirby-ms <saitcho@outlook.com>
2021-05-05 14:51:18 -0500

    arcbox_capi initial commit

---
+++ azure_jumpstart_arcbox/scripts/installCAPI.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+exec >logfile
+exec 2>&1
+
+sudo apt-get update
+
+sudo sed -i "s/PasswordAuthentication no/PasswordAuthentication yes/" /etc/ssh/sshd_config
+sudo adduser staginguser --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
+sudo echo "staginguser:ArcPassw0rd" | sudo chpasswd
+
+# Injecting environment variables
+echo '#!/bin/bash' >> vars.sh
+echo $adminUsername:$1 | awk '{print substr($1,2); }' >> vars.sh
+echo $SPN_CLIENT_ID:$2 | awk '{print substr($1,2); }' >> vars.sh
+echo $SPN_CLIENT_SECRET:$3 | awk '{print substr($1,2); }' >> vars.sh
+echo $SPN_TENANT_ID:$4 | awk '{print substr($1,2); }' >> vars.sh
+echo $vmName:$5 | awk '{print substr($1,2); }' >> vars.sh
+echo $location:$6 | awk '{print substr($1,2); }' >> vars.sh
+echo $stagingStorageAccountName:$7 | awk '{print substr($1,2); }' >> vars.sh
+sed -i '2s/^/export adminUsername=/' vars.sh
+sed -i '3s/^/export SPN_CLIENT_ID=/' vars.sh
+sed -i '4s/^/export SPN_CLIENT_SECRET=/' vars.sh
+sed -i '5s/^/export SPN_TENANT_ID=/' vars.sh
+sed -i '6s/^/export vmName=/' vars.sh
+sed -i '7s/^/export location=/' vars.sh
+sed -i '8s/^/export stagingStorageAccountName=/' vars.sh
+
+chmod +x vars.sh 
+. ./vars.sh
+
+publicIp=$(curl icanhazip.com)
