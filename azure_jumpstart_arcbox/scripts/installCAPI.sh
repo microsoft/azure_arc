4513139a97057d87ba12c7eadf1617fc6eb1b549
likamrat <likamrat@microsoft.com>
2021-05-06 15:27:04 -0700

    capi_integration

--- azure_jumpstart_arcbox/scripts/installCAPI.sh
+++ azure_jumpstart_arcbox/scripts/installCAPI.sh
@@ -199,8 +199,6 @@ echo ""
 sudo cp /var/lib/waagent/custom-script/download/0/$CAPI_WORKLOAD_CLUSTER_NAME.kubeconfig ~/.kube/config.$CAPI_WORKLOAD_CLUSTER_NAME
 sudo cp /var/lib/waagent/custom-script/download/0/$CAPI_WORKLOAD_CLUSTER_NAME.kubeconfig /home/${adminUsername}/.kube/config.$CAPI_WORKLOAD_CLUSTER_NAME
 export KUBECONFIG=~/.kube/config.arcbox-capi-data
-# kubectl config set-context $CAPI_WORKLOAD_CLUSTER_NAME
-kubectl get node -o wide
 
 # Installing Azure Arc extensions
 sudo -u $adminUsername az extension add --name connectedk8s --yes
@@ -209,13 +207,13 @@ sudo -u $adminUsername az extension add --name k8s-extension --yes
 az -v
 
 echo "Onboarding the cluster as an Azure Arc enabled Kubernetes cluster"
-az connectedk8s connect --name "ArcBox-CAPI-Data" --resource-group $CAPI_WORKLOAD_CLUSTER_NAME --location $AZURE_LOCATION --kube-config $CAPI_WORKLOAD_CLUSTER_NAME.kubeconfig --tags 'Project=jumpstart_arcbox'
+sudo -u $adminUsername az connectedk8s connect --name "ArcBox-CAPI-Data" --resource-group $CAPI_WORKLOAD_CLUSTER_NAME --location $AZURE_LOCATION --kube-config $CAPI_WORKLOAD_CLUSTER_NAME.kubeconfig --tags 'Project=jumpstart_arcbox'
 
 echo "Create Azure Monitor for containers Kubernetes extension instance"
-az k8s-extension create -n "azuremonitor-containers" --cluster-name "ArcBox-CAPI-Data" --resource-group $CAPI_WORKLOAD_CLUSTER_NAME --cluster-type connectedClusters --extension-type Microsoft.AzureMonitor.Containers
+sudo -u $adminUsername az k8s-extension create -n "azuremonitor-containers" --cluster-name "ArcBox-CAPI-Data" --resource-group $CAPI_WORKLOAD_CLUSTER_NAME --cluster-type connectedClusters --extension-type Microsoft.AzureMonitor.Containers
 
 echo "Create Azure Defender Kubernetes extension instance"
-az k8s-extension create --name "azure-defender" --cluster-name "ArcBox-CAPI-Data" --resource-group $CAPI_WORKLOAD_CLUSTER_NAME --cluster-type connectedClusters --extension-type Microsoft.AzureDefender.Kubernetes
+sudo -u $adminUsername az k8s-extension create --name "azure-defender" --cluster-name "ArcBox-CAPI-Data" --resource-group $CAPI_WORKLOAD_CLUSTER_NAME --cluster-type connectedClusters --extension-type Microsoft.AzureDefender.Kubernetes
 
 sudo service sshd restart
 
