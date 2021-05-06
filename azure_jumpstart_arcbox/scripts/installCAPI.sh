4046cc5ee5742d06c0f60d024d83ca135bd4cb95
likamrat <likamrat@microsoft.com>
2021-05-06 10:31:49 -0700

    capi_integration

--- azure_jumpstart_arcbox/scripts/installCAPI.sh
+++ azure_jumpstart_arcbox/scripts/installCAPI.sh
@@ -198,7 +198,7 @@ echo ""
 # CAPI workload cluster kubeconfig housekeeping
 sudo cp /var/lib/waagent/custom-script/download/0/$CAPI_WORKLOAD_CLUSTER_NAME.kubeconfig ~/.kube/config.$CAPI_WORKLOAD_CLUSTER_NAME
 sudo cp /var/lib/waagent/custom-script/download/0/$CAPI_WORKLOAD_CLUSTER_NAME.kubeconfig /home/${adminUsername}/.kube/config.$CAPI_WORKLOAD_CLUSTER_NAME
-export KUBECONFIG=/var/lib/waagent/custom-script/download/0/$CAPI_WORKLOAD_CLUSTER_NAME.kubeconfig
+export KUBECONFIG=/var/lib/waagent/custom-script/download/0/arcbox-capi-data.kubeconfig
 
 # Installing Azure Arc extensions
 sudo -u $adminUsername az extension add --name connectedk8s 
