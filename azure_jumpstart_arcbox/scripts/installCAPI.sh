64cde45daa61ffacf6be0cd7795922684965693b
likamrat <likamrat@microsoft.com>
2021-05-05 19:11:32 -0700

    capi_integration

--- azure_jumpstart_arcbox/scripts/installCAPI.sh
+++ azure_jumpstart_arcbox/scripts/installCAPI.sh
@@ -61,9 +61,9 @@ sudo snap install helm --classic
 # Installing Azure CLI & Azure Arc Extensions
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 
-sudo az extension add --name "connectedk8s"
-sudo az extension add --name "k8s-configuration"
-sudo az extension add --name "k8s-extension"
+sudo -u $adminUsername az extension add --name connectedk8s
+sudo -u $adminUsername az extension add --name k8s-configuration
+sudo -u $adminUsername az extension add --name k8s-extension
 
 az -v
 
