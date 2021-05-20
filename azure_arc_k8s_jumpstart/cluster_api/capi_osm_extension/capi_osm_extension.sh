d61ec5d8d9efd435f13c16399f5034417c8f7d7e
Sowmyan Soman <sowmyans@outlook.com>
2021-05-20 13:23:08 -0400

    final content ready for PR

--- azure_arc_k8s_jumpstart/cluster_api/capi_osm_extension/capi_osm_extension.sh
+++ azure_arc_k8s_jumpstart/cluster_api/capi_osm_extension/capi_osm_extension.sh
@@ -2,25 +2,17 @@
 
 # <--- Change the following environment variables according to your Azure service principal name --->
 
-# export subscriptionId='<Your Azure subscription ID>'
-# export appId='<Your Azure service principal name>'
-# export password='<Your Azure service principal password>'
-# export tenantId='<Your Azure tenant ID>'
-# export resourceGroup='<Azure resource group name>'
-# export arcClusterName='<Azure Arc Cluster Name>'
-# export k8sExtensionName='<OSM extension name' #default: 'osm'
+export subscriptionId='<Your Azure subscription ID>'
+export appId='<Your Azure service principal name>'
+export password='<Your Azure service principal password>'
+export tenantId='<Your Azure tenant ID>'
+export resourceGroup='<Azure resource group name>'
+export arcClusterName='<Azure Arc Cluster Name>'
+export k8sOSMExtensionName='<OSM extension name>'
+export k8sMonitorExtensionName='<azuremonitor extenion name>'
 
-export subscriptionId='ebb856bd-cdcb-4e79-92bd-02911c47395f'
-export appId='9df28080-4024-4114-8320-4b6b2bbbbd3d'
-export password='2TXSUqXpC121s4m0Hd4qJD7h5_TuWl_t4y'
-export tenantId='72f988bf-86f1-41af-91ab-2d7cd011db47'
-export resourceGroup='arc-capi-azure'
-export arcClusterName='arc-capi-azure'
-export k8sOSMExtensionName='osm'
-export k8sMonitorExtensionName='azuremonitor-containers'
-export capiWorkLoadClusterName='arc-capi-azure'
-export osmVersion=0.8.3
-export system=$(uname -s)
+export osmCliDownloadUrl="https://github.com/openservicemesh/osm/releases/download/v0.8.4/osm-v0.8.4-linux-amd64.tar.gz"
+export osmCliPkg="osm-v0.8.4-linux-amd64.tar.gz"
 
 export bookStoreNameSpace="bookstore"
 export bookbuyerNameSpace="bookbuyer"
@@ -75,52 +67,53 @@ az k8s-extension create --cluster-name $arcClusterName --resource-group $resourc
 echo "Create Azure Monitor k8s extension instance"
 az k8s-extension create --name $k8sMonitorExtensionName --cluster-name $arcClusterName --resource-group $resourceGroup --cluster-type connectedClusters --extension-type Microsoft.AzureMonitor.Containers
 
-echo "verify OSM component instllation"
-kubectl get pods -n arc-osm-system
+echo "Setup OSM cli "
 
-#Setup OSM cli 
+echo "Remove older copies of OSM binaries"
+sudo rm -rf /usr/local/bin/osm
 
-# remove older copies of OSM binaries
-rm -rf /usr/local/bin/osm
+echo "Download OSM binaries"
+wget --quiet $osmCliDownloadUrl
 
-# download OSM binaries
-curl -L https://github.com/openservicemesh/osm/releases/download/${osmVersion}/osm-${osmVersion}-${system}-amd64.tar.gz | tar -vxzf -
+tar -xvf $osmCliPkg
 
-./${system}-amd64/osm version
-
-#copy the OSM binary to local bin folder
+echo "Copy the OSM binary to local bin folder"
 sudo cp ./linux-amd64/osm /usr/local/bin/osm
 
 #display current osm version
 osm version
 
-# Create the Bookstore Application Namespaces
+echo "Create the Bookstore Application Namespaces"
 kubectl create namespace $bookStoreNameSpace
 kubectl create namespace $bookbuyerNameSpace
 kubectl create namespace $bookthiefNameSpace
 kubectl create namespace $bookwarehouseNameSpace
 
-# Onboard the Namespaces to the OSM Mesh and enable sidecar injection on the namespaces
+echo "Onboard the Namespaces to the OSM Mesh and enable sidecar injection on the namespaces"
 osm namespace add $bookStoreNameSpace
 osm namespace add $bookbuyerNameSpace
 osm namespace add $bookthiefNameSpace
 osm namespace add $bookwarehouseNameSpace
 
-# Enable metrics for pods belonging to app namespaces
+echo "Enable metrics for pods belonging to app namespaces"
 osm metrics enable --namespace "$bookStoreNameSpace,$bookbuyerNameSpace,$bookthiefNameSpace,$bookwarehouseNameSpace"
 
-# update the namespaces to be monitored first in the yaml file below
+echo "update the namespaces to be monitored first in the yaml file below"
 kubectl apply -f container-azm-ms-osmconfig.yaml
 
-# Create the Kubernetes resources for the bookstore demo applications
+echo "Create the Kubernetes resources for the bookstore demo applications"
 kubectl apply -f https://raw.githubusercontent.com/openservicemesh/osm/main/docs/example/manifests/apps/bookbuyer.yaml
 kubectl apply -f https://raw.githubusercontent.com/openservicemesh/osm/main/docs/example/manifests/apps/bookthief.yaml
 kubectl apply -f https://raw.githubusercontent.com/openservicemesh/osm/main/docs/example/manifests/apps/bookstore.yaml
 kubectl apply -f https://raw.githubusercontent.com/openservicemesh/osm/main/docs/example/manifests/apps/bookwarehouse.yaml
 
-# Checkpoint: What Got Installed?
-kubectl get svc --all-namespaces
-kubectl get deployment --all-namespaces
+echo "Checkpoint: What Got Installed?"
+
+echo "Namespaces got created"
+kubectl get ns
+
+echo "Deployments, pods and services got created"
+kubectl get all --all-namespaces
 
 # port forward UI apps
 # bookThiefPod="$(kubectl get pods --selector app=bookthief -n "$bookthiefNameSpace" --no-headers | grep 'Running' | awk 'NR==1{print $1}')"
