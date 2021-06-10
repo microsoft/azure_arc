de99d738c0bfa0e3937ab7c1817af995d1fa00d5
likamrat <likamrat@microsoft.com>
2021-06-10 07:02:21 -0700

    aks app services

--- azure_arc_app_services_jumpstart/aks/app_service/arm_template/scripts/AppServicesLogonScript.ps1
+++ azure_arc_app_services_jumpstart/aks/app_service/arm_template/scripts/AppServicesLogonScript.ps1
@@ -23,6 +23,7 @@ az extension add --name "connectedk8s" -y
 az extension add --name "k8s-configuration" -y
 az extension add --name "k8s-extension" -y
 az extension add --name "customlocation" -y
+az extension add --yes --source "https://aka.ms/appsvc/appservice_kube-latest-py2.py3-none-any.whl"
 
 Write-Host "`n"
 az -v
@@ -45,9 +46,63 @@ Write-Host "`n"
 az connectedk8s connect --name $env:clusterName --resource-group $env:resourceGroup --location $env:azureLocation --tags 'Project=jumpstart_azure_arc_app_services' --custom-locations-oid '51dfe1e8-70c6-4de5-a08e-e18aff23d815'
 Start-Sleep -Seconds 10
 $kubectlMonShell = Start-Process -PassThru PowerShell {for (0 -lt 1) {kubectl get pod -n appservice; Start-Sleep -Seconds 5; Clear-Host }}
-az k8s-extension create --name arc-data-services --extension-type microsoft.arcdataservices --cluster-type connectedClusters --cluster-name $env:clusterName --resource-group $env:resourceGroup --auto-upgrade false --scope cluster --release-namespace arc --config Microsoft.CustomLocation.ServiceAccount=sa-bootstrapper
-az extension remove --name appservice-kube
-az extension add --yes --name appservice --source "https://aka.ms/appsvc/appservice_kube-latest-py2.py3-none-any.whl"
+
+$namespace="appservices"
+# $kubeEnvironmentName="$env:clusterName-appsvc"
+# $workspaceId = $(az resource show --resource-group $env:resourceGroup --name $env:workspaceName --resource-type "Microsoft.OperationalInsights/workspaces" --query properties.customerId -o tsv)
+# $workspaceIdBase64 = $([Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes("$workspaceId'")))
+# $workspaceKey = $(az monitor log-analytics workspace get-shared-keys --resource-group $env:resourceGroup --workspace-name $env:workspaceName --query primarySharedKey -o tsv)
+# $workspaceKeyBase64 = $([Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes("$workspaceKey'")))
+
+# $logAnalyticsKeyEnc=$(echo -n "${workspaceKeyBase64}")
+
+az network public-ip create --resource-group $env:resourceGroup --name "Arc-AppSvc-PIP" --sku STANDARD
+$publicIp=$(az network public-ip show --resource-group $env:resourceGroup --name "Arc-AppSvc-PIP" --output tsv --query ipAddress)
+
+az k8s-extension create `
+   --resource-group $env:resourceGroup `
+   --name arc-app-services `
+   --cluster-type connectedClusters `
+   --cluster-name $env:clusterName `
+   --extension-type 'Microsoft.Web.Appservice' `
+   --release-train stable `
+   --auto-upgrade-minor-version true `
+   --scope cluster `
+   --release-namespace $namespace `
+   --configuration-settings "Microsoft.CustomLocation.ServiceAccount=default" `
+   --configuration-settings "appsNamespace=$namespace" `
+   --configuration-settings "clusterName=$env:clusterName" `
+   --configuration-settings "loadBalancerIp=$publicIp" `
+   --configuration-settings "keda.enabled=true" `
+   --configuration-settings "buildService.storageClassName=default" `
+   --configuration-settings "buildService.storageAccessMode=ReadWriteOnce" `
+   --configuration-settings "customConfigMap=$namespace/kube-environment-config" `
+   --configuration-settings "envoy.annotations.service.beta.kubernetes.io/azure-load-balancer-resource-group=$env:resourceGroup"
+
+# az k8s-extension create `
+#    --resource-group $env:resourceGroup `
+#    --name arc-app-services `
+#    --cluster-type connectedClusters `
+#    --cluster-name $env:clusterName `
+#    --extension-type 'Microsoft.Web.Appservice' `
+#    --release-train stable `
+#    --auto-upgrade-minor-version true `
+#    --scope cluster `
+#    --release-namespace $namespace `
+#    --configuration-settings "Microsoft.CustomLocation.ServiceAccount=default" `
+#    --configuration-settings "appsNamespace=$namespace" `
+#    --configuration-settings "clusterName=$env:clusterName" `
+#    --configuration-settings "loadBalancerIp=$publicIp" `
+#    --configuration-settings "keda.enabled=true" `
+#    --configuration-settings "buildService.storageClassName=default" `
+#    --configuration-settings "buildService.storageAccessMode=ReadWriteOnce" `
+#    --configuration-settings "customConfigMap=$namespace/kube-environment-config" `
+#    --configuration-settings "envoy.annotations.service.beta.kubernetes.io/azure-load-balancer-resource-group=$env:resourceGroup" `
+#    --configuration-settings "logProcessor.appLogs.destination=log-analytics" `
+#    --configuration-protected-settings "logProcessor.appLogs.logAnalyticsConfig.customerId=$workspaceIdBase64" `
+#    --configuration-protected-settings "logProcessor.appLogs.logAnalyticsConfig.sharedKey=$logAnalyticsKeyEnc"
+
+
 
 # Do {
 #     Write-Host "Waiting for bootstrapper pod, hold tight..."
@@ -58,12 +113,12 @@ az extension add --yes --name appservice --source "https://aka.ms/appsvc/appserv
 $connectedClusterId = az connectedk8s show --name $env:clusterName --resource-group $env:resourceGroup --query id -o tsv
 $extensionId = az k8s-extension show --name appservice --cluster-type connectedClusters --cluster-name $env:clusterName --resource-group $env:resourceGroup --query id -o tsv
 Start-Sleep -Seconds 20
-az customlocation create --name 'jumpstart-cl' --resource-group $env:resourceGroup --namespace arc --host-resource-id $connectedClusterId --cluster-extension-ids $extensionId
+az customlocation create --name 'jumpstart-cl' --resource-group $env:resourceGroup --namespace appservice --host-resource-id $connectedClusterId --cluster-extension-ids $extensionId
 
-# # Deploying Azure Defender Kubernetes extension instance
-# Write-Host "Create Azure Defender Kubernetes extension instance"
-# Write-Host "`n"
-# az k8s-extension create --name "azure-defender" --cluster-name $env:clusterName --resource-group $env:resourceGroup --cluster-type connectedClusters --extension-type Microsoft.AzureDefender.Kubernetes
+$customLocationId = $(az customlocation show --name "jumpstart-cl" --resource-group $env:resourceGroup --query id -o tsv)
+
+
+
 
 
 
