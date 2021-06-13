072691bddd0b6b433976885db44bdb4fa75bee35
Raki Rahman <mdrakiburrahman@gmail.com>
2021-06-13 18:08:49 -0400

    Update install_microk8s.sh

--- azure_arc_data_jumpstart/microk8s/azure/arm_template/dc_vanilla/scripts/install_microk8s.sh
+++ azure_arc_data_jumpstart/microk8s/azure/arm_template/dc_vanilla/scripts/install_microk8s.sh
@@ -57,14 +57,16 @@ sudo snap alias microk8s.kubectl kubectl
 # Enable microk8s features
 sudo microk8s status --wait-ready
 
-# Set to Azure DNS: https://docs.microsoft.com/en-us/azure/virtual-network/virtual-networks-name-resolution-for-vms-and-role-instances#considerations
-#microk8s enable dns:168.63.129.16
-microk8s enable dns
+# Local mode
+microk8s enable dashboard dns storage
 
-sleep 5
+# Set to Azure DNS: https://docs.microsoft.com/en-us/azure/virtual-network/virtual-networks-name-resolution-for-vms-and-role-instances#considerations
+# microk8s enable dns:168.63.129.16
+
+# sleep 5
 
 # Enable other add-ons
-sudo microk8s enable storage ingress helm3 dashboard
+# sudo microk8s enable storage ingress helm3 dashboard
 
 echo "###########################################################################"
 echo "Networking enablement..." 
