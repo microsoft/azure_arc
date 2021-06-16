5fafc6574766cac4db2343c78ac0f25cd377ac98
dkirby-ms <saitcho@outlook.com>
2021-06-16 15:30:31 -0500

    update

--- azure_arc_data_jumpstart/gke/terraform/example/TF_VAR_datacontroller_postgresql_example.sh
+++ azure_arc_data_jumpstart/gke/terraform/example/TF_VAR_datacontroller_postgresql_example.sh
@@ -18,5 +18,5 @@ export TF_VAR_ARC_DC_NAME='arcdatactrl'
 export TF_VAR_ARC_DC_SUBSCRIPTION='32323232-XXXXX-YYYYY-9e8f-88888888888'
 export TF_VAR_ARC_DC_RG='Arc-Data-GKE-Demo'
 export TF_VAR_ARC_DC_REGION='eastus'
-export TF_VAR_deploy_SQLMI=true
-export TF_VAR_deploy_deploy_PostgreSQL=false
+export TF_VAR_deploy_SQLMI=false
+export TF_VAR_deploy_deploy_PostgreSQL=true
