f5f893eb6d48ddb44eac9e55dbd3df83c8266226
dkirby-ms <saitcho@outlook.com>
2021-06-16 13:20:28 -0500

    remove old scenarios

--- azure_arc_data_jumpstart/gke/terraform/artifacts/DeploySQLMI.ps1
+++ azure_arc_data_jumpstart/gke/terraform/artifacts/DeploySQLMI.ps1
@@ -1,9 +1,14 @@
+Start-Transcript -Path C:\Temp\deploySQL.log
+
+# Deployment environment variables
+# $deploymentNamespace = "arc"
+$controllerName = "Jumpstart-DC"
+
 # Deploying Azure Arc SQL Managed Instance
-Start-Transcript -Path C:\Temp\DeploySQLMI.log
 Write-Host "Deploying Azure Arc SQL Managed Instance"
 Write-Host "`n"
 
-$controllerName = "Jumpstart-DC"
+# $deploymentNamespace = "dataservices"
 $dataControllerId = $(az resource show --resource-group $env:resourceGroup --name $controllerName --resource-type "Microsoft.AzureArcData/dataControllers" --query id -o tsv)
 $vCoresMax = 4
 $memoryMax = "8"
@@ -14,7 +19,7 @@ $dataLogsStorageSize = "5"
 $backupsStorageSize = "5"
 $replicas = 1 # Value can be either 1 or 3
 
-$SQLParams = "C:\Temp\sqlmi.parameters.json"
+$SQLParams = "C:\Temp\SQLMI.parameters.json"
 
 (Get-Content -Path $SQLParams) -replace 'resourceGroup-stage',$env:resourceGroup | Set-Content -Path $SQLParams
 (Get-Content -Path $SQLParams) -replace 'dataControllerId-stage',$dataControllerId | Set-Content -Path $SQLParams
@@ -31,7 +36,7 @@ $SQLParams = "C:\Temp\sqlmi.parameters.json"
 (Get-Content -Path $SQLParams) -replace 'backupsSize-stage',$backupsStorageSize | Set-Content -Path $SQLParams
 (Get-Content -Path $SQLParams) -replace 'replicasStage' ,$replicas | Set-Content -Path $SQLParams
 
-az deployment group create --resource-group $env:resourceGroup --template-file "C:\Temp\sqlmi.json" --parameters "C:\Temp\sqlmi.parameters.json"
+az deployment group create --resource-group $env:resourceGroup --template-file "C:\Temp\SQLMI.json" --parameters "C:\Temp\SQLMI.parameters.json"
 Write-Host "`n"
 
 Do {
@@ -42,29 +47,10 @@ Do {
 Write-Host "Azure Arc SQL Managed Instance is ready!"
 Write-Host "`n"
 
-# Downloading demo database and restoring onto SQL MI
-$podname = "jumpstart-sql" + "-0"
-Write-Host "Downloading AdventureWorks database for MS SQL... (1/2)"
-kubectl exec $podname -n arc -c arc-sqlmi -- wget https://github.com/Microsoft/sql-server-samples/releases/download/adventureworks/AdventureWorks2019.bak -O /var/opt/mssql/data/AdventureWorks2019.bak 2>&1 | Out-Null
-Write-Host "Restoring AdventureWorks database for MS SQL. (2/2)"
-kubectl exec $podname -n arc -c arc-sqlmi -- /opt/mssql-tools/bin/sqlcmd -S localhost -U $env:AZDATA_USERNAME -P $env:AZDATA_PASSWORD -Q "RESTORE DATABASE AdventureWorks2019 FROM  DISK = N'/var/opt/mssql/data/AdventureWorks2019.bak' WITH MOVE 'AdventureWorks2017' TO '/var/opt/mssql/data/AdventureWorks2019.mdf', MOVE 'AdventureWorks2017_Log' TO '/var/opt/mssql/data/AdventureWorks2019_Log.ldf'" 2>&1 $null
-
 # Creating Azure Data Studio settings for SQL Managed Instance connection
 Write-Host ""
 Write-Host "Creating Azure Data Studio settings for SQL Managed Instance connection"
 $settingsTemplate = "C:\Temp\settingsTemplate.json"
-if ( $env:deployPostgreSQL -eq $false)
-{
-    $string = Get-Content -Path $settingsTemplate
-    $string[25] = $string[25] -replace ",",""
-    $string | Set-Content -Path $settingsTemplate
-    $string = Get-Content -Path $settingsTemplate | Select-Object -First 25 -Last 4
-    $string | Set-Content -Path $settingsTemplate
-}
-
-New-Item -Path "C:\Users\$env:adminUsername\AppData\Roaming\azuredatastudio\" -Name "User" -ItemType "directory" -Force
-Copy-Item -Path "C:\Temp\settingsTemplate.json" -Destination "C:\Users\$env:adminUsername\AppData\Roaming\azuredatastudio\User\settings.json"
-$settingsFile = "C:\Users\$env:adminUsername\AppData\Roaming\azuredatastudio\User\settings.json"
 kubectl describe svc jumpstart-sql-external-svc -n arc | Select-String "LoadBalancer Ingress" | Tee-Object "C:\Temp\sql_instance_list.txt" | Out-Null
 $sqlfile = "C:\Temp\sql_instance_list.txt"
 $sqlstring = Get-Content $sqlfile
@@ -72,12 +58,21 @@ $sqlstring.split(" ") | Tee-Object "C:\Temp\sql_instance_list.txt" | Out-Null
 (Get-Content $sqlfile | Select-Object -Skip 7) | Set-Content $sqlfile
 $sqlstring = Get-Content $sqlfile
 
-(Get-Content -Path $settingsFile) -replace 'arc_sql_mi',$sqlstring | Set-Content -Path $settingsFile
-(Get-Content -Path $settingsFile) -replace 'sa_username',$env:AZDATA_USERNAME | Set-Content -Path $settingsFile
-(Get-Content -Path $settingsFile) -replace 'sa_password',$env:AZDATA_PASSWORD | Set-Content -Path $settingsFile
-(Get-Content -Path $settingsFile) -replace 'false','true' | Set-Content -Path $settingsFile
+(Get-Content -Path $settingsTemplate) -replace 'arc_sql_mi',$sqlstring | Set-Content -Path $settingsTemplate
+(Get-Content -Path $settingsTemplate) -replace 'sa_username',$env:AZDATA_USERNAME | Set-Content -Path $settingsTemplate
+(Get-Content -Path $settingsTemplate) -replace 'sa_password',$env:AZDATA_PASSWORD | Set-Content -Path $settingsTemplate
+(Get-Content -Path $settingsTemplate) -replace 'false','true' | Set-Content -Path $settingsTemplate
 
-
+if ( $env:deployPostgreSQL -eq $false )
+{
+    $string = Get-Content $settingsTemplate
+    $string[25] = $string[25] -replace ",",""
+    $string | Set-Content $settingsTemplate
+    $string = Get-Content $settingsTemplate | Select-Object -First 25 -Last 4
+    (Get-Content -Path $string) | Set-Content -Path $settingsTemplate
+}
 
 # Cleaning garbage
 Remove-Item "C:\Temp\sql_instance_list.txt" -Force
+
+Stop-Transcript
