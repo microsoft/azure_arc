9bf6dad15ad01e073f218797bf4b6613cb827d90
likamrat <likamrat@microsoft.com>
2021-06-21 12:18:06 -0700

    arc_data_capi

--- azure_arc_data_jumpstart/cluster_api/capi_azure/arm_template/artifacts/DeployPostgreSQL.ps1
+++ azure_arc_data_jumpstart/cluster_api/capi_azure/arm_template/artifacts/DeployPostgreSQL.ps1
@@ -46,7 +46,7 @@ Write-Host "`n"
 # Downloading demo database and restoring onto Postgres
 $podname = "$env:POSTGRES_NAME" + "c-0"
 Write-Host "Downloading AdventureWorks.sql template for Postgres... (1/3)"
-kubectl exec $podname -n $env:arcDcName -c postgres -- /bin/bash -c "cd /tmp && curl -k -O https://raw.githubusercontent.com/microsoft/azure_arc/capi_data_connected/azure_jumpstart_arcbox/scripts/AdventureWorks2019.sql" 2>&1 | Out-Null
+kubectl exec $podname -n $env:arcDcName -c postgres -- /bin/bash -c "cd /tmp && curl -k -O https://raw.githubusercontent.com/microsoft/azure_arc/capi_data_connected/azure_arc_data_jumpstart/cluster_api/capi_azure/arm_template/artifacts/AdventureWorks2019.sql" 2>&1 | Out-Null
 Write-Host "Creating AdventureWorks database on Postgres... (2/3)"
 kubectl exec $podname -n $env:arcDcName -c postgres -- sudo -u postgres psql -c 'CREATE DATABASE "adventureworks2019";' postgres 2>&1 | Out-Null
 Write-Host "Restoring AdventureWorks database on Postgres. (3/3)"
