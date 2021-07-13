872161365163b5a8fb751b62a2b31ccac9e807a3
lanicolas <lauranicolas87@outlook.com>
2021-07-13 16:35:55 +0200

    PR update

--- azure_arc_servers_jumpstart/automanage/artifacts/arcautomanage.sh
+++ azure_arc_servers_jumpstart/automanage/artifacts/arcautomanage.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
 # Environment Variables
-export automanage_account_name='<Name of your Azure Automanage Account>'
+export automanageAccountName='<Name of your Azure Automanage Account>'
 export location='<Azure Region where your Azure Arc enabled Server is registered>'
-export resource_group='<Azure Resource Group where your Azure Arc enabled server is registered>'
-export machine_name='<Name of your Azure Arc enabled Server>'
+export resourceGroup='<Azure Resource Group where your Azure Arc enabled server is registered>'
+export machinename='<Name of your Azure Arc enabled Server>'
 export profile='<environment of your Azure Arc enabled Server Production or Dev/Test>'
 
 # Replace environment variables in parameters file
 
-sed -i "s/<name of your Automanage Account identity>/$automanage_account_name/" automanageaccount.parameters.json
-sed -i "s/<azure region>/$location/" automanageaccount.parameters.json
+sed -i "s/<name of your Automanage Account identity>/$automanageAccountName/" automanageAccount.parameters.json
+sed -i "s/<azure region>/$location/" automanageAccount.parameters.json
 
 # Create Azure Automanage Account
 
-az deployment group create --resource-group $resource_group --template-file automanageaccount.json --parameters automanageaccount.parameters.json
+az deployment group create --resource-group $resourceGroup --template-file automanageAccount.json --parameters automanageAccount.parameters.json
 
 # Grant permissions to Azure Automanage Account
 
-objectid=$(az ad sp list --filter "displayname eq '$automanage_account_name'" --query '[].objectId' -o tsv)
-sed -i "s/<Object ID of the Automanage Account>/$objectid/" automanagepermissions.parameters.json
-az deployment sub create  --template-file automanagepermissions.json --parameters automanagepermissions.parameters.json --location $location
+objectid=$(az ad sp list --filter "displayname eq '$automanageAccountName'" --query '[].objectId' -o tsv)
+sed -i "s/<Object ID of the Automanage Account>/$objectid/" automanagePermissions.parameters.json
+az deployment sub create  --template-file automanagePermissions.json --parameters automanagePermissions.parameters.json --location $location
 
 # Enable Azure Automanage
-sed -i "s/<Name of your Azure Automanage Account>/$automanage_account_name/" automanage.parameters.json
-sed -i "s/<Name of your Azure Arc enabled Server>/$machine_name/" automanage.parameters.json
+sed -i "s/<Name of your Azure Automanage Account>/$automanageAccountName/" automanage.parameters.json
+sed -i "s/<Name of your Azure Arc enabled Server>/$machinename/" automanage.parameters.json
 sed -i "s/<environment of your Azure Arc enabled server Production or DevTest>/$profile/" automanage.parameters.json
 
-az deployment group create --resource-group $resource_group --template-file automanage.json --parameters automanage.parameters.json
+az deployment group create --resource-group $resourceGroup --template-file automanage.json --parameters automanage.parameters.json
