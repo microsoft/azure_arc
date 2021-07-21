8d24d62f2ce05d6b2305bbb55a04682e5170ec83
likamrat <likamrat@microsoft.com>
2021-07-21 14:59:48 -0700

    settingsTemplate fix

--- azure_jumpstart_arcbox/artifacts/DeploySQLMI.ps1
+++ azure_jumpstart_arcbox/artifacts/DeploySQLMI.ps1
@@ -49,18 +49,18 @@ Write-Host "Azure Arc SQL Managed Instance is ready!"
 Write-Host "`n"
 
 # Downloading demo database and restoring onto SQL MI
-$podname = "jumpstart-sql-0"
+$podname = "arcbox-sql-0"
 Write-Host "Downloading AdventureWorks database for MS SQL... (1/2)"
 kubectl exec $podname -n arc -c arc-sqlmi -- wget https://github.com/Microsoft/sql-server-samples/releases/download/adventureworks/AdventureWorks2019.bak -O /var/opt/mssql/data/AdventureWorks2019.bak 2>&1 | Out-Null
 Write-Host "Restoring AdventureWorks database for MS SQL. (2/2)"
 kubectl exec $podname -n arc -c arc-sqlmi -- /opt/mssql-tools/bin/sqlcmd -S localhost -U $env:AZDATA_USERNAME -P $env:AZDATA_PASSWORD -Q "RESTORE DATABASE AdventureWorks2019 FROM  DISK = N'/var/opt/mssql/data/AdventureWorks2019.bak' WITH MOVE 'AdventureWorks2017' TO '/var/opt/mssql/data/AdventureWorks2019.mdf', MOVE 'AdventureWorks2017_Log' TO '/var/opt/mssql/data/AdventureWorks2019_Log.ldf'" 2>&1 $null
 
 # Creating Azure Data Studio settings for SQL Managed Instance connection
-Write-Host "`n"
+Write-Host ""
 Write-Host "Creating Azure Data Studio settings for SQL Managed Instance connection"
 $settingsTemplate = "C:\ArcBox\settingsTemplate.json"
 # Retrieving SQL MI connection endpoint
-$sqlstring = kubectl get sqlmanagedinstances jumpstart-sql -n arc -o=jsonpath='{.status.primaryEndpoint}'
+$sqlstring = kubectl get sqlmanagedinstances arcbox-sql -n arc -o=jsonpath='{.status.primaryEndpoint}'
 
 # Replace placeholder values in settingsTemplate.json
 (Get-Content -Path $settingsTemplate) -replace 'arc_sql_mi',$sqlstring | Set-Content -Path $settingsTemplate
