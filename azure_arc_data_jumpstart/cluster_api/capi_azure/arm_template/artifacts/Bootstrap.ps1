e06849d9c58ad61ba7418b7a3d58fe74f40819ef
likamrat <likamrat@microsoft.com>
2021-07-26 11:41:24 -0700

    az cli alias

--- azure_arc_data_jumpstart/cluster_api/capi_azure/arm_template/artifacts/Bootstrap.ps1
+++ azure_arc_data_jumpstart/cluster_api/capi_azure/arm_template/artifacts/Bootstrap.ps1
@@ -143,18 +143,18 @@ Register-ScheduledTask -TaskName "DataServicesLogonScript" -Trigger $Trigger -Us
 # Disabling Windows Server Manager Scheduled Task
 Get-ScheduledTask -TaskName ServerManager | Disable-ScheduledTask
 
-# Configure alias to az cli for this execution
-New-Item -path alias:az -value 'C:\Program Files (x86)\Microsoft SDKs\Azure\CLI2\wbin\az.cmd'
+# # Configure alias to az cli for this execution
+# New-Item -path alias:az -value 'C:\Program Files (x86)\Microsoft SDKs\Azure\CLI2\wbin\az.cmd'
 
 # Adding Azure Arc CLI extensions
 Write-Host "Adding Azure Arc CLI extensions"
 Write-Host "`n"
 
-az extension add --name "connectedk8s" -y
-az extension add --name "k8s-configuration" -y
-az extension add --name "k8s-extension" -y
-az extension add --name "customlocation" -y
-az extension add --name "arcdata" -y
+az.cmd extension add --name "connectedk8s" -y
+az.cmd extension add --name "k8s-configuration" -y
+az.cmd extension add --name "k8s-extension" -y
+az.cmd extension add --name "customlocation" -y
+az.cmd extension add --name "arcdata" -y
 
 # Validate
 Write-Host "`n"
