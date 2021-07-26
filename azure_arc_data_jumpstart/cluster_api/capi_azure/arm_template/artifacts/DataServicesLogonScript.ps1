17e56564f6fbdc47ea44050adaa7377b529a4ca9
likamrat <likamrat@microsoft.com>
2021-07-26 14:20:20 -0700

    force kube-config

--- azure_arc_data_jumpstart/cluster_api/capi_azure/arm_template/artifacts/DataServicesLogonScript.ps1
+++ azure_arc_data_jumpstart/cluster_api/capi_azure/arm_template/artifacts/DataServicesLogonScript.ps1
@@ -50,14 +50,14 @@ Write-Host "`n"
 az provider show --namespace Microsoft.AzureArcData -o table
 Write-Host "`n"
 
-# # Adding Azure Arc CLI extensions
-# Write-Host "Adding Azure Arc CLI extensions"
-# Write-Host "`n"
-# az extension add --name "connectedk8s" -y
-# az extension add --name "k8s-configuration" -y
-# az extension add --name "k8s-extension" -y
-# az extension add --name "customlocation" -y
-# az extension add --name "arcdata" -y
+# Adding Azure Arc CLI extensions
+Write-Host "Adding Azure Arc CLI extensions"
+Write-Host "`n"
+az extension add --name "connectedk8s" -y
+az extension add --name "k8s-configuration" -y
+az extension add --name "k8s-extension" -y
+az extension add --name "customlocation" -y
+az extension add --name "arcdata" -y
 
 Write-Host "`n"
 az -v
@@ -89,7 +89,7 @@ azdata --version
 # Onboarding the CAPI cluster as an Azure Arc enabled Kubernetes cluster
 Write-Host "Onboarding the cluster as an Azure Arc enabled Kubernetes cluster"
 Write-Host "`n"
-az connectedk8s connect --name $connectedClusterName --resource-group $env:resourceGroup --location $env:azureLocation --tags 'Project=jumpstart_azure_arc_data_services' --custom-locations-oid '51dfe1e8-70c6-4de5-a08e-e18aff23d815'
+az connectedk8s connect --name $connectedClusterName --resource-group $env:resourceGroup --location $env:azureLocation --tags 'Project=jumpstart_azure_arc_data_services' --custom-locations-oid '51dfe1e8-70c6-4de5-a08e-e18aff23d815' --kube-config "C:\Users\$env:USERNAME\.kube\config"
 Start-Sleep -Seconds 10
 $kubectlMonShell = Start-Process -PassThru PowerShell {for (0 -lt 1) {kubectl get pod -n arc; Start-Sleep -Seconds 5; Clear-Host }}
 az k8s-extension create --name arc-data-services --extension-type microsoft.arcdataservices --cluster-type connectedClusters --cluster-name $connectedClusterName --resource-group $env:resourceGroup --auto-upgrade false --scope cluster --release-namespace arc --config Microsoft.CustomLocation.ServiceAccount=sa-bootstrapper
