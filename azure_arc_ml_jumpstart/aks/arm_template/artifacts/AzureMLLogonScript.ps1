d7149cfd7093e564f0fb4a9fe258d5956626aff9
Raki Rahman <mdrrahman@microsoft.com>
2021-08-01 15:03:44 -0400

    Added Azure ML onboarding logic

--- azure_arc_ml_jumpstart/aks/arm_template/artifacts/AzureMLLogonScript.ps1
+++ azure_arc_ml_jumpstart/aks/arm_template/artifacts/AzureMLLogonScript.ps1
@@ -1,32 +1,15 @@
-Start-Transcript -Path C:\Temp\DataServicesLogonScript.log
+Start-Transcript -Path C:\Temp\AzureMLLogonScript.log
 
 # Deployment environment variables
-$connectedClusterName = "Arc-Data-AKS"
+$connectedClusterName = "Arc-AML-AKS"
 
 Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
 
+# Login as service principal
 az login --service-principal --username $env:spnClientId --password $env:spnClientSecret --tenant $env:spnTenantId
 
+# Set default subscription
 az account set --subscription $env:subscriptionId
-
-Write-Host "Installing Azure Data Studio Extensions"
-Write-Host "`n"
-
-$env:argument1="--install-extension"
-$env:argument2="Microsoft.arc"
-$env:argument3="microsoft.azuredatastudio-postgresql"
-
-& "C:\Program Files\Azure Data Studio\bin\azuredatastudio.cmd" $env:argument1 $env:argument2
-& "C:\Program Files\Azure Data Studio\bin\azuredatastudio.cmd" $env:argument1 $env:argument3
-
-Write-Host "Creating Azure Data Studio Desktop shortcut"
-Write-Host "`n"
-$TargetFile = "C:\Program Files\Azure Data Studio\azuredatastudio.exe"
-$ShortcutFile = "C:\Users\$env:adminUsername\Desktop\Azure Data Studio.lnk"
-$WScriptShell = New-Object -ComObject WScript.Shell
-$Shortcut = $WScriptShell.CreateShortcut($ShortcutFile)
-$Shortcut.TargetPath = $TargetFile
-$Shortcut.Save()
 
 # Registering Azure Arc providers
 Write-Host "Registering Azure Arc providers, hold tight..."
@@ -34,15 +17,12 @@ Write-Host "`n"
 az provider register --namespace Microsoft.Kubernetes --wait
 az provider register --namespace Microsoft.KubernetesConfiguration --wait
 az provider register --namespace Microsoft.ExtendedLocation --wait
-az provider register --namespace Microsoft.AzureArcData --wait
 
 az provider show --namespace Microsoft.Kubernetes -o table
 Write-Host "`n"
 az provider show --namespace Microsoft.KubernetesConfiguration -o table
 Write-Host "`n"
 az provider show --namespace Microsoft.ExtendedLocation -o table
-Write-Host "`n"
-az provider show --namespace Microsoft.AzureArcData -o table
 Write-Host "`n"
 
 # Adding Azure Arc CLI extensions
@@ -75,57 +55,116 @@ $kubectlMonShell = Start-Process -PassThru PowerShell {for (0 -lt 1) {kubectl ge
 az connectedk8s connect --name $connectedClusterName `
                         --resource-group $env:resourceGroup `
                         --location $env:azureLocation `
-                        --tags 'Project=jumpstart_azure_arc_data_services' ` --custom-locations-oid '51dfe1e8-70c6-4de5-a08e-e18aff23d815'
+                        --tags 'Project=jumpstart_azure_arc_ml_services' `
+						--custom-locations-oid '51dfe1e8-70c6-4de5-a08e-e18aff23d815'
                         # This is the Custom Locations Enterprise Application ObjectID from AAD
 
 Start-Sleep -Seconds 10
 
-############################################################################################################################
-# Create Azure Machine Learning extension (Ctrl + K + C/U to Block/Unblock)
+###################################################################################################################
+# Azure Arc-enabled Machine Learning enablement components
+Write-Host """
+ Enabling Azure Arc-enabled Machine Learning enablement components:
 
-# Training only 
-# az k8s-extension create --name amlarc-compute `
-#                         --extension-type Microsoft.AzureML.Kubernetes `
-# 								  --cluster-type connectedClusters `
-#                         --cluster-name $connectedClusterName `
-#                         --resource-group $env:resourceGroup `
-#                         --scope cluster `
-#                         --configuration-settings enableTraining=True
+                 /((((((((#,
+                 /(((((((##,
+                 (((#######,
+                 (#########,
+                 (#########,
+                 (#########,
+                 ((((((((((
+              /((((((((((    (/
+            ((((((((((/    ((((((
+          ((((((((((,    ###((((###
+       .///(((((((.     /############
+     ,//////((((           ############*
+    **//////((%%%%%%%%%%%%%%%############
+    *///////&&&%%%%%%%%%%%%%%%%&########,
+     /////&&&&&&%%%%%%%%%%%%%&&&&&######
+      /(&&&&&&&&&&%&%%%%%%&&&&&&&&&&&##
+"""
 
-# Inferencing only
-# az k8s-extension create --name amlarc-compute `
-# 								--extension-type Microsoft.AzureML.Kubernetes `
-# 								--cluster-type connectedClusters `
-# 								--cluster-name $connectedClusterName `
-# 								--resource-group $env:resourceGroup `
-# 								--scope cluster `
-# 								--configuration-settings enableInference=True allowInsecureConnections=True inferenceLoadBalancerHA=False # This is since our K8s is 1 node
+# Adding Azure ML CLI extension
+az extension add -n ml
+az ml -h
 
-# Print out extension status
-# Write-Host "Waiting for extension install, hold tight..."
-# Do 
-# {
-#     $response = az k8s-extension show --name amlarc-compute `
-# 											--cluster-type connectedClusters `
-# 											--cluster-name $connectedClusterName `
-# 											--resource-group $env:resourceGroup `
-# 											--output json | ConvertFrom-Json
+# Installing Azure ML Python SDK
+python -m pip install --upgrade pip
+pip install azureml-core
 
-# 		Write-Host ("Status: ", $response.installState)
+# Deploying AML workspace
+$ws = $env:resourceGroup + "-amlws" # AML workspace name
+az configure --defaults workspace=$ws group=$env:resourceGroup
+az ml workspace create -g $env:resourceGroup
 
-# 		If ($response.installState -eq "Failed") {break}
+# Arc K8s extension enablement: Training and Inferencing
+az k8s-extension create --name amlarc-compute `
+						--extension-type Microsoft.AzureML.Kubernetes `
+						--cluster-type connectedClusters `
+						--cluster-name $connectedClusterName `
+						--resource-group $env:resourceGroup `
+						--scope cluster `
+						--configuration-settings enableTraining=True enableInference=True allowInsecureConnections=True inferenceLoadBalancerHA=False # This is since our K8s is 1 node
 
-# 		Start-Sleep -Seconds 20
+# Poll extension status from ARM
+Write-Host "Waiting for extension install, hold tight..."
+Do 
+{
+    $response = az k8s-extension show --name amlarc-compute `
+									  --cluster-type connectedClusters `
+									  --cluster-name $connectedClusterName `
+									  --resource-group $env:resourceGroup `
+									  --output json | ConvertFrom-Json
 
-# } while (($response.installState -ne "Installed") -and ($response.installState -ne "Failed"))
+		Write-Host ("Status: ", $response.installState)
 
-# # Received error
-# If ($response.installState -eq "Failed"){
-# 	Write-Host "Installation failed:" -ForegroundColor Red
-# 	Write-Host $response.errorInfo
-# }
+		If ($response.installState -eq "Failed") {break}
 
-############################################################################################################################
+		Start-Sleep -Seconds 20
+
+} while (($response.installState -ne "Installed") -and ($response.installState -ne "Failed"))
+
+# Get Extension status
+If ($response.installState -eq "Failed"){
+	Write-Host "K8s-Extension installation failed:" -ForegroundColor Red
+	Write-Host $response.errorInfo
+}
+elseif ($response.installState -eq "Installed"){
+	Write-Host "K8s-Extension installation successful." -ForegroundColor Green
+}
+else
+{
+    Write-Host "Something else went wrong."
+}
+
+# Get Arc Cluster Resource ID
+$connectedClusterId = az connectedk8s show --name $connectedClusterName --resource-group $env:resourceGroup --query id -o tsv
+
+# Replace staging values in Python Script
+# 1.Get_WS.py
+$Script = "C:\Temp\1.Get_WS.py"
+(Get-Content -Path $Script) -replace 'subscription_id-stage',$env:subscriptionId | Set-Content -Path $Script
+(Get-Content -Path $Script) -replace 'resource_group-stage',$env:resourceGroup | Set-Content -Path $Script
+(Get-Content -Path $Script) -replace 'workspace_name-stage',$ws | Set-Content -Path $Script
+
+# 2.Attach_Arc.py
+$Script = "C:\Temp\2.Attach_Arc.py"
+(Get-Content -Path $Script) -replace 'connectedClusterName-stage',$connectedClusterName | Set-Content -Path $Script
+(Get-Content -Path $Script) -replace 'connectedClusterId-stage',$connectedClusterId | Set-Content -Path $Script
+
+# Write Azure ML Workspace info
+python "C:\Temp\1.Get_WS.py"
+
+# Attach Arc Cluster to Azure ML Workspace
+python "C:\Temp\2.Attach_Arc.py"
+
+
+#### TODO #####
+# Training deployment Stuff
+# Inference deployment Stuff
+# Inference call stuff
+
+###################################################################################################################
 
 # Changing to Client VM wallpaper
 $imgPath="C:\Temp\wallpaper.png"
@@ -148,12 +187,11 @@ add-type $code
 [Win32.Wallpaper]::SetWallpaper($imgPath)
 
 # Kill the open PowerShell monitoring kubectl get pods
-# Stop-Process -Id $kubectlMonShell.Id
+Stop-Process -Id $kubectlMonShell.Id
 
 # Removing the LogonScript Scheduled Task so it won't run on next reboot
-Unregister-ScheduledTask -TaskName "DataServicesLogonScript" -Confirm:$false
+Unregister-ScheduledTask -TaskName "AzureMLLogonScript" -Confirm:$false
 Start-Sleep -Seconds 5
 
 # Stop-Process -Name powershell -Force
-
 Stop-Transcript
