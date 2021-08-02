c9111ab22839c05090eadda6a17425e9bd92650f
Laura Nicolas <51124125+lanicolas@users.noreply.github.com>
2021-08-02 14:38:01 +0200

    Updated script

--- azure_arc_servers_jumpstart/automanage/artifacts/arcAutomanage.sh
+++ azure_arc_servers_jumpstart/automanage/artifacts/arcAutomanage.sh
@@ -8,11 +8,10 @@ export resourceGroup='<Azure Resource Group where your Azure Arc enabled server 
 export machinename='<Name of your Azure Arc enabled Server>'
 export profile='<environment of your Azure Arc enabled Server Production or Dev/Test>'
 
-
 # ARM template configuration - Do not change!
 
 sed -i "s/<name of your Automanage Account identity>/$automanageAccountName/" automanageAccount.parameters.json
-sed -i "s/<azure region>/$location/" automanageAccount.parameters.json
+sed -i "s/<Azure Region>/$location/" automanageAccount.parameters.json
 
 # Create Azure Automanage Account
 
@@ -31,7 +30,7 @@ az deployment sub create  --template-file automanagePermissions.json --parameter
 
 sed -i "s/<Name of your Azure Automanage Account>/$automanageAccountName/" automanage.parameters.json
 sed -i "s/<Name of your Azure Arc enabled Server>/$machinename/" automanage.parameters.json
-sed -i "s/<environment of your Azure Arc enabled server Production or DevTest>/$profile/" automanage.parameters.json
+sed -i "s/<environment of your Azure Arc enabled Server Production or DevTest>/$profile/" automanage.parameters.json
 
 # Enable Azure Automanage
 
