80e4cc64f17013c62ade8e2e5cd6ad1d559425fc
likamrat <likamrat@microsoft.com>
2021-08-03 14:23:24 -0700

    arc k8s refresh

--- azure_arc_k8s_jumpstart/rancher_k3s/azure/arm_template/scripts/install_k3s.sh
+++ azure_arc_k8s_jumpstart/rancher_k3s/azure/arm_template/scripts/install_k3s.sh
@@ -8,16 +8,16 @@ sudo apt-get update
 echo '#!/bin/bash' >> vars.sh
 echo $adminUsername:$1 | awk '{print substr($1,2); }' >> vars.sh
 echo $appId:$2 | awk '{print substr($1,2); }' >> vars.sh
-# echo $password:$3 | awk '{print substr($1,2); }' >> vars.sh
-# echo $tenantId:$4 | awk '{print substr($1,2); }' >> vars.sh
-# echo $vmName:$5 | awk '{print substr($1,2); }' >> vars.sh
-# echo $location:$6 | awk '{print substr($1,2); }' >> vars.sh
+echo $password:$3 | awk '{print substr($1,2); }' >> vars.sh
+echo $tenantId:$4 | awk '{print substr($1,2); }' >> vars.sh
+echo $vmName:$5 | awk '{print substr($1,2); }' >> vars.sh
+echo $location:$6 | awk '{print substr($1,2); }' >> vars.sh
 sed -i '2s/^/export adminUsername=/' vars.sh
 sed -i '3s/^/export appId=/' vars.sh
-# sed -i '4s/^/export password=/' vars.sh
-# sed -i '5s/^/export tenantId=/' vars.sh
-# sed -i '6s/^/export vmName=/' vars.sh
-# sed -i '7s/^/export location=/' vars.sh
+sed -i '4s/^/export password=/' vars.sh
+sed -i '5s/^/export tenantId=/' vars.sh
+sed -i '6s/^/export vmName=/' vars.sh
+sed -i '7s/^/export location=/' vars.sh
 
 chmod +x vars.sh 
 . ./vars.sh
