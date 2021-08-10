e3139c77f0f6fddcb15eb6f75cef70cfd208ac48
likamrat <likamrat@microsoft.com>
2021-08-10 13:53:09 -0700

    testing BusinessCritical

--- azure_arc_data_jumpstart/aks/arm_template/artifacts/Bootstrap.ps1
+++ azure_arc_data_jumpstart/aks/arm_template/artifacts/Bootstrap.ps1
@@ -75,7 +75,6 @@ Invoke-WebRequest ($templateBaseUrl + "artifacts/SQLMI.json") -OutFile "C:\Temp\
 Invoke-WebRequest ($templateBaseUrl + "artifacts/SQLMI.parameters.json") -OutFile "C:\Temp\SQLMI.parameters.json"
 Invoke-WebRequest ($templateBaseUrl + "artifacts/postgreSQL.json") -OutFile "C:\Temp\postgreSQL.json"
 Invoke-WebRequest ($templateBaseUrl + "artifacts/postgreSQL.parameters.json") -OutFile "C:\Temp\postgreSQL.parameters.json"
-Invoke-WebRequest ($templateBaseUrl + "artifacts/addRestoredDBToAG.sql") -OutFile "C:\Temp\addRestoredDBToAG.sql"
 Invoke-WebRequest "https://raw.githubusercontent.com/microsoft/azure_arc/main/img/jumpstart_wallpaper.png" -OutFile "C:\Temp\wallpaper.png"
 
 # Installing tools
