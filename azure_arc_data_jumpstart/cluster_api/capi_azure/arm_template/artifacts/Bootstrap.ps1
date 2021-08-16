90bab3cde7a4dc515a046716c97b5570cfe20583
likamrat <likamrat@microsoft.com>
2021-08-16 12:33:04 -0700

    scenario

--- azure_arc_data_jumpstart/cluster_api/capi_azure/arm_template/artifacts/Bootstrap.ps1
+++ azure_arc_data_jumpstart/cluster_api/capi_azure/arm_template/artifacts/Bootstrap.ps1
@@ -78,12 +78,13 @@ Invoke-WebRequest ($templateBaseUrl + "artifacts/SQLMI.json") -OutFile "C:\Temp\
 Invoke-WebRequest ($templateBaseUrl + "artifacts/SQLMI.parameters.json") -OutFile "C:\Temp\SQLMI.parameters.json"
 Invoke-WebRequest ($templateBaseUrl + "artifacts/postgreSQL.json") -OutFile "C:\Temp\postgreSQL.json"
 Invoke-WebRequest ($templateBaseUrl + "artifacts/postgreSQL.parameters.json") -OutFile "C:\Temp\postgreSQL.parameters.json"
+Invoke-WebRequest ($templateBaseUrl + "artifacts/Endpoints.ps1") -OutFile "C:\Temp\Endpoints.ps1"
 Invoke-WebRequest "https://raw.githubusercontent.com/microsoft/azure_arc/main/img/jumpstart_wallpaper.png" -OutFile "C:\Temp\wallpaper.png"
 
 # Installing tools
 workflow ClientTools_01
         {
-            $chocolateyAppList = 'azure-cli,az.powershell,kubernetes-cli,vcredist140,microsoft-edge,azcopy10,vscode,putty.install,kubernetes-helm,grep'
+            $chocolateyAppList = 'azure-cli,az.powershell,kubernetes-cli,vcredist140,microsoft-edge,azcopy10,vscode,putty.install,kubernetes-helm,grep,ssms'
             #Run commands in parallel.
             Parallel 
                 {
