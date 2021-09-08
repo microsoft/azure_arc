5488cb7787299f5883af70c929ccb083f6c132c5
likamrat <likamrat@microsoft.com>
2021-09-07 17:01:51 -0700

    adding readme

--- azure_arc_data_jumpstart/cluster_api/capi_azure/arm_template/artifacts/SQLMIEndpoints.ps1
+++ azure_arc_data_jumpstart/cluster_api/capi_azure/arm_template/artifacts/SQLMIEndpoints.ps1
@@ -1,8 +1,10 @@
-Start-Transcript -Path C:\Temp\Endpoints.log
+Start-Transcript -Path C:\Temp\SQLMIEndpoints.log
 
-# Retrieving SQL MI connection endpoint
-New-Item -Path "C:\Temp\" -Name "Endpoints.txt" -ItemType "file" 
-$Endpoints = "C:\Temp\Endpoints.txt"
+# Creating SQLMI Endpoints file 
+New-Item -Path "C:\Temp\" -Name "SQLMIEndpoints.txt" -ItemType "file" 
+$Endpoints = "C:\Temp\SQLMIEndpoints.txt"
+
+# Retrieving SQL MI connection endpoints
 Add-Content $Endpoints "Primary SQL Managed Instance external endpoint:"
 $primaryEndpoint = kubectl get sqlmanagedinstances jumpstart-sql -n arc -o=jsonpath='{.status.primaryEndpoint}'
 $primaryEndpoint = $primaryEndpoint.Substring(0, $primaryEndpoint.IndexOf(',')) | Add-Content $Endpoints
@@ -13,10 +15,10 @@ $secondaryEndpoint = kubectl get sqlmanagedinstances jumpstart-sql -n arc -o=jso
 $secondaryEndpoint = $secondaryEndpoint.Substring(0, $secondaryEndpoint.IndexOf(',')) | Add-Content $Endpoints
 
 Write-Host "`n"
-Write-Host "Creating Endpoints file Desktop shortcut"
+Write-Host "Creating SQLMI Endpoints file Desktop shortcut"
 Write-Host "`n"
 $TargetFile = $Endpoints
-$ShortcutFile = "C:\Users\$env:adminUsername\Desktop\Arc Data Endpoints.lnk"
+$ShortcutFile = "C:\Users\$env:adminUsername\Desktop\SQLMI Endpoints.lnk"
 $WScriptShell = New-Object -ComObject WScript.Shell
 $Shortcut = $WScriptShell.CreateShortcut($ShortcutFile)
 $Shortcut.TargetPath = $TargetFile
