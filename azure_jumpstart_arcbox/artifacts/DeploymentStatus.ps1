6a05cebab4bcb237da39155588797c3a4a316d81
Lior Kamrat <46730876+likamrat@users.noreply.github.com>
2021-10-27 13:27:24 -0700

    Update DeploymentStatus.ps1

--- azure_jumpstart_arcbox/artifacts/DeploymentStatus.ps1
+++ azure_jumpstart_arcbox/artifacts/DeploymentStatus.ps1
@@ -1,5 +1,8 @@
 Start-Transcript -Path C:\ArcBox\DeploymentStatus.log
 
+# Adding Resource Graph CLI extension
+az extension add --name "resource-graph" -y
+
 # Sending deployement status message to Azure storage account queue
 $arcNumResources = az graph query -q "Resources | where type =~ 'Microsoft.HybridCompute/machines' or type=~'Microsoft.Kubernetes/connectedClusters' or type=~'Microsoft.AzureArcData/SqlServerInstances' or type=~'Microsoft.AzureArcData/dataControllers' or type=~'Microsoft.AzureArcData/sqlManagedInstances' or type=~'Microsoft.AzureArcData/postgresInstances' | where resourceGroup=~'$env:resourceGroup' | where tags.Project=~'jumpstart_arcbox' | project name, location, resourceGroup, tags | summarize count()" | Select-String "count_"
 $arcNumResources = $arcNumResources -replace "[^0-9]" , ''
