75cd9f342ab04a9605b00919c14b6b8eb86b894d
dkirby-ms <saitcho@outlook.com>
2021-10-27 13:11:41 -0500

    missing env:

--- azure_jumpstart_arcbox/artifacts/DeploymentStatus.ps1
+++ azure_jumpstart_arcbox/artifacts/DeploymentStatus.ps1
@@ -7,24 +7,24 @@ Write-Host "You now have $arcNumResources Azure Arc resources in '$env:resourceG
 Write-Host "`n"
 
 # ArcBox Full edition report if applicabale
-if ($flavor -eq "Full" -Or $flavor -eq "Developer") {
+if ($env:flavor -eq "Full" -Or $env:flavor -eq "Developer") {
     if ( $arcNumResources -eq 11 )
     {
         Write-Host "Great success!"
-        az storage message put --content "Successful Jumpstart ArcBox ($flavor) deployment" --account-name "jumpstartusage" --queue-name "arcboxusage" --sas-token "?sv=2020-08-04&ss=q&srt=sco&sp=wa&se=2031-11-01T07:34:50Z&st=2021-10-18T23:34:50Z&spr=https&sig=tQMbCj5EthrwachHMuzLSJ4SgSOtEqqWcAbid6b87vU%3D" --time-to-live -1
+        az storage message put --content "Successful Jumpstart ArcBox ($env:flavor) deployment" --account-name "jumpstartusage" --queue-name "arcboxusage" --sas-token "?sv=2020-08-04&ss=q&srt=sco&sp=wa&se=2031-11-01T07:34:50Z&st=2021-10-18T23:34:50Z&spr=https&sig=tQMbCj5EthrwachHMuzLSJ4SgSOtEqqWcAbid6b87vU%3D" --time-to-live -1
     }
 }
 
 # ArcBox IT Pro edition report if applicabale
-if ($flavor -eq "ITPro") {
+if ($env:flavor -eq "ITPro") {
     if ( $arcNumResources -eq 6 )
     {
         Write-Host "Great success!"
-        az storage message put --content "Successful Jumpstart ArcBox ($flavor) deployment" --account-name "jumpstartusage" --queue-name "arcboxusage" --sas-token "?sv=2020-08-04&ss=q&srt=sco&sp=wa&se=2031-11-01T07:34:50Z&st=2021-10-18T23:34:50Z&spr=https&sig=tQMbCj5EthrwachHMuzLSJ4SgSOtEqqWcAbid6b87vU%3D" --time-to-live -1
+        az storage message put --content "Successful Jumpstart ArcBox ($env:flavor) deployment" --account-name "jumpstartusage" --queue-name "arcboxusage" --sas-token "?sv=2020-08-04&ss=q&srt=sco&sp=wa&se=2031-11-01T07:34:50Z&st=2021-10-18T23:34:50Z&spr=https&sig=tQMbCj5EthrwachHMuzLSJ4SgSOtEqqWcAbid6b87vU%3D" --time-to-live -1
     }
 }
 
 if ( $arcNumResources -cne 11 -and $arcNumResources -cne 6) {
     Write-Host "Too bad, not all Azure Arc resources onboarded"
-    az storage message put --content "Failed Jumpstart ArcBox ($flavor) deployment" --account-name "jumpstartusage" --queue-name "arcboxusage" --sas-token "?sv=2020-08-04&ss=q&srt=sco&sp=wa&se=2031-11-01T07:34:50Z&st=2021-10-18T23:34:50Z&spr=https&sig=tQMbCj5EthrwachHMuzLSJ4SgSOtEqqWcAbid6b87vU%3D" --time-to-live -1
+    az storage message put --content "Failed Jumpstart ArcBox ($env:flavor) deployment" --account-name "jumpstartusage" --queue-name "arcboxusage" --sas-token "?sv=2020-08-04&ss=q&srt=sco&sp=wa&se=2031-11-01T07:34:50Z&st=2021-10-18T23:34:50Z&spr=https&sig=tQMbCj5EthrwachHMuzLSJ4SgSOtEqqWcAbid6b87vU%3D" --time-to-live -1
 }
