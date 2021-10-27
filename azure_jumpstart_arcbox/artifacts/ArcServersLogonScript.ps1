8e3b252af723ffcd95d5be785aa76a21caadbbea
Lior Kamrat <46730876+likamrat@users.noreply.github.com>
2021-10-27 16:53:05 -0700

    Update ArcServersLogonScript.ps1

--- azure_jumpstart_arcbox/artifacts/ArcServersLogonScript.ps1
+++ azure_jumpstart_arcbox/artifacts/ArcServersLogonScript.ps1
@@ -252,7 +252,7 @@ $Command = "sudo sh /home/$nestedLinuxUsername/installArcAgentModifiedCentOS.sh"
 Invoke-SSHCommand -Index $sessionid.sessionid -Command $Command -TimeOut 120 -WarningAction SilentlyContinue | Out-Null
 
 # Sending deployement status message to Azure storage account queue
-if ($flavor -eq "ITPro") {
+if ($env:flavor -eq "ITPro") {
     # Sleeping for allowing Azure Resource Manager API updates
     Start-Sleep -Seconds 30
     & "C:\ArcBox\DeploymentStatus.ps1"
