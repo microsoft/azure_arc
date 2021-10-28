068a45e858d78c81e9a442c900ee12785b48c314
lanicolas <lauranicolas87@outlook.com>
2021-10-28 16:13:04 +0200

    issue

--- azure_arc_k8s_jumpstart/aks_stack_hci/gitops/basic/az_k8sconfig_aks_hci.ps1
+++ azure_arc_k8s_jumpstart/aks_stack_hci/gitops/basic/az_k8sconfig_aks_hci.ps1
@@ -40,7 +40,7 @@ kubectl create ns hello-arc
 az k8s-configuration create `
 --cluster-name $arcClusterName `
 --resource-group $resourceGroup `
---name hello-arc `
+--name cluster-config `
 --operator-instance-name cluster-config --operator-namespace cluster-config `
 --repository-url $appClonedRepo `
 --scope cluster --cluster-type connectedClusters `
