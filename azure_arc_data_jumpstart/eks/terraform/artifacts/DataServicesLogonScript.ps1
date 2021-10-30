222e4ea9dac3cfc2fef9151e0bd4a698e0c80209
Raki Rahman <mdrrahman@microsoft.com>
2021-10-29 20:57:24 -0400

    Rolling back kubeconfig for EKS since it does not work, passing in OID via TF

--- azure_arc_data_jumpstart/eks/terraform/artifacts/DataServicesLogonScript.ps1
+++ azure_arc_data_jumpstart/eks/terraform/artifacts/DataServicesLogonScript.ps1
@@ -91,17 +91,13 @@ Write-Host "`n"
 # Monitor pods across namespaces
 $kubectlMonShell = Start-Process -PassThru PowerShell {for (0 -lt 1) {kubectl get pods --all-namespaces; Start-Sleep -Seconds 5; Clear-Host }}
 
-# Localize kubeconfig
-$env:KUBECONTEXT = kubectl config current-context
-$env:KUBECONFIG = "C:\Users\Administrator\.kube\config"
-
 # Create Kubernetes - Azure Arc Cluster
 az connectedk8s connect --name $connectedClusterName `
                         --resource-group $env:resourceGroup `
                         --location $env:azureLocation `
                         --tags 'Project=jumpstart_azure_arc_data_services' `
-                        --kube-config $env:KUBECONFIG `
-                        --kube-context $env:KUBECONTEXT
+                        --custom-locations-oid $env:customLocationOid
+                        # This is the Custom Locations Enterprise Application ObjectID from AAD
 
 Start-Sleep -Seconds 10
 
@@ -136,8 +132,7 @@ az customlocation create --name 'jumpstart-cl' `
                          --resource-group $env:resourceGroup `
                          --namespace arc `
                          --host-resource-id $connectedClusterId `
-                         --cluster-extension-ids $extensionId `
-                         --kubeconfig $env:KUBECONFIG
+                         --cluster-extension-ids $extensionId
 
 # Deploying Azure Monitor for containers Kubernetes extension instance
 Write-Host "Create Azure Monitor for containers Kubernetes extension instance"
