3343a90b7cbd159bc3dec191652e856d8d1974fd
Deepu K S <deepu@platform9.com>
2021-11-11 19:11:28 +0000

    Changes made as suggested in https://github.com/microsoft/azure_arc/pull/819

--- azure_arc_k8s_jumpstart/pf9/pf9_az_connect_k8s.sh
+++ azure_arc_k8s_jumpstart/pf9/pf9_az_connect_k8s.sh
@@ -4,43 +4,26 @@ if [ $(awk -F= '/^NAME/{print $2}' /etc/os-release | tr -d '"') != "Ubuntu" ]; t
 	echo "Please run this script on an Ubuntu host"
 	exit 1;
 fi
-	
-sudo apt-get update -y
 
-if [ -e "./pf9_az_sp.conf"]; then
-    source ./pf9_az_sp.conf
+if [ -e "./pf9_az.env"]; then
+    . ./pf9_az.env
 else
-    echo "The service principal config file doesn't exist. Please complete the Pre-requisites before running this script";
-    exit 1
+    echo "The pf9_az environment config file doesn't exist. Please complete the Pre-requisites before running this script";
+    exit 1;
 fi
 
-# Installing Azure CLI & Azure Arc Extensions
-echo "Installing Azure CLI & Azure Arc Extensions"
-curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+# Register the Microsoft providers
+az provider register --namespace Microsoft.Kubernetes --wait
+az provider register --namespace Microsoft.KubernetesConfiguration --wait
+az provider register --namespace Microsoft.ExtendedLocation --wait
 
-sudo az extension add --name connectedk8s
-sudo az extension add --name k8s-configuration
-
-# Installing Helm 3
-echo "Installing Helm 3"
-curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
-
+# Installing the Azure Arc Extensions
+echo "Installing the Azure Arc Extensions"
+az extension add --name connectedk8s
+az extension add --name k8s-configuration
 
 echo "Log in to Azure using service principal"
-sudo az login --service-principal --username $appId --password $password --tenant $tenantId
-
-sudo cat <<EOT >> az.sh
-#!/bin/sh
-sudo chown -R $USER /home/${USER}/.kube
-sudo chown -R $USER /home/${USER}/.kube/config
-sudo chown -R $USER /home/${USER}/.azure/config
-sudo chown -R $USER /home/${USER}/.azure
-sudo chmod -R 777 /home/${USER}/.azure/config
-sudo chmod -R 777 /home/${USER}/.azure
-EOT
-sudo chmod +x az.sh
-. ./az.sh
-sudo rm az.sh
+az login --service-principal --username $appId --password $password --tenant $tenantId
 
 echo "Connecting the cluster to Azure Arc"
 az connectedk8s connect --name $arcClusterName --resource-group $resourceGroup
