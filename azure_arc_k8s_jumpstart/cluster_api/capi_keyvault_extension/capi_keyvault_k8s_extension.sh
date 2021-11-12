f1112bd0cf54ecbc8f7b68bb38441a28fd95dc36
Zaid <mozaid@microsoft.com>
2021-11-12 13:24:59 -0500

    Fixed comments

--- azure_arc_k8s_jumpstart/cluster_api/capi_keyvault_extension/capi_keyvault_k8s_extension.sh
+++ azure_arc_k8s_jumpstart/cluster_api/capi_keyvault_extension/capi_keyvault_k8s_extension.sh
@@ -20,15 +20,7 @@ chmod 700 get_helm.sh
 
 # Installing Azure CLI & Azure Arc Extensions
 echo "Installing Azure CLI"
-sudo apt-get update
-sudo apt-get install -y ca-certificates curl apt-transport-https lsb-release gnupg
-curl -sL https://packages.microsoft.com/keys/microsoft.asc |
-gpg --dearmor |
-sudo tee /etc/apt/trusted.gpg.d/microsoft.asc.gpg > /dev/null
-AZ_REPO=$(lsb_release -cs)
-echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" |
-sudo tee /etc/apt/sources.list.d/azure-cli.list
-sudo apt-get install azure-cli
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 
 echo "Clear cached helm Azure Arc Helm Charts"
 rm -rf ~/.azure/AzureArcCharts
