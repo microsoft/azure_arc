21c18f3d9b7ab6e33e46e60ef54194fd62d9698b
Laura Nicolas <51124125+lanicolas@users.noreply.github.com>
2021-11-16 17:14:19 +0100

    Aligned env variables

--- azure_arc_k8s_jumpstart/aks_stack_hci/gitops/helm/az_k8sconfig_helm_aks.ps1
+++ azure_arc_k8s_jumpstart/aks_stack_hci/gitops/helm/az_k8sconfig_helm_aks.ps1
@@ -5,7 +5,7 @@ $appId ='<Your Azure service principal name>'
 $password ='<Your Azure service principal password>'
 $tenantId ='<Your Azure tenant ID>'
 $resourceGroup ='<Azure resource group name>'
-$ClusterName ='<The name of your AKS cluster running on Azure Stack HCI>'
+$clusterName ='<The name of your AKS cluster running on Azure Stack HCI>'
 $appClonedRepo ='<The URL for the "Hello Arc" cloned GitHub repository>'
 $subscriptionId ='<Your subscription ID>'
 
@@ -18,13 +18,13 @@ az account set --subscription $subscriptionId
 az config set extension.use_dynamic_install=yes_without_prompt
 
 Write-Host "Getting AKS on HCI credentials (kubeconfig)"
-Get-AksHciCredential -Name $ClusterName 
+Get-AksHciCredential -Name $clusterName 
 
 # Create Cluster-level GitOps-Config for deploying nginx-ingress
 Write-Host "Create Cluster-level GitOps-Config for deploying nginx-ingress"
 az k8s-configuration create `
 --name nginx-ingress `
---cluster-name $arcClusterName --resource-group $resourceGroup `
+--cluster-name $clusterName --resource-group $resourceGroup `
 --operator-instance-name cluster-mgmt --operator-namespace cluster-mgmt `
 --enable-helm-operator `
 --helm-operator-params='--set helm.versions=v3' `
@@ -36,7 +36,7 @@ az k8s-configuration create `
 Write-Host "Create Namespace-level GitOps-Config for deploying the 'Hello Arc' application"
 az k8s-configuration create `
 --name hello-arc `
---cluster-name $arcClusterName --resource-group $resourceGroup `
+--cluster-name $clusterName --resource-group $resourceGroup `
 --operator-instance-name hello-arc --operator-namespace prod `
 --enable-helm-operator `
 --helm-operator-params='--set helm.versions=v3' `
