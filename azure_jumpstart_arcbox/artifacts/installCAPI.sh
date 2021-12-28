4d492fc5b3dce2757c0748def00703b5038a4133
likamrat <likamrat@microsoft.com>
2021-12-27 18:21:24 -0800

    testing 1.1.0

--- azure_jumpstart_arcbox/artifacts/installCAPI.sh
+++ azure_jumpstart_arcbox/artifacts/installCAPI.sh
@@ -52,9 +52,9 @@ sudo snap install kubectl --classic
 # Set CAPI deployment environment variables
 export CLUSTERCTL_VERSION="1.0.2" # Do not change!
 export CAPI_PROVIDER="azure" # Do not change!
-export CAPI_PROVIDER_VERSION="1.0.1" # Do not change!
+export CAPI_PROVIDER_VERSION="1.1.0" # Do not change!
 export AZURE_ENVIRONMENT="AzurePublicCloud" # Do not change!
-export KUBERNETES_VERSION="1.22.4" # Do not change!
+export KUBERNETES_VERSION="1.22.0" # Do not change!
 export CONTROL_PLANE_MACHINE_COUNT="1"
 export WORKER_MACHINE_COUNT="3"
 export AZURE_LOCATION=$location # Name of the Azure datacenter location.
