361f0a80a5681c89af5385c26605867bf6a79df4
Zaid <mozaid@microsoft.com>
2022-01-10 17:36:20 -0500

    Update based on PR review

--- azure_arc_k8s_jumpstart/cluster_api/gitops/basic/az_k8sconfig_capi.sh
+++ azure_arc_k8s_jumpstart/cluster_api/gitops/basic/az_k8sconfig_capi.sh
@@ -18,16 +18,27 @@ curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scr
 chmod 700 get_helm.sh
 ./get_helm.sh
 
-# Installing Azure CLI & Azure Arc Extensions
+# Installing Azure CLI
 echo "Installing Azure CLI"
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 
+# Installing required Azure Arc CLI extensions
 az extension add --name connectedk8s
 az extension add --name k8s-configuration
 
 # Login to Azure
 echo "Log in to Azure with Service Principal"
 az login --service-principal --username $appId --password $password --tenant $tenantId
+
+# Registering Azure Arc providers
+echo "Registering Azure Arc providers"
+az provider register --namespace Microsoft.Kubernetes --wait
+az provider register --namespace Microsoft.KubernetesConfiguration --wait
+az provider register --namespace Microsoft.ExtendedLocation --wait
+
+az provider show -n Microsoft.Kubernetes -o table
+az provider show -n Microsoft.KubernetesConfiguration -o table
+az provider show -n Microsoft.ExtendedLocation -o table
 
 # Create a namespace for your ingress and app resources
 kubectl create ns $namespace
