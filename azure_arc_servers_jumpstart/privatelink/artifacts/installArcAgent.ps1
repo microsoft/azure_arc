4691e1f22761f47e4e3c5d3b7ba435f3f8c4e6b1
lanicolas <lauranicolas87@outlook.com>
2022-01-12 16:14:06 +0100

    Mat nested

---
+++ azure_arc_servers_jumpstart/privatelink/artifacts/installArcAgent.ps1
@@ -0,0 +1,40 @@
+param(
+    [string]$Token,
+    [string]$Location,
+    [string]$SubscriptionId,
+    [string]$ResourceGroup
+)
+
+Start-Transcript -Path "C:\Temp\installArcAgent.log"
+
+# Block Azure IMDS communication
+New-NetFirewallRule -DisplayName "Block Azure IMDS Service" `
+    -Direction Outbound `
+    -LocalPort Any `
+    -Protocol TCP `
+    -Action Block `
+    -RemoteAddress "169.254.169.254"
+
+# Download the installation package
+Invoke-WebRequest -Uri "https://aka.ms/azcmagent-windows" -TimeoutSec 30 -OutFile "$env:TEMP\install_windows_azcmagent.ps1"
+
+# Install the hybrid agent
+& "$env:TEMP\install_windows_azcmagent.ps1"
+if($LASTEXITCODE -ne 0) {
+    throw "Failed to install the hybrid agent"
+}
+
+# Run connect command
+& "$env:ProgramW6432\AzureConnectedMachineAgent\azcmagent.exe" connect `
+    --access-token $Token `
+    --location $Location `
+    --subscription-id $SubscriptionId `
+    --resource-group $ResourceGroup `
+    --cloud "AzureCloud" `
+    --tags "Project=jumpstart_arcbox"
+
+if($LastExitCode -eq 0){
+    Write-Host -ForegroundColor yellow "To view your onboarded server(s), navigate to https://portal.azure.com/#blade/HubsExtension/BrowseResource/resourceType/Microsoft.HybridCompute%2Fmachines"
+}
+
+Stop-Transcript
