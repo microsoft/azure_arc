f7bc4db2a08c56291a62f993944dfd721f32cb7e
Travis Wright <twright@microsoft.com>
2022-02-25 01:18:06 -0800

    Updates for Feb 2022 release

--- arc_data_services/deploy/scripts/pull-and-push-arc-data-services-images-to-private-registry.py
+++ arc_data_services/deploy/scripts/pull-and-push-arc-data-services-images-to-private-registry.py
@@ -36,7 +36,7 @@ else:
     SOURCE_DOCKER_PASSWORD = os.environ["SOURCE_DOCKER_PASSWORD"]
 
 if (os.getenv("SOURCE_DOCKER_TAG") is None):
-    SOURCE_DOCKER_TAG = input("Provide container image tag for the images at the source - press ENTER for using 'v1.3.0_2022-01-27': ") or "v1.3.0_2022-01-27"
+    SOURCE_DOCKER_TAG = input("Provide container image tag for the images at the source - press ENTER for using 'v1.4.0_2022-02-25': ") or "v1.4.0_2022-02-25"
 else:
     SOURCE_DOCKER_TAG = os.environ["SOURCE_DOCKER_TAG"]
 
