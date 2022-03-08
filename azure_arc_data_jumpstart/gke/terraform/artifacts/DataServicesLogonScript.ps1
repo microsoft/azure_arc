27e7fd06424ef9c350f5f82618f303844b223ea0
likamrat <likamrat@microsoft.com>
2022-03-08 11:11:22 -0800

    testing CNI + API versions

--- azure_arc_data_jumpstart/gke/terraform/artifacts/DataServicesLogonScript.ps1
+++ azure_arc_data_jumpstart/gke/terraform/artifacts/DataServicesLogonScript.ps1
@@ -83,8 +83,8 @@ Write-Host "`n"
 kubectl get nodes
 Write-Host "`n"
 
-# Onboarding the GKE cluster as an Azure Arc enabled Kubernetes cluster
-Write-Host "Onboarding the cluster as an Azure Arc enabled Kubernetes cluster"
+# Onboarding the GKE cluster as an Azure Arc-enabled Kubernetes cluster
+Write-Host "Onboarding the cluster as an Azure Arc-enabled Kubernetes cluster"
 Write-Host "`n"
 
 # Monitor pods across namespaces
@@ -104,7 +104,7 @@ az connectedk8s connect --name $connectedClusterName `
 
 Start-Sleep -Seconds 10
 
-# Create Azure Arc enabled Data Services extension
+# Create Azure Arc-enabled Data Services extension
 az k8s-extension create --name arc-data-services `
                         --extension-type microsoft.arcdataservices `
                         --cluster-type connectedClusters `
