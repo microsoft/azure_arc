766fca8a15829b14575a99f3abd555f9d3788550
Seif Bassem <38246040+sebassem@users.noreply.github.com>
2022-03-10 17:23:05 +0200

    added logging

--- azure_arc_k8s_jumpstart/aro/arm_template/scripts/az_connect_aro.sh
+++ azure_arc_k8s_jumpstart/aro/arm_template/scripts/az_connect_aro.sh
@@ -1,4 +1,8 @@
-#!/bin/sh
+#!/bin/bash
+
+# Enable logging
+exec >onboardARO.log
+exec 2>&1
 
 # <--- Change the following environment variables according to your Azure service principal name --->
 
@@ -8,12 +12,16 @@ export AZURE_CLIENT_SECRET='<Azure SPN application client secret>'
 export AZURE_TENANT_ID="<Azure tenant id>"
 export AZURE_RESOURCE_GROUP='<AZURE_RESOURCE_GROUP>'
 export AZURE_ARC_CLUSTER_RESOURCE_NAME="<Azure Arc-enabled Kubernetes cluster resource name>" # Name of the Azure Arc-enabled Kubernetes cluster resource name as it will shown in the Azure portal
+echo ""
 
 # Getting ARO cluster credentials
 echo "Log in to Azure with Service Principle & Getting ARO credentials (kubeconfig)"
 az login --service-principal --username $AZURE_CLIENT_ID --password $AZURE_CLIENT_SECRET --tenant $AZURE_TENANT_ID
-kubcepass=$(az aro list-credentials --name $CLUSTER_NAME -g $AZURE_RESOURCE_GROUP --query kubeadminPassword -o tsv)
+echo ""
+kubcepass=$(az aro list-credentials --name $AZURE_ARC_CLUSTER_RESOURCE_NAME -g $AZURE_RESOURCE_GROUP --query kubeadminPassword -o tsv)
+echo ""
 rm -rf ~/.azure/AzureArcCharts
+echo ""
 
 # Installing Azure Arc k8s CLI extensions
 echo "Checking if you have up-to-date Azure Arc AZ CLI 'connectedk8s' extension..."
@@ -49,16 +57,21 @@ fi
 echo ""
 
 # Install ARO CLI
+echo "Installing the ARO CLI..."
 cd ~
 wget https://mirror.openshift.com/pub/openshift-v4/clients/ocp/latest/openshift-client-linux.tar.gz
+echo ""
 mkdir openshift
 tar -zxvf openshift-client-linux.tar.gz -C openshift
 echo 'export PATH=$PATH:~/openshift' >> ~/.bashrc && source ~/.bashrc
-apiServer=$(az aro show -g $AZURE_RESOURCE_GROUP -n $CLUSTER_NAME --query apiserverProfile.url -o tsv)
-oc login $apiServer -u kubeadmin -p $kubcepass
+echo ""
 
-# OpenShift prep before connecting
+echo "Logging into the ARO cluster..."
+apiServer=$(az aro show -g $AZURE_RESOURCE_GROUP -n $AZURE_ARC_CLUSTER_RESOURCE_NAME --query apiserverProfile.url -o tsv)
+oc login $apiServer -u kubeadmin -p $kubcepass
+# Openshift prep before connecting
 oc adm policy add-scc-to-user privileged system:serviceaccount:azure-arc:azure-arc-kube-aad-proxy-sa
+echo ""
 
 #Getting thre ARO context
 apiServerURI="${apiServer#https://}"
@@ -66,7 +79,6 @@ clusterName="${apiServerURI//[.]/-}"
 user="kube:admin"
 context="default/$clusterName$user"
 
-# Connecting the cluster to Azure Arc
 echo "Connecting the cluster to Azure Arc"
-az connectedk8s connect --name $CLUSTER_NAME --resource-group $AZURE_RESOURCE_GROUP --location 'eastus' --tags 'Project=jumpstart_azure_arc_k8s' --kube-context $context
-
+az connectedk8s connect --name $AZURE_ARC_CLUSTER_RESOURCE_NAME --resource-group $AZURE_RESOURCE_GROUP --location 'eastus' --tags 'Project=jumpstart_azure_arc_k8s' --kube-context $context
+echo ""
