3c2d13c6a1e6b6e6e8bc5117323ac48fe15a6af5
likamrat <likamrat@microsoft.com>
2022-03-14 17:06:01 -0700

    fix az ordering

--- azure_arc_app_services_jumpstart/aks/ARM/artifacts/deployAppService.ps1
+++ azure_arc_app_services_jumpstart/aks/ARM/artifacts/deployAppService.ps1
@@ -2,10 +2,10 @@ Start-Transcript -Path C:\Temp\deployAppService.log
 
 Write-Host "Creating App Service plan. Hold tight, this might take a few minutes..."
 Write-Host "`n"
-$customLocationId = $(az customlocation show --name "jumpstart-cl" --resource-group $env:resourceGroup --query id -o tsv)
-az appservice plan create --resource-group $env:resourceGroup --name Jumpstart --custom-location $customLocationId --per-site-scaling --is-linux --sku K1
+$customLocationId = $(az customlocation show --name "jumpstart-cl" --resource-group $Env:resourceGroup --query id -o tsv)
+az appservice plan create --resource-group $Env:resourceGroup --name Jumpstart --custom-location $customLocationId --per-site-scaling --is-linux --sku K1
 
 Write-Host "Deploy a sample Azure Arc Jumpstart web application"
 Write-Host "`n"
-az webapp create --plan Jumpstart --resource-group $env:resourceGroup --name jumpstart-app --custom-location $customLocationId --deployment-container-image-name azurearcjumpstart.azurecr.io/hello-arc:latest
-az webapp config appsettings set --resource-group $env:resourceGroup --name jumpstart-app --settings WEBSITES_PORT=8080
+az webapp create --plan Jumpstart --resource-group $Env:resourceGroup --name jumpstart-app --custom-location $customLocationId --deployment-container-image-name azurearcjumpstart.azurecr.io/hello-arc:latest
+az webapp config appsettings set --resource-group $Env:resourceGroup --name jumpstart-app --settings WEBSITES_PORT=8080
