6ad8b3ed3eeb85b782080dbc0a099f479128b1b7
lanicolas <lauranicolas87@outlook.com>
2022-03-17 16:29:13 +0100

    token remove

---
+++ azure_arc_servers_jumpstart/local/vagrant/windows/vars.ps1
@@ -0,0 +1,9 @@
+
+# <--- Change the following environment variables according to your Azure service principal name --->
+
+$env:subscriptionId="5af250db-aef1-46f9-8296-7a373a4b2801"
+$env:appId="c4d061d5-a7f5-4eef-8e6f-de5a1e4fe044"
+$env:password="HTer4U~-DKLW4-VArn5okMw~xjODnn~oce"
+$env:tenantId="72f988bf-86f1-41af-91ab-2d7cd011db47"
+$env:resourceGroup="arc-pl-demo"
+$env:location="westeurope"
