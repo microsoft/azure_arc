9c3250a23aa0e1db437d5b6ad3bea5415a87a740
lanicolas <lauranicolas87@outlook.com>
2022-03-18 12:46:12 +0100

    token remove

--- azure_arc_servers_jumpstart/privatelink/artifacts/installArcAgent.ps1
+++ azure_arc_servers_jumpstart/privatelink/artifacts/installArcAgent.ps1
@@ -8,42 +8,45 @@ param (
     [string]$PLscope
 )
 
-Start-Transcript -Path "C:\Temp\installArcAgent.log"
+param (
+    [string]$subscriptionId,
+    [string]$appId,
+    [string]$password,
+    [string]$tenantId,
+    [string]$resourceGroup,
+    [string]$location,
+    [string]$PLscope
+)
 
-# Block Azure IMDS communication
-#New-NetFirewallRule -DisplayName "Block Azure IMDS Service" `
-#    -Direction Outbound `
-#    -LocalPort Any `
-#    -Protocol TCP `
-#    -Action Allow `
-#    -RemoteAddress "169.254.169.254"
+New-Item -Path "C:\" -Name "tmp" -ItemType "directory" -Force
 
-# Download the installation package
-Invoke-WebRequest -Uri "https://aka.ms/azcmagent-windows" -TimeoutSec 30 -OutFile "$env:TEMP\install_windows_azcmagent.ps1"
+## Configure the OS to allow Azure Arc Agent to be deploy on an Azure VM
 
-# Install the hybrid agent
-& "$env:TEMP\install_windows_azcmagent.ps1"
-if($LASTEXITCODE -ne 0) {
-    throw "Failed to install the hybrid agent"
-}
+Write-Host "Configure the OS to allow Azure Arc Agent to be deploy on an Azure VM"
+Set-Service WindowsAzureGuestAgent -StartupType Disabled -Verbose
+Stop-Service WindowsAzureGuestAgent -Force -Verbose
+New-NetFirewallRule -Name BlockAzureIMDS -DisplayName "Block access to Azure IMDS" -Enabled True -Profile Any -Direction Outbound -Action Block -RemoteAddress 169.254.169.254 
+
+## Azure Arc agent Installation
+
+Write-Host "Onboarding to Azure Arc"
+# Download the package
+function download() {$ProgressPreference="SilentlyContinue"; Invoke-WebRequest -Uri https://aka.ms/AzureConnectedMachineAgent -OutFile AzureConnectedMachineAgent.msi}
+download
+
+# Install the package
+msiexec /i AzureConnectedMachineAgent.msi /l*v installationlog.txt /qn | Out-String
 
 # Run connect command
 & "$env:ProgramW6432\AzureConnectedMachineAgent\azcmagent.exe" connect `
-    --service-principal-id $appId `
-    --service-principal-secret $password `
-    --location $Location `
-    --tenant-id $tenantId `
-    --subscription-id $SubscriptionId `
-    --resource-group $ResourceGroup `
-    --cloud "AzureCloud" `
-    --private-link-scope $PLscope `
-    --tags "Project=jumpstart_azure_arc_servers" `
-    --correlation-id "86501baa-0b82-478c-b3cf-620533617001"
+--service-principal-id $appId `
+--service-principal-secret $password `
+--location $Location `
+--tenant-id $tenantId `
+--subscription-id $SubscriptionId `
+--resource-group $ResourceGroup `
+--cloud "AzureCloud" `
+--private-link-scope $PLscope `
+--tags "Project=jumpstart_azure_arc_servers" `
+--correlation-id "86501baa-0b82-478c-b3cf-620533617001"
 
-
-
-if($LastExitCode -eq 0){
-    Write-Host -ForegroundColor yellow "To view your onboarded server(s), navigate to https://portal.azure.com/#blade/HubsExtension/BrowseResource/resourceType/Microsoft.HybridCompute%2Fmachines"
-}
-
-Stop-Transcript
