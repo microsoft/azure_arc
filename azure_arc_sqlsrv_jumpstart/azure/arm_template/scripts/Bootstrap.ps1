b7f995ab6789200af73ef5db9f1448ea2e0deb63
Matthew Garrett <magarret@microsoft.com>
2022-03-18 21:54:54 -0700

    Inital script and template refresh

---
+++ azure_arc_sqlsrv_jumpstart/azure/arm_template/scripts/Bootstrap.ps1
@@ -0,0 +1,76 @@
+﻿param (
+    [string]$subscriptionId,
+    [string]$servicePrincipalAppId,
+    [string]$servicePrincipalSecret,
+    [string]$servicePrincipalTenantId,
+    [string]$resourceGroup,
+    [string]$location,
+    [string]$adminUsername,
+    [string]$adminPassword,
+    [string]$workspaceName,
+    [string]$templateBaseUrl
+)
+
+[System.Environment]::SetEnvironmentVariable('subscriptionId', $subscriptionId,[System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable('servicePrincipalAppId', $servicePrincipalAppId,[System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable('servicePrincipalSecret', $servicePrincipalSecret,[System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable('servicePrincipalTenantId', $servicePrincipalTenantId,[System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable('resourceGroup', $resourceGroup,[System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable('location', $location,[System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable('adminUsername', $adminUsername,[System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable('adminPassword', $adminPassword,[System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable('workspaceName', $workspaceName,[System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable('templateBaseUrl', $templateBaseUrl,[System.EnvironmentVariableTarget]::Machine)
+
+# Creating ArcBox path
+Write-Output "Creating Jumpstart path"
+
+$Env:JumpstartDir = "C:\Jumpstart"
+$Env:JumpstartLogsDir = "C:\Jumpstart\Logs"
+$Env:JumpstartScriptDir = "C:\Jumpstart\agentScript"
+$Env:JumpstartTempDir = "C:\Temp"
+
+New-Item -Path $Env:JumpstartDir -ItemType directory -Force
+New-Item -Path $Env:JumpstartLogsDir -ItemType directory -Force
+New-Item -Path $Env:JumpstartScriptDir -ItemType directory -Force
+New-Item -Path $Env:JumpstartTempDir -ItemType directory -Force
+
+Start-Transcript -Path $Env:JumpstartLogsDir\Bootstrap.log
+
+$ErrorActionPreference = 'SilentlyContinue'
+
+# Installing tools
+$chocolateyAppList = "az.powershell,azure-cli,sql-server-management-studio"
+
+try{
+    choco config get cacheLocation
+}catch{
+    Write-Output "Chocolatey not detected, trying to install now"
+    Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+}
+
+Write-Host "Installing specified Chocolatey apps..."  
+
+$appsToInstall = $chocolateyAppList -split "," | foreach { "$($_.Trim())" }
+
+foreach ($app in $appsToInstall)
+{
+    Write-Host "Installing $app"
+    & choco install $app /y -Force | Write-Output
+}
+
+# Downloading artifacts & enabling Fusion logging
+Invoke-WebRequest ($templateBaseUrl + "arm_template/scripts/LogonScript.ps1") -OutFile $Env:JumpstartScriptDir\LogonScript.ps1
+Invoke-WebRequest ($templateBaseUrl + "arm_template/scripts/installArcAgentSQL.ps1") -OutFile $Env:JumpstartScriptDir\installArcAgentSQL.ps1
+
+New-ItemProperty "HKLM:\SOFTWARE\Microsoft\Fusion" -Name "EnableLog" -Value 1 -PropertyType "DWord"
+
+(Get-Content -path "$Env:JumpstartScriptDir\installArcAgentSQL.ps1" -Raw) -replace '\$spnClientId',"'$Env:servicePrincipalAppId'" -replace '\$spnClientSecret',"'$Env:servicePrincipalSecret'" -replace '\$myResourceGroup',"'$Env:resourceGroup'" -replace '\$spnTenantId',"'$Env:servicePrincipalTenantId'" -replace '\$azureLocation',"'$Env:location'" -replace '\$subscriptionId',"'$Env:subscriptionId'" -replace '\$logAnalyticsWorkspaceName',"'$Env:workspaceName'" | Set-Content -Path "$Env:JumpstartScriptDir\installArcAgentSQLModified.ps1"
+
+# Creating LogonScript Windows Scheduled Task
+$Trigger = New-ScheduledTaskTrigger -AtLogOn
+$Action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument $Env:JumpstartScriptDir\LogonScript.ps1
+Register-ScheduledTask -TaskName "LogonScript" -Trigger $Trigger -User $adminUsername -Action $Action -RunLevel "Highest" -Force
+
+# Disabling Windows Server Manager Scheduled Task
+Get-ScheduledTask -TaskName ServerManager | Disable-ScheduledTask
---
+++ azure_arc_sqlsrv_jumpstart/azure/arm_template/scripts/LogonScript.ps1
@@ -0,0 +1,62 @@
+﻿$Env:JumpstartDir = "C:\Jumpstart"
+$Env:JumpstartLogsDir = "C:\Jumpstart\Logs"
+$Env:JumpstartScriptDir = "C:\Jumpstart\agentScript"
+$Env:JumpstartTempDir = "C:\Temp"
+
+Start-Transcript -Path $Env:JumpstartLogsDir\LogonScript.log
+
+Write-Host "Installing ConnectedMachine PowerShell Module"
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+
+Write-Host "Installing SQL Server and PowerShell Module"
+If(-not(Get-InstalledModule SQLServer -ErrorAction silentlycontinue)){
+    Install-Module SQLServer -Confirm:$False -Force
+}
+
+choco install sql-server-2019 -y --params="'/IgnorePendingReboot /INSTANCENAME=MSSQLSERVER'"
+
+Set-Service -Name SQLBrowser -StartupType Automatic
+Start-Service -Name SQLBrowser
+
+$env:PSModulePath = $env:PSModulePath + ";C:\Program Files (x86)\Microsoft SQL Server\150\Tools\PowerShell\Modules"
+Import-Module -Name "sqlps"
+
+$wmi = New-Object Microsoft.SqlServer.Management.Smo.Wmi.ManagedComputer # connects to localhost by default
+$instance = $wmi.ServerInstances | Where-Object { $_.Name -eq 'MSSQLSERVER' }
+$wmi
+
+$tcp = $instance.ServerProtocols | Where-Object { $_.Name -eq 'Tcp' }
+$tcp.IsEnabled = $true
+$tcp.Alter()
+$tcp
+
+$np = $instance.ServerProtocols | Where-Object { $_.Name -eq 'Np' }
+$np.IsEnabled = $true
+$np.Alter()
+$np
+
+Restart-Service -Name 'MSSQLSERVER'
+
+# Download and restore AdventureWorks Database
+Write-Host "Restoring AdventureWorks database"
+Invoke-WebRequest ($Env:templateBaseUrl + "arm_template/scripts/AdventureWorksLT2019.bak") -OutFile $Env:JumpstartTempDir\AdventureWorksLT2019.bak
+Start-Sleep -Seconds 3
+Restore-SqlDatabase -ServerInstance $Env:COMPUTERNAME -Database "AdventureWorksLT2019" -BackupFile $Env:JumpstartTempDir\AdventureWorksLT2019.bak -PassThru -Verbose
+
+Write-Host "Creating SQL Server Management Studio Desktop shortcut"
+$TargetFile = "C:\Program Files (x86)\Microsoft SQL Server Management Studio 18\Common7\IDE\Ssms.exe"
+$ShortcutFile = "C:\Users\$Env:USERNAME\Desktop\Microsoft SQL Server Management Studio.lnk"
+$WScriptShell = New-Object -ComObject WScript.Shell
+$Shortcut = $WScriptShell.CreateShortcut($ShortcutFile)
+$Shortcut.TargetPath = $TargetFile
+$Shortcut.Save()
+
+# Configure the ArcBox Hyper-V host to allow the nested VMs onboard as Azure Arc-enabled servers
+Write-Output "Configuring the VM to allow onboarding as an Azure Arc-enabled server"
+Set-Service WindowsAzureGuestAgent -StartupType Disabled -Verbose
+Stop-Service WindowsAzureGuestAgent -Force -Verbose
+New-NetFirewallRule -Name BlockAzureIMDS -DisplayName "Block access to Azure IMDS" -Enabled True -Profile Any -Direction Outbound -Action Block -RemoteAddress 169.254.169.254
+
+Start-Process powershell.exe -ArgumentList $Env:JumpstartScriptDir\installArcAgentSQLModified.ps1
+
+Unregister-ScheduledTask -TaskName "LogonScript" -Confirm:$False
