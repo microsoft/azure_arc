2054677d9364deb86c5c1df2277d42705475ec3e
lanicolas <lauranicolas87@outlook.com>
2022-03-21 16:54:40 +0100

    updated script

--- azure_arc_servers_jumpstart/privatelink/artifacts/installArcAgent.ps1
+++ azure_arc_servers_jumpstart/privatelink/artifacts/installArcAgent.ps1
@@ -8,11 +8,26 @@ param (
     [string]$PLscope
 )
 
-New-Item -Path "C:\" -Name "tmp" -ItemType "directory" -Force
 
-# Creating PowerShell Logon Script
-$LogonScript = @'
-Start-Transcript -Path C:\tmp\LogonScript.log
+$file = "C:\Windows\System32\drivers\etc\hosts"
+$gisfqdn=(az network private-endpoint dns-zone-group list --endpoint-name Arc-PE-demo --resource-group Arc-Azure-PL-demo --query [0].privateDnsZoneConfigs[0].recordSets[0].fqdn -o json).replace('.privatelink','').replace("`"","")
+$gisIP= (az network private-endpoint dns-zone-group list --endpoint-name Arc-PE-demo --resource-group Arc-Azure-PL-demo --query [0].privateDnsZoneConfigs[0].recordSets[0].ipAddresses[0] -o json).replace("`"","")
+$hisfqdn=(az network private-endpoint dns-zone-group list --endpoint-name Arc-PE-demo --resource-group Arc-Azure-PL-demo --query [0].privateDnsZoneConfigs[0].recordSets[1].fqdn -o json).replace('.privatelink','').replace("`"","")
+$hisIP=(az network private-endpoint dns-zone-group list --endpoint-name Arc-PE-demo --resource-group Arc-Azure-PL-demo --query [0].privateDnsZoneConfigs[0].recordSets[1].ipAddresses[0] -o json).replace('.privatelink','').replace("`"","")
+$agentfqdn=(az network private-endpoint dns-zone-group list --endpoint-name Arc-PE-demo --resource-group Arc-Azure-PL-demo --query [0].privateDnsZoneConfigs[1].recordSets[0].fqdn -o json).replace('.privatelink','').replace("`"","")
+$agentIp=(az network private-endpoint dns-zone-group list --endpoint-name Arc-PE-demo --resource-group Arc-Azure-PL-demo --query [0].privateDnsZoneConfigs[1].recordSets[0].ipAddresses[0] -o json).replace('.privatelink','').replace("`"","")
+$gasfqdn=(az network private-endpoint dns-zone-group list --endpoint-name Arc-PE-demo --resource-group Arc-Azure-PL-demo --query [0].privateDnsZoneConfigs[1].recordSets[1].fqdn -o json).replace('.privatelink','').replace("`"","")
+$gasIp=(az network private-endpoint dns-zone-group list --endpoint-name Arc-PE-demo --resource-group Arc-Azure-PL-demo --query [0].privateDnsZoneConfigs[1].recordSets[1].ipAddresses[0] -o json).replace('.privatelink','').replace("`"","")
+$dpfqdn=(az network private-endpoint dns-zone-group list --endpoint-name Arc-PE-demo --resource-group Arc-Azure-PL-demo --query [0].privateDnsZoneConfigs[2].recordSets[0].fqdn -o json).replace('.privatelink','').replace("`"","")
+$dpIp=(az network private-endpoint dns-zone-group list --endpoint-name Arc-PE-demo --resource-group Arc-Azure-PL-demo --query [0].privateDnsZoneConfigs[2].recordSets[0].ipAddresses[0] -o json).replace('.privatelink','').replace("`"","")
+$hostfile = Get-Content $file
+$hostfile += "$gisIP $gisfqdn"
+$hostfile += "$hisIP $hisfqdn"
+$hostfile += "$agentIP $agentfqdn"
+$hostfile += "$gasIP $gasfqdn"
+$hostfile += "$dpIP $dpfqdn"
+Set-Content -Path $file -Value $hostfile -Force
+
 
 ## Configure the OS to allow Azure Arc Agent to be deploy on an Azure VM
 
@@ -28,6 +43,7 @@ Write-Host "Onboarding to Azure Arc"
 function download() {$ProgressPreference="SilentlyContinue"; Invoke-WebRequest -Uri https://aka.ms/AzureConnectedMachineAgent -OutFile AzureConnectedMachineAgent.msi}
 download
 
+
 # Install the package
 msiexec /i AzureConnectedMachineAgent.msi /l*v installationlog.txt /qn | Out-String
 
@@ -38,20 +54,9 @@ msiexec /i AzureConnectedMachineAgent.msi /l*v installationlog.txt /qn | Out-Str
 --location $Location `
 --tenant-id $tenantId `
 --subscription-id $SubscriptionId `
---resource-group $ResourceGroup `
+--resource-group $resourceGroup `
 --cloud "AzureCloud" `
 --private-link-scope $PLscope `
 --tags "Project=jumpstart_azure_arc_servers" `
 --correlation-id "86501baa-0b82-478c-b3cf-620533617001"
 
-Unregister-ScheduledTask -TaskName "LogonScript" -Confirm:$False
-Stop-Process -Name powershell -Force
-'@ > C:\tmp\LogonScript.ps1
-
-# Creating LogonScript Windows Scheduled Task
-$Trigger = New-ScheduledTaskTrigger -AtLogOn
-$Action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument 'C:\tmp\LogonScript.ps1'
-Register-ScheduledTask -TaskName "LogonScript" -Trigger $Trigger -User "${adminUsername}" -Action $Action -RunLevel "Highest" -Force
-
-# Disabling Windows Server Manager Scheduled Task
-Get-ScheduledTask -TaskName ServerManager | Disable-ScheduledTask
