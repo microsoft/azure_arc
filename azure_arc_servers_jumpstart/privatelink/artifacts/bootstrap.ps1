50a554b82362725cd4efab59ffe1fec2d63d9c3f
lanicolas <lauranicolas87@outlook.com>
2022-03-22 18:36:27 +0100

    install Arc

---
+++ azure_arc_servers_jumpstart/privatelink/artifacts/bootstrap.ps1
@@ -0,0 +1,71 @@
+param (
+    [string]$appId,
+    [string]$password,
+    [string]$tenantId,
+    [string]$resourceGroup,
+    [string]$subscriptionId,
+    [string]$Location,
+    [string]$PEname, 
+    [string]$adminUsername 
+
+)
+[System.Environment]::SetEnvironmentVariable('appId', $appId,[System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable('password', $password,[System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable('tenantId', $tenantId,[System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable('resourceGroup', $resourceGroup,[System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable('subscriptionId', $subscriptionId,[System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable('Location', $location,[System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable('PEname', $PEname,[System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable('adminUsername', $adminUsername,[System.EnvironmentVariableTarget]::Machine)
+
+
+New-Item -Path "C:\" -Name "tmp" -ItemType "directory" -Force
+$LogonScript = @'
+Start-Transcript -Path C:\tmp\LogonScript.log
+
+workflow ClientTools_01
+        {
+            $chocolateyAppList = 'azure-cli,az.powershell'
+            InlineScript {
+                param (
+                    [string]$chocolateyAppList
+                )
+                if ([string]::IsNullOrWhiteSpace($using:chocolateyAppList) -eq $false)
+                {
+                    try{
+                        choco config get cacheLocation
+                    }catch{
+                        Write-Output "Chocolatey not detected, trying to install now"
+                        Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+                    }
+                }
+                if ([string]::IsNullOrWhiteSpace($using:chocolateyAppList) -eq $false){   
+                    Write-Host "Chocolatey Apps Specified"  
+                    
+                    $appsToInstall = $using:chocolateyAppList -split "," | foreach { "$($_.Trim())" }
+                
+                    foreach ($app in $appsToInstall)
+                    {
+                        Write-Host "Installing $app"
+                        & choco install $app /y -Force| Write-Output
+                    }
+                }                        
+            }
+        }
+ClientTools_01 | Format-Table
+
+Invoke-WebRequest ("https://raw.githubusercontent.com/lanicolas/azure_arc/privatelink/azure_arc_servers_jumpstart/privatelink/artifacts/installArcAgent.ps1") -OutFile C:\tmp\installArcAgent.ps1
+
+& "C:\tmp\installArcAgent.ps1"
+
+Unregister-ScheduledTask -TaskName "LogonScript" -Confirm:$False
+Stop-Process -Name powershell -Force
+'@ > C:\tmp\LogonScript.ps1
+
+# Creating LogonScript Windows Scheduled Task
+$Trigger = New-ScheduledTaskTrigger -AtLogOn
+$Action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument 'C:\tmp\LogonScript.ps1'
+Register-ScheduledTask -TaskName "LogonScript" -Trigger $Trigger -User "${adminUsername}" -Action $Action -RunLevel "Highest" -Force
+
+# Disabling Windows Server Manager Scheduled Task
+Get-ScheduledTask -TaskName ServerManager | Disable-ScheduledTask
--- azure_arc_servers_jumpstart/privatelink/artifacts/installArcAgent.ps1
+++ azure_arc_servers_jumpstart/privatelink/artifacts/installArcAgent.ps1
@@ -1,38 +1,20 @@
-param (
-    [string]$appId,
-    [string]$password,
-    [string]$tenantId,
-    [string]$resourceGroup,
-    [string]$subscriptionId,
-    [string]$Location,
-    [string]$PEname 
-
-)
-[System.Environment]::SetEnvironmentVariable('appId', $appId,[System.EnvironmentVariableTarget]::Machine)
-[System.Environment]::SetEnvironmentVariable('password', $password,[System.EnvironmentVariableTarget]::Machine)
-[System.Environment]::SetEnvironmentVariable('tenantId', $tenantId,[System.EnvironmentVariableTarget]::Machine)
-[System.Environment]::SetEnvironmentVariable('resourceGroup', $resourceGroup,[System.EnvironmentVariableTarget]::Machine)
-[System.Environment]::SetEnvironmentVariable('subscriptionId', $subscriptionId,[System.EnvironmentVariableTarget]::Machine)
-[System.Environment]::SetEnvironmentVariable('Location', $location,[System.EnvironmentVariableTarget]::Machine)
-[System.Environment]::SetEnvironmentVariable('PEname', $PEname,[System.EnvironmentVariableTarget]::Machine)
-
-
+New-Item -Path "C:\" -Name "tmp" -ItemType "directory" -Force
 $LogonScript = @'
-Start-Transcript -Path C:\tmp\LogonScript.log
-
+Start-Transcript -Path C:\tmp\LogonScript_Install.log
 
 #Configure hosts file for PL 
 $file = "C:\Windows\System32\drivers\etc\hosts"
-$gisfqdn=(az network private-endpoint dns-zone-group list --endpoint-name $PEname --resource-group $resourceGroup --query [0].privateDnsZoneConfigs[0].recordSets[0].fqdn -o json).replace('.privatelink','').replace("`"","")
-$gisIP= (az network private-endpoint dns-zone-group list --endpoint-name $PEname --resource-group $resourceGroup --query [0].privateDnsZoneConfigs[0].recordSets[0].ipAddresses[0] -o json).replace("`"","")
-$hisfqdn=(az network private-endpoint dns-zone-group list --endpoint-name $PEname --resource-group $resourceGroup --query [0].privateDnsZoneConfigs[0].recordSets[1].fqdn -o json).replace('.privatelink','').replace("`"","")
-$hisIP=(az network private-endpoint dns-zone-group list --endpoint-name $PEname --resource-group $resourceGroup --query [0].privateDnsZoneConfigs[0].recordSets[1].ipAddresses[0] -o json).replace('.privatelink','').replace("`"","")
-$agentfqdn=(az network private-endpoint dns-zone-group list --endpoint-name $PEname --resource-group $resourceGroup --query [0].privateDnsZoneConfigs[1].recordSets[0].fqdn -o json).replace('.privatelink','').replace("`"","")
-$agentIp=(az network private-endpoint dns-zone-group list --endpoint-name $PEname --resource-group $resourceGroup --query [0].privateDnsZoneConfigs[1].recordSets[0].ipAddresses[0] -o json).replace('.privatelink','').replace("`"","")
-$gasfqdn=(az network private-endpoint dns-zone-group list --endpoint-name $PEname --resource-group $resourceGroup --query [0].privateDnsZoneConfigs[1].recordSets[1].fqdn -o json).replace('.privatelink','').replace("`"","")
-$gasIp=(az network private-endpoint dns-zone-group list --endpoint-name $PEname --resource-group $resourceGroup --query [0].privateDnsZoneConfigs[1].recordSets[1].ipAddresses[0] -o json).replace('.privatelink','').replace("`"","")
-$dpfqdn=(az network private-endpoint dns-zone-group list --endpoint-name $PEname --resource-group $resourceGroup --query [0].privateDnsZoneConfigs[2].recordSets[0].fqdn -o json).replace('.privatelink','').replace("`"","")
-$dpIp=(az network private-endpoint dns-zone-group list --endpoint-name $PEname --resource-group $resourceGroup --query [0].privateDnsZoneConfigs[2].recordSets[0].ipAddresses[0] -o json).replace('.privatelink','').replace("`"","")
+$command = (az network private-endpoint dns-zone-group list --endpoint-name $Env:PEname --resource-group $Env:resourceGroup --query)
+$gisfqdn=($command [0].privateDnsZoneConfigs[0].recordSets[0].fqdn -o json).replace('.privatelink','').replace("`"","")
+$gisIP= ($command [0].privateDnsZoneConfigs[0].recordSets[0].ipAddresses[0] -o json).replace("`"","")
+$hisfqdn=($command [0].privateDnsZoneConfigs[0].recordSets[1].fqdn -o json).replace('.privatelink','').replace("`"","")
+$hisIP=($command [0].privateDnsZoneConfigs[0].recordSets[1].ipAddresses[0] -o json).replace('.privatelink','').replace("`"","")
+$agentfqdn=($command [0].privateDnsZoneConfigs[1].recordSets[0].fqdn -o json).replace('.privatelink','').replace("`"","")
+$agentIp=($command [0].privateDnsZoneConfigs[1].recordSets[0].ipAddresses[0] -o json).replace('.privatelink','').replace("`"","")
+$gasfqdn=($command [0].privateDnsZoneConfigs[1].recordSets[1].fqdn -o json).replace('.privatelink','').replace("`"","")
+$gasIp=($command [0].privateDnsZoneConfigs[1].recordSets[1].ipAddresses[0] -o json).replace('.privatelink','').replace("`"","")
+$dpfqdn=($command [0].privateDnsZoneConfigs[2].recordSets[0].fqdn -o json).replace('.privatelink','').replace("`"","")
+$dpIp=($command [0].privateDnsZoneConfigs[2].recordSets[0].ipAddresses[0] -o json).replace('.privatelink','').replace("`"","")
 $hostfile = Get-Content $file
 $hostfile += "$gisIP $gisfqdn"
 $hostfile += "$hisIP $hisfqdn"
@@ -61,26 +43,16 @@ download
 msiexec /i AzureConnectedMachineAgent.msi /l*v installationlog.txt /qn | Out-String
 
 # Run connect command
-& "$env:ProgramW6432\AzureConnectedMachineAgent\azcmagent.exe" connect `
---service-principal-id $env:appId `
---service-principal-secret $env:password `
---location $env:Location `
---tenant-id $env:tenantId `
---subscription-id $env:SubscriptionId `
---resource-group $env:resourceGroup `
+& "$Env:ProgramW6432\AzureConnectedMachineAgent\azcmagent.exe" connect `
+--service-principal-id $Env:appId `
+--service-principal-secret $Env:password `
+--location $Env:Location `
+--tenant-id $Env:tenantId `
+--subscription-id $Env:SubscriptionId `
+--resource-group $Env:resourceGroup `
 --cloud "AzureCloud" `
---private-link-scope $env:PLscope `
+--private-link-scope $Env:PLscope `
 --tags "Project=jumpstart_azure_arc_servers" `
 --correlation-id "86501baa-0b82-478c-b3cf-620533617001"
 
-Unregister-ScheduledTask -TaskName "LogonScript" -Confirm:$False
-Stop-Process -Name powershell -Force
-'@ > C:\tmp\LogonScript.ps1
-
-# Creating LogonScript Windows Scheduled Task
-$Trigger = New-ScheduledTaskTrigger -AtLogOn
-$Action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument 'C:\tmp\LogonScript.ps1'
-Register-ScheduledTask -TaskName "LogonScript" -Trigger $Trigger -User "${adminUsername}" -Action $Action -RunLevel "Highest" -Force
-
-# Disabling Windows Server Manager Scheduled Task
-Get-ScheduledTask -TaskName ServerManager | Disable-ScheduledTask
+'@ > C:\tmp\LogonScript_Install.log
