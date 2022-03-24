24083783fae03650f1959870ebbbb0640bccfff4
Matthew Garrett <magarret@microsoft.com>
2022-03-24 09:40:27 -0700

    Updated variables to read from required AWS vars

--- azure_arc_k8s_jumpstart/eks/terraform/variables.tf
+++ azure_arc_k8s_jumpstart/eks/terraform/variables.tf
@@ -1,4 +1,14 @@
-variable "aws_region" {
+variable "AWS_ACCESS_KEY_ID" {
+  description = "Your AWS Access Key ID"
+  type        = string
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  description = "Your AWS Secret Key"
+  type        = string
+}
+
+variable "AWS_REGION" {
   type        = string
   description = "Target AWS region."
   default     = "us-west-1"
