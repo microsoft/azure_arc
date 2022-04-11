36ff52ca9f4883a053421ade0d4610e9844a34e6
Federico Arambarri <v-fearam@microsoft.com>
2022-04-11 08:11:50 -0300

    rename directory name

--- azure_jumpstart_arcbox/artifacts/Bootstrap.ps1
+++ azure_jumpstart_arcbox/artifacts/Bootstrap.ps1
@@ -144,8 +144,8 @@ workflow ClientTools_01
             Invoke-WebRequest ($templateBaseUrl + "artifacts/ArcSQLIcon.ico") -OutFile $Env:ArcBoxDir\ArcSQLIcon.ico
             Invoke-WebRequest ($templateBaseUrl + "artifacts/ArcSQLManualOnboarding.ps1") -OutFile $Env:ArcBoxDir\ArcSQLManualOnboarding.ps1
             Invoke-WebRequest ($templateBaseUrl + "artifacts/installArcAgentSQLUser.ps1") -OutFile $Env:ArcBoxDir\installArcAgentSQLUser.ps1
-            Invoke-WebRequest ($templateBaseUrl + "artifacts/GHAction/GHActionDeploy.ps1") -OutFile "C:\ArcBox\GHActionDeploy.ps1"
-            Invoke-WebRequest ($templateBaseUrl + "artifacts/GHAction/OpenSSHDeploy.ps1") -OutFile "C:\ArcBox\OpenSSHDeploy.ps1"
+            Invoke-WebRequest ($templateBaseUrl + "artifacts/ghaction_scripts/GHActionDeploy.ps1") -OutFile "C:\ArcBox\GHActionDeploy.ps1"
+            Invoke-WebRequest ($templateBaseUrl + "artifacts/ghaction_scripts/OpenSSHDeploy.ps1") -OutFile "C:\ArcBox\OpenSSHDeploy.ps1"
           
             # Workbook template
             if ($flavor -eq "ITPro") {
