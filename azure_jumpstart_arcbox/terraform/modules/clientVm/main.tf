719a19ceae6f66a9d114de38dd2bfbe4489b409b
Seif Bassem <38246040+sebassem@users.noreply.github.com>
2022-04-12 14:40:13 +0200

    removed empty NSG rule

--- azure_jumpstart_arcbox/terraform/modules/clientVm/main.tf
+++ azure_jumpstart_arcbox/terraform/modules/clientVm/main.tf
@@ -225,8 +225,6 @@ resource "azurerm_network_security_group" "nsg" {
   name                = local.nsg_name
   location            = data.azurerm_resource_group.rg.location
   resource_group_name = data.azurerm_resource_group.rg.name
-
-  security_rule {}
 }
 
 resource "azurerm_network_interface" "nic" {
