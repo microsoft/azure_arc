760b82fa4f13e32af6a9253caf82333375a0349d
Federico Arambarri <v-fearam@microsoft.com>
2022-04-12 07:46:46 -0300

    Adjusting after rebasing and Upload lint results

--- azure_jumpstart_arcbox/artifacts/ghaction_scripts/TerraformDeploy.sh
+++ azure_jumpstart_arcbox/artifacts/ghaction_scripts/TerraformDeploy.sh
@@ -5,16 +5,15 @@ azure_location=$3
 spn_client_id=$4
 spn_client_secret=$5
 spn_tenant_id=$6
-user_ip_address=$7
-client_admin_ssh=$8
-client_admin_username=$9
-client_admin_password=${10}
-deployment_flavor=${11}
-workspace_name=${12}
-github_repo=${13}
-github_branch=${14}
-deploy_bastion=${15}
-spn_tenant_subcriptionid=${16}
+client_admin_ssh=$7
+client_admin_username=$8
+client_admin_password=${9}
+deployment_flavor=${10}
+workspace_name=${11}
+github_repo=${12}
+github_branch=${13}
+deploy_bastion=${14}
+spn_tenant_subcriptionid=${15}
 
 cd "$terraformFolder" || exit
 
@@ -28,7 +27,6 @@ echo "azure_location=\"$azure_location\"" >> "$terraformVasrs"
 echo "spn_client_id=\"$spn_client_id\"" >> "$terraformVasrs"
 echo "spn_client_secret=\"$spn_client_secret\"" >> "$terraformVasrs"
 echo "spn_tenant_id=\"$spn_tenant_id\"" >> "$terraformVasrs"
-echo "user_ip_address=\"$user_ip_address\"" >> "$terraformVasrs"
 echo "client_admin_ssh=\"$path\"" >> "$terraformVasrs"
 echo "client_admin_username=\"$client_admin_username\"" >> "$terraformVasrs"
 echo "client_admin_password=\"$client_admin_password\"" >> "$terraformVasrs"
