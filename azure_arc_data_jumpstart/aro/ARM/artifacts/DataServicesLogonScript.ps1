7f55b9856eac2ff6e10d69cefdfde3431ae20d59
Seif Bassem <38246040+sebassem@users.noreply.github.com>
2022-04-17 15:29:54 +0200

    remove the upgrade switches

--- azure_arc_data_jumpstart/aro/ARM/artifacts/DataServicesLogonScript.ps1
+++ azure_arc_data_jumpstart/aro/ARM/artifacts/DataServicesLogonScript.ps1
@@ -14,11 +14,11 @@ az config set extension.use_dynamic_install=yes_without_prompt
 # Installing Azure CLI extensions
 Write-Host "`n"
 Write-Host "Installing Azure CLI extensions"
-az extension add --name arcdata --upgrade
-az extension add --name connectedk8s --upgrade
-az extension add --name k8s-extension --upgrade
-az extension add --name k8s-configuration --upgrade
-az extension add --name customlocation --upgrade
+az extension add --name arcdata
+az extension add --name connectedk8s
+az extension add --name k8s-extension
+az extension add --name k8s-configuration
+az extension add --name customlocation
 Write-Host "`n"
 az -v
 
