1172bc6ab7027549546912a9563f8547e05118d7
jessieziyuantang <76741191+jessieziyuantang@users.noreply.github.com>
2022-04-20 13:58:53 -0400

    update the shell command

--- docs/azure_arc_jumpstart/azure_arc_k8s/day2/multi_distributions/gitops/calico_k8sconfig_gitops.sh
+++ docs/azure_arc_jumpstart/azure_arc_k8s/day2/multi_distributions/gitops/calico_k8sconfig_gitops.sh
@@ -19,18 +19,10 @@ chmod 700 get_helm.sh
 
 # Installing Azure CLI & Azure Arc Extensions
 # <--- Replacing 'apt-get' to 'yum' for EKS if needed --->
-echo "Installing Azure CLI & Azure Arc Extensions"
-sudo apt-get update
-sudo apt-get install -y ca-certificates curl apt-transport-https lsb-release gnupg
-curl -sL https://packages.microsoft.com/keys/microsoft.asc |
-gpg --dearmor |
-sudo tee /etc/apt/trusted.gpg.d/microsoft.asc.gpg > /dev/null
-AZ_REPO=$(lsb_release -cs)
-echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" |
-sudo tee /etc/apt/sources.list.d/azure-cli.list
-sudo apt-get update
-sudo apt-get install azure-cli
+echo "Installing Azure CLI"
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 
+echo "Installing Azure Arc Extensions"
 # Clean up k8s-configuration Extensions and install again
 az extension remove --name k8s-configuration
 rm -rf ~/.azure/AzureArcCharts
