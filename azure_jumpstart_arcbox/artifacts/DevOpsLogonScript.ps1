42cf91871628daa4f3ddd230b9deb36e2f55aa64
Zaid Mohammad <mozaid@microsoft.com>
2022-04-21 10:40:28 -0400

    osm extension - remove train and version

--- azure_jumpstart_arcbox/artifacts/DevOpsLogonScript.ps1
+++ azure_jumpstart_arcbox/artifacts/DevOpsLogonScript.ps1
@@ -7,7 +7,6 @@ $Env:ArcBoxIconDir = "C:\ArcBox\Icons"
 
 $osmRelease = "v1.1.0"
 $osmMeshName = "osm"
-$osmExtension = "1.0.0"
 $ingressNamespace = "ingress-nginx"
 
 $certname = "ingress-cert"
@@ -103,7 +102,7 @@ Write-Host "`n"
 az -v
 
 # "Create OSM Kubernetes extension instance"
-az k8s-extension create --cluster-name $Env:capiArcDataClusterName --resource-group $Env:resourceGroup --cluster-type connectedClusters --extension-type Microsoft.openservicemesh --scope cluster --release-train pilot --name $osmMeshName --version $osmExtension
+az k8s-extension create --cluster-name $Env:capiArcDataClusterName --resource-group $Env:resourceGroup --cluster-type connectedClusters --extension-type Microsoft.openservicemesh --scope cluster --name $osmMeshName
 
 # Create Kubernetes Namespaces
 foreach ($namespace in @('bookstore', 'bookbuyer', 'bookwarehouse', 'hello-arc', 'ingress-nginx')) {
