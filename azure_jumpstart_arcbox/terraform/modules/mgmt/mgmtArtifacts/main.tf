9300b322ae40e8aad46bc114d621d2281f815be4
Seif Bassem <38246040+sebassem@users.noreply.github.com>
2022-04-22 14:48:26 +0200

    typo

--- azure_jumpstart_arcbox/terraform/modules/mgmt/mgmtArtifacts/main.tf
+++ azure_jumpstart_arcbox/terraform/modules/mgmt/mgmtArtifacts/main.tf
@@ -78,7 +78,7 @@ resource "azurerm_virtual_network" "vnet" {
 }
 
 resource "azurerm_network_security_group" "nsg" {
-  name                = azurerm_network_security_group.nsg.name
+  name                = local.nsg_name
   location            = data.azurerm_resource_group.rg.location
   resource_group_name = data.azurerm_resource_group.rg.name
 }
