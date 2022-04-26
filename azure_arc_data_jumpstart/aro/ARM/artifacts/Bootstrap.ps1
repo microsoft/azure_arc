56d5d5015db1b74b0e863656b18914028b31539c
Seif Bassem <38246040+sebassem@users.noreply.github.com>
2022-04-26 23:18:45 +0200

    Modified SCC file name

--- azure_arc_data_jumpstart/aro/ARM/artifacts/Bootstrap.ps1
+++ azure_arc_data_jumpstart/aro/ARM/artifacts/Bootstrap.ps1
@@ -80,7 +80,7 @@ Invoke-WebRequest ($templateBaseUrl + "artifacts/postgreSQL.parameters.json") -O
 Invoke-WebRequest ($templateBaseUrl + "artifacts/SQLMIEndpoints.ps1") -OutFile "C:\Temp\SQLMIEndpoints.ps1"
 Invoke-WebRequest "https://github.com/ErikEJ/SqlQueryStress/releases/download/102/SqlQueryStress.zip" -OutFile "C:\Temp\SqlQueryStress.zip"
 Invoke-WebRequest "https://raw.githubusercontent.com/microsoft/azure_arc/main/img/jumpstart_wallpaper.png" -OutFile "C:\Temp\wallpaper.png"
-Invoke-WebRequest ($templateBaseUrl + "artifacts/arc-scc.yaml") -OutFile "C:\Temp\arc-scc.yaml"
+Invoke-WebRequest ($templateBaseUrl + "artifacts/AROSCC.yaml") -OutFile "C:\Temp\AROSCC.yaml"
 
 
 # Installing tools
