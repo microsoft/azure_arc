229645a275099b03c88fe02997fc5fb9687a70cf
likamrat <likamrat@microsoft.com>
2022-04-29 12:21:07 -0700

    func pods

--- azure_arc_app_services_jumpstart/aks/ARM/artifacts/deployFunction.ps1
+++ azure_arc_app_services_jumpstart/aks/ARM/artifacts/deployFunction.ps1
@@ -34,7 +34,7 @@ Do {
 Do {
     Write-Host "Waiting for log-processor to become available. Hold tight, this might take a few minutes..."
     Start-Sleep -Seconds 45
-    $logProcessorStatus = $(if(kubectl describe daemonset ($extensionName + "-k8se-log-processor") -n appservices | Select-String "Pods Status:  4 Running" -Quiet){"Ready!"}Else{"Nope"})
+    $logProcessorStatus = $(if(kubectl describe daemonset ($extensionName + "-k8se-log-processor") -n appservices | Select-String "Pods Status:  3 Running" -Quiet){"Ready!"}Else{"Nope"})
     } while ($logProcessorStatus -eq "Nope")
    
 
