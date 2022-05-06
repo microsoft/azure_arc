f50ea807a23c073e52f4cdb0bbd39b33e43bf10e
Federico Arambarri <v-fearam@microsoft.com>
2022-05-06 11:23:42 -0300

    extracting download script

--- tests/DesktopValidation.sh
+++ tests/DesktopValidation.sh
@@ -8,17 +8,25 @@ vmPassword=${6}
 
 validations=true
 
+# Getting expected values
 config=$(cat "$DeployTestParametersFile")
 
+# Count element on  C:\Users\Public\Desktop
 countDesktopFilesPublic=$(plink -ssh -P 2204 $vmUser@$(az vm show -d -g "$ResourceGroup" -n ArcBox-Client --query publicIps -o tsv)   -pw "$vmPassword" -batch   'dir C:\Users\Public\Desktop /b | find /v /c "::"') || countDesktopFilesPublic=0
 countDesktopFilesPublic=${countDesktopFilesPublic//[$'\t\r\n']}
 
+# Count element on user Desktop
 countDesktopFilesArcDemo=$(plink -ssh -P 2204 $vmUser@$(az vm show -d -g "$ResourceGroup" -n ArcBox-Client --query publicIps -o tsv)   -pw "$vmPassword" -batch   'dir c:\Users\'$vmUser'\Desktop /b | find /v /c "::"') || countDesktopFilesArcDemo=0
 countDesktopFilesArcDemo=${countDesktopFilesArcDemo//[$'\t\r\n']}
 
+# Real Desktop elements
 countDesktopFiles=$(( $countDesktopFilesPublic + $countDesktopFilesArcDemo))
+
+# Get expected result
 jqueryDesktopElementsExpected=".$Flavor.desktopElementsExpected"
 desktopElementsExpected=$(echo "$config" |  jq "$jqueryDesktopElementsExpected")
+
+# Do the validation
 if [ "$countDesktopFiles" -ge "$desktopElementsExpected" ]; then
   echo "Number of element on Desktop: $countDesktopFiles"
 else
@@ -26,6 +34,7 @@ else
   validations=false
 fi
 
+# fail if some validation was not reach
 if [ "$validations" = "false" ]; then
    echo "Something was wrong. Failing"
    exit 1
