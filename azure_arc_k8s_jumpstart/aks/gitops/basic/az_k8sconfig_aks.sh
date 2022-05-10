fbd32b9a4dc48e9acf1318d7dd598a76c65e2f0c
Zaid Mohammad <mozaid@microsoft.com>
2022-05-10 18:41:21 -0400

    update script ingress

--- azure_arc_k8s_jumpstart/aks/gitops/basic/az_k8sconfig_aks.sh
+++ azure_arc_k8s_jumpstart/aks/gitops/basic/az_k8sconfig_aks.sh
@@ -9,32 +9,47 @@ export tenantId='<Your Azure tenant ID>'
 export resourceGroup='<Azure resource group name>'
 export arcClusterName='<The name of your k8s cluster as it will be shown in Azure Arc>'
 export appClonedRepo='<The URL for the "Azure Arc Jumpstart" forked GitHub repository>'
-export appNnamespace='hello-arc'
+export namespace='hello-arc'
 
 # Getting AKS credentials
 echo "Log in to Azure with Service Principal & Getting AKS credentials (kubeconfig)"
 az login --service-principal --username $appId --password $password --tenant $tenantId
 az aks get-credentials --name $arcClusterName --resource-group $resourceGroup --overwrite-existing
 
-# Create a namespace for your ingress resources
-kubectl create ns cluster-mgmt
+# Create a namespace for your app & ingress resources
+kubectl create ns $namespace
 
 # Add the official stable repo
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm repo update
 
 # Use Helm to deploy an NGINX ingress controller
-helm install nginx ingress-nginx/ingress-nginx -n cluster-mgmt
+helm upgrade --install ingress-nginx ingress-nginx \
+  --repo https://kubernetes.github.io/ingress-nginx \
+  --namespace $namespace
 
-# Create GitOps config
-echo "Creating GitOps config"
+# Create GitOps config for Hello-Arc app
+echo "Creating GitOps config for Hello-Arc app"
 az k8s-configuration flux create \
 --cluster-name $arcClusterName \
 --resource-group $resourceGroup \
---name cluster-config \
---namespace $appNamespace \
+--name config-helloarc \
+--namespace $namespace \
 --cluster-type connectedClusters \
---scope cluster \
+--scope namespace \
 --url $appClonedRepo \
 --branch main --sync-interval 3s \
 --kustomization name=app path=./hello-arc/yaml
+
+# Create GitOps config for Hello-Arc Ingress
+echo "Creating GitOps config for Hello-Arc Ingress"
+az k8s-configuration flux create \
+--cluster-name $arcClusterName \
+--resource-group $resourceGroup \
+--name config-helloarc-ingress \
+--namespace $namespace \
+--cluster-type connectedClusters \
+--scope namespace \
+--url $appClonedRepo \
+--branch main \
+--kustomization name=app path=./hello-arc/ingress
