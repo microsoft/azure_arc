8d2f8ac6db043db1655abf0ca0dcabb3e09b91a7
Venkata Chintala <29983008+chintalavr@users.noreply.github.com>
2022-05-14 17:41:14 -0400

    Update DataServicesLogonScript.ps1

--- azure_arc_data_jumpstart/aks/ARM/artifacts/DataServicesLogonScript.ps1
+++ azure_arc_data_jumpstart/aks/ARM/artifacts/DataServicesLogonScript.ps1
@@ -258,6 +258,7 @@ Start-Sleep -Seconds 5
 
 
 # If AD Auth is required join computer to ADDS domain and restart computer
+$addsDomainName = $Env.addsDomainName
 if ($addsDomainName.Length -gt 0)
 {
     # Install Install-WindowsFeature RSAT-AD-PowerShell windows feature to setup OU and User Accounts in ADDS
@@ -267,7 +268,7 @@ if ($addsDomainName.Length -gt 0)
 
     Write-Host "Joining computer to Active Directory domain ${addsDomainName}. Computer will be rebooted after joining domain."
     # Get NitBios name from FQDN
-    $netbiosname = $Env.addsDomainName.Split(".")[0]
+    $netbiosname = $addsDomainName.Split(".")[0]
     $computername = $env:COMPUTERNAME
 
     $domainCred = New-Object pscredential -ArgumentList ([pscustomobject]@{
@@ -280,10 +281,10 @@ if ($addsDomainName.Length -gt 0)
         Password = (ConvertTo-SecureString -String $Env:adminPassword -AsPlainText -Force)[0]
     })
  
-    Write-Host "Domain Name: $Env:addsDomainName, Admin User: $adminUsername, NetBios Name: $netbiosname, Computer Name: $computername"
+    Write-Host "Domain Name: $addsDomainName, Admin User: $adminUsername, NetBios Name: $netbiosname, Computer Name: $computername"
     
-    Add-Computer -DomainName $Env:addsDomainName -LocalCredential $localCred -Credential $domainCred
-    Write-Host "Joined Client VM to $Env:addsDomainName domain."
+    Add-Computer -DomainName $addsDomainName -LocalCredential $localCred -Credential $domainCred
+    Write-Host "Joined Client VM to $addsDomainName domain."
     Restart-Computer
 }
 
