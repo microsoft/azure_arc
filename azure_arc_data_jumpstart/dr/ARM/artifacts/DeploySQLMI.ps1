497a6924720672691c6499664bb527360640a401
Seif Bassem <38246040+sebassem@users.noreply.github.com>
2022-05-16 11:44:17 +0200

    renamed pod name

--- azure_arc_data_jumpstart/dr/ARM/artifacts/DeploySQLMI.ps1
+++ azure_arc_data_jumpstart/dr/ARM/artifacts/DeploySQLMI.ps1
@@ -107,7 +107,7 @@ if ( $env:SQLMIHA -eq $true )
 }
 
 # Downloading demo database and restoring onto SQL MI
-$podname = "jumpstart-sql-primary-0"
+$podname = "js-sql-pr-0"
 Write-Host "`n"
 Write-Host "Downloading AdventureWorks database for MS SQL... (1/2)"
 kubectl exec $podname -n arc -c arc-sqlmi -- wget https://github.com/Microsoft/sql-server-samples/releases/download/adventureworks/AdventureWorks2019.bak -O /var/opt/mssql/data/AdventureWorks2019.bak 2>&1 | Out-Null
@@ -115,7 +115,7 @@ Write-Host "Restoring AdventureWorks database for MS SQL. (2/2)"
 kubectl exec $podname -n arc -c arc-sqlmi -- /opt/mssql-tools/bin/sqlcmd -S localhost -U $Env:AZDATA_USERNAME -P $Env:AZDATA_PASSWORD -Q "RESTORE DATABASE AdventureWorks2019 FROM  DISK = N'/var/opt/mssql/data/AdventureWorks2019.bak' WITH MOVE 'AdventureWorks2017' TO '/var/opt/mssql/data/AdventureWorks2019.mdf', MOVE 'AdventureWorks2017_Log' TO '/var/opt/mssql/data/AdventureWorks2019_Log.ldf'" 2>&1 $null
 
 # Retrieving SQL MI connection endpoint
-$sqlstringPrimary = kubectl get sqlmanagedinstances jumpstart-sql-primary -n arc -o=jsonpath='{.status.endpoints.primary}'
+$sqlstringPrimary = kubectl get sqlmanagedinstances $primarySqlMIInstance -n arc -o=jsonpath='{.status.endpoints.primary}'
 
 # Replace placeholder values in settingsTemplate.json
 (Get-Content -Path $settingsTemplate) -replace 'arc_sql_mi_primary',$sqlstringPrimary | Set-Content -Path $settingsTemplate
@@ -179,7 +179,7 @@ Write-Host "Creating Azure Data Studio settings for SQL Managed Instance connect
 $settingsTemplate = "$Env:TempDir\settingsTemplate.json"
 
 # Retrieving SQL MI connection endpoint
-$sqlstringSecondary = kubectl get sqlmanagedinstances jumpstart-sql-secondary -n arc -o=jsonpath='{.status.endpoints.primary}'
+$sqlstringSecondary = kubectl get sqlmanagedinstances $secondarySqlMIInstance -n arc -o=jsonpath='{.status.endpoints.primary}'
 
 # Replace placeholder values in settingsTemplate.json
 (Get-Content -Path $settingsTemplate) -replace 'arc_sql_mi_secondary',$sqlstringSecondary | Set-Content -Path $settingsTemplate
