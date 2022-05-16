d7a2da3de85f641a93ea42a6de968c6766f97e60
Seif Bassem <38246040+sebassem@users.noreply.github.com>
2022-05-16 18:31:59 +0200

    removed special character

--- azure_arc_data_jumpstart/dr/ARM/artifacts/DeploySQLMI.ps1
+++ azure_arc_data_jumpstart/dr/ARM/artifacts/DeploySQLMI.ps1
@@ -198,7 +198,7 @@ Write-Host "`n"
 Write-Host "Configuring the primary cluster"
 Write-Host "`n"
 kubectx primary
-az sql mi-arc get-mirroring-cert --name $primarySqlMIInstance --cert-file "$Env:TempDir/sqlcerts/sqlprimary.pem"​ --k8s-namespace arc --use-k8s
+az sql mi-arc get-mirroring-cert --name $primarySqlMIInstance --cert-file "$Env:TempDir/sqlcerts/sqlprimary.pem" --k8s-namespace arc --use-k8s
 Write-Host "`n"
 
 Write-Host "Configuring the secondary cluster"
