aba9966035181b906682bce610782ada16b7eac8
Seif Bassem <38246040+sebassem@users.noreply.github.com>
2022-05-17 09:06:09 +0200

    adding the mirroring endpoints

--- azure_arc_data_jumpstart/dr/ARM/artifacts/DeploySQLMI.ps1
+++ azure_arc_data_jumpstart/dr/ARM/artifacts/DeploySQLMI.ps1
@@ -207,17 +207,21 @@ Write-Host "`n"
 Write-Host "Configuring the primary cluster"
 Write-Host "`n"
 kubectx primary
+$primaryMirroringEndpoint = $(az sql mi-arc show $primarySqlMIInstance --k8s-namespace arc --use-k8s -o=jsonpath='{.status.mirroringEndpoint}')
 az sql mi-arc get-mirroring-cert --name $primarySqlMIInstance --cert-file "$Env:TempDir/sqlcerts/sqlprimary.pem" --k8s-namespace arc --use-k8s
 Write-Host "`n"
 
 Write-Host "Configuring the secondary cluster"
 Write-Host "`n"
 kubectx secondary
+$secondaryMirroringEndpoint = $(az sql mi-arc show $secondarySqlMIInstance --k8s-namespace arc --use-k8s -o=jsonpath='{.status.mirroringEndpoint}')
 az sql mi-arc get-mirroring-cert --name $secondarySqlMIInstance --cert-file "$Env:TempDir/sqlcerts/sqlsecondary.pem" --k8s-namespace arc --use-k8s
-az sql instance-failover-group-arc create --shared-name jumpstartDAG --name secondarycr --mi $secondarySqlMIInstance --role secondary --partner-mi js-sql-pr  --partner-mirroring-url tcp://10.10.5.20:970 --partner-mirroring-cert-file "$Env:TempDir/sqlcerts/sqlprimary.pem"​ --k8s-namespace arc --use-k8s
 Write-Host "`n"
 
 Write-Host "`n"
 kubectx primary
-az sql instance-failover-group-arc create --shared-name jumpstartDAG --name primarycr --mi $primarySqlMIInstance --role primary --partner-mi js-sql-dr  --partner-mirroring-url tcp://10.20.5.20:970 --partner-mirroring-cert-file "$Env:TempDir/sqlcerts/sqlsecondary.pem" --k8s-namespace arc --use-k8s
+az sql instance-failover-group-arc create --shared-name jumpstartDAG --name primarycr --mi $primarySqlMIInstance --role primary --partner-mi js-sql-dr  --partner-mirroring-url "tcp://$secondaryMirroringEndpoint" --partner-mirroring-cert-file "$Env:TempDir/sqlcerts/sqlsecondary.pem" --k8s-namespace arc --use-k8s
+Write-Host "`n"
+kubectx secondary
+az sql instance-failover-group-arc create --shared-name jumpstartDAG --name secondarycr --mi $secondarySqlMIInstance --role secondary --partner-mi js-sql-pr  --partner-mirroring-url "tcp://$primaryMirroringEndpoint" --partner-mirroring-cert-file "$Env:TempDir/sqlcerts/sqlprimary.pem" --k8s-namespace arc --use-k8s
 
