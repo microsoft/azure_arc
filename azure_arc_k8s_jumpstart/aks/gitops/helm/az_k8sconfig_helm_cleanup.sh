7951ac1b8094e8a4f5ee50cdbbf0e56ee46ece30
Zaid Mohammad <mozaid@microsoft.com>
2022-05-18 16:39:47 -0400

    AKS Helm GitOps Update

--- azure_arc_k8s_jumpstart/aks/gitops/helm/az_k8sconfig_helm_cleanup.sh
+++ azure_arc_k8s_jumpstart/aks/gitops/helm/az_k8sconfig_helm_cleanup.sh
@@ -3,11 +3,13 @@
 # <--- Change the following environment variables according to your Azure service principal name --->
 
 echo "Exporting environment variables"
-export resourceGroup='<Azure resource group name>'
-export arcClusterName='<The name of your k8s cluster as it will be shown in Azure Arc>'
 export appId='<Your Azure service principal name>'
 export password='<Your Azure service principal password>'
 export tenantId='<Your Azure tenant ID>'
+export resourceGroup='<Azure resource group name>'
+export arcClusterName='<The name of your k8s cluster as it will be shown in Azure Arc>'
+export ingressNamespace='ingress-nginx'
+export namespace='hello-arc'
 
 # Login to Azure using the service principal name
 echo "Log in to Azure with Service Principal & Getting AKS credentials (kubeconfig)"
@@ -16,13 +18,17 @@ az aks get-credentials --name $arcClusterName --resource-group $resourceGroup --
 
 # Deleting GitOps Configurations from Azure Arc Kubernetes cluster
 echo "Deleting GitOps Configurations from Azure Arc Kubernetes cluster"
-az k8s-configuration delete --name hello-arc --cluster-name $arcClusterName --resource-group $resourceGroup --cluster-type connectedClusters -y
-az k8s-configuration delete --name nginx-ingress --cluster-name $arcClusterName --resource-group $resourceGroup --cluster-type connectedClusters -y
+az k8s-configuration flux delete --name config-nginx --cluster-name $arcClusterName --resource-group $resourceGroup --cluster-type connectedClusters -y
+az k8s-configuration flux delete --name config-helloarc --cluster-name $arcClusterName --resource-group $resourceGroup --cluster-type connectedClusters -y
+
+# Deleting GitOps Flux extension
+echo "Deleting GitOps Flux extension"
+az k8s-extension delete --name flux --cluster-name $arcClusterName --resource-group $resourceGroup --cluster-type connectedClusters -y
 
 # Cleaning Kubernetes cluster
-echo "Cleaning Kubernetes cluster. You can safely ignore non-exist resources"
-kubectl delete ns prod
-kubectl delete ns cluster-mgmt
+echo "Cleaning Kubernetes cluster. You can safely ignore non-existing resources"
+kubectl delete ns $ingressNamespace
+kubectl delete ns $namespace
 
 kubectl delete clusterrole cluster-mgmt-helm-cluster-mgmt-helm-operator
 kubectl delete clusterrole hello-arc-helm-prod-helm-operator-crd
