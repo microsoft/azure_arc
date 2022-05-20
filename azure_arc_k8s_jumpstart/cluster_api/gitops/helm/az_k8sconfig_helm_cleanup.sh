f40f54e60f5350abd6aafcf93b450b1e46125750
Zaid Mohammad <mozaid@microsoft.com>
2022-05-20 17:32:31 -0400

    Update cleanup script reference

--- azure_arc_k8s_jumpstart/cluster_api/gitops/helm/az_k8sconfig_helm_cleanup.sh
+++ azure_arc_k8s_jumpstart/cluster_api/gitops/helm/az_k8sconfig_helm_cleanup.sh
@@ -14,7 +14,6 @@ export namespace='hello-arc'
 # Login to Azure using the service principal name
 echo "Log in to Azure with Service Principal & Getting AKS credentials (kubeconfig)"
 az login --service-principal --username $appId --password $password --tenant $tenantId
-az aks get-credentials --name $arcClusterName --resource-group $resourceGroup --overwrite-existing
 
 # Deleting GitOps Configurations from Azure Arc Kubernetes cluster
 echo "Deleting GitOps Configurations from Azure Arc Kubernetes cluster"
