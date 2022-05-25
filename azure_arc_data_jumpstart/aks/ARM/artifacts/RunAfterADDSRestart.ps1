090967162122b2e5300b9fc3ac6e3e533aa3c6ac
Venkata Chintala <29983008+chintalavr@users.noreply.github.com>
2022-05-25 16:57:24 -0400

    Updated scripts

--- azure_arc_data_jumpstart/aks/ARM/artifacts/RunAfterADDSRestart.ps1
+++ azure_arc_data_jumpstart/aks/ARM/artifacts/RunAfterADDSRestart.ps1
@@ -3,5 +3,39 @@
 # This script will be executed after ADDS domain setup and restarted
 # to continue configure reverse DNS lookup to support SQLMI AD authentication
 ##############################################
+Import-Module ActiveDirectory
+Import-Module DnsServer
+
+Start-Transcript -Path "C:\Temp\SetupReverseDNS.log"
+
+# Get Activectory Information
+$dcInfo = Get-ADDomainController
+$dcIPv4 = ([System.Net.IPAddress]$dcInfo.IPv4Address).GetAddressBytes()
+$reverseLookupCidr = [System.String]::Concat($dcIPv4[0], '.', $dcIPv4[1], '.', $dcIPv4[2], '.0/24')
+
 # Setup reverse lookup zone
-Add-DnsServerPrimaryZone -NetworkId "172.16.1.0/24" -ReplicationScope Domain -DomainNetbiosName "contoso"
+try {
+    Add-DnsServerPrimaryZone -NetworkId $reverseLookupCidr -ReplicationScope "Forest" -ComputerName $dcInfo.HostName
+    Write-Host "Successfully created reverse DNS Zone."
+
+    $ReverseDnsZone = Get-DnsServerZone | Where-Object {$_.IsAutoCreated -eq $false -and $_.IsReverseLookupZone -eq $true}
+}
+catch {
+    # Reverse DNS already setup
+    $ReverseDnsZone = Get-DnsServerZone | Where-Object {$_.IsAutoCreated -eq $false -and $_.IsReverseLookupZone -eq $true}
+    Write-Host "Reverse DNS Zone ${ReverseDnsZone.Name} already exists for this domain controller."
+}
+
+# Create reverse DNS for domain controller
+try {
+    Add-DNSServerResourceRecordPTR -ZoneName $ReverseDnsZone -Name 4 -PTRDomainName $dcInfo.HostName
+    Write-Host "Created PTR record for domain controller."
+}
+catch {
+    Write-Host "PTR record already exists for domain controller."
+}
+
+# Delete schedule task
+schtasks.exe /delete /f /tn RunAfterADDSRestart
+
+Stop-Transcript
