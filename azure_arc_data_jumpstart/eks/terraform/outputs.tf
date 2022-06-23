0978798c3e74c691336c51b82039c0088669a425
Seif Bassem <38246040+sebassem@users.noreply.github.com>
2022-06-23 12:19:27 +0200

    adding worker nodes config

--- azure_arc_data_jumpstart/eks/terraform/outputs.tf
+++ azure_arc_data_jumpstart/eks/terraform/outputs.tf
@@ -1,4 +1,4 @@
-/*locals {
+locals {
   config_map_aws_auth = <<CONFIGMAPAWSAUTH
 
 
@@ -15,42 +15,8 @@ data:
         - system:bootstrappers
         - system:nodes
 CONFIGMAPAWSAUTH
-
-  kubeconfig = <<KUBECONFIG
-
-
-apiVersion: v1
-clusters:
-- cluster:
-    server: ${module.eks_cluster.cluster_endpoint}
-    certificate-authority-data: ${module.eks_cluster.cluster_ca_data}
-  name: kubernetes
-contexts:
-- context:
-    cluster: kubernetes
-    user: aws
-  name: aws
-current-context: aws
-kind: Config
-preferences: {}
-users:
-- name: aws
-  user:
-    exec:
-      apiVersion: client.authentication.k8s.io/v1alpha1
-      command: aws-iam-authenticator
-      args:
-        - "token"
-        - "-i"
-        - "${var.cluster_name}"
-KUBECONFIG
 }
 
 output "config_map_aws_auth" {
   value = local.config_map_aws_auth
 }
-
-output "kubeconfig" {
-  value = local.kubeconfig
-}
-*/
