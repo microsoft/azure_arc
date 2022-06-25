f38a40a4f3e7fef04a59a7d6bb2b68ef32eaac32
Seif Bassem <38246040+sebassem@users.noreply.github.com>
2022-06-25 17:48:36 +0200

    remove eks cli

--- azure_arc_data_jumpstart/eks/terraform/artifacts/Bootstrap.ps1
+++ azure_arc_data_jumpstart/eks/terraform/artifacts/Bootstrap.ps1
@@ -92,7 +92,7 @@ Invoke-WebRequest "https://raw.githubusercontent.com/microsoft/azure_arc/main/im
 # Installing tools
 workflow ClientTools_01
         {
-            $chocolateyAppList = 'awscli,eksctl,azure-cli,az.powershell,kubernetes-cli,vcredist140,microsoft-edge,azcopy10,vscode,putty.install,kubernetes-helm,grep,ssms,dotnetcore-3.1-sdk'
+            $chocolateyAppList = 'awscli,azure-cli,az.powershell,kubernetes-cli,vcredist140,microsoft-edge,azcopy10,vscode,putty.install,kubernetes-helm,grep,ssms,dotnetcore-3.1-sdk'
             #Run commands in parallel.
             Parallel 
                 {
