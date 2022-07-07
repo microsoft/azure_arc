bcd1fec85154e1593903da5a488b9d4354517334
Seif Bassem <38246040+sebassem@users.noreply.github.com>
2022-07-07 09:48:39 +0200

    removed unneeded code

--- azure_arc_data_jumpstart/eks/terraform/artifacts/DataServicesLogonScript.ps1
+++ azure_arc_data_jumpstart/eks/terraform/artifacts/DataServicesLogonScript.ps1
@@ -77,7 +77,6 @@ az -v
 Write-Host "Getting EKS cluster token"
 Write-Host "`n"
 aws eks update-kubeconfig --region $env:AWS_DEFAULT_REGION --name $connectedClusterName
-#kubectl apply -f "C:\Temp\configmap.yml"
 Write-Host "`n"
 
 Write-Host "Checking kubernetes nodes"
