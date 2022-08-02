d47e5e91db4a5a31e20fc6d8a5cb664f6bc91e71
saitcho <saitcho@outlook.com>
2022-08-02 13:38:15 -0500

    script updates

--- azure_stack_hci/hcibox/artifacts/Register-AzSHCI.ps1
+++ azure_stack_hci/hcibox/artifacts/Register-AzSHCI.ps1
@@ -1,44 +1,37 @@
 
-write-host -ForegroundColor Green -Object "Register the Cluster to Azure Subscription"
-#Variables
-$adcred=Get-Credential -UserName "contoso\administrator" -Message "Provide AD Account Password"
+$WarningPreference = "SilentlyContinue"
+$ErrorActionPreference = "Stop" 
+$ProgressPreference = 'SilentlyContinue'
+
+# Import Configuration Module
+$ConfigurationDataFile = '.\AzSHCISandbox-Config.psd1'
+$SDNConfig = Import-PowerShellDataFile -Path $ConfigurationDataFile
+
+Write-Host -ForegroundColor Green -Object "Register the Cluster to Azure Subscription"
+$user = "jumpstart.local\administrator"
+$password = ConvertTo-SecureString -String $SDNConfig.SDNAdminPassword -AsPlainText -Force
+$adcred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $user, $password
 
 #Azure Account Info
-  #install modules
-       Write-Host "Installing Required Modules" -ForegroundColor Green -BackgroundColor Black
-        Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
-        Install-WindowsFeature -name RSAT-Clustering-Powershell
-        $ModuleNames="Az.Resources","Az.Accounts", "Az.stackhci"
-        foreach ($ModuleName in $ModuleNames){
-            if (!(Get-InstalledModule -Name $ModuleName -ErrorAction Ignore)){
-                Install-Module -Name $ModuleName -Force
-            }
-        }
+#install modules
+Write-Host "Installing Required Modules" -ForegroundColor Green -BackgroundColor Black
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+Install-WindowsFeature -name RSAT-Clustering-Powershell
+$ModuleNames = "Az.Resources", "Az.Accounts", "Az.stackhci"
+foreach ($ModuleName in $ModuleNames) {
+    Install-Module -Name $ModuleName -Force
+}
 
-
-
-
-#login to Azure
-        $azcred=Get-AzContext
-
-        if (-not (Get-AzContext)){
-            $azcred=Login-AzAccount -UseDeviceAuthentication
-        }
-
-    #select context
-        $context=Get-AzContext -ListAvailable
-        if (($context).count -gt 1){
-            $context=$context | Out-GridView -OutputMode Single
-            $context | Set-AzContext
-        }
-
-    #location (all locations where HostPool can be created)
-        $region=(Get-AzLocation | Where-Object Providers -Contains "Microsoft.DesktopVirtualization" | Out-GridView -OutputMode Single -Title "Please select Location for AVD Host Pool metadata").Location
+# Required for CLI commands
+Write-Host "Az Login"
+$azureAppCred = (New-Object System.Management.Automation.PSCredential $env:spnClientID, (ConvertTo-SecureString -String $env:spnClientSecret -AsPlainText -Force))
+Connect-AzAccount -ServicePrincipal -Subscription $env:subscriptionId -Tenant $env:spnTenantId -Credential $azureAppCred
 
 #Register the Cluster
 Write-Host "Registering the Cluster" -ForegroundColor Green -BackgroundColor Black
 $armtoken = Get-AzAccessToken
 $graphtoken = Get-AzAccessToken -ResourceTypeName AadGraph
-$clustername=Get-Cluster
-Register-AzStackHCI -SubscriptionId $context.Subscription.Id -ComputerName azshost1 -AccountId $context.Account.Id -ArmAccessToken $armtoken.Token -GraphAccessToken $graphtoken.Token -EnableAzureArcServer -Credential $adcred -Region $region -ResourceName $clustername.Name
+$clustername = 'hciboxcluster'
+$azureLocation = 'eastus' # hard coded region due to HCI registration region restrictions
+Register-AzStackHCI -SubscriptionId $env:subscriptionId -ComputerName azshost1 -AccountId $env:spnClientID -ArmAccessToken $armtoken.Token -GraphAccessToken $graphtoken.Token -EnableAzureArcServer -Credential $adcred -Region $azureLocation -ResourceName $clustername -ResourceGroupName $env:resourceGroup
     
