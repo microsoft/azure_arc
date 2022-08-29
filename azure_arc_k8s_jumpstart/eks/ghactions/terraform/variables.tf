3714a23d7661609ab510da8603af1807e66a1260
Omar Aviles <oaviles@live.com>
2022-08-29 12:36:56 -0500

    Resources path redefined

---
+++ azure_arc_k8s_jumpstart/eks/ghactions/terraform/variables.tf
@@ -0,0 +1,21 @@
+variable "AWS_ACCESS_KEY_ID" {
+  description = "Your AWS Access Key ID"
+  type        = string
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  description = "Your AWS Secret Key"
+  type        = string
+}
+
+variable "AWS_REGION" {
+  type        = string
+  description = "Target AWS region."
+  default     = "us-west-1"
+}
+
+variable "cluster_name" {
+  type        = string
+  description = "EKS cluster name."
+  default     = "Arc-EKS-Demo"
+}
