d98f99faaa0e56b5df51fdb2ba679669294d0d7e
likamrat <likamrat@microsoft.com>
2022-09-14 20:11:00 -0700

    pg svc fix

--- azure_jumpstart_arcbox/artifacts/DeployPostgreSQL.ps1
+++ azure_jumpstart_arcbox/artifacts/DeployPostgreSQL.ps1
@@ -60,13 +60,13 @@ az deployment group create --resource-group $Env:resourceGroup --template-file "
 Write-Host "`n"
 
 # Ensures postgres container is initiated and ready to accept restores
-$pgControllerPodName = "jumpstartpsc0-0"
-$pgWorkerPodName = "jumpstartpsw0-0"
+# $pgControllerPodName = "jumpstartpsc0-0"
+$pgWorkerPodName = "jumpstartps-0"
 
     Do {
         Write-Host "Waiting for PostgreSQL. Hold tight, this might take a few minutes...(45s sleeping loop)"
         Start-Sleep -Seconds 45
-        $buildService = $(if((kubectl get pods -n arc | Select-String $pgControllerPodName| Select-String "Running" -Quiet) -and (kubectl get pods -n arc | Select-String $pgWorkerPodName| Select-String "Running" -Quiet)){"Ready!"}Else{"Nope"})
+        $buildService = $(if((kubectl get pods -n arc | Select-String $pgWorkerPodName| Select-String "Running" -Quiet)){"Ready!"}Else{"Nope"})
     } while ($buildService -eq "Nope")
 
 Start-Sleep -Seconds 60
@@ -78,18 +78,20 @@ Start-Sleep -Seconds 60
 
 # Downloading demo database and restoring onto Postgres
 Write-Host "Downloading AdventureWorks.sql template for Postgres... (1/3)"
-kubectl exec $pgControllerPodName -n arc -c postgres -- /bin/bash -c "curl -o /tmp/AdventureWorks2019.sql 'https://jumpstart.blob.core.windows.net/jumpstartbaks/AdventureWorks2019.sql?sp=r&st=2021-09-08T21:04:16Z&se=2030-09-09T05:04:16Z&spr=https&sv=2020-08-04&sr=b&sig=MJHGMyjV5Dh5gqyvfuWRSsCb4IMNfjnkM%2B05F%2F3mBm8%3D'" 2>&1 | Out-Null
+kubectl exec $pgWorkerPodName -n arc -c postgres -- /bin/bash -c "curl -o /tmp/AdventureWorks2019.sql 'https://jumpstart.blob.core.windows.net/jumpstartbaks/AdventureWorks2019.sql?sp=r&st=2021-09-08T21:04:16Z&se=2030-09-09T05:04:16Z&spr=https&sv=2020-08-04&sr=b&sig=MJHGMyjV5Dh5gqyvfuWRSsCb4IMNfjnkM%2B05F%2F3mBm8%3D'" 2>&1 | Out-Null
 Write-Host "Creating AdventureWorks database on Postgres... (2/3)"
-kubectl exec $pgControllerPodName -n arc -c postgres -- psql -U postgres -c 'CREATE DATABASE "adventureworks2019";' postgres 2>&1 | Out-Null
+kubectl exec $pgWorkerPodName -n arc -c postgres -- psql -U postgres -c 'CREATE DATABASE "adventureworks2019";' postgres 2>&1 | Out-Null
 Write-Host "Restoring AdventureWorks database on Postgres. (3/3)"
-kubectl exec $pgControllerPodName -n arc -c postgres -- psql -U postgres -d adventureworks2019 -f /tmp/AdventureWorks2019.sql 2>&1 | Out-Null
+kubectl exec $pgWorkerPodName -n arc -c postgres -- psql -U postgres -d adventureworks2019 -f /tmp/AdventureWorks2019.sql 2>&1 | Out-Null
 
 # Creating Azure Data Studio settings for PostgreSQL connection
 Write-Host ""
 Write-Host "Creating Azure Data Studio settings for PostgreSQL connection"
 $settingsTemplate = "$Env:ArcBoxDir\settingsTemplate.json"
 # Retrieving PostgreSQL connection endpoint
-$pgsqlstring = kubectl get postgresql jumpstartps -n arc -o=jsonpath='{.status.primaryEndpoint}'
+# $pgsqlstring = kubectl get postgresql jumpstartps -n arc -o=jsonpath='{.status.primaryEndpoint}'
+$pgsqlstring = kubectl get service/jumpstartps-external-svc -n arc --output jsonpath='{.status.loadBalancer.ingress[0].ip}'
+$pgsqlstring = $pgsqlstring + ":15432"
 
 # Replace placeholder values in settingsTemplate.json
 (Get-Content -Path $settingsTemplate) -replace 'arc_postgres_host',$pgsqlstring.split(":")[0] | Set-Content -Path $settingsTemplate
