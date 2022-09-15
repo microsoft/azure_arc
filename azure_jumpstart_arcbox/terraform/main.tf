8be2b833a2c7bc3a5d65021e9b8b2d21e6e038d8
Seif Bassem <38246040+sebassem@users.noreply.github.com>
2022-09-15 20:17:13 +0200

    missing hyphen

--- azure_jumpstart_arcbox/terraform/main.tf
+++ azure_jumpstart_arcbox/terraform/main.tf
@@ -260,7 +260,7 @@ module "capi_vm" {
   workspace_name             = var.workspace_name
   deploy_bastion             = var.deploy_bastion
   deployment_flavor          = var.deployment_flavor
-  capi_arc_data_cluster_name = "${local.capi_arc_data_cluster_name}${random_string.guid.result}"
+  capi_arc_data_cluster_name = "${local.capi_arc_data_cluster_name}-${random_string.guid.result}"
 
   depends_on = [
     azurerm_resource_group.rg,
@@ -276,7 +276,7 @@ module "rancher_vm" {
   count  = contains(["Full", "DevOps"], var.deployment_flavor) ? 1 : 0
 
   resource_group_name  = azurerm_resource_group.rg.name
-  vm_name              = "${local.k3s_arc_data_cluster_name}${random_string.guid.result}"
+  vm_name              = "${local.k3s_arc_data_cluster_name}-${random_string.guid.result}"
   virtual_network_name = var.virtual_network_name
   subnet_name          = var.subnet_name
   template_base_url    = local.template_base_url
