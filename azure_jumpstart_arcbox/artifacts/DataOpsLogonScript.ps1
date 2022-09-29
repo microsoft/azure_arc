3f523334a3d1e6523dadb0911f7dfe3e4a59e8cb
Seif Bassem <38246040+sebassem@users.noreply.github.com>
2022-09-29 17:17:53 +0200

    added nested sql in job

--- azure_jumpstart_arcbox/artifacts/DataOpsLogonScript.ps1
+++ azure_jumpstart_arcbox/artifacts/DataOpsLogonScript.ps1
@@ -92,109 +92,118 @@ Write-Host "`n"
 ################################################
 
 Write-Header "Creating Nested SQL VM"
+Start-Job -Name arcbox -ScriptBlock {
+    $ErrorActionPreference = "SilentlyContinue"
+    Start-Transcript -Path "$Env:ArcBoxLogsDir\NestedSQL.log"
+    # Install and configure DHCP service (used by Hyper-V nested VMs)
+    Write-Host "Configuring DHCP Service"
+    $dnsClient = Get-DnsClient | Where-Object { $_.InterfaceAlias -eq "Ethernet" }
+    Add-DhcpServerv4Scope -Name "ArcBox" `
+        -StartRange 10.10.1.100 `
+        -EndRange 10.10.1.200 `
+        -SubnetMask 255.255.255.0 `
+        -LeaseDuration 1.00:00:00 `
+        -State Active
 
-# Install and configure DHCP service (used by Hyper-V nested VMs)
-Write-Host "Configuring DHCP Service"
-$dnsClient = Get-DnsClient | Where-Object { $_.InterfaceAlias -eq "Ethernet" }
-Add-DhcpServerv4Scope -Name "ArcBox" `
-    -StartRange 10.10.1.100 `
-    -EndRange 10.10.1.200 `
-    -SubnetMask 255.255.255.0 `
-    -LeaseDuration 1.00:00:00 `
-    -State Active
-
-Set-DhcpServerv4OptionValue -ComputerName localhost `
-    -DnsDomain $dnsClient.ConnectionSpecificSuffix `
-    -DnsServer 168.63.129.16, 10.16.2.100 `
-    -Router 10.10.1.1 `
-    -Force
-
-
-Add-DhcpServerInDC -DnsName "arcbox-client.jumpstart.local"
-Restart-Service dhcpserver
-
-# Create the NAT network
-Write-Host "Creating Internal NAT"
-$natName = "InternalNat"
-New-NetNat -Name $natName -InternalIPInterfaceAddressPrefix 10.10.1.0/24
-
-# Create an internal switch with NAT
-Write-Host "Creating Internal vSwitch"
-$switchName = 'InternalNATSwitch'
-New-VMSwitch -Name $switchName -SwitchType Internal
-$adapter = Get-NetAdapter | Where-Object { $_.Name -like "*" + $switchName + "*" }
-
-# Create an internal network (gateway first)
-Write-Host "Creating Gateway"
-New-NetIPAddress -IPAddress 10.10.1.1 -PrefixLength 24 -InterfaceIndex $adapter.ifIndex
-
-# Enable Enhanced Session Mode on Host
-Write-Host "Enabling Enhanced Session Mode"
-Set-VMHost -EnableEnhancedSessionMode $true
-
-Write-Host "Fetching Nested VMs"
-$sourceFolder = 'https://jumpstart.blob.core.windows.net/v2images'
-$sas = "?sp=rl&st=2022-01-27T01:47:01Z&se=2025-01-27T09:47:01Z&spr=https&sv=2020-08-04&sr=c&sig=NB8g7f4JT3IM%2FL6bUfjFdmnGIqcc8WU015socFtkLYc%3D"
-$Env:AZCOPY_BUFFER_GB = 4
-Write-Output "Downloading nested VMs VHDX file for SQL. This can take some time, hold tight..."
-azcopy cp "$sourceFolder/ArcBox-SQL.vhdx$sas" "$Env:ArcBoxVMDir\ArcBox-SQL.vhdx" --check-length=false --log-level=ERROR
+    Set-DhcpServerv4OptionValue -ComputerName localhost `
+        -DnsDomain $dnsClient.ConnectionSpecificSuffix `
+        -DnsServer 168.63.129.16, 10.16.2.100 `
+        -Router 10.10.1.1 `
+        -Force
 
 
-# Create the nested SQL VM
-Write-Host "Create Hyper-V VMs"
-New-VM -Name ArcBox-SQL -MemoryStartupBytes 12GB -BootDevice VHD -VHDPath "$Env:ArcBoxVMDir\ArcBox-SQL.vhdx" -Path $Env:ArcBoxVMDir -Generation 2 -Switch $switchName
-Set-VMProcessor -VMName ArcBox-SQL -Count 2
+    Add-DhcpServerInDC -DnsName "arcbox-client.jumpstart.local"
+    Restart-Service dhcpserver
 
-# We always want the VMs to start with the host and shut down cleanly with the host
-Write-Host "Set VM Auto Start/Stop"
-Set-VM -Name ArcBox-SQL -AutomaticStartAction Start -AutomaticStopAction ShutDown
+    # Create the NAT network
+    Write-Host "Creating Internal NAT"
+    $natName = "InternalNat"
+    New-NetNat -Name $natName -InternalIPInterfaceAddressPrefix 10.10.1.0/24
 
-Write-Host "Enabling Guest Integration Service"
-Get-VM | Get-VMIntegrationService | Where-Object { -not($_.Enabled) } | Enable-VMIntegrationService -Verbose
+    # Create an internal switch with NAT
+    Write-Host "Creating Internal vSwitch"
+    $switchName = 'InternalNATSwitch'
+    New-VMSwitch -Name $switchName -SwitchType Internal
+    $adapter = Get-NetAdapter | Where-Object { $_.Name -like "*" + $switchName + "*" }
 
-# Start all the VMs
-Write-Host "Starting SQL VM"
-Start-VM -Name ArcBox-SQL
+    # Create an internal network (gateway first)
+    Write-Host "Creating Gateway"
+    New-NetIPAddress -IPAddress 10.10.1.1 -PrefixLength 24 -InterfaceIndex $adapter.ifIndex
+
+    # Enable Enhanced Session Mode on Host
+    Write-Host "Enabling Enhanced Session Mode"
+    Set-VMHost -EnableEnhancedSessionMode $true
+
+    Write-Host "Fetching Nested VMs"
+    $sourceFolder = 'https://jumpstart.blob.core.windows.net/v2images'
+    $sas = "?sp=rl&st=2022-01-27T01:47:01Z&se=2025-01-27T09:47:01Z&spr=https&sv=2020-08-04&sr=c&sig=NB8g7f4JT3IM%2FL6bUfjFdmnGIqcc8WU015socFtkLYc%3D"
+    $Env:AZCOPY_BUFFER_GB = 4
+    Write-Output "Downloading nested VMs VHDX file for SQL. This can take some time, hold tight..."
+    azcopy cp "$sourceFolder/ArcBox-SQL.vhdx$sas" "$Env:ArcBoxVMDir\ArcBox-SQL.vhdx" --check-length=false --log-level=ERROR
 
 
-Write-Host "Creating VM Credentials"
-# Hard-coded username and password for the nested VMs
-$nestedWindowsUsername = "Administrator"
-$nestedWindowsPassword = "ArcDemo123!!"
+    # Create the nested SQL VM
+    Write-Host "Create Hyper-V VMs"
+    New-VM -Name ArcBox-SQL -MemoryStartupBytes 12GB -BootDevice VHD -VHDPath "$Env:ArcBoxVMDir\ArcBox-SQL.vhdx" -Path $Env:ArcBoxVMDir -Generation 2 -Switch $switchName
+    Set-VMProcessor -VMName ArcBox-SQL -Count 2
 
-# Create Windows credential object
-$secWindowsPassword = ConvertTo-SecureString $nestedWindowsPassword -AsPlainText -Force
-$winCreds = New-Object System.Management.Automation.PSCredential ($nestedWindowsUsername, $secWindowsPassword)
+    # We always want the VMs to start with the host and shut down cleanly with the host
+    Write-Host "Set VM Auto Start/Stop"
+    Set-VM -Name ArcBox-SQL -AutomaticStartAction Start -AutomaticStopAction ShutDown
 
-# Restarting Windows VM Network Adapters
-Write-Host "Restarting Network Adapters"
-Start-Sleep -Seconds 20
-Invoke-Command -VMName ArcBox-SQL -ScriptBlock { Get-NetAdapter | Restart-NetAdapter } -Credential $winCreds
-Start-Sleep -Seconds 5
+    Write-Host "Enabling Guest Integration Service"
+    Get-VM | Get-VMIntegrationService | Where-Object { -not($_.Enabled) } | Enable-VMIntegrationService -Verbose
 
-# Configuring the local SQL VM
-Write-Host "Setting local SQL authentication and adding a SQL login"
-$localSQLUser = $Env:AZDATA_USERNAME
-$localSQLPassword = $Env:AZDATA_PASSWORD
-Invoke-Command -VMName ArcBox-SQL -Credential $winCreds -ScriptBlock {
-    Install-Module -Name SqlServer -AllowClobber -Force
-    $server = "localhost"
-    $user = $Using:localSQLUser
-    $LoginType = "SqlLogin"
-    $pass = ConvertTo-SecureString -String $Using:localSQLPassword -AsPlainText -Force
-    $Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $user, $pass
-    Add-SqlLogin -ServerInstance $Server -LoginName $User -LoginType $LoginType -DefaultDatabase AdventureWorksLT2019 -Enable -GrantConnectSql -LoginPSCredential $Credential
-    $svr = New-Object ('Microsoft.SqlServer.Management.Smo.Server') $server
-    $svr.Settings.LoginMode = [Microsoft.SqlServer.Management.SMO.ServerLoginMode]::Mixed
-    $svr.Alter()
-    Restart-Service -Force MSSQLSERVER
-    $svrole = $svr.Roles | where { $_.Name -eq 'sysadmin' }
-    $svrole.AddMember($user)
+    # Start all the VMs
+    Write-Host "Starting SQL VM"
+    Start-VM -Name ArcBox-SQL
+
+
+    Write-Host "Creating VM Credentials"
+    # Hard-coded username and password for the nested VMs
+    $nestedWindowsUsername = "Administrator"
+    $nestedWindowsPassword = "ArcDemo123!!"
+
+    # Create Windows credential object
+    $secWindowsPassword = ConvertTo-SecureString $nestedWindowsPassword -AsPlainText -Force
+    $winCreds = New-Object System.Management.Automation.PSCredential ($nestedWindowsUsername, $secWindowsPassword)
+
+    # Restarting Windows VM Network Adapters
+    Write-Host "Restarting Network Adapters"
+    Start-Sleep -Seconds 20
+    Invoke-Command -VMName ArcBox-SQL -ScriptBlock { Get-NetAdapter | Restart-NetAdapter } -Credential $winCreds
+    Start-Sleep -Seconds 5
+
+    # Configuring the local SQL VM
+    Write-Host "Setting local SQL authentication and adding a SQL login"
+    $localSQLUser = $Env:AZDATA_USERNAME
+    $localSQLPassword = $Env:AZDATA_PASSWORD
+    Invoke-Command -VMName ArcBox-SQL -Credential $winCreds -ScriptBlock {
+        Install-Module -Name SqlServer -AllowClobber -Force
+        $server = "localhost"
+        $user = $Using:localSQLUser
+        $LoginType = "SqlLogin"
+        $pass = ConvertTo-SecureString -String $Using:localSQLPassword -AsPlainText -Force
+        $Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $user, $pass
+        Add-SqlLogin -ServerInstance $Server -LoginName $User -LoginType $LoginType -DefaultDatabase AdventureWorksLT2019 -Enable -GrantConnectSql -LoginPSCredential $Credential
+        $svr = New-Object ('Microsoft.SqlServer.Management.Smo.Server') $server
+        $svr.Settings.LoginMode = [Microsoft.SqlServer.Management.SMO.ServerLoginMode]::Mixed
+        $svr.Alter()
+        Restart-Service -Force MSSQLSERVER
+        $svrole = $svr.Roles | where { $_.Name -eq 'sysadmin' }
+        $svrole.AddMember($user)
+    }
+
+    # Creating Hyper-V Manager desktop shortcut
+    Write-Host "Creating Hyper-V Shortcut"
+    Copy-Item -Path "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Administrative Tools\Hyper-V Manager.lnk" -Destination "C:\Users\All Users\Desktop" -Force
+    Stop-Transcript
 }
 
-# Creating Hyper-V Manager desktop shortcut
-Write-Host "Creating Hyper-V Shortcut"
-Copy-Item -Path "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Administrative Tools\Hyper-V Manager.lnk" -Destination "C:\Users\All Users\Desktop" -Force
+while ($(Get-Job -Name arcbox).State -eq 'Running') {
+    Receive-Job -Name arcbox -WarningAction SilentlyContinue
+    Start-Sleep -Seconds 10
+}
 
 # Downloading CAPI Kubernetes cluster kubeconfig file
 Write-Header "Downloading CAPI K8s Kubeconfig"
@@ -284,9 +293,9 @@ foreach ($cluster in $clusters) {
 # - Deploying data services on CAPI cluster
 ################################################
 
-$kubectlMonShellCapi = Start-Process -PassThru PowerShell {$host.ui.RawUI.WindowTitle = 'CAPI Cluster'; for (0 -lt 1) { kubectl get pods -n arc --kubeconfig "C:\Users\$Env:USERNAME\.kube\config-capi" ; Start-Sleep -Seconds 5; Clear-Host } }
-$kubectlMonShellAKS = Start-Process -PassThru PowerShell {$host.ui.RawUI.WindowTitle = 'AKS Cluster'; for (0 -lt 1) { kubectl get pods -n arc --kubeconfig "C:\Users\$Env:USERNAME\.kube\config-aks" ; Start-Sleep -Seconds 5; Clear-Host } }
-$kubectlMonShellAKSDr = Start-Process -PassThru PowerShell {$host.ui.RawUI.WindowTitle = 'AKS-DR Cluster'; for (0 -lt 1) { kubectl get pods -n arc --kubeconfig "C:\Users\$Env:USERNAME\.kube\config-aksdr" ; Start-Sleep -Seconds 5; Clear-Host } }
+$kubectlMonShellCapi = Start-Process -PassThru PowerShell { $host.ui.RawUI.WindowTitle = 'CAPI Cluster'; for (0 -lt 1) { kubectl get pods -n arc --kubeconfig "C:\Users\$Env:USERNAME\.kube\config-capi" ; Start-Sleep -Seconds 5; Clear-Host } }
+$kubectlMonShellAKS = Start-Process -PassThru PowerShell { $host.ui.RawUI.WindowTitle = 'AKS Cluster'; for (0 -lt 1) { kubectl get pods -n arc --kubeconfig "C:\Users\$Env:USERNAME\.kube\config-aks" ; Start-Sleep -Seconds 5; Clear-Host } }
+$kubectlMonShellAKSDr = Start-Process -PassThru PowerShell { $host.ui.RawUI.WindowTitle = 'AKS-DR Cluster'; for (0 -lt 1) { kubectl get pods -n arc --kubeconfig "C:\Users\$Env:USERNAME\.kube\config-aksdr" ; Start-Sleep -Seconds 5; Clear-Host } }
 
 Write-Header "Deploying Azure Arc Data Controller"
 foreach ($cluster in $clusters) {
