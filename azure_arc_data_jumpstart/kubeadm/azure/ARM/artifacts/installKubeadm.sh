c95b2ab63decde12bc137d58f2dbb4c99b2bd2bb
Pablo Ameijeiras Canay <pabloam@microsoft.com>
2022-09-29 22:33:59 +0200

    Update Azure Arc Data Kubeadm scenario

--- azure_arc_data_jumpstart/kubeadm/azure/ARM/artifacts/installKubeadm.sh
+++ azure_arc_data_jumpstart/kubeadm/azure/ARM/artifacts/installKubeadm.sh
@@ -59,11 +59,7 @@ az -v
 echo ""
 
 # Installing Helm 3
-curl https://baltocdn.com/helm/signing.asc | sudo apt-key add -
-sudo apt-get install apt-transport-https --yes
-echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
-sudo apt-get update
-sudo apt-get install helm
+sudo snap install helm --classic
 
 # Create Kubeadm cluster master node
 echo ""
