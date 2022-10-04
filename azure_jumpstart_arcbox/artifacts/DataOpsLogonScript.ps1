47d190a9840738d327255d7675a826722b615d9e
Seif Bassem <38246040+sebassem@users.noreply.github.com>
2022-10-04 08:53:31 +0200

    testing parallel connectedk8s

--- azure_jumpstart_arcbox/artifacts/DataOpsLogonScript.ps1
+++ azure_jumpstart_arcbox/artifacts/DataOpsLogonScript.ps1
@@ -146,6 +146,7 @@ kubectx aks-dr="$Env:aksdrArcClusterName-admin"
 kubectx capi="arcbox-capi"
 
 Start-Sleep -Seconds 10
+az extension add --name connectedk8s
 
 Write-Header "Onboarding clusters as an Azure Arc-enabled Kubernetes cluster"
 foreach ($cluster in $clusters) {
@@ -167,30 +168,11 @@ foreach ($cluster in $clusters) {
 }
 
 while ($(Get-Job -Name arcbox).State -eq 'Running') {
-    Write-Host -NoNewLine "."
-    Start-Sleep -Seconds 10
+    Receive-Job -Name arcbox -WarningAction SilentlyContinue
+    Start-Sleep -Seconds 5
 }
 write-host "Successfully Arc-enabled the Kubernetes clusters"
-get-job -name arcbox | remove-job
-
-Write-Header "Onboarding clusters as an Azure Arc-enabled Kubernetes cluster"
-foreach ($cluster in $clusters) {
-    if ($cluster.context -ne 'capi') {
-        Start-Job -Name arcbox -ScriptBlock {
-        $cluster = $using:cluster
-        Write-Host "Checking K8s Nodes"
-        kubectl get nodes --kubeconfig $cluster.kubeConfig
-        Write-Host "`n"
-        az connectedk8s connect --name $cluster.clusterName `
-            --resource-group $Env:resourceGroup `
-            --location $Env:azureLocation `
-            --correlation-id "6038cc5b-b814-4d20-bcaa-0f60392416d5" `
-            --kube-config $cluster.kubeConfig
-
-        Start-Sleep -Seconds 10
-        }
-    }
-}
+Get-job -name arcbox | Remove-job
 
 foreach ($cluster in $clusters){
       # Enabling Container Insights and Azure Policy cluster extension on Arc-enabled cluster
