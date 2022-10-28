64ada455e59cc0b6f3b4c18b7389343285467c57
likamrat <likamrat@microsoft.com>
2022-10-28 06:26:35 -0700

    implement ArcResourceName

--- azure_jumpstart_arcbox/artifacts/ArcServersLogonScript.ps1
+++ azure_jumpstart_arcbox/artifacts/ArcServersLogonScript.ps1
@@ -312,9 +312,9 @@ else {
     # Onboarding the nested VMs as Azure Arc-enabled servers
     Write-Output "Onboarding the nested Windows VMs as Azure Arc-enabled servers"
 
-    Invoke-Command -VMName "ArcBox-Win2K19" -ScriptBlock { powershell -File C:\ArcBox\installArcAgent.ps1 } -Credential $winCreds
-    Invoke-Command -VMName "ArcBox-Win2K22" -ScriptBlock { powershell -File C:\ArcBox\installArcAgent.ps1 } -Credential $winCreds
-    Invoke-Command -VMName "ArcBox-SQL" -ScriptBlock { powershell -File C:\ArcBox\installArcAgentSQL.ps1 } -Credential $winCreds
+    # Invoke-Command -VMName "ArcBox-Win2K19" -ScriptBlock { powershell -File C:\ArcBox\installArcAgent.ps1 } -Credential $winCreds
+    # Invoke-Command -VMName "ArcBox-Win2K22" -ScriptBlock { powershell -File C:\ArcBox\installArcAgent.ps1 } -Credential $winCreds
+    # Invoke-Command -VMName "ArcBox-SQL" -ScriptBlock { powershell -File C:\ArcBox\installArcAgentSQL.ps1 } -Credential $winCreds
 
     Write-Output "Onboarding the nested Linux VMs as an Azure Arc-enabled servers"
 
@@ -336,48 +336,48 @@ else {
     Write-Header "Creating Hyper-V Shortcut"
     Copy-Item -Path "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Administrative Tools\Hyper-V Manager.lnk" -Destination "C:\Users\All Users\Desktop" -Force
 
-    # Prepare ArcBox-SQL onboarding script and create shortcut on desktop if the current Service Principal doesn't have appropriate permission to onboard the VM to Azure Arc
-    if (-not $hasPermission) {
-        Write-Header "Creating Arc-enabled SQL Shortcut"
+    # # Prepare ArcBox-SQL onboarding script and create shortcut on desktop if the current Service Principal doesn't have appropriate permission to onboard the VM to Azure Arc
+    # if (-not $hasPermission) {
+    #     Write-Header "Creating Arc-enabled SQL Shortcut"
 
-        # Replace variables in Arc-enabled SQL onboarding scripts
-        $sqlServerName = "ArcBox-SQL"
+    #     # Replace variables in Arc-enabled SQL onboarding scripts
+    #     $sqlServerName = "ArcBox-SQL"
 
-    (Get-Content -path "$Env:ArcBoxDir\installArcAgentSQLUser.ps1" -Raw) -replace '<subscriptionId>', "$Env:subscriptionId" -replace '<resourceGroup>', "$Env:resourceGroup" -replace '<location>', "$Env:azureLocation" | Set-Content -Path "$Env:ArcBoxDir\installArcAgentSQLUser.ps1"
-    (Get-Content -path "$Env:ArcBoxDir\ArcSQLManualOnboarding.ps1" -Raw) -replace '<subscriptionId>', "$Env:subscriptionId" -replace '<resourceGroup>', "$Env:resourceGroup" -replace '<sqlServerName>', "$sqlServerName" | Set-Content -Path "$Env:ArcBoxDir\ArcSQLManualOnboarding.ps1"
+    # (Get-Content -path "$Env:ArcBoxDir\installArcAgentSQLUser.ps1" -Raw) -replace '<subscriptionId>', "$Env:subscriptionId" -replace '<resourceGroup>', "$Env:resourceGroup" -replace '<location>', "$Env:azureLocation" | Set-Content -Path "$Env:ArcBoxDir\installArcAgentSQLUser.ps1"
+    # (Get-Content -path "$Env:ArcBoxDir\ArcSQLManualOnboarding.ps1" -Raw) -replace '<subscriptionId>', "$Env:subscriptionId" -replace '<resourceGroup>', "$Env:resourceGroup" -replace '<sqlServerName>', "$sqlServerName" | Set-Content -Path "$Env:ArcBoxDir\ArcSQLManualOnboarding.ps1"
 
-        # Set Edge as the Default Browser
-        & SetDefaultBrowser.exe HKLM "Microsoft Edge"
+    #     # Set Edge as the Default Browser
+    #     & SetDefaultBrowser.exe HKLM "Microsoft Edge"
 
-        # Disable Edge 'First Run' Setup
-        $edgePolicyRegistryPath = 'HKLM:SOFTWARE\Policies\Microsoft\Edge'
-        $desktopSettingsRegistryPath = 'HKCU:SOFTWARE\Microsoft\Windows\Shell\Bags\1\Desktop'
-        $firstRunRegistryName = 'HideFirstRunExperience'
-        $firstRunRegistryValue = '0x00000001'
-        $savePasswordRegistryName = 'PasswordManagerEnabled'
-        $savePasswordRegistryValue = '0x00000000'
-        $autoArrangeRegistryName = 'FFlags'
-        $autoArrangeRegistryValue = '1075839525'
+    #     # Disable Edge 'First Run' Setup
+    #     $edgePolicyRegistryPath = 'HKLM:SOFTWARE\Policies\Microsoft\Edge'
+    #     $desktopSettingsRegistryPath = 'HKCU:SOFTWARE\Microsoft\Windows\Shell\Bags\1\Desktop'
+    #     $firstRunRegistryName = 'HideFirstRunExperience'
+    #     $firstRunRegistryValue = '0x00000001'
+    #     $savePasswordRegistryName = 'PasswordManagerEnabled'
+    #     $savePasswordRegistryValue = '0x00000000'
+    #     $autoArrangeRegistryName = 'FFlags'
+    #     $autoArrangeRegistryValue = '1075839525'
 
-        If (-NOT (Test-Path -Path $edgePolicyRegistryPath)) {
-            New-Item -Path $edgePolicyRegistryPath -Force | Out-Null
-        }
+    #     If (-NOT (Test-Path -Path $edgePolicyRegistryPath)) {
+    #         New-Item -Path $edgePolicyRegistryPath -Force | Out-Null
+    #     }
 
-        New-ItemProperty -Path $edgePolicyRegistryPath -Name $firstRunRegistryName -Value $firstRunRegistryValue -PropertyType DWORD -Force
-        New-ItemProperty -Path $edgePolicyRegistryPath -Name $savePasswordRegistryName -Value $savePasswordRegistryValue -PropertyType DWORD -Force
-        Set-ItemProperty -Path $desktopSettingsRegistryPath -Name $autoArrangeRegistryName -Value $autoArrangeRegistryValue -Force
+    #     New-ItemProperty -Path $edgePolicyRegistryPath -Name $firstRunRegistryName -Value $firstRunRegistryValue -PropertyType DWORD -Force
+    #     New-ItemProperty -Path $edgePolicyRegistryPath -Name $savePasswordRegistryName -Value $savePasswordRegistryValue -PropertyType DWORD -Force
+    #     Set-ItemProperty -Path $desktopSettingsRegistryPath -Name $autoArrangeRegistryName -Value $autoArrangeRegistryValue -Force
 
-        # Creating Arc-enabled SQL Server onboarding desktop shortcut
-        $sourceFileLocation = "${Env:ArcBoxDir}\ArcSQLManualOnboarding.ps1"
-        $shortcutLocation = "$Env:Public\Desktop\Onboard SQL Server.lnk"
-        $wScriptShell = New-Object -ComObject WScript.Shell
-        $shortcut = $wScriptShell.CreateShortcut($shortcutLocation)
-        $shortcut.TargetPath = "powershell.exe"
-        $shortcut.Arguments = "-ExecutionPolicy Bypass -File $sourceFileLocation"
-        $shortcut.IconLocation = "${Env:ArcBoxIconDir}\arcsql.ico, 0"
-        $shortcut.WindowStyle = 3
-        $shortcut.Save()
-    }
+    #     # Creating Arc-enabled SQL Server onboarding desktop shortcut
+    #     $sourceFileLocation = "${Env:ArcBoxDir}\ArcSQLManualOnboarding.ps1"
+    #     $shortcutLocation = "$Env:Public\Desktop\Onboard SQL Server.lnk"
+    #     $wScriptShell = New-Object -ComObject WScript.Shell
+    #     $shortcut = $wScriptShell.CreateShortcut($shortcutLocation)
+    #     $shortcut.TargetPath = "powershell.exe"
+    #     $shortcut.Arguments = "-ExecutionPolicy Bypass -File $sourceFileLocation"
+    #     $shortcut.IconLocation = "${Env:ArcBoxIconDir}\arcsql.ico, 0"
+    #     $shortcut.WindowStyle = 3
+    #     $shortcut.Save()
+    # }
 
     # Changing to Jumpstart ArcBox wallpaper
     $code = @' 
