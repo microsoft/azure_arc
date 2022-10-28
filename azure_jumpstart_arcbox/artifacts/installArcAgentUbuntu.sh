6c230c105ee2c8c696a62a5aaf07b898641448c0
likamrat <likamrat@microsoft.com>
2022-10-27 18:38:54 -0700

    implement ArcResourceName

--- azure_jumpstart_arcbox/artifacts/installArcAgentUbuntu.sh
+++ azure_jumpstart_arcbox/artifacts/installArcAgentUbuntu.sh
@@ -14,4 +14,4 @@ bash ~/install_linux_azcmagent.sh # 2>/dev/null
 export ArcResourceName="$(echo $HOSTNAME | sed 's/\b\([[:alpha:]]\)\([[:alpha:]]*\)\b/\u\1\L\2/g')"
 
 # Run connect command
-azcmagent connect --service-principal-id $spnClientId --service-principal-secret $spnClientSecret --resource-group $resourceGroup --tenant-id $spnTenantId --location $Azurelocation --subscription-id $subscriptionId --resource-name $ArcResourceName --cloud "AzureCloud" --tags "Project=jumpstart_arcbox" --correlation-id "d009f5dd-dba8-4ac7-bac9-b54ef3a6671a"
+azcmagent connect --service-principal-id $spnClientId --service-principal-secret $spnClientSecret --resource-group $resourceGroup --tenant-id $spnTenantId --location $Azurelocation --subscription-id $subscriptionId --resource-name "$ArcResourceName" --cloud "AzureCloud" --tags "Project=jumpstart_arcbox" --correlation-id "d009f5dd-dba8-4ac7-bac9-b54ef3a6671a"
