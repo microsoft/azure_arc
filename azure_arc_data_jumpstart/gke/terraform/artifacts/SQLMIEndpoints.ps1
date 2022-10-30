3d0e58c8f1ab9d652e90d19a13c61215e8591ec3
pabloameijeirascanay <pabloam@microsoft.com>
2022-10-30 22:20:27 +0100

    Update GKE Azure Arc–Enabled Data Services Scenario

--- azure_arc_data_jumpstart/gke/terraform/artifacts/SQLMIEndpoints.ps1
+++ azure_arc_data_jumpstart/gke/terraform/artifacts/SQLMIEndpoints.ps1
@@ -10,7 +10,7 @@ $primaryEndpoint = kubectl get sqlmanagedinstances jumpstart-sql -n arc -o=jsonp
 $primaryEndpoint = $primaryEndpoint.Substring(0, $primaryEndpoint.IndexOf(',')) + ",11433" | Add-Content $Endpoints
 Add-Content $Endpoints ""
 
-if ( $env:SQLMIHA -eq $true )
+if ( $Env:SQLMIHA -eq $true )
 {
     Add-Content $Endpoints "Secondary SQL Managed Instance external endpoint:"
     $secondaryEndpoint = kubectl get sqlmanagedinstances jumpstart-sql -n arc -o=jsonpath='{.status.endpoints.secondary}'
@@ -20,17 +20,17 @@ if ( $env:SQLMIHA -eq $true )
 # Retrieving SQL MI connection username and password
 Add-Content $Endpoints ""
 Add-Content $Endpoints "SQL Managed Instance username:"
-$env:AZDATA_USERNAME | Add-Content $Endpoints
+$Env:AZDATA_USERNAME | Add-Content $Endpoints
 
 Add-Content $Endpoints ""
 Add-Content $Endpoints "SQL Managed Instance password:"
-$env:AZDATA_PASSWORD | Add-Content $Endpoints
+$Env:AZDATA_PASSWORD | Add-Content $Endpoints
 
 Write-Host "`n"
 Write-Host "Creating SQLMI Endpoints file Desktop shortcut"
 Write-Host "`n"
 $TargetFile = $Endpoints
-$ShortcutFile = "C:\Users\$env:adminUsername\Desktop\SQLMI Endpoints.lnk"
+$ShortcutFile = "C:\Users\$Env:adminUsername\Desktop\SQLMI Endpoints.lnk"
 $WScriptShell = New-Object -ComObject WScript.Shell
 $Shortcut = $WScriptShell.CreateShortcut($ShortcutFile)
 $Shortcut.TargetPath = $TargetFile
