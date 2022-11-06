3392a5e649db52e14eb4bb9a0fddb1e40af1c3b4
likamrat <likamrat@microsoft.com>
2022-11-06 05:59:30 -0800

    remove password

--- azure_arc_k8s_jumpstart/rancher_k3s/azure/arm_template/scripts/installK3s.sh
+++ azure_arc_k8s_jumpstart/rancher_k3s/azure/arm_template/scripts/installK3s.sh
@@ -16,14 +16,14 @@ echo $password:$3 | awk '{print substr($1,2); }' >> vars.sh
 echo $tenantId:$4 | awk '{print substr($1,2); }' >> vars.sh
 echo $vmName:$5 | awk '{print substr($1,2); }' >> vars.sh
 echo $k3sArcClusterName:$6 | awk '{print substr($1,2); }' >> vars.sh
-echo $location:$7 | awk '{print substr($1,2); }' >> vars.sh
+echo $azureLocation:$7 | awk '{print substr($1,2); }' >> vars.sh
 sed -i '2s/^/export adminUsername=/' vars.sh
 sed -i '3s/^/export appId=/' vars.sh
 sed -i '4s/^/export password=/' vars.sh
 sed -i '5s/^/export tenantId=/' vars.sh
 sed -i '6s/^/export vmName=/' vars.sh
 sed -i '7s/^/export k3sArcClusterName=/' vars.sh
-sed -i '8s/^/export location=/' vars.sh
+sed -i '8s/^/export azureLocation=/' vars.sh
 
 chmod +x vars.sh 
 . ./vars.sh
@@ -74,9 +74,8 @@ sudo -u $adminUsername az login --service-principal --username $appId --password
 # Onboard the cluster to Azure Arc and enabling Container Insights using Kubernetes extension
 echo ""
 resourceGroup=$(sudo -u $adminUsername az resource list --query "[?name=='$vmName']".[resourceGroup] --resource-type "Microsoft.Compute/virtualMachines" -o tsv)
-echo $resourceGroup
-sudo -u $adminUsername az connectedk8s connect --name $vmName --resource-group $resourceGroup --location $location --kube-config /home/${adminUsername}/.kube/config --tags 'Project=jumpstart_azure_arc_k8s' --correlation-id "d009f5dd-dba8-4ac7-bac9-b54ef3a6671a"
-sudo -u $adminUsername az k8s-extension create -n "azuremonitor-containers" --cluster-name $vmName --resource-group $resourceGroup --cluster-type connectedClusters --extension-type Microsoft.AzureMonitor.Containers
+sudo -u $adminUsername az connectedk8s connect --name $k3sArcClusterName --resource-group $resourceGroup --location $azureLocation --kube-config /home/${adminUsername}/.kube/config --tags 'Project=jumpstart_azure_arc_k8s' --correlation-id "d009f5dd-dba8-4ac7-bac9-b54ef3a6671a"
+sudo -u $adminUsername az k8s-extension create -n "azuremonitor-containers" --cluster-name $k3sArcClusterName --resource-group $resourceGroup --cluster-type connectedClusters --extension-type Microsoft.AzureMonitor.Containers
 
 sudo service sshd restart
 
