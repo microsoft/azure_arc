4975ac9a093d35c270a75e0a473d0510c4326ddb
saitcho <saitcho@outlook.com>
2022-11-11 15:56:15 -0600

    goofy

--- azure_jumpstart_hcibox/artifacts/Deploy-ArcResourceBridge.ps1
+++ azure_jumpstart_hcibox/artifacts/Deploy-ArcResourceBridge.ps1
@@ -93,11 +93,12 @@ $spnTenantId = $env:spnTenantId
 $resource_name = "HCIBox-ResourceBridge"
 $location = "eastus"
 $custom_location_name = "hcibox-rb-cl"
+$cloudServiceIP = $SDNConfig.AKSCloudSvcidr.Substring(0, $SDNConfig.AKSCloudSvcidr.IndexOf('/'))
 
 if ($env:deployAKSHCI -eq "false") {
     Invoke-Command -VMName $SDNConfig.HostList[0] -Credential $adcred -ScriptBlock {
         $vnet = New-MocNetworkSetting -Name $using:SDNConfig.AKSvnetname -vswitchName $using:SDNConfig.AKSvSwitchName -vipPoolStart $using:SDNConfig.AKSVIPStartIP -vipPoolEnd $using:SDNConfig.AKSVIPEndIP
-        Set-MocConfig -workingDir $using:csv_path\ResourceBridge -vnet $vnet -imageDir $using:csv_path\imageStore -skipHostLimitChecks -cloudConfigLocation $using:csv_path\cloudStore -catalog aks-hci-stable-catalogs-ext -ring stable -CloudServiceIP $using:SDNConfig.AKSCloudSvcidr -createAutoConfigContainers $false
+        Set-MocConfig -workingDir $using:csv_path\ResourceBridge -vnet $vnet -imageDir $using:csv_path\imageStore -skipHostLimitChecks -cloudConfigLocation $using:csv_path\cloudStore -catalog aks-hci-stable-catalogs-ext -ring stable -CloudServiceIP $using:cloudServiceIP -createAutoConfigContainers $false
         Install-Moc
     }
 }
