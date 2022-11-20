361c4774d1b4db20a225ac19be40d1c4da503d70
Seif Bassem <38246040+sebassem@users.noreply.github.com>
2022-11-20 18:14:43 +0200

    downloading data controller files

--- azure_jumpstart_hcibox/artifacts/Deploy-SQLMI.ps1
+++ azure_jumpstart_hcibox/artifacts/Deploy-SQLMI.ps1
@@ -140,21 +140,20 @@ Invoke-Command -VMName $SDNConfig.HostList  -Credential $adcred -ScriptBlock {
     Write-Host "`n"
 }
 
-Invoke-Command -VMName AzSMGMT -Credential $localcred -ScriptBlock {
-    Copy-VMFile AdminCenter -SourcePath "C:\HCIBoxDir\dataController.json" -DestinationPath "C:\vhd\dataController.json" -FileSource Host
-    Copy-VMFile AdminCenter -SourcePath "C:\HCIBoxDir\dataController.parameters.json" -DestinationPath "C:\vhd\dataController.parameters.json" -FileSource Host
+# Downloading ARM templates for Azure Arc Data Controller
+Invoke-WebRequest ($env:templateBaseUrl + "artifacts/dataController.json") -OutFile $Env:HCIBoxKVDir\dataController.json
+Invoke-WebRequest ($env:templateBaseUrl + "artifacts/dataController.parameters.json") -OutFile $Env:HCIBoxKVDir\dataController.parameters.json
+Copy-VMFile $SDNConfig.HostList[0] -SourcePath "$Env:HCIBoxKVDir\dataController.json" -DestinationPath "C:\VHD\dataController.json" -FileSource Host
+Copy-VMFile $SDNConfig.HostList[0] -SourcePath "$Env:HCIBoxKVDir\dataController.parameters.json" -DestinationPath "C:\VHD\dataController.parameters.json" -FileSource Host
 
-}
 
 # Configuring Azure Arc Custom Location on the cluster
-Write-Header "Configuring Azure Arc Custom Location"
-$connectedClusterId = az connectedk8s show --name $clusterName --resource-group $Env:resourceGroup --query id -o tsv
-$extensionId = az k8s-extension show --name arc-data-services --cluster-type connectedClusters --cluster-name $clusterName --resource-group $Env:resourceGroup --query id -o tsv
-Start-Sleep -Seconds 20
-az customlocation create --name "jumpstart-cl" --resource-group $Env:resourceGroup --namespace arc --host-resource-id $connectedClusterId --cluster-extension-ids $extensionId
-
-# Deploying Azure Arc Data Controller
 Invoke-Command -VMName $SDNConfig.HostList  -Credential $adcred -ScriptBlock {
+    Write-Header "Configuring Azure Arc Custom Location"
+    $connectedClusterId = az connectedk8s show --name $clusterName --resource-group $Env:resourceGroup --query id -o tsv
+    $extensionId = az k8s-extension show --name arc-data-services --cluster-type connectedClusters --cluster-name $clusterName --resource-group $Env:resourceGroup --query id -o tsv
+    Start-Sleep -Seconds 20
+    az customlocation create --name "jumpstart-cl" --resource-group $Env:resourceGroup --namespace arc --host-resource-id $connectedClusterId --cluster-extension-ids $extensionId
     Write-Header "Deploying Azure Arc Data Controller"
 
     $customLocationId = $(az customlocation show --name "jumpstart-cl" --resource-group $Env:resourceGroup --query id -o tsv)
@@ -162,7 +161,7 @@ Invoke-Command -VMName $SDNConfig.HostList  -Credential $adcred -ScriptBlock {
     $workspaceId = $(az resource show --resource-group $Env:resourceGroup --name $Env:workspaceName --resource-type "Microsoft.OperationalInsights/workspaces" --query properties.customerId -o tsv)
     $workspaceKey = $(az monitor log-analytics workspace get-shared-keys --resource-group $Env:resourceGroup --workspace-name $Env:workspaceName --query primarySharedKey -o tsv)
 
-    $dataControllerParams = "C:\vhd\dataController.parameters.json"
+    $dataControllerParams = "C:\VHD\dataController.parameters.json"
 
 (Get-Content -Path $dataControllerParams) -replace 'dataControllerName-stage', "arcbox-dc" | Set-Content -Path $dataControllerParams
 (Get-Content -Path $dataControllerParams) -replace 'resourceGroup-stage', $Env:resourceGroup | Set-Content -Path $dataControllerParams
@@ -176,7 +175,7 @@ Invoke-Command -VMName $SDNConfig.HostList  -Credential $adcred -ScriptBlock {
 (Get-Content -Path $dataControllerParams) -replace 'logAnalyticsWorkspaceId-stage', $workspaceId | Set-Content -Path $dataControllerParams
 (Get-Content -Path $dataControllerParams) -replace 'logAnalyticsPrimaryKey-stage', $workspaceKey | Set-Content -Path $dataControllerParams
 
-    az deployment group create --resource-group $Env:resourceGroup --template-file "C:\vhd\dataController.json" --parameters "C:\vhd\dataController.parameters.json"
+    az deployment group create --resource-group $Env:resourceGroup --template-file "C:\VHD\dataController.json" --parameters "C:\VHD\dataController.parameters.json"
     Write-Host "`n"
 
     Do {
@@ -187,8 +186,6 @@ Invoke-Command -VMName $SDNConfig.HostList  -Credential $adcred -ScriptBlock {
     Write-Host "Azure Arc data controller is ready!"
     Write-Host "`n"
 }
-
-
 
 # Set env variable deployAKSHCI to true (in case the script was run manually)
 [System.Environment]::SetEnvironmentVariable('deploySQLMI', 'true', [System.EnvironmentVariableTarget]::Machine)
