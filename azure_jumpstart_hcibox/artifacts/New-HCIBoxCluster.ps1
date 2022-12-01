e89a1201ce0b9e7a23695d51848e7e34312f404d
Seif Bassem <38246040+sebassem@users.noreply.github.com>
2022-12-01 12:57:17 +0200

    added logic to resume failed bits jobs

--- azure_jumpstart_hcibox/artifacts/New-HCIBoxCluster.ps1
+++ azure_jumpstart_hcibox/artifacts/New-HCIBoxCluster.ps1
@@ -34,6 +34,9 @@ function BITSRequest {
     $download = Start-BitsTransfer -Source $url -Destination $filename -Asynchronous
     $ProgressPreference = "Continue"
     while ($download.JobState -ne "Transferred") {
+        if ($download.JobState -eq "TransientError"){
+            Get-BitsTransfer $download.name | Resume-BitsTransfer -Asynchronous
+        }
         [int] $dlProgress = ($download.BytesTransferred / $download.BytesTotal) * 100;
         Write-Progress -Activity "Downloading File $filename..." -Status "$dlProgress% Complete:" -PercentComplete $dlProgress; 
     }
