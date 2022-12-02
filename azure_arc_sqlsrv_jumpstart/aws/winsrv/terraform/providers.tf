97ae7e9cdcd161862a30338b17e65c27b92dab17
saitcho <saitcho@outlook.com>
2022-12-02 12:21:23 -0600

    auto delete if not empty resource group

--- azure_arc_sqlsrv_jumpstart/aws/winsrv/terraform/providers.tf
+++ azure_arc_sqlsrv_jumpstart/aws/winsrv/terraform/providers.tf
@@ -23,7 +23,11 @@ data "aws_region" "current" {}
 data "aws_availability_zones" "available" {}
 
 provider "azurerm" {
-  features {}
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
   subscription_id = var.subId
   client_id       = var.servicePrincipalAppId
   client_secret   = var.servicePrincipalSecret
