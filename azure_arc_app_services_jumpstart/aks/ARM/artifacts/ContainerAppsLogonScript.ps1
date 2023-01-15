2cc388c614a77995d7fb1b82587c414992ab886b
Zaid Mohammad <mozaid@microsoft.com>
2023-01-14 22:18:29 -0500

    Update readme

--- azure_arc_app_services_jumpstart/aks/ARM/artifacts/ContainerAppsLogonScript.ps1
+++ azure_arc_app_services_jumpstart/aks/ARM/artifacts/ContainerAppsLogonScript.ps1
@@ -160,7 +160,7 @@ $containerAppEnvId = $(az containerapp connected-env show `
      --query id `
      --output tsv)
 
-az resource wait --ids $containerAppEnvId --custom "properties.installState!='Pending'"
+az resource wait --ids $containerAppEnvId --created
 
 # Deploying Products API Container App
 Write-Host "`n"
