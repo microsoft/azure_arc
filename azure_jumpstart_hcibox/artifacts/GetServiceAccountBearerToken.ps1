e9bba7e8dd239bf925e1a6b8844515788341cae1
saitcho <saitcho@outlook.com>
2023-01-17 15:51:38 -0600

    jan2_fixes

--- azure_jumpstart_hcibox/artifacts/GetServiceAccountBearerToken.ps1
+++ azure_jumpstart_hcibox/artifacts/GetServiceAccountBearerToken.ps1
@@ -2,6 +2,6 @@ kubectl create serviceaccount jumpstart-user
 
 kubectl create clusterrolebinding jumpstart-user-binding --clusterrole cluster-admin --serviceaccount default:jumpstart-user
 
-kubectl apply -f demo-user-secret.yaml
+kubectl apply -f jumpstart-user-secret.yaml
 
-$TOKEN = ([System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String((kubectl get secret demo-user-secret -o jsonpath='{$.data.token}'))))
+$TOKEN = ([System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String((kubectl get secret jumpstart-user-secret -o jsonpath='{$.data.token}'))))
