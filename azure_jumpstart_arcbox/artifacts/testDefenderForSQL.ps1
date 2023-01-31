70c0247529a1c9c838111af5442d91c6d6da7702
Venkata Chintala <29983008+chintalavr@users.noreply.github.com>
2023-01-31 11:42:14 -0500

    Update testDefenderForSQL.ps1

--- azure_jumpstart_arcbox/artifacts/testDefenderForSQL.ps1
+++ azure_jumpstart_arcbox/artifacts/testDefenderForSQL.ps1
@@ -4,7 +4,13 @@ $attempts = 0
 
 while ($attempts -le 5)
 {
-    $moduleFile = (Get-ChildItem -Path "$Env:ProgramFiles\Microsoft Monitoring Agent\Agent\Health Service State\Resources\" -File SqlAdvancedThreatProtectionShell.psm1 -Recurse).FullName
+    try {
+        $moduleFile = (Get-ChildItem -Path "$Env:ProgramFiles\Microsoft Monitoring Agent\Agent\Health Service State\Resources\" -File SqlAdvancedThreatProtectionShell.psm1 -Recurse).FullName
+    }
+    catch {
+        <#Do this if a terminating exception happens#>
+    }
+
     $attempts = $attempts + 1
     if ($true -eq [System.IO.File]::Exists($moduleFile))
     {
@@ -13,14 +19,14 @@ while ($attempts -le 5)
     }
     else 
     {
-        Write-Error "Module file $moduleFile not installed. Waiting for the module to be installed. Attempt: $attempts"
+        Write-Information "Module file $moduleFile not installed. Waiting for the module to be installed. Attempt: $attempts"
         Start-Sleep(60) # Wait for agent to isntall all modules
     }
 }
 
 if ($true -ne [System.IO.File]::Exists($moduleFile))
 {
-    Write-Error "Module file $moduleFile not installed. Try running script mannually later."
+    Write-Information "Module file $moduleFile not installed. Try running script mannually later. Search for PowerShell module file 'SqlAdvancedThreatProtectionShell.psm1' in one of the '$Env:ProgramFiles\Microsoft Monitoring Agent\Agent\Health Service State\Resources\' sub folders to re-run this test script."
     Exit
 }
 
