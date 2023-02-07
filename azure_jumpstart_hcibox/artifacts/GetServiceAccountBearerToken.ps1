7d333d1b596f6b7747339a8a7e2f81f3f123e588
saitcho <saitcho@outlook.com>
2023-02-07 14:37:31 -0600

    generate k8s bearer token

--- azure_jumpstart_hcibox/artifacts/GetServiceAccountBearerToken.ps1
+++ azure_jumpstart_hcibox/artifacts/GetServiceAccountBearerToken.ps1
@@ -5,3 +5,5 @@ kubectl create clusterrolebinding jumpstart-user-binding --clusterrole cluster-a
 kubectl apply -f jumpstart-user-secret.yaml
 
 $TOKEN = ([System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String((kubectl get secret jumpstart-user-secret -o jsonpath='{$.data.token}'))))
+
+Write-Console $TOKEN
