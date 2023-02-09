12b017309ec9ceb57a9e3b21f1a3dcec03291ff6
pabloameijeirascanay <pabloam@microsoft.com>
2023-02-09 21:02:17 +0100

    AKS Edge Essentials Scenario Update

--- azure_arc_k8s_jumpstart/aks_hybrid/aks_edge_essentials/arm_template/artifacts/Bootstrap.ps1
+++ azure_arc_k8s_jumpstart/aks_hybrid/aks_edge_essentials/arm_template/artifacts/Bootstrap.ps1
@@ -7,7 +7,7 @@ param (
     [string]$location,
     [string]$templateBaseUrl,
     [string]$resourceGroup,
-    # [string]$windowsNode,
+    [string]$windowsNode,
     [string]$kubernetesDistribution
 )
 
@@ -20,7 +20,7 @@ param (
 [System.Environment]::SetEnvironmentVariable('subscriptionId', $subscriptionId,[System.EnvironmentVariableTarget]::Machine)
 [System.Environment]::SetEnvironmentVariable('templateBaseUrl', $templateBaseUrl,[System.EnvironmentVariableTarget]::Machine)
 [System.Environment]::SetEnvironmentVariable('kubernetesDistribution', $kubernetesDistribution,[System.EnvironmentVariableTarget]::Machine)
-# [System.Environment]::SetEnvironmentVariable('windowsNode', $windowsNode,[System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable('windowsNode', $windowsNode,[System.EnvironmentVariableTarget]::Machine)
 
 # Create path
 Write-Output "Create deployment path"
