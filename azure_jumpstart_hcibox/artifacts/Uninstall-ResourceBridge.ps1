1eabc8a46d24cb328d844012eee2952748cb40a1
saitcho <saitcho@outlook.com>
2023-02-16 15:54:52 -0600

    .

--- azure_jumpstart_hcibox/artifacts/Uninstall-ResourceBridge.ps1
+++ azure_jumpstart_hcibox/artifacts/Uninstall-ResourceBridge.ps1
@@ -22,6 +22,7 @@ $user = "jumpstart.local\administrator"
 $password = ConvertTo-SecureString -String $SDNConfig.SDNAdminPassword -AsPlainText -Force
 $adcred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $user, $password
 
+$csv_path = "C:\ClusterStorage\S2D_vDISK1"
 $subId = $env:subscriptionId
 $rg = $env:resourceGroup
 $spnClientId = $env:spnClientId
@@ -41,6 +42,6 @@ Invoke-Command -VMName $SDNConfig.HostList[0] -Credential $adcred -ScriptBlock {
     az azurestackhci galleryimage delete --subscription $using:subId --resource-group $using:rg --name "ubuntu20"
     az azurestackhci galleryimage delete --subscription $using:subId --resource-group $using:rg --name "win2k22"
     az customlocation delete --resource-group $using:rg --name $using:custom_location_name --yes
-    az k8s-extension delete --cluster-type appliances --cluster-name $using:resource_name --resource-group $using:rg
+    az k8s-extension delete --cluster-type appliances --cluster-name $using:resource_name --resource-group $using:rg --name vmss-hci --yes
     az arcappliance delete hci --config-file $using:csv_path\ResourceBridge\hci-appliance.yaml --yes
 }
