36498c6ad262c6f538cf02093bfac91e0a2d470f
likamrat <likamrat@microsoft.com>
2023-02-19 11:18:02 -0800

    initial workflow

--- check_releases.py
+++ check_releases.py
@@ -5,15 +5,19 @@ import re
 with open("repositories.txt") as f:
     repositories = [line.strip() for line in f]
 
-for repo in repositories:
-    url = f"https://api.github.com/repos/{repo}/releases/latest"
-    response = requests.get(url)
+# create and open the output file for writing
+with open("report.txt", "w") as f:
+    for repo in repositories:
+        url = f"https://api.github.com/repos/{repo}/releases/latest"
+        response = requests.get(url)
 
-    if response.status_code == 200:
-        data = response.json()
-        latest_release = data["name"]
-        # extract version number using regular expression
-        version_number = re.search(r"\d+(\.\d+)*", latest_release).group()
-        print(f"Latest release for {repo}: {version_number}")
-    else:
-        print(f"Error retrieving latest release for {repo}")
+        if response.status_code == 200:
+            data = response.json()
+            latest_release = data["name"]
+            # extract version number using regular expression
+            version_number = re.search(r"\d+(\.\d+)*", latest_release).group()
+            # write the output to the file
+            f.write(f"Latest release for {repo}: {version_number}\n")
+        else:
+            # write error message to the file
+            f.write(f"Error retrieving latest release for {repo}\n")
