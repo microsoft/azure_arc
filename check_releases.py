110ecd2bc85e99e66e1a279e90dbe0f08304de16
likamrat <likamrat@microsoft.com>
2023-02-19 09:44:33 -0800

    initial workflow

--- check_releases.py
+++ check_releases.py
@@ -1,14 +1,11 @@
 import requests
 
-# replace with your GitHub username
-username = "likamrat"
-
 # read list of repositories from config file
 with open("repositories.txt") as f:
     repositories = [line.strip() for line in f]
 
 for repo in repositories:
-    url = f"https://api.github.com/repos/{username}/{repo}/releases/latest"
+    url = f"https://api.github.com/repos/{repo}/releases/latest"
     response = requests.get(url)
 
     if response.status_code == 200:
