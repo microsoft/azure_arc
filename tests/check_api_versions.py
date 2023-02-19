1bea6ecb32e8fe711e842101c95af65f47d2096c
likamrat <likamrat@microsoft.com>
2023-02-19 12:09:55 -0800

    initial workflow

--- tests/check_api_versions.py
+++ tests/check_api_versions.py
@@ -1,58 +1,55 @@
 import os
-import json
+import re
 import requests
 
-# Function to get the latest published Azure API version for a resource type
-def get_latest_api_version(resource_type):
-    url = f"https://management.azure.com/providers/Microsoft.{resource_type}/?api-version=2022-02-01"
-    response = requests.get(url)
-    response_json = json.loads(response.content)
-    return response_json["resourceTypes"][0]["apiVersions"][0]
+# Define the regex pattern for matching Azure API versions
+api_version_pattern = re.compile(r"(?<=apiVersion\": \")[\d-\.]+(?=\")")
 
-# Define the directory to start scanning
-repo_path = "/github/workspace"
+# Define the directory to start scanning from (root of repository)
+start_dir = os.getcwd()
 
-# Define the directory to exclude
-excluded_dirs = [".github", "docs", "img", "social", "tests"]
+# Define the directories to exclude
+exclude_dirs = [".github", "docs", "img", "social", "tests"]
 
-# Initialize a dictionary to store the API versions
-api_versions = {}
+# Define the output file and location
+output_file = os.path.join(start_dir, 'tests', 'api_report.txt')
 
-# Loop through each file and directory in the repository, excluding the excluded directory
-for root, dirs, files in os.walk(repo_path, topdown=True):
-    dirs[:] = [d for d in dirs if d != excluded_dir]
-    for file in files:
-        if file.endswith((".json", ".bicep", ".tf")):
-            with open(os.path.join(root, file), "r") as f:
-                template = json.load(f)
-                if "apiVersion" in template:
-                    api_version = template["apiVersion"]
-                    if api_version not in api_versions:
-                        api_versions[api_version] = []
-                    api_versions[api_version].append(os.path.join(root, file))
-                elif "azurerm" in template:
-                    modules = template["azurerm"]["modules"]
-                    for module in modules:
-                        if "version" in module:
-                            api_version = module["version"]
-                            if api_version not in api_versions:
-                                api_versions[api_version] = []
-                            api_versions[api_version].append(os.path.join(root, file))
+# Define the Azure API version endpoint URL
+api_endpoint = 'https://management.azure.com/providers/Microsoft.ApiManagement/apiVersionSet?api-version=2020-06-01-preview'
 
-# Get the latest published API versions for each resource type
-latest_api_versions = {}
-for resource_type in api_versions.keys():
-    latest_api_versions[resource_type] = get_latest_api_version(resource_type)
+# Send a request to the API endpoint to get the latest published API versions
+response = requests.get(api_endpoint)
+if response.status_code == 200:
+    latest_api_versions = set(response.json().get('value')[0].get('versionSet').get('versions'))
+else:
+    print(f'Error getting latest API versions: {response.status_code}')
 
-# Generate the report
-report = ""
-for api_version, files in api_versions.items():
-    if api_version not in latest_api_versions.values():
-        report += f"\nAPI version {api_version} is out of date for the following files: {', '.join(files)}"
+# Recursively search through all directories, skipping any excluded directories
+with open(output_file, 'w') as f:
+    for root, dirs, files in os.walk(start_dir):
+        # Skip any excluded directories
+        dirs[:] = [d for d in dirs if d not in exclude_dirs]
+        for file in files:
+            # Check if the file is an ARM, Bicep, or Terraform template
+            if file.endswith('.json') or file.endswith('.bicep') or file.endswith('.tf'):
+                # Read the file contents and look for the API version
+                with open(os.path.join(root, file), 'r') as file_contents:
+                    contents = file_contents.read()
+                    api_versions = api_version_pattern.findall(contents)
+                    for api_version in api_versions:
+                        f.write(f'{api_version}\n')
 
-# Write the report to a file
-report_file_path = os.path.join(repo_path, "api_report.txt")
-with open(report_file_path, "w") as f:
-    f.write(report)
+# Read the API versions from the report file and compare to the latest published API versions
+with open(output_file, 'r') as f:
+    repo_api_versions = set(f.read().splitlines())
 
-print(f"Report saved to {report_file_path}")
+# Create a report of the API versions and the latest published API versions
+report = []
+for api_version in repo_api_versions:
+    if api_version not in latest_api_versions:
+        report.append(f'API version {api_version} is not the latest published version')
+
+if report:
+    print('\n'.join(report))
+else:
+    print('All API versions in the repo are the latest published versions')
