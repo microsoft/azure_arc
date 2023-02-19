a5068047ac792d89c4af55cd6f85b27026f7d264
Venkata Chintala <29983008+chintalavr@users.noreply.github.com>
2023-02-18 20:03:05 -0500

    Customized management ports

--- azure_jumpstart_arcbox/artifacts/Bootstrap.ps1
+++ azure_jumpstart_arcbox/artifacts/Bootstrap.ps1
@@ -28,7 +28,8 @@ param (
     [string]$githubUser,
     [string]$templateBaseUrl,
     [string]$flavor,
-    [string]$automationTriggerAtLogon
+    [string]$rdpPort,
+    [string]$sshPort
 )
 
 [System.Environment]::SetEnvironmentVariable('adminUsername', $adminUsername, [System.EnvironmentVariableTarget]::Machine)
@@ -275,6 +276,35 @@ if ($flavor -eq "Full" -Or $flavor -eq "DataOps") {
     Start-Process msiexec.exe -Wait -ArgumentList "/I $Env:ArcBoxDir\AZDataCLI.msi /quiet"
 }
 
+# Change RDP Port
+if (($rdpPort -ne $null) -and ($rdpPort -ne "") -and ($rdpPort -ne "3389"))
+{
+    $TSPath = 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server'
+    $RDPTCPpath = $TSPath + '\Winstations\RDP-Tcp'
+    Set-ItemProperty -Path $TSPath -name 'fDenyTSConnections' -Value 0
+    
+    # RDP port
+    $portNumber = (Get-ItemProperty -Path $RDPTCPpath -Name 'PortNumber').PortNumber
+    Write-Host Get RDP PortNumber: $portNumber
+    if (!($portNumber -eq $rdpPort))
+    {
+      Write-Host Setting RDP PortNumber to $rdpPort
+      Set-ItemProperty -Path $RDPTCPpath -name 'PortNumber' -Value $rdpPort
+      Restart-Service TermService -force
+    }
+    
+    #Setup firewall rules
+    if ($rdpPort -eq 3389)
+    {
+      netsh advfirewall firewall set rule group="remote desktop" new Enable=Yes
+    } 
+    else
+    {
+      $systemroot = get-content env:systemroot
+      netsh advfirewall firewall add rule name="Remote Desktop - Custom Port" dir=in program=$systemroot\system32\svchost.exe service=termservice action=allow protocol=TCP localport=$RDPPort enable=yes
+    }
+}
+
 Write-Header "Configuring Logon Scripts"
 
 if ($flavor -eq "Full" -Or $flavor -eq "ITPro") {
@@ -367,5 +397,4 @@ else {
     Stop-Transcript
     $logSuppress = Get-Content $Env:ArcBoxLogsDir\Bootstrap.log | Where { $_ -notmatch "Host Application: powershell.exe" } 
     $logSuppress | Set-Content $Env:ArcBoxLogsDir\Bootstrap.log -Force
-
 }
