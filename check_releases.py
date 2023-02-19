f7894a576ec2e887e245c750d60f7aa56b5e3cad
likamrat <likamrat@microsoft.com>
2023-02-19 09:25:36 -0800

    initial workflow

---
+++ check_releases.py
@@ -0,0 +1,19 @@
+import requests
+
+# replace with your GitHub username
+username = "likamrat"
+
+# read list of repositories from config file
+with open("repositories.txt") as f:
+    repositories = [line.strip() for line in f]
+
+for repo in repositories:
+    url = f"https://api.github.com/repos/{username}/{repo}/releases/latest"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+        latest_release = data["name"]
+        print(f"Latest release for {repo}: {latest_release}")
+    else:
+        print(f"Error retrieving latest release for {repo}")
