55331356e089e38d50885a802ec7857979f4b7b5
likamrat <likamrat@microsoft.com>
2023-02-20 12:40:29 -0800

    initial workflow

--- tests/check-api-versions.ps1
+++ tests/check-api-versions.ps1
@@ -1,63 +1,54 @@
-[CmdletBinding()]
-param (
-    [Parameter(Mandatory=$true)]
-    [string]$Repository,
+# Define the GitHub repository and branch
+$repository = "azure/azure_arc"
+$branch = "main"
 
-    [Parameter(Mandatory=$false)]
-    [string]$Branch = "main"
-)
+# Define the Azure API endpoint
+$latest_api_url = "https://management.azure.com/providers?api-version=2022-01-01"
 
-function Get-OutdatedApiVersion ($resourceType, $apiVersion) {
-    $provider = Get-AzResourceProvider -ProviderNamespace $resourceType.Split('/')[0]
-    $latestApiVersion = ($provider.ResourceTypes | Where-Object { $_.ResourceTypeName -eq $resourceType.Split('/')[1] }).ApiVersions[0]
+# Define the regular expressions to find the templates and resource types
+$template_regex = "\.(json|bicep|tf|tf\.json)$"
+$resource_type_regex = [regex]'"type"\s*:\s*"([^"]+)"\s*,\s*"apiVersion"\s*:\s*"([^"]+)"'
 
-    if ($apiVersion -ne $latestApiVersion) {
-        [PSCustomObject]@{
-            ResourceType = $resourceType
-            OutdatedApiVersion = $apiVersion
-            LatestApiVersion = $latestApiVersion
-            FilePath = $file.Path
-        }
-    }
+# Function to get the contents of a file in a GitHub repository
+function Get-GitHubFileContent($repository, $branch, $path) {
+    $url = "https://raw.githubusercontent.com/$repository/$branch/$path"
+    return Invoke-RestMethod $url
 }
 
-$files = Invoke-RestMethod "https://api.github.com/repos/$Repository/contents?ref=$Branch" -Headers @{
-    "Accept" = "application/vnd.github.v3+json"
-    "User-Agent" = "GitHub Actions"
+# Function to compare two Azure API versions and return whether the first version is outdated
+function Test-OutdatedApiVersion($current_api_version, $latest_api_version) {
+    $current_api_version_int = [int]($current_api_version -replace "\D", "")
+    $latest_api_version_int = [int]($latest_api_version -replace "\D", "")
+    return $current_api_version_int -lt $latest_api_version_int
 }
 
-$outdatedResources = foreach ($file in $files) {
-    if ($file.Name -match "\.(json|bicep)$") {
-        $content = Invoke-RestMethod $file.Download_Url -Headers @{
-            "Accept" = "application/vnd.github.v3.raw"
-            "User-Agent" = "GitHub Actions"
-        }
+# Find all the ARM templates, bicep templates, and Terraform templates in the repository
+$templates = (Invoke-RestMethod "https://api.github.com/repos/$repository/contents" -QueryParams @{ref=$branch}).where{ $_.type -eq "file" -and $_.name -match $template_regex }
 
-        if ($file.Name -like "*.json") {
-            $template = ConvertFrom-Json $content
-        }
-        elseif ($file.Name -like "*.bicep") {
-            $template = Invoke-Expression $content
-        }
-
-        foreach ($resource in $template.resources) {
-            $resourceType = $resource.type
-            $apiVersion = $resource.apiVersion
-
-            if ($resourceType -match "^Microsoft\.(.*)\/(.*)$") {
-                $outdatedApiVersion = Get-OutdatedApiVersion -resourceType $resourceType -apiVersion $apiVersion
-                if ($outdatedApiVersion) {
-                    $outdatedApiVersion
-                }
+# Loop through each template and find the resource types and API versions
+$results = foreach ($template in $templates) {
+    $path = $template.path
+    $content = Get-GitHubFileContent -repository $repository -branch $branch -path $path
+    $matches = $content | Select-String -AllMatches -Pattern $resource_type_regex
+    foreach ($match in $matches) {
+        $resource_type = $match.matches.groups[1].value
+        $api_version = $match.matches.groups[2].value
+        $latest_api_version = (Invoke-RestMethod $latest_api_url | Where-Object { $_.namespace -eq $resource_type }).apiVersions | Select-Object -Last 1
+        if ($latest_api_version -and (Test-OutdatedApiVersion $api_version $latest_api_version)) {
+            [pscustomobject]@{
+                ResourceType = $resource_type
+                OutdatedApiVersion = $api_version
+                LatestApiVersion = $latest_api_version
+                Path = $path
             }
         }
     }
 }
 
-if ($outdatedResources) {
-    $outdatedResources | Format-Table
-    Write-Output "::set-output name=table::$(($outdatedResources | ConvertTo-Json))"
-}
-else {
-    Write-Output "No outdated API versions found."
-}
+# Write the results to a JSON file with a table
+$results | ConvertTo-Json -Depth 100 | Out-File "report.json"
+$table = $results | Select-Object ResourceType, OutdatedApiVersion, LatestApiVersion, Path | Format-Table -AutoSize | Out-String
+[pscustomobject]@{
+    Results = $results
+    Table = $table
+} | ConvertTo-Json -Depth 100 | Out-File "report-with-table.json"
