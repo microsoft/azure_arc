ff4dea2c3efc1df808812712d30884d94404e633
likamrat <likamrat@microsoft.com>
2023-02-22 14:17:15 -0800

    new Query latest release GH Action

--- tests/ArmTemplateCheck.ps1
+++
@@ -1,9 +0,0 @@
-$ArmFile=$args[0]
-$ExcludeRules=$args[1]
-
-git clone https://github.com/Azure/arm-ttk.git --quiet .\arm-ttk
-Import-Module .\arm-ttk\arm-ttk
-Install-Module Pester -AllowClobber -RequiredVersion 4.10.1 -Force -SkipPublisherCheck -Scope CurrentUser
-Import-Module Pester -RequiredVersion 4.10.1 -ErrorAction Stop
-$results = Invoke-Pester -Script @{Path = ".\tests\TestArmTemplate.ps1"; Parameters = @{TemplatePath = "$ArmFile"; Skip = "$ExcludeRules"}} -OutputFormat NUnitXml -OutputFile TEST-arm_template.xml -PassThru
-if ($results.TestResult.Result -contains "Failed") {Write-Error -Message "Test Failed"}
