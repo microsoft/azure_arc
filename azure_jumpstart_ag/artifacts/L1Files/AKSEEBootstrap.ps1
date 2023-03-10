ba0ccec16f1424feca0bb402f4567ac9dd97b001
likamrat <likamrat@microsoft.com>
2023-03-09 17:38:27 -0800

    L1 Files

--- azure_jumpstart_ag/artifacts/L1Files/AKSEEBootstrap.ps1
+++ azure_jumpstart_ag/artifacts/L1Files/AKSEEBootstrap.ps1
@@ -173,7 +173,7 @@ elseif ($env:COMPUTERNAME -eq "Chicago") {
 
     Unregister-ScheduledTask -TaskName "Startup Scan" -Confirm:$false
 }
-elseif ($env:COMPUTERNAME -eq "AKSEEDev") {
+elseif ($env:COMPUTERNAME -eq "AKSEEDev-Local") {
 
     $NetIPAddress = "172.20.1.4"
     $DefaultGateway = "172.20.1.1"
