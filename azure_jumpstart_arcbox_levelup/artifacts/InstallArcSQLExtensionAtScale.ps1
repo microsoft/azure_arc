138668cbf2fb246ad5c8bd33c6e636c0bd695587
Venkata Chintala <29983008+chintalavr@users.noreply.github.com>
2023-03-10 17:09:42 -0500

    Update InstallArcSQLExtensionAtScale.ps1

--- azure_jumpstart_arcbox_levelup/artifacts/InstallArcSQLExtensionAtScale.ps1
+++ azure_jumpstart_arcbox_levelup/artifacts/InstallArcSQLExtensionAtScale.ps1
@@ -7,29 +7,34 @@ param (
     [string]$subscriptionId = $Env:subscriptionId,
     [string]$resourceGroup = $Env:resourceGroup,
     [string]$azureRegion = $Env:azureLocation,
-    [string[]]$remoteSQLServerList = "JSLU-Win-SQL-02, JSLU-Win-SQL-03",
+    [string[]]$remoteSQLServerList = [string[]]("JSLU-Win-SQL-02", "JSLU-Win-SQL-03"),
     [string]$licenseType = "PAYG",
     [string]$AzureArcDir = "C:\ArcBoxLevelup"
 )
 
+# $secWindowsPassword = ConvertTo-SecureString "ArcDemo123!!" -AsPlainText -Force 
+# .\InstallArcSQLExtensionAtScale.ps1 -remoteWindowsAdminPassword $secWindowsPassword
+
 # Download MSI into a local folder
+$msiSourceFile = "${AzureArcDir}\AzureExtensionForSQLServer.msi"
+
+Invoke-WebRequest "https://aka.ms/AzureExtensionForSQLServer/test" -OutFile $msiSourceFile
+
+# Iterate through list of servers and onboard to Azur Arc
+# Create Windows credential object
+$winCreds = New-Object System.Management.Automation.PSCredential ($remoteWindowsAdminUsername, $remoteWindowsAdminPassword)
+    
 foreach ($remoteSQLServerName in $remoteSQLServerList)
 {
-    $SqlExtensionMsi = "AzureExtensionForSQLServer.msi"
-    Invoke-WebRequest "https://aka.ms/AzureExtensionForSQLServer/test" -OutFile "${AzureArcDir}\${SqlExtensionMsi}"
-    
-    # Create Windows credential object
-    $winCreds = New-Object System.Management.Automation.PSCredential ($remoteWindowsAdminUsername, $remoteWindowsAdminPassword)
     
     # Copy MSI on remote SQL server
-    Copy-VMFile $remoteSQLServerName -SourcePath "${AzureArcDir}\${SqlExtensionMsi}" -DestinationPath "${AzureArcDir}\${SqlExtensionMsi}" -CreateFullPath -FileSource Host
+    Copy-VMFile $remoteSQLServerName -SourcePath $msiSourceFile -DestinationPath $msiSourceFile -CreateFullPath -FileSource Host -Force
     
     # Remote execute MSI on remote SQL server
-    $command = "msiexec.exe /i ${AzureArcDir}\${SqlExtensionMsi}"
+    $command = "msiexec.exe /i $msiSourceFile"
     $scriptblock = [Scriptblock]::Create($command)
     Invoke-Command -VMName $remoteSQLServerName -ScriptBlock $scriptblock -Credential $winCreds
     
     # Remote execute Arc-enabled SQL server extension
-    Invoke-Command -VMName $remoteSQLServerName -ScriptBlock {&"$env:ProgramW6432\AzureExtensionForSQLServer\AzureExtensionForSQLServer.exe" --subId $Using:subscriptionId --resourceGroup $Using:resourceGroup --location $Using:azureRegion --tenantid $Using:servicePrincipalTenantId --service-principal-app-id $Using:servicePrincipalAppId --service-principal-secret $Using:servicePrincipalSecret --licenseType $Using:licenseType} -Credential $winCreds --waitTime 5
+    Invoke-Command -VMName $remoteSQLServerName -ScriptBlock {&"$env:ProgramW6432\AzureExtensionForSQLServer\AzureExtensionForSQLServer.exe" --subId $Using:subscriptionId --resourceGroup $Using:resourceGroup --location $Using:azureRegion --tenantid $Using:servicePrincipalTenantId --service-principal-app-id $Using:servicePrincipalAppId --service-principal-secret $Using:servicePrincipalSecret --licenseType $Using:licenseType --waitTime 5} -Credential $winCreds
 }
-
