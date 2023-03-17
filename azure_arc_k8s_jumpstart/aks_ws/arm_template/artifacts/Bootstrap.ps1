407707ae5c30971f859c9c413ea73288292c8405
lanicolas <lauranicolas87@outlook.com>
2023-03-17 06:41:26 +0100

    PR fixes

--- azure_arc_k8s_jumpstart/aks_ws/arm_template/artifacts/Bootstrap.ps1
+++ azure_arc_k8s_jumpstart/aks_ws/arm_template/artifacts/Bootstrap.ps1
@@ -24,25 +24,10 @@ mkdir -Path "C:\WAC"
 
 ## Download the MSI file
 Invoke-WebRequest -UseBasicParsing -Uri 'https://aka.ms/WACDownload' -OutFile "C:\WAC\WindowsAdminCenter.msi"
-#Invoke-WebRequest -UseBasicParsing -Uri 'https://download.microsoft.com/download/1/0/5/1059800B-F375-451C-B37E-758FFC7C8C8B/WindowsAdminCenter2103.2.msi' -OutFile "C:\WAC\WindowsAdminCenter.msi"
-#Invoke-WebRequest -UseBasicParsing -Uri 'https://download.microsoft.com/download/1/0/5/1059800B-F375-451C-B37E-758FFC7C8C8B/WindowsAdminCenter2009.msi' -OutFile "C:\WAC\WindowsAdminCenter.msi"
 
 ## install Windows Admin Center
 $msiArgs = @("/i", "C:\WAC\WindowsAdminCenter.msi", "/qn", "/L*v", "log.txt", "SME_PORT=443", "SSL_CERTIFICATE_OPTION=generate")
 Start-Process msiexec.exe -Wait -ArgumentList $msiArgs
-
-<# Update WAC Extensions
-Import-Module "$env:ProgramFiles\windows admin center\PowerShell\Modules\ExtensionTools" -Verbose
-
-# Specify the WAC gateway
-$WAC = "https://$env:COMPUTERNAME"
-
-# List the WAC extensions
-$extensions = Get-Extension $WAC | Where-Object { $_.isLatestVersion -like 'False' }
-
-ForEach ($extension in $extensions) {    
-    Update-Extension $WAC -ExtensionId $extension.Id -Verbose | out-file -append C:\Users\Public\WACUpdateLog$(get-date -f MM-dd-yyyy_HH_mm).txt
-} #>
 
 # Create path
 Write-Output "Create deployment path"
@@ -92,7 +77,6 @@ workflow ClientTools_01
         }
 
 ClientTools_01 | Format-Table 
-
 
 New-Item -path alias:kubectl -value 'C:\ProgramData\chocolatey\lib\kubernetes-cli\tools\kubernetes\client\bin\kubectl.exe'
 
