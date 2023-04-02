0c5bce86d362c53ba28fdb319b04e70dc08f887a
Seif Bassem <38246040+sebassem@users.noreply.github.com>
2023-04-02 08:49:17 +0200

    change order

--- azure_jumpstart_ag/artifacts/AgLogonScript.ps1
+++ azure_jumpstart_ag/artifacts/AgLogonScript.ps1
@@ -20,62 +20,6 @@ $adminUsername = $env:adminUsername
 # Disable Windows firewall
 Set-NetFirewallProfile -Profile Domain, Public, Private -Enabled False
 
-#############################################################
-# Install Windows Terminal, WSL2, and Ubuntu
-#############################################################
-Write-Header "Installing Windows Terminal, WSL2 and Ubuntu"
-If ($PSVersionTable.PSVersion.Major -ge 7){ Write-Error "This script needs be run by version of PowerShell prior to 7.0" }
-$downloadDir = "C:\WinTerminal"
-$gitRepo = "microsoft/terminal"
-$filenamePattern = "*.msixbundle"
-$framworkPkgUrl = "https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx"
-$framworkPkgPath = "$downloadDir\Microsoft.VCLibs.x64.14.00.Desktop.appx"
-$msiPath = "$downloadDir\Microsoft.WindowsTerminal.msixbundle"
-$releasesUri = "https://api.github.com/repos/$gitRepo/releases/latest"
-$downloadUri = ((Invoke-RestMethod -Method GET -Uri $releasesUri).assets | Where-Object name -like $filenamePattern ).browser_download_url | Select-Object -SkipLast 1
-
-# Download C++ Runtime framework packages for Desktop Bridge and Windows Terminal latest release msixbundle
-Invoke-WebRequest -Uri $framworkPkgUrl -OutFile ( New-Item -Path $framworkPkgPath -Force )
-Invoke-WebRequest -Uri $downloadUri -OutFile ( New-Item -Path $msiPath -Force )
-
-# Install WSL latest kernel update
-msiexec /i "$AgToolsDir\wsl_update_x64.msi" /qn
-
-# Install C++ Runtime framework packages for Desktop Bridge and Windows Terminal latest release
-Add-AppxPackage -Path $framworkPkgPath
-Add-AppxPackage -Path $msiPath
-Add-AppxPackage -Path "$AgToolsDir\Ubuntu.appx"
-
-# Setting WSL environment variables
-$userenv = [System.Environment]::GetEnvironmentVariable("Path", "User")
-[System.Environment]::SetEnvironmentVariable("PATH", $userenv + ";C:\Users\$adminUsername\Ubuntu", "User")
-
-# Initializing the wsl ubuntu app without requiring user input
-$ubuntu_path="c:/users/$adminUsername/AppData/Local/Microsoft/WindowsApps/ubuntu"
-Invoke-Expression -Command "$ubuntu_path install --root"
-
-# Cleanup
-Remove-Item $downloadDir -Recurse -Force
-
-#############################################################
-# Install Docker Desktop
-#############################################################
-Write-Header "Installing Docker Dekstop"
-# Download and Install Docker Desktop
-$arguments = 'install --quiet --accept-license'
-Start-Process "$AgToolsDir\DockerDesktopInstaller.exe" -Wait -ArgumentList $arguments
-Get-ChildItem "$env:USERPROFILE\Desktop\Docker Desktop.lnk" | Remove-Item -Confirm:$false
-
-#############################################################
-# Install VSCode extensions
-#############################################################
-Write-Header "Installing VSCode extensions"
-# Install VSCode extensions
-foreach ($extension in $AgConfig.VSCodeExtensions) {
-  Write-Host "Installing $extension"
-  code --install-extension $extension
-}
-
 ##############################################################
 # Setup Azure CLI
 ##############################################################
@@ -425,6 +369,62 @@ kubectx aksDev="$Env:aksDevClusterName-admin"
 Write-Header "Attaching ACRs to AKS clusters"
 # az aks update -n $Env:aksProdClusterName -g $Env:resourceGroup --attach-acr $Env:acrNameProd
 az aks update -n $Env:aksDevClusterName -g $Env:resourceGroup --attach-acr $Env:acrNameDev
+
+#############################################################
+# Install Windows Terminal, WSL2, and Ubuntu
+#############################################################
+Write-Header "Installing Windows Terminal, WSL2 and Ubuntu"
+If ($PSVersionTable.PSVersion.Major -ge 7){ Write-Error "This script needs be run by version of PowerShell prior to 7.0" }
+$downloadDir = "C:\WinTerminal"
+$gitRepo = "microsoft/terminal"
+$filenamePattern = "*.msixbundle"
+$framworkPkgUrl = "https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx"
+$framworkPkgPath = "$downloadDir\Microsoft.VCLibs.x64.14.00.Desktop.appx"
+$msiPath = "$downloadDir\Microsoft.WindowsTerminal.msixbundle"
+$releasesUri = "https://api.github.com/repos/$gitRepo/releases/latest"
+$downloadUri = ((Invoke-RestMethod -Method GET -Uri $releasesUri).assets | Where-Object name -like $filenamePattern ).browser_download_url | Select-Object -SkipLast 1
+
+# Download C++ Runtime framework packages for Desktop Bridge and Windows Terminal latest release msixbundle
+Invoke-WebRequest -Uri $framworkPkgUrl -OutFile ( New-Item -Path $framworkPkgPath -Force )
+Invoke-WebRequest -Uri $downloadUri -OutFile ( New-Item -Path $msiPath -Force )
+
+# Install WSL latest kernel update
+msiexec /i "$AgToolsDir\wsl_update_x64.msi" /qn
+
+# Install C++ Runtime framework packages for Desktop Bridge and Windows Terminal latest release
+Add-AppxPackage -Path $framworkPkgPath
+Add-AppxPackage -Path $msiPath
+Add-AppxPackage -Path "$AgToolsDir\Ubuntu.appx"
+
+# Setting WSL environment variables
+$userenv = [System.Environment]::GetEnvironmentVariable("Path", "User")
+[System.Environment]::SetEnvironmentVariable("PATH", $userenv + ";C:\Users\$adminUsername\Ubuntu", "User")
+
+# Initializing the wsl ubuntu app without requiring user input
+$ubuntu_path="c:/users/$adminUsername/AppData/Local/Microsoft/WindowsApps/ubuntu"
+Invoke-Expression -Command "$ubuntu_path install --root"
+
+# Cleanup
+Remove-Item $downloadDir -Recurse -Force
+
+#############################################################
+# Install Docker Desktop
+#############################################################
+Write-Header "Installing Docker Dekstop"
+# Download and Install Docker Desktop
+$arguments = 'install --quiet --accept-license'
+Start-Process "$AgToolsDir\DockerDesktopInstaller.exe" -Wait -ArgumentList $arguments
+Get-ChildItem "$env:USERPROFILE\Desktop\Docker Desktop.lnk" | Remove-Item -Confirm:$false
+
+#############################################################
+# Install VSCode extensions
+#############################################################
+Write-Header "Installing VSCode extensions"
+# Install VSCode extensions
+foreach ($extension in $AgConfig.VSCodeExtensions) {
+  Write-Host "Installing $extension"
+  code --install-extension $extension
+}
 
 ##############################################################
 # Start Docker Desktop without dashboard
