6e6b68fb074333a7034863f09bfc82c5ddfef8ea
Seif Bassem <38246040+sebassem@users.noreply.github.com>
2023-04-17 16:46:31 +0200

    fixes

--- azure_jumpstart_ag/artifacts/AgLogonScript.ps1
+++ azure_jumpstart_ag/artifacts/AgLogonScript.ps1
@@ -236,9 +236,6 @@ Invoke-Command -VMName $VMnames -Credential $Credentials -ScriptBlock {
 
     # Fetching required GitHub artifacts from Jumpstart repository
     Write-Host "Fetching GitHub artifacts"
-    #Invoke-WebRequest ($using:templateBaseUrl+"artifacts/L1Files/AKSEEBootstrap.ps1") -OutFile "$deploymentFolder\AKSEEBootstrap.ps1"
-    #Invoke-WebRequest ($using:templateBaseUrl+"artifacts/L1Files/ScalableCluster.json") -OutFile "$deploymentFolder\ScalableCluster.json"
-    #Invoke-WebRequest ($using:templateBaseUrl+"artifacts/L1Files/StartupScan.ps1") -OutFile "$deploymentFolder\StartupScan.ps1"
     $repoName = "azure_arc" # While testing, change to your GitHub fork's repository name
     $githubApiUrl = "https://api.github.com/repos/$using:githubAccount/$repoName/contents/azure_jumpstart_ag/artifacts/L1Files?ref=$using:githubBranch"
     $response = Invoke-RestMethod -Uri $githubApiUrl
@@ -246,7 +243,7 @@ Invoke-Command -VMName $VMnames -Credential $Credentials -ScriptBlock {
     $fileUrls | ForEach-Object {
         $fileName = $_.Substring($_.LastIndexOf("/") + 1)
         $outputFile = Join-Path $deploymentFolder $fileName
-        Invoke-WebRequest -Uri $_ -OutFile $outputFile
+        Invoke-RestMethod -Uri $_ -OutFile $outputFile
     }
 
     # Setting up replacment parameters for AKS Edge Essentials config json file
