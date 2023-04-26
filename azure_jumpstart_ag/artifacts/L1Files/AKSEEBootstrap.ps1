ab08fa265e6f2d0933b6f007bb12e5d8525c4f9b
saitcho <saitcho@outlook.com>
2023-04-26 10:36:02 -0500

    time sync move

--- azure_jumpstart_ag/artifacts/L1Files/AKSEEBootstrap.ps1
+++ azure_jumpstart_ag/artifacts/L1Files/AKSEEBootstrap.ps1
@@ -25,14 +25,6 @@ Start-Transcript -Path $logsFolder\AKSEEBootstrap.log
 #########################################
 # Deplying AKS Edge Essentials clusters #
 #########################################
-Write-Host "INFO: Configuring L1 VM with AKS Edge Essentials." -ForegroundColor Gray
-# Force time sync
-$string = Get-Date
-Write-Host "INFO: Time before forced time sync:" $string.ToString("u") -ForegroundColor Gray
-net start w32time
-W32tm /resync
-$string = Get-Date
-Write-Host "INFO: Time after forced time sync:" $string.ToString("u") -ForegroundColor Gray
 
 # Validating internet connectivity
 while (-not (Test-Connection -ComputerName google.com -Quiet)) {
