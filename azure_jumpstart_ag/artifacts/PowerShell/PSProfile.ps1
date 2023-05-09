e054ba35750eff888880520149e750129e01378f
saitcho <saitcho@outlook.com>
2023-05-09 11:34:41 -0500

    fixes

--- azure_jumpstart_ag/artifacts/PowerShell/PSProfile.ps1
+++ azure_jumpstart_ag/artifacts/PowerShell/PSProfile.ps1
@@ -11,38 +11,86 @@ function Write-Header {
     Write-Host
 }
 
-function exec
-{
-    param
-    (
-        [ScriptBlock] $ScriptBlock,
-        [string] $StderrPrefix = "",
-        [int[]] $AllowedExitCodes = @(0)
+Function Invoke-CommandLineTool {
+    #Region Parameters
+    Param (
+        [Parameter(Mandatory = $True,
+            ValueFromPipeline = $True,
+            ValueFromPipelineByPropertyName = $True)]
+        [Alias('C')]
+        [string]$CommandLine
+        ,
+        [Parameter(Mandatory = $False,
+            ValueFromPipeline = $True,
+            ValueFromPipelineByPropertyName = $True)]
+        [string]$WorkingDirectory
+        ,
+        [Parameter(Mandatory = $False,
+            ValueFromPipeline = $True,
+            ValueFromPipelineByPropertyName = $True)]
+        [string]$TempFilePath
+        ,
+        [switch]$ReturnResultObject
     )
- 
-    $backupErrorActionPreference = $script:ErrorActionPreference
- 
-    $script:ErrorActionPreference = "Continue"
-    try
-    {
-        & $ScriptBlock 2>&1 | ForEach-Object -Process `
-            {
-                if ($_ -is [System.Management.Automation.ErrorRecord])
-                {
-                    "$StderrPrefix$_"
+    #EndRegion Parameters
+    Begin {
+    }
+    Process {
+        Foreach ($cmd in $CommandLine) {
+            
+            Write-Verbose ("CommandLine= '{0}'" -f $cmd)
+            
+            # File Paths
+            $fn = [System.IO.Path]::GetRandomFileName() + ".bat"
+            If ($TempFilePath -and (Test-Path $TempFilePath -ErrorAction SilentlyContinue)) {
+                $td = $TempFilePath
+            }
+            Else {
+                $td = [System.IO.Path]::GetTempPath()
+            }
+            Write-Verbose ("td= {0}" -f $td)
+            
+            $bat = [System.IO.Path]::Combine($td, $fn)
+            New-Item -Path $bat -ItemType file | Out-Null
+            Add-Content $bat $cmd
+            
+            $stdOutPath = Join-Path $td "stdout.log"
+            $stdErrPath = Join-Path $td "stderr.log"
+            
+            $proc = Start-Process -Wait -PassThru -RedirectStandardOutput $stdOutPath -RedirectStandardError $stdErrPath -FilePath cmd.exe -ArgumentList @('/c', $bat) -NoNewWindow -WorkingDirectory:$WorkingDirectory
+            
+            @($stdOutPath, $stdErrPath) | Remove-BlankLines | Out-Null
+            
+            $result = "" | Select Result, ExitCode, StdOut, StdErr, StartTime, ExitTime, ElapsedTime
+            $result.ExitCode = $proc.ExitCode
+            $result.StdOut = Get-Content $stdOutPath | select -Skip 1 # skip prevents command line from being returned in stdOut
+            $result.StdErr = Get-Content $stdErrPath
+            $result.StartTime = $proc.StartTime
+            $result.ExitTime = $proc.ExitTime
+            $result.ElapsedTime = ($proc.ExitTime).Subtract($proc.StartTime)
+            $result.Result = $result.StdOut
+            
+            If (-not $TempFilePath) {
+                Remove-Item -Path $bat        -Force | Out-Null
+                Remove-Item -Path $stdOutPath -Force | Out-Null
+                Remove-Item -Path $stdErrPath -Force | Out-Null
+            }
+            
+            If ($ReturnResultObject) {
+                Return $result
+            }
+            Else {
+                If ($result.StdErr) {
+                    Write-Error ("{0}`n{1}" -f $result.StdErr, $result.StdOut)
                 }
-                else
-                {
-                    "$_"
+                Else {
+                    Return $result.Result
                 }
             }
-        if ($AllowedExitCodes -notcontains $LASTEXITCODE)
-        {
-            throw "Execution failed with exit code $LASTEXITCODE"
         }
     }
-    finally
-    {
-        $script:ErrorActionPreference = $backupErrorActionPreference
+    End {
     }
 }
+
+  
