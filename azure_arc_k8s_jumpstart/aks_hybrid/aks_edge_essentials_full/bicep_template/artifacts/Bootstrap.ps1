e4b83d326bed6f9fd845e99ed1a85dcd5db2cf13
pabloameijeirascanay <pabloam@microsoft.com>
2023-05-17 01:35:18 +0200

    AKS Edge Essentials full deployment scenario

---
+++ azure_arc_k8s_jumpstart/aks_hybrid/aks_edge_essentials_full/bicep_template/artifacts/Bootstrap.ps1
@@ -0,0 +1,132 @@
+param (
+    [string]$adminUsername,
+    [string]$adminPassword,
+    [string]$spnClientId,
+    [string]$spnClientSecret,
+    [string]$spnTenantId,
+    [string]$spnAuthority,
+    [string]$subscriptionId,
+    [string]$resourceGroup,
+    [string]$azureLocation,
+    [string]$templateBaseUrl,
+    [string]$rdpPort,
+    [string]$githubAccount,
+    [string]$githubBranch,
+    [string]$kubernetesDistribution
+)
+
+# Inject ARM template parameters as environment variables
+[System.Environment]::SetEnvironmentVariable('adminUsername', $adminUsername, [System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable('adminPassword', $adminPassword, [System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable('spnClientID', $spnClientId, [System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable('spnClientSecret', $spnClientSecret, [System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable('spnTenantId', $spnTenantId, [System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable('spnAuthority', $spnAuthority, [System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable('SPN_CLIENT_ID', $spnClientId, [System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable('resourceGroup', $resourceGroup, [System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable('subscriptionId', $subscriptionId, [System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable('azureLocation', $azureLocation, [System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable('templateBaseUrl', $templateBaseUrl, [System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable('githubAccount', $githubAccount, [System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable('githubBranch', $githubBranch, [System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable('kubernetesDistribution', $kubernetesDistribution, [System.EnvironmentVariableTarget]::Machine)
+
+# Create path
+Write-Output "Create deployment path"
+$tempDir = "C:\Temp"
+New-Item -Path $tempDir -ItemType directory -Force
+
+Start-Transcript "$tempDir\Bootstrap.log"
+
+$ErrorActionPreference = "SilentlyContinue"
+
+# Copy PowerShell Profile and Reload
+Invoke-WebRequest ($templateBaseUrl + "artifacts/PSProfile.ps1") -OutFile $PsHome\Profile.ps1
+.$PsHome\Profile.ps1
+
+# Extending C:\ partition to the maximum size
+Write-Host "Extending C:\ partition to the maximum size"
+Resize-Partition -DriveLetter C -Size $(Get-PartitionSupportedSize -DriveLetter C).SizeMax
+
+# Download artifacts
+Invoke-WebRequest ($templateBaseUrl + "artifacts/LogonScript.ps1") -OutFile "$tempDir\LogonScript.ps1"
+Invoke-WebRequest "https://raw.githubusercontent.com/microsoft/azure_arc/main/img/jumpstart_wallpaper.png" -OutFile "$tempDir\wallpaper.png"
+
+# Installing tools
+workflow ClientTools_01
+        {
+            $chocolateyAppList = 'azure-cli,az.powershell,kubernetes-cli,microsoft-edge,azcopy10,kubernetes-helm'
+            #Run commands in parallel.
+            Parallel 
+                {
+                    InlineScript {
+                        param (
+                            [string]$chocolateyAppList
+                        )
+                        if ([string]::IsNullOrWhiteSpace($using:chocolateyAppList) -eq $false)
+                        {
+                            try{
+                                choco config get cacheLocation
+                            }catch{
+                                Write-Output "Chocolatey not detected, trying to install now"
+                                Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+                            }
+                        }
+                        if ([string]::IsNullOrWhiteSpace($using:chocolateyAppList) -eq $false){   
+                            Write-Host "Chocolatey Apps Specified"  
+                            
+                            $appsToInstall = $using:chocolateyAppList -split "," | ForEach-Object { "$($_.Trim())" }
+                        
+                            foreach ($app in $appsToInstall)
+                            {
+                                Write-Host "Installing $app"
+                                & choco install $app /y -Force| Write-Output
+                            }
+                        }                        
+                    }
+                }
+        }
+
+ClientTools_01 | Format-Table
+
+# Disable Microsoft Edge sidebar
+$RegistryPath = 'HKLM:\SOFTWARE\Policies\Microsoft\Edge'
+$Name         = 'HubsSidebarEnabled'
+$Value        = '00000000'
+# Create the key if it does not exist
+If (-NOT (Test-Path $RegistryPath)) {
+  New-Item -Path $RegistryPath -Force | Out-Null
+}
+New-ItemProperty -Path $RegistryPath -Name $Name -Value $Value -PropertyType DWORD -Force
+
+# Disable Microsoft Edge first-run Welcome screen
+$RegistryPath = 'HKLM:\SOFTWARE\Policies\Microsoft\Edge'
+$Name         = 'HideFirstRunExperience'
+$Value        = '00000001'
+# Create the key if it does not exist
+If (-NOT (Test-Path $RegistryPath)) {
+  New-Item -Path $RegistryPath -Force | Out-Null
+}
+New-ItemProperty -Path $RegistryPath -Name $Name -Value $Value -PropertyType DWORD -Force
+
+# Installing NuGet
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+
+# Creating scheduled task for LogonScript.ps1
+$Trigger = New-ScheduledTaskTrigger -AtLogOn
+$Action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "$tempDir\LogonScript.ps1"
+Register-ScheduledTask -TaskName "LogonScript" -Trigger $Trigger -User $adminUsername -Action $Action -RunLevel "Highest" -Force
+
+# Disabling Windows Server Manager Scheduled Task
+Get-ScheduledTask -TaskName ServerManager | Disable-ScheduledTask
+
+# Install Hyper-V and reboot
+Write-Header "Installing Hyper-V"
+Enable-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform -NoRestart
+Install-WindowsFeature -Name Hyper-V -IncludeAllSubFeature -IncludeManagementTools -Restart
+
+# Clean up Bootstrap.log
+Write-Host "Clean up Bootstrap.log"
+Stop-Transcript
+$logSuppress = Get-Content "$tempDir\Bootstrap.log" | Where-Object { $_ -notmatch "Host Application: powershell.exe" } 
+$logSuppress | Set-Content "$tempDir\Bootstrap.log" -Force
