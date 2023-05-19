2d26eae37466fbf78a0e51b1c59a431714e2d229
Seif Bassem <38246040+sebassem@users.noreply.github.com>
2023-05-19 17:05:37 +0300

    settings folder name

--- azure_jumpstart_ag/artifacts/PowerShell/AgLogonScript.ps1
+++ azure_jumpstart_ag/artifacts/PowerShell/AgLogonScript.ps1
@@ -1035,8 +1035,8 @@ Get-ChildItem "$env:USERPROFILE\Desktop\Docker Desktop.lnk" | Remove-Item -Confi
 Start-Process "C:\Program Files\Docker\Docker\Docker Desktop.exe" | Out-File -Append -FilePath ($AgConfig.AgDirectories["AgLogsDir"] + "\Docker.log")
 Start-Sleep -Seconds 10
 Get-Process | Where-Object { $_.name -like "Docker Desktop" } | Stop-Process -Force
-Copy-Item "$AgToolsDir\Settings\settings.json" -Destination "$env:USERPROFILE\AppData\Roaming\Docker Desktop\settings.json" -Force
-Copy-Item "$AgToolsDir\Settings\settings.json" -Destination "$env:USERPROFILE\AppData\Roaming\Docker\settings.json" -Force
+Copy-Item "$AgToolsDir\settings\settings.json" -Destination "$env:USERPROFILE\AppData\Roaming\Docker Desktop\settings.json" -Force
+Copy-Item "$AgToolsDir\settings\settings.json" -Destination "$env:USERPROFILE\AppData\Roaming\Docker\settings.json" -Force
 
 Start-Sleep -Seconds 5
 Start-Process "C:\Program Files\Docker\Docker\Docker Desktop.exe"
