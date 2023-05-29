79bbb78f5a824f8eaec7d42bbc3bb237b0ef5098
Seif Bassem <38246040+sebassem@users.noreply.github.com>
2023-05-29 13:04:07 +0300

    reduce wait time

--- azure_jumpstart_ag/artifacts/PowerShell/AgLogonScript.ps1
+++ azure_jumpstart_ag/artifacts/PowerShell/AgLogonScript.ps1
@@ -971,7 +971,7 @@ foreach ($cluster in $AgConfig.SiteConfig.GetEnumerator()) {
                 }
                 else {
                     if($configStatus.ComplianceState -ne "Non-compliant"){
-                        Start-Sleep -Seconds 30
+                        Start-Sleep -Seconds 10
                     }
                     elseif ($configStatus.ComplianceState -eq "Non-compliant" -and $retryCount -lt $maxRetries) {
                         $retryCount++
