fa40c58253d155755d1ec947ceabb94a4e4788b7
likamrat <likamrat@microsoft.com>
2023-06-28 11:03:32 -0700

    cross repo version updates

--- azure_arc_k8s_jumpstart/cluster_api/capi_azure/installCAPI.sh
+++ azure_arc_k8s_jumpstart/cluster_api/capi_azure/installCAPI.sh
@@ -16,7 +16,7 @@ echo ""
 
   # Set deployment environment variables
   export GUID=$(echo $RANDOM | md5sum | head -c 4; echo;) # Do not change!
-  export CLUSTERCTL_VERSION="1.4.2" # Do not change!
+  export CLUSTERCTL_VERSION="1.4.4" # Do not change!
   export CAPI_PROVIDER="azure" # Do not change!
   export CAPI_PROVIDER_VERSION="1.7.6" # Do not change!
   export KUBERNETES_VERSION="1.27.1" # Do not change!
