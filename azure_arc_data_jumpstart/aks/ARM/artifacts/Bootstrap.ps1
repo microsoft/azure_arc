54d920948ce297a93fc4d17146c4623790189f7e
Seif Bassem <38246040+sebassem@users.noreply.github.com>
2023-07-17 20:11:34 +0300

    revert pinnging

--- azure_arc_data_jumpstart/aks/ARM/artifacts/Bootstrap.ps1
+++ azure_arc_data_jumpstart/aks/ARM/artifacts/Bootstrap.ps1
@@ -97,7 +97,7 @@ Invoke-WebRequest ($templateBaseUrl + "artifacts/RunAfterClientVMADJoin.ps1") -O
 # Installing tools
 workflow ClientTools_01
         {
-            $chocolateyAppList = 'az.powershell,kubernetes-cli,vcredist140,microsoft-edge,azcopy10,vscode,putty.install,kubernetes-helm,grep,ssms,dotnetcore-3.1-sdk'
+            $chocolateyAppList = 'azure-cli,az.powershell,kubernetes-cli,vcredist140,microsoft-edge,azcopy10,vscode,putty.install,kubernetes-helm,grep,ssms,dotnetcore-3.1-sdk'
             #Run commands in parallel.
             Parallel 
                 {
@@ -124,7 +124,6 @@ workflow ClientTools_01
                                 Write-Host "Installing $app"
                                 & choco install $app /y -Force| Write-Output
                             }
-                            choco install azure-cli -y --version=2.49.0
                         }
                     }
                     Invoke-WebRequest "https://azuredatastudio-update.azurewebsites.net/latest/win32-x64-archive/stable" -OutFile "${tempDir}\azuredatastudio.zip"
