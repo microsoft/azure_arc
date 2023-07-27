1eecea155cf11d06d9dd4a388dc5e609722f3395
Seif Bassem <38246040+sebassem@users.noreply.github.com>
2023-07-27 08:54:01 +0300

    testing

--- azure_jumpstart_ag/artifacts/PowerShell/AgLogonScript.ps1
+++ azure_jumpstart_ag/artifacts/PowerShell/AgLogonScript.ps1
@@ -1045,6 +1045,10 @@ Write-Host
 #####################################################################
 Write-Host "[$(Get-Date -Format t)] INFO: Installing flux extension on clusters (Step 11/17)" -ForegroundColor DarkGreen
 
+## Convert AKS cluster to MSI based
+
+az aks update -g $Env:resourceGroup -n $Env:aksStagingClusterName --enable-managed-identity
+
 $resourceTypes = @($AgConfig.ArcK8sResourceType, $AgConfig.AksResourceType)
 $resources = Get-AzResource -ResourceGroupName $Env:resourceGroup | Where-Object { $_.ResourceType -in $resourceTypes }
 
