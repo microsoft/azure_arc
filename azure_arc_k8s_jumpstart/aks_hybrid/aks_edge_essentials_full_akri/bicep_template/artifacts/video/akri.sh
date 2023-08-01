58d7e575ddbc8ff44c4ecb797eff40738effcf67
lanicolas <lauranicolas87@outlook.com>
2023-08-01 11:00:56 +0200

    git URL

--- azure_arc_k8s_jumpstart/aks_hybrid/aks_edge_essentials_full_akri/bicep_template/artifacts/video/akri.sh
+++ azure_arc_k8s_jumpstart/aks_hybrid/aks_edge_essentials_full_akri/bicep_template/artifacts/video/akri.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo ip route add 239.255.255.250/32 dev cni0  
-sudo iptables -A INPUT -p udp --dport 3702 -j ACCEPT"
-sudo sed -i '/-A OUTPUT -j ACCEPT/i-A INPUT -p udp -m udp --dport 3702 -j ACCEPT' /etc/systemd/scripts/ip4save"   
-sudo iptables -A INPUT -p udp --sport 3702 -j ACCEPT"
-sudo sed -i '/-A OUTPUT -j ACCEPT/i-A INPUT -p udp -m udp --sport 3702 -j ACCEPT' /etc/systemd/scripts/ip4save" 
+sudo iptables -A INPUT -p udp --dport 3702 -j ACCEPT
+sudo sed -i '/-A OUTPUT -j ACCEPT/i-A INPUT -p udp -m udp --dport 3702 -j ACCEPT' /etc/systemd/scripts/ip4save
+sudo iptables -A INPUT -p udp --sport 3702 -j ACCEPT
+sudo sed -i '/-A OUTPUT -j ACCEPT/i-A INPUT -p udp -m udp --sport 3702 -j ACCEPT' /etc/systemd/scripts/ip4save
