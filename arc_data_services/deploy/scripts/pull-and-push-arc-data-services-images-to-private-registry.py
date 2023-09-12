c0bcfb369616b0d3e61d8cb6131e9802fbe0646b
jkleinschnitz-msft <112512530+jkleinschnitz-msft@users.noreply.github.com>
2023-09-12 12:05:01 -0600

    changes for 1.22.0

--- arc_data_services/deploy/scripts/pull-and-push-arc-data-services-images-to-private-registry.py
+++ arc_data_services/deploy/scripts/pull-and-push-arc-data-services-images-to-private-registry.py
@@ -54,9 +54,9 @@ else:
 if os.getenv("SOURCE_DOCKER_TAG") is None:
     SOURCE_DOCKER_TAG = (
         input(
-            "Provide container image tag for the images at the source - press ENTER for using 'v1.22.0_2023-08-08': "
+            "Provide container image tag for the images at the source - press ENTER for using 'v1.23.0_2023-09-12': "
         )
-        or "v1.22.0_2023-08-08"
+        or "v1.23.0_2023-09-12"
     )
 else:
     SOURCE_DOCKER_TAG = os.environ["SOURCE_DOCKER_TAG"]
