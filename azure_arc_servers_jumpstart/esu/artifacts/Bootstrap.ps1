225b858c9c929b83d0060ec5b80cc6b5a20063cc
lanicolas <lauranicolas87@outlook.com>
2023-09-24 05:35:25 +0200

    ESU

---
+++ azure_arc_servers_jumpstart/esu/artifacts/Bootstrap.ps1
@@ -0,0 +1,171 @@
+param (
+    [string]$adminUsername,
+    [string]$adminPassword,
+    [string]$spnClientId,
+    [string]$spnClientSecret,
+    [string]$spnTenantId,
+    [string]$spnAuthority,
+    [string]$subscriptionId,
+    [string]$resourceGroup,
+    [string]$templateBaseUrl,
+    [string]$azureLocation,
+    [string]$esu
+)
+
+[System.Environment]::SetEnvironmentVariable('adminUsername', $adminUsername, [System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable('adminPassword', $adminPassword, [System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable('spnClientID', $spnClientId, [System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable('spnClientSecret', $spnClientSecret, [System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable('spnTenantId', $spnTenantId, [System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable('spnAuthority', $spnAuthority, [System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable('SPN_CLIENT_ID', $spnClientId, [System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable('SPN_CLIENT_SECRET', $spnClientSecret, [System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable('SPN_TENANT_ID', $spnTenantId, [System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable('SPN_AUTHORITY', $spnAuthority, [System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable('resourceGroup', $resourceGroup, [System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable('subscriptionId', $subscriptionId, [System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable('templateBaseUrl', $templateBaseUrl, [System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable('azureLocation', $azureLocation, [System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable('ESUDir', "C:\ESU", [System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable('esu', $esu, [System.EnvironmentVariableTarget]::Machine)
+
+# Creating ESU path
+Write-Output "Creating ESU path"
+$Env:ESUDir = "C:\ESU"
+$Env:ESULogsDir = "$Env:ESUDir\Logs"
+$Env:ESUVMDir = "$Env:ESUDir\Virtual Machines"
+$Env:ESUIconDir = "$Env:ESUDir\Icons"
+$Env:agentScript = "$Env:ESUDir\agentScript"
+$Env:ToolsDir = "C:\Tools"
+$Env:tempDir = "C:\Temp"
+
+New-Item -Path $Env:ESUDir -ItemType directory -Force
+New-Item -Path $Env:ESULogsDir -ItemType directory -Force
+New-Item -Path $Env:ESUVMDir -ItemType directory -Force
+New-Item -Path $Env:ESUKVDir -ItemType directory -Force
+New-Item -Path $Env:ESUIconDir -ItemType directory -Force
+New-Item -Path $Env:ToolsDir -ItemType Directory -Force
+New-Item -Path $Env:tempDir -ItemType directory -Force
+New-Item -Path $Env:agentScript -ItemType directory -Force
+
+
+Start-Transcript -Path $Env:ESULogsDir\Bootstrap.log
+
+$ErrorActionPreference = 'SilentlyContinue'
+
+# Extending C:\ partition to the maximum size
+Write-Host "Extending C:\ partition to the maximum size"
+Resize-Partition -DriveLetter C -Size $(Get-PartitionSupportedSize -DriveLetter C).SizeMax
+
+# Installing Posh-SSH PowerShell Module
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+Install-Module -Name Posh-SSH -Force
+
+# Installing DHCP service
+Write-Output "Installing DHCP service"
+Install-WindowsFeature -Name "DHCP" -IncludeManagementTools
+
+# Installing tools
+Write-Header "Installing Chocolatey Apps"
+$chocolateyAppList = 'azure-cli,az.powershell,vcredist140,microsoft-edge,azcopy10,7zip,ssms,dotnet-sdk,setdefaultbrowser,zoomit,openssl.light'
+
+try {
+    choco config get cacheLocation
+}
+catch {
+    Write-Output "Chocolatey not detected, trying to install now"
+    Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+}
+
+Write-Host "Chocolatey Apps Specified"
+
+$appsToInstall = $chocolateyAppList -split "," | ForEach-Object { "$($_.Trim())" }
+
+foreach ($app in $appsToInstall) {
+    Write-Host "Installing $app"
+    & choco install $app /y -Force | Write-Output
+
+}
+
+Write-Header "Fetching GitHub Artifacts"
+
+Write-Host "Fetching Artifacts"
+Invoke-WebRequest "https://raw.githubusercontent.com/microsoft/azure_arc/main/img/jumpstart_wallpaper.png" -OutFile $Env:ESUDir\wallpaper.png
+
+Write-Host "Fetching Artifacts"
+Invoke-WebRequest ($Env:templateBaseUrl + "artifacts/LogonScript.ps1") -OutFile $Env:ESUDir\LogonScript.ps1
+Invoke-WebRequest ($Env:templateBaseUrl + "artifacts/installArcAgent.ps1") -OutFile $Env:ESUDir\agentScript\installArcAgent.ps1
+Invoke-WebRequest ($Env:templateBaseUrl + "artifacts/installArcAgentSQL.ps1") -OutFile $Env:ESUDir\agentScript\installArcAgentSQL.ps1
+
+
+# Disable Microsoft Edge sidebar
+$RegistryPath = 'HKLM:\SOFTWARE\Policies\Microsoft\Edge'
+$Name         = 'HubsSidebarEnabled'
+$Value        = '00000000'
+# Create the key if it does not exist
+If (-NOT (Test-Path $RegistryPath)) {
+  New-Item -Path $RegistryPath -Force | Out-Null
+}
+New-ItemProperty -Path $RegistryPath -Name $Name -Value $Value -PropertyType DWORD -Force
+
+# Disable Microsoft Edge first-run Welcome screen
+$RegistryPath = 'HKLM:\SOFTWARE\Policies\Microsoft\Edge'
+$Name         = 'HideFirstRunExperience'
+$Value        = '00000001'
+# Create the key if it does not exist
+If (-NOT (Test-Path $RegistryPath)) {
+  New-Item -Path $RegistryPath -Force | Out-Null
+}
+New-ItemProperty -Path $RegistryPath -Name $Name -Value $Value -PropertyType DWORD -Force
+
+
+# Change RDP Port
+Write-Host "RDP port number from configuration is $rdpPort"
+if (($rdpPort -ne $null) -and ($rdpPort -ne "") -and ($rdpPort -ne "3389"))
+{
+    Write-Host "Configuring RDP port number to $rdpPort"
+    $TSPath = 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server'
+    $RDPTCPpath = $TSPath + '\Winstations\RDP-Tcp'
+    Set-ItemProperty -Path $TSPath -name 'fDenyTSConnections' -Value 0
+
+    # RDP port
+    $portNumber = (Get-ItemProperty -Path $RDPTCPpath -Name 'PortNumber').PortNumber
+    Write-Host "Current RDP PortNumber: $portNumber"
+    if (!($portNumber -eq $rdpPort))
+    {
+      Write-Host Setting RDP PortNumber to $rdpPort
+      Set-ItemProperty -Path $RDPTCPpath -name 'PortNumber' -Value $rdpPort
+      Restart-Service TermService -force
+    }
+
+    #Setup firewall rules
+    if ($rdpPort -eq 3389)
+    {
+      netsh advfirewall firewall set rule group="remote desktop" new Enable=Yes
+    }
+    else
+    {
+      $systemroot = get-content env:systemroot
+      netsh advfirewall firewall add rule name="Remote Desktop - Custom Port" dir=in program=$systemroot\system32\svchost.exe service=termservice action=allow protocol=TCP localport=$RDPPort enable=yes
+    }
+
+    Write-Host "RDP port configuration complete."
+}
+
+Write-Header "Configuring Logon Scripts"
+ # Creating scheduled task 
+    $Trigger = New-ScheduledTaskTrigger -AtLogOn
+    $Action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument $Env:ESUDir\LogonScript.ps1
+    Register-ScheduledTask -TaskName "LogonScript" -Trigger $Trigger -User $adminUsername -Action $Action -RunLevel "Highest" -Force
+
+    # Install Hyper-V and reboot
+    Write-Host "Installing Hyper-V and restart"
+    Enable-WindowsOptionalFeature -Online -FeatureName Containers -All -NoRestart
+    Enable-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform -NoRestart
+    Install-WindowsFeature -Name Hyper-V -IncludeAllSubFeature -IncludeManagementTools -Restart
+
+    # Clean up Bootstrap.log
+    Write-Host "Clean up Bootstrap.log"
+    Stop-Transcript
+    $logSuppress = Get-Content $Env:ESULogsDir\Bootstrap.log | Where-Object { $_ -notmatch "Host Application: powershell.exe" }
+    $logSuppress | Set-Content $Env:ESULogsDir\Bootstrap.log -Force
---
+++ azure_arc_servers_jumpstart/esu/bicep/host/host.bicep
@@ -0,0 +1,193 @@
+@description('The name of your Virtual Machine')
+param vmName string = 'ESU-Host'
+
+@description('Username for the Virtual Machine')
+param windowsAdminUsername string = 'arcdemo'
+
+@description('Password for Windows account. Password must have 3 of the following: 1 lower case character, 1 upper case character, 1 number, and 1 special character. The value must be between 12 and 123 characters long')
+@minLength(12)
+@maxLength(123)
+@secure()
+param windowsAdminPassword string
+
+@description('The Windows version for the VM. This will pick a fully patched image of this given Windows version')
+param windowsOSVersion string = '2022-datacenter-g2'
+
+@description('Location for all resources')
+param location string = resourceGroup().location
+
+@description('Resource Id of the subnet in the virtual network')
+param subnetId string
+
+param resourceTags object = {
+  Project: 'jumpstart_servers'
+}
+
+@description('ESU deployment type, could be ws, sql or both')
+param esu string = 'both'
+
+@description('Client id of the service principal')
+param spnClientId string
+
+@description('Client secret of the service principal')
+@secure()
+param spnClientSecret string
+
+@description('Tenant id of the service principal')
+param spnTenantId string
+
+@description('The base URL used for accessing artifacts and automation artifacts.')
+param templateBaseUrl string
+
+@description('Option to disable automatic cluster registration. Setting this to false will also disable deploying AKS and Resource bridge')
+param registerCluster bool = true
+
+@description('Choice to deploy Bastion to connect to the client VM')
+param deployBastion bool = false
+
+@description('Public DNS to use for the domain')
+param natDNS string = '8.8.8.8'
+
+@description('Override default RDP port using this parameter. Default is 3389. No changes will be made to the client VM.')
+param rdpPort string = '3389'
+
+var encodedPassword = base64(windowsAdminPassword)
+var bastionName = 'ESU-Bastion'
+var publicIpAddressName = deployBastion == false ? '${vmName}-PIP' : '${bastionName}-PIP'
+var networkInterfaceName = '${vmName}-NIC'
+var osDiskType = 'Premium_LRS'
+var PublicIPNoBastion = {
+  id: publicIpAddress.id
+}
+
+resource networkInterface 'Microsoft.Network/networkInterfaces@2021-03-01' = {
+  name: networkInterfaceName
+  location: location
+  properties: {
+    ipConfigurations: [
+      {
+        name: 'ipconfig1'
+        properties: {
+          subnet: {
+            id: subnetId
+          }
+          privateIPAllocationMethod: 'Dynamic'
+          publicIPAddress: deployBastion == false ? PublicIPNoBastion : json('null')
+        }
+      }
+    ]
+  }
+}
+
+resource publicIpAddress 'Microsoft.Network/publicIpAddresses@2021-03-01' = if (deployBastion == false) {
+  name: publicIpAddressName
+  location: location
+  properties: {
+    publicIPAllocationMethod: 'Static'
+    publicIPAddressVersion: 'IPv4'
+    idleTimeoutInMinutes: 4
+  }
+  sku: {
+    name: 'Basic'
+  }
+}
+
+resource vm 'Microsoft.Compute/virtualMachines@2022-03-01' = {
+  name: vmName
+  location: location
+  tags: resourceTags
+  properties: {
+    hardwareProfile: {
+      vmSize: 'Standard_E32s_v5'
+    }
+    storageProfile: {
+      osDisk: {
+        name: '${vmName}-OSDisk'
+        caching: 'ReadWrite'
+        createOption: 'FromImage'
+        managedDisk: {
+          storageAccountType: osDiskType
+        }
+        diskSizeGB: 1024
+      }
+      imageReference: {
+        publisher: 'MicrosoftWindowsServer'
+        offer: 'WindowsServer'
+        sku: windowsOSVersion
+        version: 'latest'
+      }
+      dataDisks: [
+        {
+          name: 'ESUHost001_DataDisk_0'
+          diskSizeGB: 256
+          createOption: 'Empty'
+          lun: 0
+          caching: 'None'
+          writeAcceleratorEnabled: false
+          managedDisk: {
+            storageAccountType: 'Premium_LRS'
+          }
+        }
+        {
+          name: 'ESUHost001_DataDisk_1'
+          diskSizeGB: 256
+          createOption: 'Empty'
+          lun: 1
+          caching: 'None'
+          writeAcceleratorEnabled: false
+          managedDisk: {
+            storageAccountType: 'Premium_LRS'
+          }
+        }
+        {
+          name: 'ESUHost001_DataDisk_2'
+          diskSizeGB: 256
+          createOption: 'Empty'
+          lun: 2
+          caching: 'None'
+          writeAcceleratorEnabled: false
+          managedDisk: {
+            storageAccountType: 'Premium_LRS'
+          }
+        }
+      ]
+    }
+    networkProfile: {
+      networkInterfaces: [
+        {
+          id: networkInterface.id
+        }
+      ]
+    }
+    osProfile: {
+      computerName: vmName
+      adminUsername: windowsAdminUsername
+      adminPassword: windowsAdminPassword
+      windowsConfiguration: {
+        provisionVMAgent: true
+        enableAutomaticUpdates: false
+      }
+    }
+  }
+}
+
+resource vmBootstrap 'Microsoft.Compute/virtualMachines/extensions@2022-03-01' = {
+  parent: vm
+  name: 'Bootstrap'
+  location: location
+  properties: {
+    publisher: 'Microsoft.Compute'
+    type: 'CustomScriptExtension'
+    typeHandlerVersion: '1.10'
+    autoUpgradeMinorVersion: true
+    protectedSettings: {
+      fileUris: [
+        uri(templateBaseUrl, 'artifacts/Bootstrap.ps1')
+      ]
+      commandToExecute: 'powershell.exe -ExecutionPolicy Bypass -File Bootstrap.ps1 -adminUsername ${windowsAdminUsername} -adminPassword ${encodedPassword} -spnClientId ${spnClientId} -spnClientSecret ${spnClientSecret} -spnTenantId ${spnTenantId} -subscriptionId ${subscription().subscriptionId} -resourceGroup ${resourceGroup().name} -azureLocation ${location} -templateBaseUrl ${templateBaseUrl} -registerCluster ${registerCluster} -natDNS ${natDNS} -rdpPort ${rdpPort} -esu ${esu}'
+    }
+  }
+}
+
+output adminUsername string = windowsAdminUsername
+output publicIP string = deployBastion == false ? concat(publicIpAddress.properties.ipAddress) : ''
