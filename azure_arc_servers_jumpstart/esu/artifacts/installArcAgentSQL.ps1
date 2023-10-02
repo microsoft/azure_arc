54e9c0f60b4058df0b4a4ddb4f24e43a45bd2f2b
lanicolas <lauranicolas87@outlook.com>
2023-10-02 19:38:17 +0200

    PR review

--- azure_arc_servers_jumpstart/esu/artifacts/installArcAgentSQL.ps1
+++ azure_arc_servers_jumpstart/esu/artifacts/installArcAgentSQL.ps1
@@ -30,8 +30,7 @@ try {
     & "$env:ProgramW6432\AzureExtensionForSQLServer\AzureExtensionForSQLServer.exe" --subId $subscriptionId `
     --resourceGroup $resourceGroup --location $Azurelocation `
     --tenantid $spnTenantId --service-principal-app-id $spnClientId `
-	--machineName $vmName `
-    --service-principal-secret $spnClientSecret --licenseType Paid  
+    --service-principal-secret $spnClientSecret --licenseType Paid  --vmName $vmName
 	
 
 	if($LASTEXITCODE -eq 0){
