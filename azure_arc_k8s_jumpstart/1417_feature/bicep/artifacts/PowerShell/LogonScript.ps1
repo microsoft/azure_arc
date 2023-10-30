457ce75ad9af9b8c59db8cb3d5a3895eb9743541
Seif Bassem <38246040+sebassem@users.noreply.github.com>
2023-10-30 17:31:49 +0200

    add wait

--- azure_arc_k8s_jumpstart/1417_feature/bicep/artifacts/PowerShell/LogonScript.ps1
+++ azure_arc_k8s_jumpstart/1417_feature/bicep/artifacts/PowerShell/LogonScript.ps1
@@ -335,6 +335,8 @@ $eventGridHostName = (az eventgrid namespace list --resource-group testing11111 
 kubectl apply -f $Ft1ToolsDir\e4k.yml
 kubectl apply -f $Ft1ToolsDir\mq_bridge_eventgrid.yml
 
+Start-Sleep -Seconds 30
+
 ##############################################################
 # Deploy the simulator
 ##############################################################
