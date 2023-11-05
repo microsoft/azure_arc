939ab0f9c8e9ba98faeb9ecc2c974f88990493c2
Seif Bassem <38246040+sebassem@users.noreply.github.com>
2023-11-05 11:55:04 +0200

    adx

--- azure_arc_k8s_jumpstart/1417_feature/bicep/artifacts/PowerShell/LogonScript.ps1
+++ azure_arc_k8s_jumpstart/1417_feature/bicep/artifacts/PowerShell/LogonScript.ps1
@@ -418,7 +418,7 @@ kubectl apply -f $eventGrideBrideYaml -n azure-iot-operations
 # ADX Dashboards
 ########################################################################
 
-<#
+
 Write-Host "Importing Azure Data Explorer dashboards..."
 
 # Get the ADX/Kusto cluster info
@@ -428,7 +428,7 @@ $adxEndPoint = $kustoCluster.Uri
 # Update the dashboards files with the new ADX cluster name and URI
 $templateBaseUrl = "https://raw.githubusercontent.com/${githubAccount}/azure_arc/${githubBranch}/azure_arc_k8s_jumpstart/1417_feature/bicep"
 $dashboardBody = (Invoke-WebRequest -Method Get -Uri "$templateBaseUrl/artifacts/adx_dashboard/dashboard.json").Content -replace '{{ADX_CLUSTER_URI}}', $adxEndPoint
-
+<#
 # Get access token to make REST API call to Azure Data Explorer Dashabord API. Replace double quotes surrounding access token
 $token = (az account get-access-token --scope "https://rtd-metadata.azurewebsites.net/user_impersonation openid profile offline_access" --query "accessToken") -replace "`"", ""
 
