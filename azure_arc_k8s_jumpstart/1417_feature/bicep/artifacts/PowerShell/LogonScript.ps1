babc52038e3315d0a58d72248c7eeb6b7f4c2551
Seif Bassem <38246040+sebassem@users.noreply.github.com>
2023-11-08 09:36:03 +0200

    test

--- azure_arc_k8s_jumpstart/1417_feature/bicep/artifacts/PowerShell/LogonScript.ps1
+++ azure_arc_k8s_jumpstart/1417_feature/bicep/artifacts/PowerShell/LogonScript.ps1
@@ -379,9 +379,11 @@ az iot ops init --cluster $arcClusterName -g $resourceGroup --kv-id $keyVaultId 
 ##### Add health check to continue
 $extensionPrincipalId = (az k8s-extension show --cluster-name $arcClusterName --name "mq" --resource-group $resourceGroup --cluster-type "connectedClusters" --output json | ConvertFrom-Json).identity.principalId
 $eventGridTopicId = (az eventgrid topic list --resource-group $resourceGroup --query "[0].id" -o tsv)
+$eventGridNamespaceName = (az eventgrid namespace list --resource-group $resourceGroup --query "[0].name" -o tsv)
+$eventGridTopicSpaceId = (az eventgrid namespace topic-space list --resource-group $resourceGroup --namespace-name $eventGridNamespaceName)
 
-az role assignment create --assignee $extensionPrincipalId --role "EventGrid TopicSpaces Publisher" --resource-group $resourceGroup --only-show-errors
-az role assignment create --assignee $extensionPrincipalId --role "EventGrid TopicSpaces Subscriber" --resource-group $resourceGroup --only-show-errors
+az role assignment create --assignee $extensionPrincipalId --role "EventGrid TopicSpaces Publisher" --scope $eventGridTopicSpaceId --only-show-errors
+az role assignment create --assignee $extensionPrincipalId --role "EventGrid TopicSpaces Subscriber" --scope $eventGridTopicSpaceId --only-show-errors
 az role assignment create --assignee-object-id $extensionPrincipalId --role "EventGrid Data Sender" --scope $eventGridTopicId --assignee-principal-type ServicePrincipal
 az role assignment create --assignee-object-id $spnObjectId --role "EventGrid Data Sender" --scope $eventGridTopicId --assignee-principal-type ServicePrincipal
 
