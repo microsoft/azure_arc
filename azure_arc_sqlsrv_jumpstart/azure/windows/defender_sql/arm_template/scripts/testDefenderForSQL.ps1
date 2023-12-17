746734d82c9c072be21704e304a57a9c87e242f4
Venkata Chintala <29983008+chintalavr@users.noreply.github.com>
2023-12-17 18:32:13 -0500

    Update testDefenderForSQL.ps1

--- azure_arc_sqlsrv_jumpstart/azure/windows/defender_sql/arm_template/scripts/testDefenderForSQL.ps1
+++ azure_arc_sqlsrv_jumpstart/azure/windows/defender_sql/arm_template/scripts/testDefenderForSQL.ps1
@@ -1,23 +1,6 @@
 # Execute sql commands to generate defender for cloud alerts
 Write-Host "Executing Defender for SQL threat simulation script."
-$attempts = 0
-
-while ($attempts -le 5)
-{
-    $moduleFile = (Get-ChildItem -Path "$Env:ProgramFiles\Microsoft Monitoring Agent\Agent\Health Service State\Resources\" -File SqlAdvancedThreatProtectionShell.psm1 -Recurse -ErrorAction SilentlyContinue).FullName
-    $attempts = $attempts + 1
-    if ($true -eq [System.IO.File]::Exists($moduleFile))
-    {
-        Write-Host "Found module file $moduleFile installed."
-        break
-    }
-    else 
-    {
-        Write-Host "Module file $moduleFile not installed. Waiting for the module to be installed. Attempt: $attempts"
-        Start-Sleep(60) # Wait for agent to isntall all modules
-    }
-}
-
+$moduleFile = 'SqlAdvancedThreatProtectionShell.psm1'
 if ($true -ne [System.IO.File]::Exists($moduleFile))
 {
     Write-Host "Module file $moduleFile not installed. Try running script mannually later. Search for PowerShell module file 'SqlAdvancedThreatProtectionShell.psm1' in one of the '$Env:ProgramFiles\Microsoft Monitoring Agent\Agent\Health Service State\Resources\' sub folders to re-run this test script."
@@ -29,7 +12,7 @@ Import-Module $moduleFile
 Get-Command -Module SqlAdvancedThreatProtectionShell
 
 Write-Host "Executing SQL injection"
-$saPasswordEncrypted = ConvertTo-SecureString -String "ArcDemo123!!" -AsPlainText -Force
+$saPasswordEncrypted = ConvertTo-SecureString -String "JS123!!" -AsPlainText -Force
 Test-SqlAtpInjection -UserName sa -Password $saPasswordEncrypted # High risk
 Start-Sleep(30) # Wait between tests
 
