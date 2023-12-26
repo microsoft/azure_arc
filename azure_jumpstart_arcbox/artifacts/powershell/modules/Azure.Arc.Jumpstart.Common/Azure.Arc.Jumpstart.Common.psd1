dea4b024ecd42eb305e3cd4c8b4674220c7d5e64
Jan Egil Ring <janegilring@microsoft.com>
2023-12-26 17:48:01 +0000

    Bugfix - path
    Signed-off-by: Jan Egil Ring <janegilring@microsoft.com>

--- azure_jumpstart_arcbox/artifacts/powershell/modules/Azure.Arc.Jumpstart.Common/Azure.Arc.Jumpstart.Common.psd1
+++ azure_jumpstart_arcbox/artifacts/powershell/modules/Azure.Arc.Jumpstart.Common/Azure.Arc.Jumpstart.Common.psd1
@@ -12,7 +12,7 @@
 RootModule = 'Azure.Arc.Jumpstart.Common.psm1'
 
 # Version number of this module.
-ModuleVersion = '0.0.3'
+ModuleVersion = '0.0.4'
 
 # Supported PSEditions
 # CompatiblePSEditions = @()
--- azure_jumpstart_arcbox/artifacts/powershell/modules/Azure.Arc.Jumpstart.Common/Functions/Public/Convert-JSImageToBitMap.ps1
+++ azure_jumpstart_arcbox/artifacts/powershell/modules/Azure.Arc.Jumpstart.Common/Functions/Public/Convert-JSImageToBitMap.ps1
@@ -6,5 +6,5 @@ function Convert-JSImageToBitMap {
     [Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms") | Out-Null
     $file = Get-Item $SourceFilePath
     $convertfile = new-object System.Drawing.Bitmap($file.Fullname)
-    $convertfile.Save($newfilname, "bmp")
+    $convertfile.Save($DestinationFilePath, "bmp")
 }
