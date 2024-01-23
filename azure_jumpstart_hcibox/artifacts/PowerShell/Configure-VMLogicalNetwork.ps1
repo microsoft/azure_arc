b795f4e5355376bb4e2c04e445a826a4ddfa65ac
saitcho <saitcho@outlook.com>
2024-01-23 10:42:37 -0600

    vm logical network

--- azure_jumpstart_hcibox/artifacts/PowerShell/Configure-VMLogicalNetwork.ps1
+++ azure_jumpstart_hcibox/artifacts/PowerShell/Configure-VMLogicalNetwork.ps1
@@ -7,10 +7,10 @@ $Env:HCIBoxDir = "C:\HCIBox"
 
 # Import Configuration Module
 $HCIBoxConfig = Import-PowerShellDataFile -Path $Env:HCIBoxConfigFile
-Start-Transcript -Path "$($HCIBoxConfig.Paths.LogsDir)\Configure-VMManagement.log"
+Start-Transcript -Path "$($HCIBoxConfig.Paths.LogsDir)\Configure-VMLogicalNetwork.log"
 
-# Install AZ Resource Bridge and prerequisites
-Write-Host "Now Preparing to configure guest VM management"
+$domainCred = new-object -typename System.Management.Automation.PSCredential `
+    -argumentlist (($HCIBoxConfig.SDNDomainFQDN.Split(".")[0]) +"\Administrator"), (ConvertTo-SecureString $HCIBoxConfig.SDNAdminPassword -AsPlainText -Force)
 
 $subId = $env:subscriptionId
 $rg = $env:resourceGroup
@@ -21,15 +21,18 @@ $location = "eastus"
 $customLocName = $HCIBoxConfig.rbCustomLocationName
 
 # Create logical networks
-az login --service-principal --username $env:spnClientID --password=$env:spnClientSecret --tenant $env:spnTenantId
-az config set extension.use_dynamic_install=yes_without_prompt
-$customLocationID=(az customlocation show --resource-group $env:resourceGroup --name $customLocName --query id -o tsv)
+Invoke-Command -ComputerName "$($HCIBoxConfig.NodeHostConfig[0].Hostname).$($HCIBoxConfig.SDNDomainFQDN)" -Credential $domainCred -Authentication CredSSP -ArgumentList $HCIBoxConfig -ScriptBlock {
+    $HCIBoxConfig = $args[0]
+    az login --service-principal --username $using:spnClientID --password=$using:spnSecret --tenant $using:spnTenantId
+    az config set extension.use_dynamic_install=yes_without_prompt
+    $customLocationID=(az customlocation show --resource-group $using:rg --name $using:customLocName --query id -o tsv)
 
-$switchName='"ConvergedSwitch(hci)"'
-$lnetName = "hcibox-vm-lnet-static"
-$addressPrefixes = $HCIBoxConfig.vmIpPrefix
-$gateway = $HCIBoxConfig.vmGateway
-$dnsServers = $HCIBoxConfig.vmDNS
-$vlanid = $HCIBoxConfig.vmVLAN
+    $switchName='"ConvergedSwitch(hci)"'
+    $lnetName = "hcibox-vm-lnet-vlan200"
+    $addressPrefixes = $HCIBoxConfig.vmIpPrefix
+    $gateway = $HCIBoxConfig.vmGateway
+    $dnsServers = $HCIBoxConfig.vmDNS
+    $vlanid = $HCIBoxConfig.vmVLAN
 
-az stack-hci-vm network lnet create --subscription $env:subscriptionId --resource-group $env:resourceGroup --custom-location $customLocationID --location $location --name $lnetName --vm-switch-name $switchName --ip-allocation-method "static" --address-prefixes $addressPrefixes --gateway $gateway --dns-servers $dnsServers --vlan $vlanid
+    az stack-hci-vm network lnet create --subscription $using:subId --resource-group $using:rg --custom-location $customLocationID --location $using:location --name $lnetName --vm-switch-name $switchName --ip-allocation-method "static" --address-prefixes $addressPrefixes --gateway $gateway --dns-servers $dnsServers --vlan $vlanid
+}
