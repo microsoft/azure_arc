c5a795258aa08990552509e40989354b794fffab
Jan Egil Ring <janegilring@microsoft.com>
2024-03-14 13:55:00 +0100

    Update Kubernetes version in variables.tf
    Signed-off-by: Jan Egil Ring <janegilring@microsoft.com>

--- azure_arc_k8s_jumpstart/aks/terraform/variables.tf
+++ azure_arc_k8s_jumpstart/aks/terraform/variables.tf
@@ -20,7 +20,7 @@ variable "location" {
 
 variable "kubernetes_version" {
   description = "Kubernetes version deployed"
-  default     = "1.27.7"
+  default     = "1.28.5"
 }
 
 variable "node_count" {
