95a0e5c01bb69a19803ad403dfa7a4aaac4b2100
ldabas-msft <lakshitdabas@microsoft.com>
2024-03-22 18:02:41 +0000

    Dockerised main.py but video_stream does not show overlays

--- azure_edge_iot_ops_jumpstart/aio_esa/artifacts/esa_ai/main.py
+++ azure_edge_iot_ops_jumpstart/aio_esa/artifacts/esa_ai/main.py
@@ -13,11 +13,10 @@ import numpy as np
 import json
 import sys
 
-from ultralytics import YOLO
+
 
 
 # GLOBAL Variables
-CONFIG_FILE = './resources/config.json'
 
 OBJECT_AREA_MIN = 9000
 OBJECT_AREA_MAX = 50000
@@ -44,7 +43,8 @@ model = YOLO("yolo-Weights/yolov8n.pt")
 classNames = classNames = os.environ.get('CLASS_NAMES').split(',')
 print("Class names read from CLASS_NAMES variable: ", classNames)
 """
-source = "rtsp://127.0.0.1:554/stream"
+source = "rtsp://host.docker.internal:554/stream"
+
 if 'RTSP_URL' in os.environ:
     source = os.environ.get('RTSP_URL')
 print("Incoming video feed read from RTSP_URL: ", source)
@@ -109,6 +109,7 @@ def flaw_detection():
     while cap.isOpened():
         # Read the frame from the stream
         ret, frame = cap.read()
+        base_dir = os.getcwd()
 
         if not ret:
             break
@@ -210,7 +211,7 @@ def flaw_detection():
                         cv2.putText(frame_nodefect, "Width (mm): {}".format(WIDTH_OF_OBJ),
                                     (5, 110), cv2.FONT_HERSHEY_SIMPLEX, 0.75,
                                     (255, 255, 255), 2)
-                        cv2.imwrite("{}/no_defect/Nodefect_{}.png".format(
+                        cv2.imwrite("{}/esa_storage/Nodefect_{}.png".format(
                             base_dir, COUNT_OBJECT),
                                     frame[y : y + h,
                                           x : x + w])
@@ -250,188 +251,22 @@ def flaw_detection():
                     cv2.FONT_HERSHEY_SIMPLEX, 0.75, (255, 255, 255), 2)
         cv2.putText(frame, "Width (mm): {}".format(WIDTH_OF_OBJ), (5, 110),
                     cv2.FONT_HERSHEY_SIMPLEX, 0.75, (255, 255, 255), 2)
-        cv2.imshow("Out", frame)
+        """cv2.imshow("Out", frame)
         keypressed = cv2.waitKey(40)
         if keypressed == 113 or keypressed == 81:
-            break
+            break"""
 
 #adding the below code embeds the video in the video_feed page but fails to show overlays on the faults
-        """ret, buffer = cv2.imencode('.png', frame)
+        ret, buffer = cv2.imencode('.png', frame)
         frame=buffer.tobytes()
 
         yield (b'--frame\r\n'
-              b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n') """
+              b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
     cv2.destroyAllWindows()
     cap.release()
 
-def flaw_detection_old():
-    """
-    Measurement and defects such as color, crack and orientation of the object
-    are found.
 
-    :return: None
-    """
-    global HEIGHT_OF_OBJ
-    global WIDTH_OF_OBJ
-    global COUNT_OBJECT
-    global OBJ_DEFECT
-    global FRAME_COUNT
-    global OBJECT_COUNT
-
-    while cap.isOpened():
-        # Read the frame from the stream
-        ret, frame = cap.read()
-
-        if not ret:
-            break
-
-        FRAME_COUNT += 1
-
-        # Check every given frame number
-        # (Number chosen based on the frequency of object on conveyor belt)
-        if FRAME_COUNT % frame_number == 0:
-            HEIGHT_OF_OBJ = 0
-            WIDTH_OF_OBJ = 0
-            OBJ_DEFECT = []
-            data_base = []
-            # Convert BGR image to HSV color space
-            img_hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
-            # Thresholding of an Image in a color range
-            img_threshold = cv2.inRange(img_hsv, (LOW_H, LOW_S, LOW_V),
-                                        (HIGH_H, HIGH_S, HIGH_V))
-
-            # Morphological opening(remove small objects from the foreground)
-            img_threshold = cv2.erode(img_threshold,
-                                      cv2.getStructuringElement(
-                                          cv2.MORPH_ELLIPSE, (5, 5)))
-            img_threshold = cv2.dilate(img_threshold,
-                                       cv2.getStructuringElement(
-                                           cv2.MORPH_ELLIPSE, (5, 5)))
-
-            # Morphological closing(fill small holes in the foreground)
-            img_threshold = cv2.dilate(img_threshold,
-                                       cv2.getStructuringElement(
-                                           cv2.MORPH_ELLIPSE, (5, 5)))
-            img_threshold = cv2.erode(img_threshold,
-                                      cv2.getStructuringElement(
-                                          cv2.MORPH_ELLIPSE, (5, 5)))
-
-            # Find the contours on the image
-            contours, hierarchy = cv2.findContours(img_threshold,
-                                                   cv2.RETR_LIST,
-                                                   cv2.CHAIN_APPROX_NONE)
-
-            for cnt in contours:
-                x, y, w, h = cv2.boundingRect(cnt)
-                if OBJECT_AREA_MAX > w * h > OBJECT_AREA_MIN:
-                    box = cv2.minAreaRect(cnt)
-                    box = cv2.boxPoints(box)
-                    height, width = dimensions(np.array(box, dtype='int'))
-                    HEIGHT_OF_OBJ = round(height * one_pixel_length * 10, 2)
-                    WIDTH_OF_OBJ = round(width * one_pixel_length * 10, 2)
-                    COUNT_OBJECT += 1
-                    frame_orient = frame.copy()
-                    frame_clr = frame.copy()
-                    frame_crack = frame.copy()
-                    frame_nodefect = frame.copy()
-                    OBJECT_COUNT = "Object Number : {}".format(COUNT_OBJECT)
-
-                    # Check for the orientation of the object
-                    orientation_flag, orientation_defect = \
-                        detect_orientation(frame_orient, cnt)
-                    if orientation_flag:
-                        value = 1
-                        data_base.append(value)
-                        OBJ_DEFECT.append(str(orientation_defect))
-                    else:
-                        value = 0
-                        data_base.append(value)
-
-                    # Check for the color defect of the object
-                    color_flag, color_defect = detect_color(frame_clr, cnt)
-                    if color_flag:
-                        value = 1
-                        data_base.append(value)
-                        OBJ_DEFECT.append(str(color_defect))
-                    else:
-                        value = 0
-                        data_base.append(value)
-
-                    # Check for the crack defect of the object
-                    crack_flag, crack_defect = detect_crack(frame_crack, cnt)
-                    if crack_flag:
-                        value = 1
-                        data_base.append(value)
-                        OBJ_DEFECT.append(str(crack_defect))
-                    else:
-                        value = 0
-                        data_base.append(value)
-
-                    # Check if none of the defect is found
-                    if not OBJ_DEFECT:
-                        value = 1
-                        data_base.append(value)
-                        defect = "No Defect"
-                        OBJ_DEFECT.append(defect)
-                        print("No defect detected in object {}"
-                              .format(COUNT_OBJECT))
-                        cv2.putText(frame_nodefect, "Length (mm): {}".format(HEIGHT_OF_OBJ),
-                                    (5, 80), cv2.FONT_HERSHEY_SIMPLEX, 0.75,
-                                    (255, 255, 255), 2)
-                        cv2.putText(frame_nodefect, "Width (mm): {}".format(WIDTH_OF_OBJ),
-                                    (5, 110), cv2.FONT_HERSHEY_SIMPLEX, 0.75,
-                                    (255, 255, 255), 2)
-                        cv2.imwrite("{}/no_defect/Nodefect_{}.png".format(
-                            base_dir, COUNT_OBJECT),
-                                    frame[y : y + h,
-                                          x : x + w])
-                    else:
-                        value = 0
-                        data_base.append(value)
-                    print("Length (mm) = {}, width (mm) = {}".format(
-                        HEIGHT_OF_OBJ, WIDTH_OF_OBJ))
-            if not OBJ_DEFECT:
-                continue
-
-            # Create json_body to store the defects
-            else:
-                json_body = {
-                    "measurement": "obj_flaw_detector",
-                    "tags": {
-                        "user": "User"
-                    },
-
-                    "fields": {
-                        "Object Number": COUNT_OBJECT,
-                        "Orientation": data_base[0],
-                        "Color": data_base[1],
-                        "Crack": data_base[2],
-                        "No defect": data_base[3]
-                    }
-                }
-
-        all_defects = " ".join(OBJ_DEFECT)
-        cv2.putText(frame, "Press q to quit", (410, 50),
-                    cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
-        cv2.putText(frame, OBJECT_COUNT, (5, 50), cv2.FONT_HERSHEY_SIMPLEX,
-                    0.75, (255, 255, 255), 2)
-        cv2.putText(frame, "Defect: {}".format(all_defects), (5, 140),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.75, (255, 255, 255), 2)
-        cv2.putText(frame, "Length (mm): {}".format(HEIGHT_OF_OBJ), (5, 80),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.75, (255, 255, 255), 2)
-        cv2.putText(frame, "Width (mm): {}".format(WIDTH_OF_OBJ), (5, 110),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.75, (255, 255, 255), 2)
-        cv2.imshow("Out", frame)
-        keypressed = cv2.waitKey(40)
-        if keypressed == 113 or keypressed == 81:
-            break
-
-        
-
-    cv2.destroyAllWindows()
-    cap.release()
 
 def get_orientation(contours):
     """
@@ -476,7 +311,7 @@ def detect_orientation(frame, contours, base_dir = os.getcwd()):
         x, y, w, h = cv2.boundingRect(contours)
         print("Orientation defect detected in object {}".format(COUNT_OBJECT))
         defect_flag = True
-        cv2.imwrite("{}/orientation/Orientation_{}.png"
+        cv2.imwrite("{}/esa_storage/Orientation_{}.png"
                     .format(base_dir, COUNT_OBJECT),
                     frame[y: y + h , x : x + w])
         cv2.putText(frame, OBJECT_COUNT, (5, 50), cv2.FONT_HERSHEY_SIMPLEX,
@@ -539,7 +374,7 @@ def detect_color(frame, cnt, base_dir = os.getcwd()):
     if color_flag:
         x, y, w, h = cv2.boundingRect(cnt)
         print("Color defect detected in object {}".format(COUNT_OBJECT))
-        cv2.imwrite("{}/color/Color_{}.png".format(base_dir, COUNT_OBJECT),
+        cv2.imwrite("{}/esa_storage/Color_{}.png".format(base_dir, COUNT_OBJECT),
                     frame[y : y + h, x : x + w])
         cv2.putText(frame, OBJECT_COUNT, (5, 50), cv2.FONT_HERSHEY_SIMPLEX,
                     0.75, (255, 255, 255), 2)
@@ -595,7 +430,7 @@ def detect_crack(frame, cnt, base_dir = os.getcwd()):
         if defect_flag:
             x, y, w, h = cv2.boundingRect(cnt)
             print("Crack defect detected in object {}".format(COUNT_OBJECT))
-            cv2.imwrite("{}/crack/Crack_{}.png".format(base_dir, COUNT_OBJECT),
+            cv2.imwrite("{}/esa_storage/Crack_{}.png".format(base_dir, COUNT_OBJECT),
                         frame[y : y + h , x : x + w ])
             cv2.putText(frame, OBJECT_COUNT, (5, 50), cv2.FONT_HERSHEY_SIMPLEX,
                         0.75, (255, 255, 255), 2)
@@ -667,42 +502,21 @@ if __name__ == '__main__':
 
 
 
-    config = json.loads(open(CONFIG_FILE).read())
+    input_stream = source
+    cap = cv2.VideoCapture(input_stream)
+    if not cap.isOpened():
+        print("\nCamera not plugged in... Exiting...\n")
+        sys.exit(0)
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    delay = (int)(1000 / fps)
 
-    for idx, item in enumerate(config['inputs']):
-        if item['video'].isdigit():
-            input_stream = source
-            cap = cv2.VideoCapture(input_stream)
-            if not cap.isOpened():
-                print("\nCamera not plugged in... Exiting...\n")
-                sys.exit(0)
-            fps = cap.get(cv2.CAP_PROP_FPS)
-            delay = (int)(1000 / fps)
-        else:
-            input_stream = source
-            cap = cv2.VideoCapture(input_stream)
-            if not cap.isOpened():
-                print("\nUnable to open video file... Exiting...\n")
-                sys.exit(0)
-            fps = cap.get(cv2.CAP_PROP_FPS)
-            delay = (int)(1000 / fps)
 
-        one_pixel_length = 0.0264583333
+    one_pixel_length = 0.0264583333
 
-    dir_names = ["crack", "color", "orientation", "no_defect"]
     OBJ_DEFECT = []
     frame_number = 40
     FRAME_COUNT = 0
 
-    base_dir = os.getcwd()
-    # create folders with the given dir_names to save defective objects
-    for i in range(len(dir_names)):
-        if not os.path.exists(os.path.join(base_dir, dir_names[i])):
-            os.makedirs(os.path.join(base_dir, dir_names[i]))
-        else:
-            file_list = os.listdir(os.path.join(base_dir, dir_names[i]))
-            for f in file_list:
-                os.remove(os.path.join(base_dir, dir_names[i], f))
     # Find dimensions and flaw detections such as color, crack and orientation
     # of the object.
     """flaw_detection()"""
