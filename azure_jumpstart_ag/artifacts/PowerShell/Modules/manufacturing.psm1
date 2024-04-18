610dd69e7cfc263e414e69c85e7240a1a6d18067
Venkata Chintala <29983008+chintalavr@users.noreply.github.com>
2024-04-18 13:27:35 -0400

    Fixed minor errors

--- azure_jumpstart_ag/artifacts/PowerShell/Modules/manufacturing.psm1
+++ azure_jumpstart_ag/artifacts/PowerShell/Modules/manufacturing.psm1
@@ -657,7 +657,7 @@ function Deploy-ADXDashboardReports {
     if ($null -ne $kustoCluster) {
         $adxEndPoint = $kustoCluster.Uri
         if ($null -ne $adxEndPoint -and $adxEndPoint -ne "") {
-            $ordersDashboardBody = (Invoke-WebRequest -Method Get -Uri "$templateBaseUrl/artifacts/adx_dashboards/adx-dashboard-contoso-motors-auto-parts.json").Content -replace '{{ADX_CLUSTER_URI}}', $adxEndPoint -replace '{{ADX_CLUSTER_NAME}}', $adxClusterName
+            $ordersDashboardBody = (Invoke-WebRequest -Method Get -Uri "$templateBaseUrl/artifacts/adx_dashboards/adx-dashboard-contoso-motors-auto-parts.json").Content -replace '{{ADX_CLUSTER_URI}}', $adxEndPoint -replace '{{ADX_CLUSTER_NAME}}', $adxClusterName -replace '{{GITHUB_BRANCH}}', $env:githubBranch -replace '{{GITHUB_ACCOUNT}}', $env:githubAccount
             Set-Content -Path "$adxDashBoardsDir\adx-dashboard-contoso-motors-auto-parts.json" -Value $ordersDashboardBody -Force -ErrorAction Ignore
         }
         else {
@@ -665,28 +665,27 @@ function Deploy-ADXDashboardReports {
         }
     }
 
-        # Create EventHub environment variables
-        $eventHubNamespace = (az eventhubs namespace list --resource-group $env:resourceGroup --query [0].name --output tsv)
-        if ($null -ne $eventHubNamespace) {
-            # Find EventHub and create connection string
-            $eventHub = (az eventhubs eventhub list --namespace-name $eventHubNamespace --resource-group $env:resourceGroup --query [0].name --output tsv)
-    
-            # Create authorization rule
-            $authRuleName = "data-emulator"
-            az eventhubs eventhub authorization-rule create --authorization-rule-name $authRuleName --eventhub-name $eventHub --namespace-name $eventHubNamespace --resource-group $env:resourceGroup --rights Send Listen
-    
-            # Get connection string
-            $connectionString = (az eventhubs eventhub authorization-rule keys list --resource-group $env:resourceGroup --namespace-name $eventHubNamespace --eventhub-name $eventHub --name $authRuleName --query primaryConnectionString --output tsv)
-    
-            # Set environment variables
-            [System.Environment]::SetEnvironmentVariable('EVENTHUB_CONNECTION_STRING', $connectionString, [System.EnvironmentVariableTarget]::Machine)
-            [System.Environment]::SetEnvironmentVariable('EVENTHUB_NAME', $eventHub, [System.EnvironmentVariableTarget]::Machine)
-        }
+    # Create EventHub environment variables
+    $eventHubNamespace = (az eventhubs namespace list --resource-group $env:resourceGroup --query [0].name --output tsv)
+    if ($null -ne $eventHubNamespace) {
+        # Find EventHub and create connection string
+        $eventHub = (az eventhubs eventhub list --namespace-name $eventHubNamespace --resource-group $env:resourceGroup --query [0].name --output tsv)
+
+        # Create authorization rule
+        $authRuleName = "data-emulator"
+        az eventhubs eventhub authorization-rule create --authorization-rule-name $authRuleName --eventhub-name $eventHub --namespace-name $eventHubNamespace --resource-group $env:resourceGroup --rights Send Listen
+
+        # Get connection string
+        $connectionString = (az eventhubs eventhub authorization-rule keys list --resource-group $env:resourceGroup --namespace-name $eventHubNamespace --eventhub-name $eventHub --name $authRuleName --query primaryConnectionString --output tsv)
+
+        # Set environment variables
+        [System.Environment]::SetEnvironmentVariable('EVENTHUB_CONNECTION_STRING', $connectionString, [System.EnvironmentVariableTarget]::Machine)
+        [System.Environment]::SetEnvironmentVariable('EVENTHUB_NAME', $eventHub, [System.EnvironmentVariableTarget]::Machine)
+    }
     
     # Create desktop icons
-    $AgAppsRepoDir = $AgConfig.AgDirectories["AgAppsRepo"] 
-    $dataEmulatorFile = "$AgAppsRepoDir\data-emulator.py"
     $AgDataEmulatorDir = $AgConfig.AgDirectories["AgDataEmulator"]
+    $dataEmulatorFile = "$AgDataEmulatorDir\data-emulator.py"
     Invoke-WebRequest -Method Get -Uri "$templateBaseUrl/artifacts/data_emulator/data-emulator.py" -OutFile $dataEmulatorFile
     if (!(Test-Path -Path $dataEmulatorFile)) {
         Write-Host "Unabled to download data-emulator.py file. Please download manually from GitHub into the DataEmulator folder."
