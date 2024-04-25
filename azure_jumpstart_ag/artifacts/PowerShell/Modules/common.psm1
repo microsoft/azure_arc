6d2e3b25848ad6e1f1f2ee0b8b46bbfa2cb7f0f5
alejandro <alsanch@microsoft.com>
2024-04-25 10:33:16 +0200

    troubleshooting workbook os performance

--- azure_jumpstart_ag/artifacts/PowerShell/Modules/common.psm1
+++ azure_jumpstart_ag/artifacts/PowerShell/Modules/common.psm1
@@ -776,8 +776,9 @@ function Deploy-Workbook ($workbookFileName) {
     $content = Get-Content -Path $workbookTemplateFilePath -Raw
     # Replace placeholders with actual values
     $updatedContent = $content -replace 'rg-placeholder', $resourceGroup
-    $updatedContent = $updatedContent -replace'/subscriptions/00000000-0000-0000-0000-000000000000', "/subscriptions/$($subscriptionId)"
+    Write-Host "[$(Get-Date -Format t)] INFO: ASG /subscriptions/$($subscriptionId)/resourceGroups/$($Env:resourceGroup)/providers/Microsoft.OperationalInsights/workspaces/$($Env:workspaceName)"
     $updatedContent = $updatedContent -replace'/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/xxxx/providers/Microsoft.OperationalInsights/workspaces/xxxx', "/subscriptions/$($subscriptionId)/resourceGroups/$($Env:resourceGroup)/providers/Microsoft.OperationalInsights/workspaces/$($Env:workspaceName)"
+    $updatedContent = $updatedContent -replace'/subscriptions/00000000-0000-0000-0000-000000000000', "/subscriptions/$($subscriptionId)"
     
     # Write the updated content back to the file
     Set-Content -Path $workbookTemplateFilePath -Value $updatedContent
