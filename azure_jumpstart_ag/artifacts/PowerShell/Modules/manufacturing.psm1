20cf967bb0b96031d46b4068eefa802c8ef41a3c
Seif Bassem <38246040+sebassem@users.noreply.github.com>
2024-05-07 18:30:02 +0300

    Update Influxdb URLs in manufacturing module and bookmarks

--- azure_jumpstart_ag/artifacts/PowerShell/Modules/manufacturing.psm1
+++ azure_jumpstart_ag/artifacts/PowerShell/Modules/manufacturing.psm1
@@ -293,7 +293,6 @@ function Deploy-AIO {
         Write-Host "AIO deployed successfully on the $clusterName cluster" -ForegroundColor Green
         Write-Host "`n"
         Write-Host "[$(Get-Date -Format t)] INFO: Started Event Grid role assignment process" -ForegroundColor DarkGray
-        $extName = "mq-" + $using:namingGuid
         $extensionPrincipalId =(az k8s-extension list --cluster-name $arcClusterName --resource-group $resourceGroup --cluster-type "connectedClusters" --query "[?extensionType=='microsoft.iotoperations.mq']" --output json | ConvertFrom-Json)[0].identity.principalId
         $eventGridTopicId = (az eventgrid topic list --resource-group $resourceGroup --query "[0].id" -o tsv --only-show-errors)
         $eventGridNamespaceName = (az eventgrid namespace list --resource-group $resourceGroup --query "[0].name" -o tsv --only-show-errors)
@@ -562,7 +561,7 @@ function Deploy-ManufacturingBookmarks {
 
             # Replace matching value in the Bookmarks file
             $content = Get-Content -Path $bookmarksFileName
-            $newContent = $content -replace ("Influxdb" + $cluster.Name + "-URL"), $output
+            $newContent = $content -replace ("Influxdb-" + $cluster.Name + "-URL"), $output
             $newContent | Set-Content -Path $bookmarksFileName
 
             Start-Sleep -Seconds 2
