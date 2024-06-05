f846010461152e01fe03b57949485c883074cb21
Zaid Mohammad <mozaid@microsoft.com>
2024-06-05 14:45:01 -0400

    add kubevip for service ip

--- azure_jumpstart_arcbox/artifacts/DevOpsLogonScript.ps1
+++ azure_jumpstart_arcbox/artifacts/DevOpsLogonScript.ps1
@@ -15,6 +15,12 @@ $certdns = "arcbox.devops.com"
 
 $appClonedRepo = "https://github.com/$Env:githubUser/azure-arc-jumpstart-apps"
 
+$clusters = @(
+    [pscustomobject]@{clusterName = $Env:k3sArcDataClusterName; context = $Env:k3sArcDataClusterName.ToLower() ; kubeConfig = "C:\Users\$Env:adminUsername\.kube\config-datasvc-k3s" }
+
+    [pscustomobject]@{clusterName = $Env:k3sArcClusterName; context = $Env:k3sArcClusterName.ToLower() ; kubeConfig = "C:\Users\$Env:adminUsername\.kube\config-k3s" }
+)
+
 Start-Transcript -Path $Env:ArcBoxLogsDir\DevOpsLogonScript.log
 
 # Required for azcopy and Get-AzResource
@@ -97,10 +103,128 @@ az config set extension.use_dynamic_install=yes_without_prompt
 Write-Host "`n"
 az -v
 
-# Longhorn setup for RWX-capable storage class
-Write-Header "Creating longhorn storage"
-kubectl apply -f "$Env:ArcBoxDir\longhorn.yaml"
+foreach ($cluster in $clusters) {
+
+Write-Header "Configuring kube-vip on K3s cluster"
+kubectx $cluster.context
+kubectl apply -f https://kube-vip.io/manifests/rbac.yaml
+
+$k3sVIP = az network nic ip-config list --resource-group $Env:resourceGroup --nic-name $cluster.clusterName-NIC --query "[?primary == ``true``].privateIPAddress" -otsv
+
+$kubeVipDaemonset = @"
+apiVersion: apps/v1
+kind: DaemonSet
+metadata:
+  creationTimestamp: null
+  labels:
+    app.kubernetes.io/name: kube-vip-ds
+    app.kubernetes.io/version: v0.7.0
+  name: kube-vip-ds
+  namespace: kube-system
+spec:
+  selector:
+    matchLabels:
+      app.kubernetes.io/name: kube-vip-ds
+  template:
+    metadata:
+      creationTimestamp: null
+      labels:
+        app.kubernetes.io/name: kube-vip-ds
+        app.kubernetes.io/version: v0.7.0
+    spec:
+      affinity:
+        nodeAffinity:
+          requiredDuringSchedulingIgnoredDuringExecution:
+            nodeSelectorTerms:
+            - matchExpressions:
+              - key: node-role.kubernetes.io/master
+                operator: Exists
+            - matchExpressions:
+              - key: node-role.kubernetes.io/control-plane
+                operator: Exists
+      containers:
+      - args:
+        - manager
+        env:
+        - name: vip_arp
+          value: "true"
+        - name: port
+          value: "6443"
+        - name: vip_interface
+          value: eth0
+        - name: vip_cidr
+          value: "32"
+        - name: dns_mode
+          value: first
+        - name: cp_enable
+          value: "true"
+        - name: cp_namespace
+          value: kube-system
+        - name: svc_enable
+          value: "true"
+        - name: svc_leasename
+          value: plndr-svcs-lock
+        - name: vip_leaderelection
+          value: "true"
+        - name: vip_leasename
+          value: plndr-cp-lock
+        - name: vip_leaseduration
+          value: "5"
+        - name: vip_renewdeadline
+          value: "3"
+        - name: vip_retryperiod
+          value: "1"
+        - name: address
+          value: "$k3sVIP"
+        - name: prometheus_server
+          value: :2112
+        image: ghcr.io/kube-vip/kube-vip:v0.7.0
+        imagePullPolicy: Always
+        name: kube-vip
+        resources: {}
+        securityContext:
+          capabilities:
+            add:
+            - NET_ADMIN
+            - NET_RAW
+      hostNetwork: true
+      serviceAccountName: kube-vip
+      tolerations:
+      - effect: NoSchedule
+        operator: Exists
+      - effect: NoExecute
+        operator: Exists
+  updateStrategy: {}
+status:
+  currentNumberScheduled: 0
+  desiredNumberScheduled: 0
+  numberMisscheduled: 0
+  numberReady: 0
+"@
+
+$kubeVipDaemonset | kubectl apply -f -
+
+# Kube vip cloud controller
+kubectl apply -f https://raw.githubusercontent.com/kube-vip/kube-vip-cloud-provider/main/manifest/kube-vip-cloud-controller.yaml
+
+$serviceIpRange = az network nic ip-config list --resource-group $Env:resourceGroup --nic-name $cluster.clusterName-NIC --query "[?primary == ``false``].privateIPAddress" -otsv
+$sortedIps = $serviceIpRange | Sort-Object {[System.Version]$_}
+$lowestServiceIp = $sortedIps[0]
+$highestServiceIp = $sortedIps[-1]
+
+kubectl create configmap -n kube-system kubevip --from-literal range-global=$lowestServiceIp-$highestServiceIp
 Start-Sleep -Seconds 30
+
+Write-Header "Creating longhorn storage on K3scluster"
+kubectl apply -f "$Env:ArcBoxDir\longhorn.yaml" --kubeconfig $cluster.kubeConfig
+Start-Sleep -Seconds 30
+Write-Host "`n"
+}
+
+# # Longhorn setup for RWX-capable storage class
+# Write-Header "Creating longhorn storage"
+# kubectl apply -f "$Env:ArcBoxDir\longhorn.yaml"
+# Start-Sleep -Seconds 30
 
 # "Create OSM Kubernetes extension instance"
 Write-Header "Creating OSM K8s Extension Instance"
