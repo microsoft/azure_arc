9aee59b1decdf20751e61cc0389e52725144d46f
Venkata Chintala <29983008+chintalavr@users.noreply.github.com>
2024-06-28 15:38:04 -0400

    Update installK3s.sh

--- azure_jumpstart_arcbox/artifacts/installK3s.sh
+++ azure_jumpstart_arcbox/artifacts/installK3s.sh
@@ -149,16 +149,16 @@ if [[ "$k3sControlPlane" == "true" ]]; then
     echo ""
     echo "Onboarding the cluster to Azure Arc"
     echo ""
-    #resourceGroup=$(sudo -u $adminUsername az resource list --query "[?name=='$stagingStorageAccountName']".[resourceGroup] --resource-type "Microsoft.Storage/storageAccounts" -o tsv)
-    workspaceResourceId=$(sudo -u $adminUsername az resource show --resource-group $storageAccountRG --name $logAnalyticsWorkspace --resource-type "Microsoft.OperationalInsights/workspaces" --query id -o tsv)
+    resourceGroup=$(sudo -u $adminUsername az resource list --query "[?name=='$stagingStorageAccountName']".[resourceGroup] --resource-type "Microsoft.Storage/storageAccounts" -o tsv)
+    workspaceResourceId=$(sudo -u $adminUsername az resource show --resource-group $resourceGroup --name $logAnalyticsWorkspace --resource-type "Microsoft.OperationalInsights/workspaces" --query id -o tsv)
     echo "Log Analytics workspace id $workspaceResourceId"
 
-    sudo -u $adminUsername az connectedk8s connect --name $vmName --resource-group $storageAccountRG --location $location --tags 'Project=jumpstart_arcbox'
+    sudo -u $adminUsername az connectedk8s connect --name $vmName --resource-group $resourceGroup --location $location --tags 'Project=jumpstart_arcbox'
     echo "Onboarding the k3s cluster to Azure Arc completed"
     
     # Verify if cluster is connected to Azure Arc successfully
-    connectedClusterInfo=$(sudo -u $adminUsername az connectedk8s show --name $vmName --resource-group $storageAccountRG)
-    echo $connectedClusterInfo
+    connectedClusterInfo=$(sudo -u $adminUsername az connectedk8s show --name $vmName --resource-group $resourceGroup)
+    echo "Connected cluster info: $connectedClusterInfo"
 
     # Wait
     # Enabling Container Insights and Microsoft Defender for Containers cluster extensions
