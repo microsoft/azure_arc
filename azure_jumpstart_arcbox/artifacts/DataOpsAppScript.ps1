3774223039d8bf40c1eeaa165ad855dd0a01cb8a
Zaid Mohammad <mozaid@microsoft.com>
2024-07-18 11:51:50 -0400

    fix app ingress

--- azure_jumpstart_arcbox/artifacts/DataOpsAppScript.ps1
+++ azure_jumpstart_arcbox/artifacts/DataOpsAppScript.ps1
@@ -41,16 +41,6 @@ foreach ($cluster in @('k3s', 'aks-dr')) {
 # Switch kubectl context to k3s
 kubectx $sqlInstance
 
-Write-Header "Adding CName Record for App"
-$dcInfo = Get-ADDomainController
-Do
-{
-	$appIpaddress= kubectl get svc "dataops-ingress-nginx-ingress-controller" -o jsonpath="{.status.loadBalancer.ingress[0].ip}"
-   Start-Sleep -Seconds 5
-} while ($null -eq $appIpaddress)
-Add-DnsServerResourceRecord -ComputerName $dcInfo.HostName -ZoneName $dcInfo.Domain -A -Name "$CName-$sqlInstance" -AllowUpdateAny -IPv4Address $appIpaddress -TimeToLive 01:00:00 -AgeRecord
-Add-DnsServerResourceRecordCName -Name $CName -ComputerName $dcInfo.HostName -HostNameAlias "$CName-$sqlInstance.jumpstart.local" -ZoneName jumpstart.local -TimeToLive 00:05:00
-
 # Deploy the App and service
 $appK3s = @"
 apiVersion: apps/v1
@@ -90,7 +80,7 @@ metadata:
 spec:
   selector:
     app: web
-  type: ClusterIP
+  type: LoadBalancer
   ports:
   - protocol: TCP
     port: 80
@@ -100,34 +90,15 @@ spec:
 Write-Header "Deploying App Resource"
 $appK3s | kubectl apply -n $appNamespace -f -
 
-# Deploy an Ingress Resource for the app
-$appIngress = @"
-apiVersion: networking.k8s.io/v1
-kind: Ingress
-metadata:
-  name: ingress-tls
-  annotations:
-    kubernetes.io/ingress.class: nginx
-    nginx.ingress.kubernetes.io/rewrite-target: /$1
-spec:
-  tls:
-  - hosts:
-    - "$certdns"
-    secretName: "$CName-secret"
-  rules:
-  - host: "$certdns"
-    http:
-      paths:
-      - pathType: ImplementationSpecific
-        backend:
-          service:
-            name: web-app-service
-            port:
-              number: 80
-        path: /
-"@
-Write-Header "Deploying App Ingress Resource"
-$appIngress | kubectl apply -n $appNamespace -f -
+Write-Header "Adding CName Record for App"
+$dcInfo = Get-ADDomainController
+Do
+{
+	$appIpaddress= kubectl get svc "web-app-service" -o jsonpath="{.status.loadBalancer.ingress[0].ip}"
+   Start-Sleep -Seconds 5
+} while ($null -eq $appIpaddress)
+Add-DnsServerResourceRecord -ComputerName $dcInfo.HostName -ZoneName $dcInfo.Domain -A -Name "$CName-$sqlInstance" -AllowUpdateAny -IPv4Address $appIpaddress -TimeToLive 01:00:00 -AgeRecord
+Add-DnsServerResourceRecordCName -Name $CName -ComputerName $dcInfo.HostName -HostNameAlias "$CName-$sqlInstance.jumpstart.local" -ZoneName jumpstart.local -TimeToLive 00:05:00
 
 Do {
   Write-Host "Waiting for Web App pod, hold tight..."
