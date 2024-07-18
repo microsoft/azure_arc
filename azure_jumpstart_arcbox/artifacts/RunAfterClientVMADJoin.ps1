deefd71dc08ecbef8c648e2a752efe0077d63988
Seif Bassem <38246040+sebassem@users.noreply.github.com>
2024-07-18 09:44:47 +0300

    Refactor scheduled task creation for WinGetLogonScript and DataOpsLogonScript

--- azure_jumpstart_arcbox/artifacts/RunAfterClientVMADJoin.ps1
+++ azure_jumpstart_arcbox/artifacts/RunAfterClientVMADJoin.ps1
@@ -39,10 +39,9 @@ Set-ADUser $Env:adminUsername -UserPrincipalName "$Env:adminUsername@jumpstart.l
 $cimsession = New-CimSession -Credential $adminCredential
 
 # Creating scheduled task for WinGet.ps1
-$Trigger = New-ScheduledTaskTrigger -AtLogOn
+$Trigger = New-ScheduledTaskTrigger -AtLogOn -User $adminuser
 $Action = New-ScheduledTaskAction -Execute "pwsh.exe" -Argument $Env:ArcBoxDir\WinGet.ps1
-$settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -StartWhenAvailable
-Register-ScheduledTask -TaskName "WinGetLogonScript" -Trigger $Trigger -CimSession $cimsession -Action $Action -RunLevel "Highest" -Force -Settings $settings
+Register-ScheduledTask -TaskName "WinGetLogonScript" -Trigger $Trigger -CimSession $cimsession -Action $Action -RunLevel "Highest" -Force
 
 # Creating scheduled task for DataOpsLogonScript.ps1
 $Action = New-ScheduledTaskAction -Execute "pwsh.exe" -Argument "$Env:ArcBoxDir\DataOpsLogonScript.ps1"
