2459d3023442573afff2ed27105dee8063c6faf3
Zaid Mohammad <mozaid@microsoft.com>
2024-08-02 09:12:07 -0400

    add devops test

---
+++ azure_jumpstart_arcbox/artifacts/tests/devops.tests.ps1
@@ -0,0 +1,48 @@
+
+BeforeDiscovery {
+
+    $k3sArcDataClusterName = $env:k3sArcDataClusterName
+    $k3sArcClusterName = $env:k3sArcClusterName
+
+    $clusters = @($k3sArcDataClusterName, $k3sArcClusterName)
+    $VMs = @($k3sArcDataClusterName, $k3sArcClusterName)
+
+    $null = Connect-AzAccount -Identity -Tenant $env:tenantId -Subscription $env:subscriptionId
+    az config set extension.use_dynamic_install=yes_without_prompt
+}
+
+Describe "<cluster>" -ForEach $clusters {
+    BeforeAll {
+        $cluster = $_
+    }
+    It "Cluster exists" {
+        $clusterObject = Get-AzConnectedKubernetes -ClusterName $cluster -ResourceGroupName $env:resourceGroup -SubscriptionId $env:subscriptionId
+        $clusterObject | Should -Not -BeNullOrEmpty
+    }
+    It "Azure Arc Connected cluster is connected" {
+        $connectedCluster = Get-AzConnectedKubernetes -Name $cluster -ResourceGroupName $env:resourceGroup -SubscriptionId $env:subscriptionId
+        $connectedCluster.ConnectivityStatus | Should -Be "Connected"
+    }
+}
+
+Describe "<vm>" -ForEach $VMs {
+    BeforeAll {
+        $vm = $_
+    }
+    It "VM exists" {
+        $vmobject = Get-VM -Name $vm
+        $vmobject | Should -Not -BeNullOrEmpty
+    }
+    It "VM is running" {
+        $vmobject = Get-VM -Name $vm
+        $vmobject.State | Should -Be "Running"
+    }
+    It "Azure Arc Connected Machine exists" {
+        $connectedMachine = Get-AzConnectedMachine -Name $vm -ResourceGroupName $env:resourceGroup -SubscriptionId $env:subscriptionId
+        $connectedMachine | Should -Not -BeNullOrEmpty
+    }
+    It "Azure Arc Connected Machine is connected" {
+        $connectedMachine = Get-AzConnectedMachine -Name $vm -ResourceGroupName $env:resourceGroup -SubscriptionId $env:subscriptionId
+        $connectedMachine.Status | Should -Be "Connected"
+    }
+}
