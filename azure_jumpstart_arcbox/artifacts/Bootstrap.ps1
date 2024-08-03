e7528a31a5ee5040b94ae9da7f155e4aa54ec68c
Seif Bassem <38246040+sebassem@users.noreply.github.com>
2024-08-03 23:06:05 +0300

    chore: Restart computer after creating scheduled tasks for different flavors

--- azure_jumpstart_arcbox/artifacts/Bootstrap.ps1
+++ azure_jumpstart_arcbox/artifacts/Bootstrap.ps1
@@ -353,9 +353,6 @@ if ($flavor -eq "ITPro") {
     # Creating scheduled task for ArcServersLogonScript.ps1
     $Action = New-ScheduledTaskAction -Execute $ScheduledTaskExecutable -Argument $Env:ArcBoxDir\ArcServersLogonScript.ps1
     Register-ScheduledTask -TaskName "ArcServersLogonScript" -User $adminUsername -Action $Action -RunLevel "Highest" -Force
-
-    # Restart computer
-    Restart-Computer
 }
 
 if ($flavor -eq "DevOps") {
@@ -366,9 +363,6 @@ if ($flavor -eq "DevOps") {
     # Creating scheduled task for DevOpsLogonScript.ps1
     $Action = New-ScheduledTaskAction -Execute $ScheduledTaskExecutable -Argument $Env:ArcBoxDir\DevOpsLogonScript.ps1
     Register-ScheduledTask -TaskName "DevOpsLogonScript" -User $adminUsername -Action $Action -RunLevel "Highest" -Force
-
-    # Restart computer
-    Restart-Computer
 }
 
 if ($flavor -eq "DataOps") {
@@ -443,3 +437,6 @@ else {
     $logSuppress = Get-Content $Env:ArcBoxLogsDir\Bootstrap.log | Where-Object { $_ -notmatch "Host Application: $ScheduledTaskExecutable" }
     $logSuppress | Set-Content $Env:ArcBoxLogsDir\Bootstrap.log -Force
 }
+
+    # Restart computer
+    Restart-Computer
