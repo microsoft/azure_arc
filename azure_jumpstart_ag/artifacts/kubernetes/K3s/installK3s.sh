6d1dd6a3e75900b31c0a6c2680becd6ea66893f7
Seif Bassem <38246040+sebassem@users.noreply.github.com>
2024-08-07 09:55:01 +0300

    Update K3s installation script to include azcopy installation step

--- azure_jumpstart_ag/artifacts/kubernetes/K3s/installK3s.sh
+++ azure_jumpstart_ag/artifacts/kubernetes/K3s/installK3s.sh
@@ -49,6 +49,7 @@ while sleep 1; do sudo -s rsync -a /var/lib/waagent/custom-script/download/0/ins
 echo ""
 echo "Downloading azcopy"
 echo ""
+sudo apt-get update
 wget -O azcopy.tar.gz https://aka.ms/downloadazcopy-v10-linux
 if [[ $? -ne 0 ]]; then
     echo "ERROR: Failed to download azcopy"
