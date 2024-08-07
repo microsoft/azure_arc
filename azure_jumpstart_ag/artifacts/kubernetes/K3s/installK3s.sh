93b7b8dcd6fd6f2af2850d3760648eda90a26eac
Seif Bassem <38246040+sebassem@users.noreply.github.com>
2024-08-07 12:04:21 +0300

    Update K3s installation script to include delay before uploading script logs

--- azure_jumpstart_ag/artifacts/kubernetes/K3s/installK3s.sh
+++ azure_jumpstart_ag/artifacts/kubernetes/K3s/installK3s.sh
@@ -198,6 +198,7 @@ fi
 echo ""
 echo "Uploading the script logs to staging storage"
 echo ""
+sleep 60
 log="/home/$adminUsername/jumpstart_logs/installK3s-$vmName.log"
 storageContainerNameLower=$(echo $storageContainerName | tr '[:upper:]' '[:lower:]')
 azcopy cp $log "https://$stagingStorageAccountName.blob.core.windows.net/$storageContainerNameLower/installK3s-$vmName.log" --log-level DEBUG
