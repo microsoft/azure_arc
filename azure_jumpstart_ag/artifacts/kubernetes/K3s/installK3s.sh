29575876bcaee9a46e320e64c140f6ddce479ad0
Seif Bassem <38246040+sebassem@users.noreply.github.com>
2024-08-08 10:26:36 +0300

    Update K3s installation script to include staging storage account name for log file upload

--- azure_jumpstart_ag/artifacts/kubernetes/K3s/installK3s.sh
+++ azure_jumpstart_ag/artifacts/kubernetes/K3s/installK3s.sh
@@ -199,8 +199,8 @@ fi
 echo ""
 echo "Uploading the script logs to staging storage"
 echo ""
-
+sudo -u $adminUsername az login --identity
 log="/home/${adminUsername}/jumpstart_logs/installK3s-${vmName}.log"
 storageContainerNameLower=$(echo $storageContainerName | tr '[:upper:]' '[:lower:]')
 #azcopy cp $log "https://$stagingStorageAccountName.blob.core.windows.net/$storageContainerNameLower/installK3s-$vmName.log" --check-length=false
-az storage azcopy blob upload -c $storageContainerNameLower --account-name $storageContainerNameLower -s $log -d NewBlob
+az storage azcopy blob upload -c $storageContainerNameLower --account-name $stagingStorageAccountName -s $log -d NewBlob
