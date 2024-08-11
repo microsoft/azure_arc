58fd81826cf7590222d5e9fe0071f07fc45cd2e9
Seif Bassem <38246040+sebassem@users.noreply.github.com>
2024-08-11 22:34:42 +0300

    chore: Update Grafana UI for Contoso Hypermarket scenario

--- azure_jumpstart_ag/artifacts/PowerShell/Modules/common.psm1
+++ azure_jumpstart_ag/artifacts/PowerShell/Modules/common.psm1
@@ -850,6 +850,20 @@ function Deploy-Prometheus {
         (Get-Content $_.FullName) -replace 'Welcome to Grafana', 'Welcome to Grafana for Contoso Motors' | Set-Content $_.FullName
         }
     }
+    elseif ($Env:scenario -eq "contoso_hypermarket") {
+        # Update Grafana Icons
+        Copy-Item -Path $AgIconsDir\contoso-motors.png -Destination "C:\Program Files\GrafanaLabs\grafana\public\img"
+        Copy-Item -Path $AgIconsDir\contoso-motors.svg -Destination "C:\Program Files\GrafanaLabs\grafana\public\img\grafana_icon.svg"
+
+        Get-ChildItem -Path 'C:\Program Files\GrafanaLabs\grafana\public\build\*.js' -Recurse -File | ForEach-Object {
+        (Get-Content $_.FullName) -replace 'className:u,src:"public/img/grafana_icon.svg"', 'className:u,src:"public/img/contoso-motors.png"' | Set-Content $_.FullName
+        }
+
+        # Reset Grafana UI
+        Get-ChildItem -Path 'C:\Program Files\GrafanaLabs\grafana\public\build\*.js' -Recurse -File | ForEach-Object {
+        (Get-Content $_.FullName) -replace 'Welcome to Grafana', 'Welcome to Grafana for Contoso Hypermarket' | Set-Content $_.FullName
+        }
+    }
 
     # Reset Grafana Password
     $Env:Path += ';C:\Program Files\GrafanaLabs\grafana\bin'
