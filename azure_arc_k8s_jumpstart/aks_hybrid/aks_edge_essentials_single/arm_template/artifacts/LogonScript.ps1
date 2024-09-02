7a64e0ded1373a2b731d17813bebf18f08c81b55
Jan Egil Ring <janegilring@microsoft.com>
2024-09-02 08:39:03 +0200

    Update AKS Edge deployment version and schema version in LogonScript.ps1
    Signed-off-by: Jan Egil Ring <janegilring@microsoft.com>

--- azure_arc_k8s_jumpstart/aks_hybrid/aks_edge_essentials_single/arm_template/artifacts/LogonScript.ps1
+++ azure_arc_k8s_jumpstart/aks_hybrid/aks_edge_essentials_single/arm_template/artifacts/LogonScript.ps1
@@ -3,8 +3,8 @@ Start-Transcript -Path C:\Temp\LogonScript.log
 ## Deploy AKS EE
 
 # Parameters
-$AksEdgeRemoteDeployVersion = "1.0.230221.1200"
-$schemaVersion = "1.1"
+$AksEdgeRemoteDeployVersion = "1.7.639.0"
+$schemaVersion = "1.9"
 $versionAksEdgeConfig = "1.0"
 $aksEdgeDeployModules = "main"
 $aksEEReleasesUrl = "https://api.github.com/repos/Azure/AKS-Edge/releases"
@@ -37,7 +37,7 @@ if ($AKSEEPinnedSchemaVersion -ne "useLatest") {
     Expand-Archive $output -DestinationPath "C:\temp" -Force
     $AKSEEReleaseConfigFilePath = "C:\temp\AKS-Edge-$latestReleaseTag\tools\aksedge-config.json"
     $jsonContent = Get-Content -Raw -Path $AKSEEReleaseConfigFilePath | ConvertFrom-Json
-    $schemaVersion = $jsonContent.SchemaVersion
+    $schemaVersionAksEdgeConfig = $jsonContent.SchemaVersion
     # Clean up the downloaded release files
     Remove-Item -Path $output -Force
     Remove-Item -Path "C:\temp\AKS-Edge-$latestReleaseTag" -Force -Recurse
@@ -163,7 +163,7 @@ Set-Content -Path $aksedgejson -Value $aksedgeConfig -Force
 $aksedgeShell = (Get-ChildItem -Path "$workDir" -Filter AksEdgeShell.ps1 -Recurse).FullName
 . $aksedgeShell
 
-# Download, install and deploy AKS EE 
+# Download, install and deploy AKS EE
 Write-Host "Step 2: Download, install and deploy AKS Edge Essentials"
 # invoke the workflow, the json file already stored above.
 $retval = Start-AideWorkflow -jsonFile $aidejson
@@ -204,8 +204,8 @@ az -v
 az login --service-principal --username $Env:appId --password=$Env:password --tenant $Env:tenantId
 
 # Set default subscription to run commands against
-# "subscriptionId" value comes from clientVM.json ARM template, based on which 
-# subscription user deployed ARM template to. This is needed in case Service 
+# "subscriptionId" value comes from clientVM.json ARM template, based on which
+# subscription user deployed ARM template to. This is needed in case Service
 # Principal has access to multiple subscriptions, which can break the automation logic
 az account set --subscription $Env:subscriptionId
 
@@ -347,22 +347,22 @@ $clusterName = "$env:computername-$env:kubernetesDistribution"
 
 # Changing to Client VM wallpaper
 $imgPath = "C:\Temp\wallpaper.png"
-$code = @' 
-using System.Runtime.InteropServices; 
-namespace Win32{ 
-    
-     public class Wallpaper{ 
-        [DllImport("user32.dll", CharSet=CharSet.Auto)] 
-         static extern int SystemParametersInfo (int uAction , int uParam , string lpvParam , int fuWinIni) ; 
-         
-         public static void SetWallpaper(string thePath){ 
-            SystemParametersInfo(20,0,thePath,3); 
+$code = @'
+using System.Runtime.InteropServices;
+namespace Win32{
+
+     public class Wallpaper{
+        [DllImport("user32.dll", CharSet=CharSet.Auto)]
+         static extern int SystemParametersInfo (int uAction , int uParam , string lpvParam , int fuWinIni) ;
+
+         public static void SetWallpaper(string thePath){
+            SystemParametersInfo(20,0,thePath,3);
          }
     }
- } 
+ }
 '@
 
-add-type $code 
+add-type $code
 [Win32.Wallpaper]::SetWallpaper($imgPath)
 
 # Kill the open PowerShell monitoring kubectl get pods
