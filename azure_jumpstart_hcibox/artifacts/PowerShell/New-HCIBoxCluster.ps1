25e9c295741eb1bb1b399e0b2cf23504ef23b83b
dkirby-ms <saitcho@outlook.com>
2024-10-03 14:44:21 -0500

    revert

--- azure_jumpstart_hcibox/artifacts/PowerShell/New-HCIBoxCluster.ps1
+++ azure_jumpstart_hcibox/artifacts/PowerShell/New-HCIBoxCluster.ps1
@@ -637,8 +637,6 @@ function Set-NICs {
 
             # Rename non-storage adapters
             Get-NetAdapter ((Get-NetAdapterAdvancedProperty | Where-Object {$_.DisplayValue -eq "SDN"}).Name) | Rename-NetAdapter -NewName FABRIC
-            $trimip = $($VM.IP).Substring(0, $VM.IP.IndexOf('/'))
-            New-NetIPAddress -IPAddress $trimip -DefaultGateway $HCIBoxConfig.SDNLabRoute -InterfaceAlias FABRIC -PrefixLength 24
             # Get-Netadapter ((Get-NetAdapterAdvancedProperty | Where-Object {$_.DisplayValue -eq "SDN2"}).Name) | Rename-NetAdapter -NewName FABRIC2
 
             # Enable CredSSP Settings
@@ -659,7 +657,6 @@ function Set-NICs {
         }
     }
 }
-
 
 function Set-FabricNetwork {
     param (
@@ -1485,18 +1482,6 @@ function Set-HCIDeployPrereqs {
             winrm quickconfig -quiet
             netsh advfirewall firewall add rule name="ICMP Allow incoming V4 echo request" protocol=icmpv4 dir=in action=allow
 
-            # # Register PSGallery as a trusted repo
-            # Register-PSRepository -Default -InstallationPolicy Trusted -ErrorAction SilentlyContinue
-            # Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
-            # Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
-
-            # #Install Arc registration script from PSGallery
-            # Install-Module AzsHCI.ARCinstaller -Force
-
-            # #Install required PowerShell modules in your node for registration
-            # Install-Module Az.Accounts -Force
-            # Install-Module Az.ConnectedMachine -Force
-            # Install-Module Az.Resources -Force
             $azureAppCred = (New-Object System.Management.Automation.PSCredential $clientId, (ConvertTo-SecureString -String $clientSecret -AsPlainText -Force))
             Connect-AzAccount -ServicePrincipal -SubscriptionId $subId -TenantId $tenantId -Credential $azureAppCred
             $armtoken = ConvertFrom-SecureStringToPlainText -SecureString ((Get-AzAccessToken -AsSecureString).Token)
