c7a6dfb5d9e7b2a42382fc22cba2ac7f79c965b9
Zaid Mohammad <mozaid@microsoft.com>
2024-10-17 20:19:13 -0400

    fix connectedk8s version

--- azure_arc_k8s_jumpstart/rancher_k3s/azure/terraform/scripts/installK3s.sh
+++ azure_arc_k8s_jumpstart/rancher_k3s/azure/terraform/scripts/installK3s.sh
@@ -60,7 +60,7 @@ echo ""
 sudo apt-get update
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 
-sudo -u $adminUsername az extension add --name "connectedk8s"
+sudo -u $adminUsername az extension add --name "connectedk8s" --version 1.9.3
 sudo -u $adminUsername az extension add --name "k8s-configuration"
 sudo -u $adminUsername az extension add --name "k8s-extension"
 sudo -u $adminUsername az extension add --name "customlocation"
