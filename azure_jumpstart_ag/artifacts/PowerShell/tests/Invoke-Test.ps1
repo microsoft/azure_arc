d7c11907e9c52ae3b3692a6023b3eb305e23153d
Jan Egil Ring <janegilring@microsoft.com>
2024-10-25 23:52:12 +0200

    Bugfix - variables

--- azure_jumpstart_ag/artifacts/PowerShell/tests/Invoke-Test.ps1
+++ azure_jumpstart_ag/artifacts/PowerShell/tests/Invoke-Test.ps1
@@ -6,7 +6,7 @@ $tests_passed = $tests_common.Passed.Count
 $tests_failed = $tests_common.Failed.Count
 
 
-Invoke-Pester -Path "$AgoraTestsDir\k8s.tests.ps1" -Output Detailed -PassThru -OutVariable tests_k8s
+Invoke-Pester -Path "$AgTestsDir\k8s.tests.ps1" -Output Detailed -PassThru -OutVariable tests_k8s
 $tests_passed = $tests_passed + $tests_k8s.Passed.Count
 $tests_failed = $tests_failed + $tests_k8s.Failed.Count
 
@@ -19,4 +19,4 @@ Write-Header "Adding deployment test results to wallpaper using BGInfo"
 Set-Content "$Env:windir\TEMP\agora-tests-succeeded.txt" $tests_passed
 Set-Content "$Env:windir\TEMP\agora-tests-failed.txt" $tests_failed
 
-bginfo.exe $AgoraTestsDir\ag-bginfo.bgi /timer:0 /NOLICPROMPT
+bginfo.exe $AgTestsDir\ag-bginfo.bgi /timer:0 /NOLICPROMPT
