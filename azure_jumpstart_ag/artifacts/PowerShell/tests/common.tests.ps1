6d63b4587ab7b4a16ac52f27d42e0389e5a19522
Jan Egil Ring <janegilring@microsoft.com>
2024-10-26 09:02:29 +0200

    Adjusted to number of resources provisioned

--- azure_jumpstart_ag/artifacts/PowerShell/tests/common.tests.ps1
+++ azure_jumpstart_ag/artifacts/PowerShell/tests/common.tests.ps1
@@ -8,6 +8,6 @@ Describe "ArcBox resource group" {
         $ResourceGroupName = $env:resourceGroup
     }
     It "should have 30 resources or more" {
-        (Get-AzResource -ResourceGroupName $ResourceGroupName).count | Should -BeGreaterOrEqual 30
+        (Get-AzResource -ResourceGroupName $ResourceGroupName).count | Should -BeGreaterOrEqual 27
     }
 }
