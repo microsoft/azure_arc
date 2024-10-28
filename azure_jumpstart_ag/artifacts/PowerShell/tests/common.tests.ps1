062c6cb4574b30cd43edc2281542adcd7df8bf64
Jan Egil Ring <janegilring@microsoft.com>
2024-10-28 15:13:53 +0100

    Added more tests

--- azure_jumpstart_ag/artifacts/PowerShell/tests/common.tests.ps1
+++ azure_jumpstart_ag/artifacts/PowerShell/tests/common.tests.ps1
@@ -1,13 +1,17 @@
 BeforeDiscovery {
 
-    $null = Connect-AzAccount -Identity -Tenant $env:spnTenantId  -Subscription $env:subscriptionId
+    # Login to Azure PowerShell with service principal provided by user
+    $spnpassword = ConvertTo-SecureString $env:spnClientSecret -AsPlainText -Force
+    $spncredential = New-Object System.Management.Automation.PSCredential ($env:spnClientId, $spnpassword)
+    Connect-AzAccount -ServicePrincipal -Credential $spncredential -Tenant $env:spnTenantId -Subscription $env:subscriptionId
 
 }
 Describe "ArcBox resource group" {
     BeforeAll {
         $ResourceGroupName = $env:resourceGroup
     }
-    It "should have 30 resources or more" {
+    It "should have 27 resources or more" {
         (Get-AzResource -ResourceGroupName $ResourceGroupName).count | Should -BeGreaterOrEqual 27
     }
 }
+
