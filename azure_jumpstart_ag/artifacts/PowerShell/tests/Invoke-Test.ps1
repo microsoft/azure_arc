2f548a7ba3969847b6c3838c323bd328abd51357
Jan Egil Ring <janegilring@microsoft.com>
2024-10-29 22:25:29 +0100

    Export test results to file and resource group tag

--- azure_jumpstart_ag/artifacts/PowerShell/tests/Invoke-Test.ps1
+++ azure_jumpstart_ag/artifacts/PowerShell/tests/Invoke-Test.ps1
@@ -2,7 +2,6 @@
 
 $AgConfig = Import-PowerShellDataFile -Path $Env:AgConfigPath
 $AgTestsDir = $AgConfig.AgDirectories["AgTestsDir"]
-
 Invoke-Pester -Path "$AgTestsDir\common.tests.ps1" -Output Detailed -PassThru -OutVariable tests_common
 $tests_passed = $tests_common.Passed.Count
 $tests_failed = $tests_common.Failed.Count
@@ -20,3 +19,49 @@ Set-Content "$Env:windir\TEMP\agora-tests-succeeded.txt" $tests_passed
 Set-Content "$Env:windir\TEMP\agora-tests-failed.txt" $tests_failed
 
 bginfo.exe $AgTestsDir\ag-bginfo.bgi /timer:0 /NOLICPROMPT
+
+
+$DeploymentStatusPath = "C:\Ag\Logs\DeploymentStatus.log"
+
+Write-Header "Exporting deployment test results to $DeploymentStatusPath"
+
+Write-Output "`n" | Out-File -FilePath $DeploymentStatusPath
+
+$asciiArt = @"
+██████╗ ███████╗██████╗ ██╗      ██████╗ ██╗   ██╗███╗   ███╗███████╗███╗   ██╗████████╗    ███████╗████████╗ █████╗ ████████╗██╗   ██╗███████╗
+██╔══██╗██╔════╝██╔══██╗██║     ██╔═══██╗╚██╗ ██╔╝████╗ ████║██╔════╝████╗  ██║╚══██╔══╝    ██╔════╝╚══██╔══╝██╔══██╗╚══██╔══╝██║   ██║██╔════╝
+██║  ██║█████╗  ██████╔╝██║     ██║   ██║ ╚████╔╝ ██╔████╔██║█████╗  ██╔██╗ ██║   ██║       ███████╗   ██║   ███████║   ██║   ██║   ██║███████╗
+██║  ██║██╔══╝  ██╔═══╝ ██║     ██║   ██║  ╚██╔╝  ██║╚██╔╝██║██╔══╝  ██║╚██╗██║   ██║       ╚════██║   ██║   ██╔══██║   ██║   ██║   ██║╚════██║
+██████╔╝███████╗██║     ███████╗╚██████╔╝   ██║   ██║ ╚═╝ ██║███████╗██║ ╚████║   ██║       ███████║   ██║   ██║  ██║   ██║   ╚██████╔╝███████║
+╚═════╝ ╚══════╝╚═╝     ╚══════╝ ╚═════╝    ╚═╝   ╚═╝     ╚═╝╚══════╝╚═╝  ╚═══╝   ╚═╝       ╚══════╝   ╚═╝   ╚═╝  ╚═╝   ╚═╝    ╚═════╝ ╚══════╝
+"@
+
+Write-Output $asciiArt | Out-File -FilePath $DeploymentStatusPath -Append
+
+Write-Output "`nTests succeeded: $tests_passed" | Out-File -FilePath $DeploymentStatusPath -Append
+Write-Output "Tests failed: $tests_failed`n" | Out-File -FilePath $DeploymentStatusPath -Append
+
+Write-Output "To get an updated deployment status, open Windows Terminal and run:" | Out-File -FilePath $DeploymentStatusPath -Append
+Write-Output "C:\Ag\Tests\Invoke-Test.ps1`n" | Out-File -FilePath $DeploymentStatusPath -Append
+
+Write-Output "Failed:" | Out-File -FilePath $DeploymentStatusPath -Append
+$tests_common.Failed | Out-File -FilePath $DeploymentStatusPath -Append
+$tests_k8s.Failed | Out-File -FilePath $DeploymentStatusPath -Append
+
+Write-Output "Passed:" | Out-File -FilePath $DeploymentStatusPath -Append
+$tests_k8s.Passed | Out-File -FilePath $DeploymentStatusPath -Append
+$tests_common.Passed | Out-File -FilePath $DeploymentStatusPath -Append
+
+Write-Header "Exporting deployment test results to resource group tag DeploymentStatus"
+
+$DeploymentStatusString = "Tests succeeded: $tests_passed Tests failed: $tests_failed"
+
+$tags = Get-AzResourceGroup -Name $env:resourceGroup | Select-Object -ExpandProperty Tags
+
+if ($null -ne $tags) {
+    $tags["DeploymentStatus"] = $DeploymentStatusString
+} else {
+    $tags = @{"DeploymentStatus" = $DeploymentStatusString}
+}
+
+$null = Set-AzResourceGroup -ResourceGroupName $env:resourceGroup -Tag $tags
