669e63e9d4aef5e3707fb901c3d948a4fa004db9
Seif Bassem <38246040+sebassem@users.noreply.github.com>
2024-11-03 23:10:26 +0200

    Update Azure IoT Ops version and add Azure OpenAI secrets setup

--- azure_jumpstart_ag/artifacts/PowerShell/AgLogonScript.ps1
+++ azure_jumpstart_ag/artifacts/PowerShell/AgLogonScript.ps1
@@ -226,6 +226,7 @@ if ($scenario -eq "contoso_motors") {
     Deploy-AIO-M3
     $mqttIpArray=Set-MQTTIpAddress
     Deploy-MQTTExplorer -mqttIpArray $mqttIpArray
+    Set-AzureOpenAISecrets
 }
 
 ##############################################################
