2d3009350dca3569145ef4b683ad32c55fdff5a3
Mike Rhoads <mike@5amworkshop.com>
2025-01-27 09:16:13 -0600

    remove if check for testing

--- azure_jumpstart_ag/artifacts/kubernetes/K3s/installK3s.sh
+++ azure_jumpstart_ag/artifacts/kubernetes/K3s/installK3s.sh
@@ -320,12 +320,10 @@ storageContainerNameLower=$(echo $storageContainerName | tr '[:upper:]' '[:lower
 azcopy cp $log "https://$stagingStorageAccountName.blob.core.windows.net/$storageContainerNameLower/installK3s-$vmName.log" --check-length=false >/dev/null 2>&1
 
 # Add the if statement to check the scenario and run the curl command if scenario is contoso-motors
-if [[ "$scenario" == "contoso_motors" ]]; then
     echo "Running curl command to install OpenVINO Toolkit Operator"
     curl -sL https://github.com/operator-framework/operator-lifecycle-manager/releases/download/v0.31.0/install.sh | bash -s v0.31.0
     sleep 10
     echo "Installing operator via kubectl"
     kubectl create -f https://operatorhub.io/install/ovms-operator.yaml
-fi
 
 exit 0
