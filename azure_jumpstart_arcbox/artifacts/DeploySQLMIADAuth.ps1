57d6840750f7fc9531ec42071f3e05b292f40ca4
Venkata Chintala <29983008+chintalavr@users.noreply.github.com>
2025-02-20 06:51:30 -0500

    Update DeploySQLMIADAuth.ps1

--- azure_jumpstart_arcbox/artifacts/DeploySQLMIADAuth.ps1
+++ azure_jumpstart_arcbox/artifacts/DeploySQLMIADAuth.ps1
@@ -16,16 +16,8 @@ Import-Module DnsServer
 # Get Activectory Information
 $dcInfo = Get-ADDomainController
 
-az login --identity
-az account set -s $env:subscriptionId
-
-$KeyVault = Get-AzKeyVault -ResourceGroupName $Env:resourceGroup
-if (-not (Get-SecretVault -Name $KeyVault.VaultName -ErrorAction Ignore)) {
-    Register-SecretVault -Name $KeyVault.VaultName -ModuleName Az.KeyVault -VaultParameters @{ AZKVaultName = $KeyVault.VaultName } -DefaultVault
-}
-
 # Retrieve Azure Key Vault secrets and store as runtime environment variables
-$AZDATA_PASSWORD = Get-Secret -Name 'AZDATAPASSWORD' -AsPlainText
+$AZDATA_PASSWORD = Get-Secret -Name 'windowsAdminPassword' -AsPlainText
 
 # Setup reverse DNS for AD authentication
 $dcIPv4 = ([System.Net.IPAddress]$dcInfo.IPv4Address).GetAddressBytes()
