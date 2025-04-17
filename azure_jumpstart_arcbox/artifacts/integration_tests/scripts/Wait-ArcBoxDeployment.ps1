8eb782739899456b09482f4ff2494788ac270541
Jan Egil Ring <janegilring@microsoft.com>
2025-04-17 09:30:39 +0200

    Add check for existing RetrievePesterResults command in Wait-ArcBoxDeployment.ps1
    Signed-off-by: Jan Egil Ring <janegilring@microsoft.com>

--- azure_jumpstart_arcbox/artifacts/integration_tests/scripts/Wait-ArcBoxDeployment.ps1
+++ azure_jumpstart_arcbox/artifacts/integration_tests/scripts/Wait-ArcBoxDeployment.ps1
@@ -10,14 +10,31 @@ param(
 Write-Host "Starting VM Run Command to wait for deployment and retrieve Pester test results from ArcBox-Client in resource group $ResourceGroupName"
 
 $Location = (Get-AzVM -ResourceGroupName $ResourceGroupName -Name ArcBox-Client).Location
-Set-AzVMRunCommand -ResourceGroupName $ResourceGroupName -VMName ArcBox-Client -RunCommandName RetrievePesterResults -Location $Location -SourceScriptUri "https://raw.githubusercontent.com/$githubAccount/azure_arc/$githubBranch/azure_jumpstart_arcbox/artifacts/integration_tests/scripts/Send-PesterResult.ps1" -AsyncExecution
+
+# Check if a RetrievePesterResults run command is already running
+$existingJob = Get-AzVMRunCommand -ResourceGroupName $ResourceGroupName -VMName ArcBox-Client -RunCommandName RetrievePesterResults -ErrorAction SilentlyContinue
+if ($existingJob) {
+    Write-Host "A RetrievePesterResults run command is already provisioned. Skipping new execution." -ForegroundColor Yellow
+} else {
+    Set-AzVMRunCommand -ResourceGroupName $ResourceGroupName -VMName ArcBox-Client -RunCommandName RetrievePesterResults -Location $Location -SourceScriptUri "https://raw.githubusercontent.com/$githubAccount/azure_arc/$githubBranch/azure_jumpstart_arcbox/artifacts/integration_tests/scripts/Send-PesterResult.ps1" -AsyncExecution
+}
+
+$timeoutMinutes = 55
+$elapsedMinutes = 0
 
 do {
+
     $job = Get-AzVMRunCommand -ResourceGroupName $ResourceGroupName -VMName ArcBox-Client -RunCommandName RetrievePesterResults -Expand InstanceView
 
     Write-Host "Instance view of job:" -ForegroundColor Green
     $job.InstanceView
     Start-Sleep -Seconds 60
+    $elapsedMinutes += 1
+
+    if ($elapsedMinutes -ge $timeoutMinutes) {
+        Write-Host "Timeout of 55 minutes reached. Exiting wait loop to avoid authentication token cache expiration." -ForegroundColor Yellow
+        break
+    }
 
 } while ($job.InstanceView.ExecutionState -eq "Running")
 
