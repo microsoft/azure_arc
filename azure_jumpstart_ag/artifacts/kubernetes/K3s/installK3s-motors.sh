8baa052f56c1658b145ae9fea49badac7eb90fd6
Mike Rhoads <mike@5amworkshop.com>
2025-05-27 15:41:09 -0500

    fix format

--- azure_jumpstart_ag/artifacts/kubernetes/K3s/installK3s-motors.sh
+++ azure_jumpstart_ag/artifacts/kubernetes/K3s/installK3s-motors.sh
@@ -128,7 +128,7 @@ check_dpkg_lock
 
 echo fs.inotify.max_user_instances=8192 | sudo tee -a /etc/sysctl.conf
 echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf
-echo fs.file-max = 100000 | sudo tee -a /etc/sysctl.conf
+echo fs.file-max=100000 | sudo tee -a /etc/sysctl.conf
 
 
 echo 512 | sudo tee /proc/sys/vm/nr_hugepages
