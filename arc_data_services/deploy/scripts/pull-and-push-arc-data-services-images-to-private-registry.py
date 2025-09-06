e33e86f656c403552a7ea01596dc082f0a054230
jkleinschnitz-msft <112512530+jkleinschnitz-msft@users.noreply.github.com>
2025-09-05 22:16:30 -0600

    Azure Arc-enabled data services - Aug release (#3303)
    Co-authored-by: Venkata Chintala <29983008+chintalavr@users.noreply.github.com>

--- arc_data_services/deploy/scripts/pull-and-push-arc-data-services-images-to-private-registry.py
+++ arc_data_services/deploy/scripts/pull-and-push-arc-data-services-images-to-private-registry.py
@@ -54,9 +54,9 @@ else:
 if os.getenv("SOURCE_DOCKER_TAG") is None:
     SOURCE_DOCKER_TAG = (
         input(
-            "Provide container image tag for the images at the source - press ENTER for using 'v1.39.0_2025-05-13': "
+            "Provide container image tag for the images at the source - press ENTER for using 'v1.40.0_2025-08-12': "
         )
-        or "v1.39.0_2025-05-13"
+        or "v1.40.0_2025-08-12"
     )
 else:
     SOURCE_DOCKER_TAG = os.environ["SOURCE_DOCKER_TAG"]
